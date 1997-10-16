0ad804c0b08d9e82908e4c7f81f01512c8572532
mdsplus <>
1997-10-16 17:49:17 +0000

    Dummy up more routines

--- treeshr/stuff.c
+++ treeshr/stuff.c
@@ -12,6 +12,13 @@ int name()\
   return stat;\
 }
 
+#define DummyArgs(name,ret,args,status) \
+ret name args \
+{\
+  printf("%s not currently implemented\n",#name);\
+  return status;\
+}
+
 Dummy(MdsGetCurrentShotid,0)
 Dummy(TreeCallHook,1)
 Dummy(TreeEstablishRundownEvent,1)
@@ -19,6 +26,11 @@ Dummy(TreeDoMethod,0)
 Dummy(_TreeDoMethod,0)
 Dummy(MdsEventCan,0)
 
+DummyArgs(TreeTurnOn,int,(int nid),1)
+DummyArgs(TreeTurnOff,int,(int nid),1)
+DummyArgs(TreePutRecord,int,(int nid, struct descriptor *d, ...),1)
+DummyArgs(TreeAddTag,int,(int nid,char *tag),1)
+DummyArgs(TreeRemoveNodesTags,int,(int nid),1)
 
 Dummy(TreeWait,1)
 void TreeFree(void *ptr) { free(ptr);}
