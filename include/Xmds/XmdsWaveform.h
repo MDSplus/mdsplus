64e764b4831826d65feaec8e2d26ffb4e9856daa
mdsplus <>
1997-10-31 19:40:08 +0000

    New includes

---
+++ include/Xmds/XmdsWaveform.h
@@ -0,0 +1,198 @@
+/*  CMS REPLACEMENT HISTORY, Element XMDSWAVEFORM.H */
+/*  *20   18-MAR-1997 09:52:37 TWF "Fix for DECC 5.3" */
+/*  *19   20-SEP-1994 08:02:42 TWF "Change _NO_PROTO" */
+/*  *18   24-MAR-1994 10:25:46 MRL "Fix print call." */
+/*  *17   24-MAR-1994 08:52:57 TWF "Fix xmdswaveformprint" */
+/*  *16   22-MAR-1994 14:12:12 MRL "Change print command." */
+/*  *15   22-MAR-1994 12:02:19 TWF "Fix waveformprint" */
+/*  *14    6-DEC-1993 14:41:37 TWF "Add ticks instead of grid" */
+/*  *13   23-JUL-1993 15:03:49 TWF "Add disable_update flag" */
+/*  *12   26-FEB-1993 11:27:42 JAS "port to decc" */
+/*  *11   25-FEB-1993 12:21:49 JAS "" */
+/*  *10   24-FEB-1993 14:58:43 TWF "Change print prototype" */
+/*  *9    24-FEB-1993 12:15:36 JAS "port to decc" */
+/*  *8    22-OCT-1992 12:29:52 TWF "Fix setwave" */
+/*  *7    21-OCT-1992 10:25:20 TWF "Fix AddPoint and add MovePoint code" */
+/*  *6    13-OCT-1992 12:38:57 TWF "Add closed resource" */
+/*  *5    28-MAY-1992 15:55:48 TWF "Add animation resource" */
+/*  *4    25-NOV-1991 09:52:58 TWF "Add points_only" */
+/*  *3     1-NOV-1991 08:07:10 TWF "Change Update to take min/max's" */
+/*  *2     8-OCT-1991 14:42:44 TWF "Include prototypes for convenience routines" */
+/*  *1     8-OCT-1991 13:43:05 TWF "XmdsWaveform public include" */
+/*  CMS REPLACEMENT HISTORY, Element XMDSWAVEFORM.H */
+/*------------------------------------------------------------------------------
+
+		Name:   WAVEFORM.H
+
+		Type:   C include
+
+		Author:	Thomas W. Fredian
+
+		Date:   29-MAY-1990
+
+		Purpose: Public declarations for WAVEFORM widget
+
+------------------------------------------------------------------------------
+   Copyright (c) 1989
+   Property of Massachusetts Institute of Technology, Cambridge MA 02139.
+   This program cannot be copied or distributed in any form for non-MIT
+   use without specific written approval of MIT Plasma Fusion Center
+   Management.
+----------------------------------------------------------------------------*/
+
+#ifndef _XmdsWaveform_h
+#define _XmdsWaveform_h
+
+#ifndef _Xm_h
+#include <Xm/Xm.h>
+#endif
+
+/* Global Resources */
+
+#define XmdsNtitle               "title"	/* Title string displayed at top of waveform */
+#define XmdsNpointerMode         "pointerMode"	/* Selects pointer mode (i.e. zoom, crosshairs, drag) */
+#define XmdsNlabelFont           "labelFont"	/* Font used for title and axis labels */
+#define XmdsNshowMode            "showMode"	/* Show lines, points, or both (box at data points) */
+#define XmdsNshowSelections      "showSelections"	/* Show selection points */
+#define XmdsNstepPlot	         "stepPlot"	/* Step plot mode */
+#define XmdsNcount               "count"	/* Number of X,Y datapoints */
+#define XmdsNselections          "selections"	/* Selections (char [count]) */
+#define XmdsNselectionsValStruct "selectionsValStruct"	/* Selections (using value struct mechanism) */
+#define XmdsNpenDown             "penDown"	/* Pen down (char [count]) */
+#define XmdsNpenDownValStruct    "penDownValStruct"	/* Pen down (using value struct mechanism) */
+#define XmdsNpanWith             "panWith"	/* Pan with widget */
+#define XmdsNattachCrosshairs    "attachCrosshairs"	/* Attach crosshairs to curve - boolean */
+#define XmdsNdisabled		 "disabled"	/* True = disable drawing until disabled cleared */
+#define XmdsNclosed		 "closed"	/* True = connect last point to first point - boolean */
+#define XmdsNgridStyle           "gridStyle"    /* Select type of grid */
+
+/* X-axis Resources */
+
+#define XmdsNxValue              "xValue"	/* float X-values[count] */
+#define XmdsNxValStruct          "xValStruct"	/* X-values (using value struct mechanism) */
+#define XmdsNxMin                "xMin"	/* float *min_x */
+#define XmdsNxMax                "xMax"	/* float *max_x */
+#define XmdsNxCrosshair          "xCrosshair"	/* float *x_crosshair */
+#define XmdsNxGridLines          "xGridLines"	/* int number of grid lines */
+#define XmdsNxLabels             "xLabels"	/* boolean display labels */
+
+/* Y-axis Resources */
+
+#define XmdsNyValue              "yValue"	/* SAME AS X-axis Resources */
+#define XmdsNyValStruct          "yValStruct"
+#define XmdsNyMin                "yMin"
+#define XmdsNyMax                "yMax"
+#define XmdsNyCrosshair          "yCrosshair"
+#define XmdsNyGridLines          "yGridLines"
+#define XmdsNyLabels             "yLabels"
+
+/* Callbacks */
+
+#define XmdsNbutton3Callback     "button3Callback"	/* Callback when button 3 is pressed */
+#define XmdsNcrosshairsCallback  "crosshairsCallback"	/* Callback when crosshairs change position in waveform window */
+#define XmdsNalignCallback       "alignCallback"	/* Callback when right button is depressed in crosshairs pointer mode */
+#define XmdsNlimitsCallback      "limitsCallback"	/* Callback when change in max/min axis values (after zoom, pan, etc.) */
+#define XmdsNunderlayCallback    "underlayCallback"	/* Callback to write to window before waveform */
+#define XmdsNoverlayCallback     "overlayCallback"	/* Callback to write to window after waveform */
+#define XmdsNcutCallback         "cutCallback"	/* Callback when cut occurs */
+#define XmdsNpasteCallback       "pasteCallback"	/* Callback when paste occurs */
+
+/* Value Struct resource mechanism */
+
+typedef struct _XmdsWaveformValStruct
+{
+  unsigned int size;
+  caddr_t addr;
+  void (*destroy) ();		/* Destroy called with widget and destroy arg */
+  caddr_t destroy_arg;
+} XmdsWaveformValStruct,*XmdsWaveformValStructPtr;
+/* Callback structures */
+
+typedef struct
+{
+  int reason;
+  XEvent *event;
+  float x;
+  float y;
+} XmdsWaveformCrosshairsCBStruct;
+
+typedef struct
+{
+  int reason;
+  XEvent *event;
+  float *xminval;
+  float *xmaxval;
+  float *yminval;
+  float *ymaxval;
+} XmdsWaveformLimitsCBStruct;
+/* Callback reasons */
+
+#define XmdsCRCrosshairs     1
+#define XmdsCRAlign          2
+#define XmdsCRZoomIn         3
+#define XmdsCRZoomOut        4
+#define XmdsCRBoxZoom        5
+#define XmdsCRDrag           6
+#define XmdsCRButton3        7
+#define XmdsCRNewData        8
+#define XmdsCRDragEnd        9
+
+#define XmdsPOINTER_MODE_USER  0
+#define XmdsPOINTER_MODE_NONE  1
+#define XmdsPOINTER_MODE_POINT 2
+#define XmdsPOINTER_MODE_ZOOM  3
+#define XmdsPOINTER_MODE_DRAG  4
+#define XmdsPOINTER_MODE_EDIT  5
+
+#define XmdsSHOW_MODE_LINE 0
+#define XmdsSHOW_MODE_POINTS 1
+#define XmdsSHOW_MODE_BOTH 2
+
+enum XmdsWaveformMotionRestriction
+{
+  XmdsMOTION_UNSELECTED,XmdsMOTION_BOTH,XmdsMOTION_XONLY,XmdsMOTION_YONLY,XmdsMOTION_NONE
+};
+
+enum XmdsWaveformGridStyle
+{
+  XmdsGRID_STYLE_LINES,XmdsGRID_STYLE_TICKS
+};
+/* Class record constants */
+
+externalref WidgetClass xmdsWaveformWidgetClass;
+externalref struct _XmdsWaveformClassRec xmdsWaveformClassRec;
+
+typedef struct _XmdsWaveformClassRec *XmdsWaveformWidgetClass;
+typedef struct _XmdsWaveformRec *XmdsWaveformWidget;
+/* fast XtIsSubclass define */
+#ifndef XmdsIsWaveform
+#define XmdsIsWaveform(w) XtIsSubclass (w, xmdsWaveformWidgetClass)
+#endif
+
+/* External creation routines:
+*/
+#ifdef _NO_PROTO_
+
+extern Widget XmdsCreateWaveform();
+extern void XmdsWaveformPrint();
+extern void XmdsWaveformReverse();
+extern void XmdsWaveformSetCrosshairs();
+extern void XmdsWaveformSetPointerMode();
+extern void XmdsWaveformUpdate();
+extern void XmdsWaveformSetWave();
+
+#else				/* _NO_PROTO_ */
+
+extern Widget XmdsCreateWaveform(Widget parent,char *name,ArgList al,Cardinal ac);
+extern void XmdsWaveformPrint(Widget w,FILE *fid,int width,int height,int rotate,char *title, char *window_title, int resolution);
+extern void XmdsWaveformReverse(Widget w,int reverse);
+extern void XmdsWaveformSetCrosshairs(Widget w,float *x,float *y,Boolean attach);
+extern void XmdsWaveformSetPointerMode(Widget w,int mode);
+extern void XmdsWaveformUpdate(Widget w,XmdsWaveformValStruct *x,XmdsWaveformValStruct *y,char *title,
+			       float *xmin,float *xmax,float *ymin,float *ymax,Boolean defer_update);
+extern void XmdsWaveformSetWave(Widget w,int count,float *x,float *y,Boolean *select,Boolean *pendown,
+				Boolean autoscale,Boolean defer_update);
+
+#endif				/* _NO_PROTO_ */
+
+#endif				/* _XmdsWaveform */
---
+++ include/Xmds/XmdsWaveformP.h
@@ -0,0 +1,200 @@
+/*  VAX/DEC CMS REPLACEMENT HISTORY, Element XMDSWAVEFORMP.H */
+/*  *38    3-OCT-1994 12:14:22 TWF "Unix port" */
+/*  *37   24-JUN-1994 15:33:00 TWF "Motif 1.2" */
+/*  *36   23-JUN-1994 08:57:19 TWF "Fix for motif 1.2" */
+/*  *35   23-JUN-1994 08:52:40 TWF "Fix for motif 1.2" */
+/*  *34   23-JUN-1994 08:51:12 TWF "Fix for motif 1.2" */
+/*  *33   23-JUN-1994 08:39:42 TWF "Fix for motif 1.2" */
+/*  *32   23-JUN-1994 08:31:31 TWF "Fix for motif 1.2" */
+/*  *31   23-JUN-1994 08:16:43 TWF "Fix for motif 1.2" */
+/*  *30   22-JUN-1994 16:59:56 TWF "motif 1.2" */
+/*  *29   22-JUN-1994 16:40:07 TWF "motif" */
+/*  *28   22-JUN-1994 16:36:35 TWF "Add new inherits" */
+/*  *27   22-JUN-1994 16:18:58 TWF "Motif 1.2" */
+/*  *26   22-JUN-1994 14:36:09 TWF "Add Xmprimitive" */
+/*  *25   22-MAR-1994 12:12:39 TWF "Take out prototypes" */
+/*  *24   23-FEB-1994 09:37:08 TWF "revert" */
+/*  *23   23-FEB-1994 08:33:39 TWF "Test moving memory" */
+/*  *22   23-FEB-1994 08:22:09 TWF "End of test" */
+/*  *21   23-FEB-1994 08:13:27 TWF "Test shifting memory" */
+/*  *20    6-DEC-1993 14:41:41 TWF "Add ticks instead of grid" */
+/*  *19   23-JUL-1993 15:03:52 TWF "Add disable_update flag" */
+/*  *18   12-JUL-1993 09:25:11 TWF "Revert back to original" */
+/*  *17   26-FEB-1993 11:28:27 JAS "port to decc" */
+/*  *16   25-FEB-1993 12:28:02 JAS "" */
+/*  *15   25-FEB-1993 12:14:55 JAS "port to decc" */
+/*  *14   25-FEB-1993 11:42:27 JAS "add all of the inherits" */
+/*  *13   25-FEB-1993 11:25:15 JAS "port to decc" */
+/*  *12   13-OCT-1992 12:48:24 TWF "Add closed resource" */
+/*  *11   13-OCT-1992 12:39:02 TWF "Add closed resource" */
+/*  *10   28-MAY-1992 15:55:52 TWF "Add animation resource" */
+/*  *9     8-APR-1992 10:08:08 TWF "Fix backing store" */
+/*  *8     7-APR-1992 14:01:41 TWF "Alternate method if no backing store" */
+/*  *7    11-DEC-1991 09:55:26 TWF "Fix reverse mode plotting" */
+/*  *6    10-DEC-1991 17:45:09 TWF "Fix redisplay with backingstore" */
+/*  *5     5-DEC-1991 10:10:06 TWF "Fix colors" */
+/*  *4    25-NOV-1991 09:53:04 TWF "Add points_only" */
+/*  *3     8-NOV-1991 15:54:36 TWF "Release pixvalues ASAP" */
+/*  *2     8-NOV-1991 15:00:41 TWF "Make sure deferred updates get drawn" */
+/*  *1     8-OCT-1991 13:44:36 TWF "XmdsWaveform private include" */
+/*  VAX/DEC CMS REPLACEMENT HISTORY, Element XMDSWAVEFORMP.H */
+/*------------------------------------------------------------------------------
+
+		Name:   XmdsWaveformP.H
+
+		Type:   C include
+
+		Author:	Thomas W. Fredian
+
+		Date:   9-OCT-1991
+
+		Purpose: Private declarations for WAVEFORM widget
+
+------------------------------------------------------------------------------
+   Copyright (c) 1989
+   Property of Massachusetts Institute of Technology, Cambridge MA 02139.
+   This program cannot be copied or distributed in any form for non-MIT
+   use without specific written approval of MIT Plasma Fusion Center
+   Management.
+----------------------------------------------------------------------------*/
+
+#ifndef _XmdsWaveformP_h
+#define _XmdsWaveformP_h
+
+#include <Xmds/XmdsWaveform.h>
+#include <Xm/XmP.h>
+
+#ifndef XmPRIMITIVE
+#include <Xm/PrimitiveP.h>
+#endif
+
+/* New fields for the Waveform widget class record */
+
+#ifndef XmInheritBorderHighlight /* Motif 1.1 */
+#define XmInheritBorderHighlight     ((XtWidgetProc) _XtInherit)
+#define XmInheritBorderUnhighlight   ((XtWidgetProc) _XtInherit)
+#define XmInheritArmAndActivate      ((XtWidgetProc) _XtInherit)
+#endif
+
+#define XmdsInheritTranslations ((XtTranslations) _XtInherit)
+#define XmdsInheritUpdate ((void (*)(Widget, XmdsWaveformValStruct *, XmdsWaveformValStruct *, char *,\
+                                     float *, float *, float *, float *,Boolean)) _XtInherit)
+#define XmdsInheritSetCrosshairs ((void (*)(Widget, float *, float *, Boolean)) _XtInherit)
+#define XmdsInheritSetPointerMode ((void (*)(Widget, int)) _XtInherit)
+#define XmdsInheritPrint   ((void (*)(Widget, FILE *, int, int, int, char *, char *, int)) _XtInherit)
+#define XmdsInheritReverse ((void (*)(Widget, int)) _XtInherit)
+#define XmdsInheritSetWave ((void (*)(Widget, int, float *, float *, Boolean *, Boolean *, Boolean, Boolean)) _XtInherit)
+
+typedef struct _WaveformClassPart
+{
+  XtTranslations default_trans;
+  XtTranslations zoom_trans;
+  XtTranslations drag_trans;
+  XtTranslations point_trans;
+  XtTranslations edit_trans;
+  void (*update_proc)(Widget, XmdsWaveformValStruct *, XmdsWaveformValStruct *, char *, float *, float *, float *, float *,Boolean);
+  /* WaveformUpdate(widget w,WaveformValStruct *x,WaveformValStruct *y,char *title,Boolean autoscale) */
+  void (*set_crosshairs_proc)(Widget, float *, float *, Boolean);
+  /* WaveformSetCrosshairs(widget w,float *x,float *y, Boolean attach) */
+  void (*set_pointer_mode_proc)(Widget, int);
+  /* WaveformSetPointerMode(widget w,int mode) */
+  void (*print_proc)(Widget, FILE *, int, int, int, char *, char *, int);
+  /* WaveformPrint(widget w, FILE *filefid, int inp_total_width, int inp_total_height, int inp_rotate, char *title,
+                                   char *window_title, int inp_resolution); */
+  void (*reverse_proc)(Widget, int);
+  /* WaveformReverse(widget w,int reverse) */
+  void (*set_wave_proc)(Widget, int, float *, float *, Boolean *, Boolean *, Boolean, Boolean);
+  /* WaveformSetWave(widget w, int count, float *x, float *y, Boolean *select, Boolean *pendown, Boolean autoscale, Boolean defer) */
+  caddr_t extension;
+} XmdsWaveformClassPart;
+/* Full class record declaration */
+
+typedef struct _XmdsWaveformClassRec
+{
+  CoreClassPart core_class;
+  XmPrimitiveClassPart primitive_class;
+  XmdsWaveformClassPart waveform_class;
+} XmdsWaveformClassRec;
+
+externalref XmdsWaveformClassRec xmdsWaveformClassRec;
+/* New fields for the Waveform widget record */
+
+/* Fields describing each axis */
+
+typedef struct _XmdsWaveformAxis
+{
+  float *minval;
+  float *maxval;
+  double crosshair;
+  int grid_lines;
+  Boolean labels;
+  int motion;
+  float *val_ptr;
+  XmdsWaveformValStruct *val_struct;
+  float *value;
+  int *pix_value;
+  int offset;
+  double pointer_resolution;
+} XmdsWaveformAxis;
+/* Fields global to widget */
+
+typedef struct _WaveformPart
+{
+  char *title;
+  int pointer_mode;
+  int grid_style;
+  XtCallbackList crosshairs_callback;
+  XtCallbackList align_callback;
+  XtCallbackList limits_callback;
+  XtCallbackList button3_callback;
+  XtCallbackList underlay_callback;
+  XtCallbackList overlay_callback;
+  XtCallbackList cut_callback;
+  XtCallbackList paste_callback;
+  Widget pan_with;
+  Boolean attach_crosshairs;
+  GC plot_gc;
+  GC grid_gc;
+  GC flip_gc;
+  GC erase_gc;
+  int count;
+  Boolean *selections;
+  Boolean *selections_val_ptr;
+  XmdsWaveformValStruct *selections_val_struct;
+  Boolean *pen_down;
+  Boolean *pen_down_val_ptr;
+  XmdsWaveformValStruct *pen_down_val_struct;
+  XFontStruct *font_struct;
+  union
+  {
+    XSegment crosshairs_pix[2];
+    XRectangle rectangle[2];
+  } cross_box;
+  Boolean panning;
+  Boolean show_mode;
+  Boolean show_selections;
+  Boolean step_plot;
+  Pixmap pixmap;
+  Window drawable;
+  XmdsWaveformAxis x;
+  XmdsWaveformAxis y;
+  Boolean reverse;
+  Boolean disabled;
+  Boolean redraw;
+  Boolean closed;
+} XmdsWaveformPart;
+/****************************************************************
+ *
+ * Full instance record declaration
+ *
+ ****************************************************************/
+
+typedef struct _XmdsWaveformRec
+{
+  CorePart core;
+  XmPrimitivePart primitive;
+  XmdsWaveformPart waveform;
+} XmdsWaveformRec;
+
+#endif				/* _XmdsWaveformP_h */
+/* DON'T ADD ANYTHING AFTER THIS #endif */
