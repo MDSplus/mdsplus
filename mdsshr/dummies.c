6b40f04cd2cd57c6187cc4903263c0155e8520ad
mdsplus <>
1997-11-25 16:19:08 +0000

    Add dummy routines

---
+++ mdsshr/dummies.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define Dummy(name,status) \
+int name()\
+{\
+  int stat = status;\
+  if (!(stat & 1))\
+    printf("%s not currently implemented\n",#name);\
+  return stat;\
+}
+
+Dummy(MdsGetCurrentShotid,0)
+Dummy(MdsEventCan,0)
