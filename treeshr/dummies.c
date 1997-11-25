7fd49a8a6e6940b6c176ed0c6589872246569945
mdsplus <>
1997-11-25 16:20:00 +0000

    move some dummy routines to mdsshr

--- treeshr/dummies.c
+++ treeshr/dummies.c
@@ -19,14 +19,10 @@ ret name args \
   return status;\
 }
 
-Dummy(MdsGetCurrentShotid,0)
 Dummy(TreeCallHook,1)
 Dummy(TreeEstablishRundownEvent,1)
 Dummy(TreeDoMethod,0)
 Dummy(_TreeDoMethod,0)
-Dummy(MdsEventCan,0)
-
 DummyArgs(TreeAddTag,int,(int nid,char *tag),1)
 DummyArgs(TreeRemoveNodesTags,int,(int nid),1)
-
 Dummy(TreeWait,1)
