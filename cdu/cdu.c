a758194020fcd69c8923afaaa104fec56fb118c9
mdsplus <>
1998-02-19 20:56:37 +0000

    include stdlib.h

--- cdu/cdu.c
+++ cdu/cdu.c
@@ -1,9 +1,9 @@
 #include        <stdio.h>
+#include        <stdlib.h>
 #include        <string.h>
 #include        "cdudef.h"
 #ifdef vms
 #include        <lib$routines.h>
-#include        <stdlib.h>
 #include        <ssdef.h>
 #else
 #include        <malloc.h>
