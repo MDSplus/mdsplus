e0291a70c27e122071713ceb52d7fd4ddfecf62c
mdsplus <>
1998-03-06 15:34:24 +0000

    *** empty log message ***

--- treeshr/TreeAddNode.c
+++ treeshr/TreeAddNode.c
@@ -467,7 +467,10 @@ int _TreeAddConglom(void *dbid, char *path, char *congtype, int *nid)
   int (*addr)();
   if (!IS_OPEN_FOR_EDIT(dblist))
     return TreeNOEDIT;
-  sprintf(exp,"%s__add('%s',_nid)",congtype,path);
+  if (path[0] == '\\')
+    sprintf(exp,"%s__add('\\%s',_nid)",congtype,path);
+  else
+    sprintf(exp,"%s__add('%s',_nid)",congtype,path);
   status = LibFindImageSymbol(&tdishr,&tdiexecute,&addr);
   if (status & 1)
   {
