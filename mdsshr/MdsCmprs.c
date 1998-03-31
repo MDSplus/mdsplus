05a767e3cdf0a0aeddb50f7dd63038c1941437fa
Bill Meyer <meyer8@llnl.gov>
1998-03-31 00:07:19 +0000

    RCS Keyword fix

--- mdsshr/MdsCmprs.c
+++ mdsshr/MdsCmprs.c
@@ -67,7 +67,7 @@
 #include <mdsshr.h>
 #include <string.h>
 #include <librtl_messages.h>
-static char *cvsrev = "@(#)$RCSFILE$ $Revision$ $Date$";
+static char *cvsrev = "@(#)$RCSfile$ $Revision$ $Date$";
 
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #define MAXX 1024		/*length of longest run allowed*/
