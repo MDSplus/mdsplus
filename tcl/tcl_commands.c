ecefa6a72db73640f535a392a37f9817f09be631
mdsplus <>
1998-04-07 20:21:14 +0000

    *** empty log message ***

--- tcl/tcl_commands.c
+++ tcl/tcl_commands.c
@@ -1,7 +1,7 @@
 #include        "clisysdef.h"
 
 		/* Filename: tcl_commands.c
-		 * created by cdu :  07-Apr-1998 16:13:15
+		 * created by cdu :  07-Apr-1998 16:19:34
 		 ********************************************************/
 
 extern int   TclDispatch_abort_server();
