16cb4d0326c27cec4c223c105ab282c6109ee724
Tom Gibney <gibney@beanie.pppl.gov>
1998-04-27 20:26:28 +0000

    *** empty log message ***

---
+++ tcl/tcl_show_current.c
@@ -0,0 +1,34 @@
+#include        "tclsysdef.h"
+#include        <mdsshr.h>
+
+/**********************************************************************
+* TCL_SHOW_CURRENT.C --
+*
+* TclShowCurrent:  Display current shotid.
+*
+* History:
+*  27-Apr-1998  TRG  Create.  Ported from original mds code.
+*
+************************************************************************/
+
+
+	/***************************************************************
+	 * TclShowCurrent:
+	 ***************************************************************/
+int TclShowCurrent()
+   {
+    int   shot;
+    char  text[80];
+    static DYNAMIC_DESCRIPTOR(dsc_experiment);
+
+    cli_get_value("EXPERIMENT",&dsc_experiment);
+    shot = MdsGetCurrentShotId(dsc_experiment.dscA_pointer);
+    if (shot)
+       {
+        sprintf(text,"Current shot is %d",shot);
+        TclTextOut(text);
+       }
+    else
+        MdsMsg(0,"Failed to get shotid!");
+    return 1;
+   }
