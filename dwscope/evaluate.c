1cbd03159b152b2a15a24d95f02ba6715d61b2cf
Thomas W. Fredian <twf@psfc.mit.edu>
1998-08-14 18:18:20 +0000

    Fix StrConcat call

--- dwscope/evaluate.c
+++ dwscope/evaluate.c
@@ -390,7 +390,7 @@ static Boolean Error(Boolean brief, String topic, String *error, struct descript
     topic_d.length = strlen(topic);
     topic_d.pointer = topic;
     TdiDebug(&get_messages,&messages MDS_END_ARG);
-    StrConcat(&messages,&topic_d,&lflf,&messages,0);
+    StrConcat(&messages,&topic_d,&lflf,&messages MDS_END_ARG);
     *error = memcpy(XtMalloc(messages.length+1),messages.pointer,messages.length);
     (*error)[messages.length]='\0';
     StrFree1Dx(&messages);
