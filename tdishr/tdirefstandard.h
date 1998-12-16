c61042bb6d8b75ea79a8200c5ee3c5012d40564f
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
1998-12-16 11:46:03 +0000

    Added vxWorks support for Alpha platform

--- tdishr/tdirefstandard.h
+++ tdishr/tdirefstandard.h
@@ -5,9 +5,10 @@
 
 	Ken Klare, LANL P-4	(c)1989,1991
 */
-#include <mdsdescrip.h>
-#include <mdsdescrip.h>
 #include <mds_stdarg.h>
+#include <mdsdescrip.h>
+/*#include <mdsdescrip.h>*/
+
 static struct descriptor_xd const EMPTY_XD = {0,DTYPE_DSC,CLASS_XD,0,0};
 
 #define TdiRefStandard(name) int name(\
