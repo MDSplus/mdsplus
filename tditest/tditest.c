17887ad60d4bba33e1e08a7a15baf7ae006c1cf0
Thomas W. Fredian <twf@psfc.mit.edu>
1999-04-22 15:51:50 +0000

    Fix for bad input file specification

--- tditest/tditest.c
+++ tditest/tditest.c
@@ -1,34 +1,3 @@
-/*  CMS REPLACEMENT HISTORY, Element TDITEST.C */
-/*  *29   26-AUG-1996 13:56:23 TWF "" */
-/*  *28   26-AUG-1996 13:55:30 TWF "No immediate" */
-/*  *27   26-AUG-1996 13:50:25 TWF "Do not do immediate evaluation" */
-/*  *26   31-JUL-1996 15:40:12 TWF "Clear out the message buffer" */
-/*  *25   30-JUL-1996 11:47:42 TWF "Use TDI$WRITE for all output" */
-/*  *24   30-JUL-1996 11:44:32 TWF "Use TDI$WRITE for all output" */
-/*  *23   30-JUL-1996 10:34:09 TWF "Use TDI$WRITE for all output" */
-/*  *22   30-JUL-1996 10:30:52 TWF "Use TDI$WRITE for all output" */
-/*  *21   10-JUL-1996 14:23:44 TWF "Put unit in variable" */
-/*  *20   10-JUL-1996 08:51:16 TWF "Format output of multi line" */
-/*  *19   10-JUL-1996 08:47:00 TWF "Add multi line capability" */
-/*  *18   10-JUL-1996 08:46:08 TWF "Add multi line capability" */
-/*  *17    9-JUL-1996 17:19:14 TWF "error output" */
-/*  *16    9-JUL-1996 17:11:09 TWF "Add error output" */
-/*  *15    8-JUL-1996 15:59:54 TWF "Use decompile of immediate" */
-/*  *14    8-JUL-1996 11:16:05 TWF "Write to same out" */
-/*  *13    8-JUL-1996 11:11:33 TWF "put back lf" */
-/*  *12    8-JUL-1996 11:08:19 TWF "Remove lf" */
-/*  *11    8-JUL-1996 11:06:50 TWF "Remove lf" */
-/*  *10    8-JUL-1996 11:03:57 TWF "" */
-/*  *9     8-JUL-1996 11:00:01 TWF "Format output" */
-/*  *8     8-JUL-1996 10:57:16 TWF "Format output" */
-/*  *7     8-JUL-1996 10:05:44 TWF "Allow input file on command line" */
-/*  *6     8-JUL-1996 10:01:33 TWF "Allow input file on command line" */
-/*  *5    26-JUN-1996 09:58:54 TWF "Port to Unix/Windows" */
-/*  *4    20-JUN-1996 15:29:04 JAS "" */
-/*  *3    20-JUN-1996 15:26:45 JAS "" */
-/*  *2    20-JUN-1996 15:22:21 JAS "echo the input" */
-/*  *1    20-JUN-1996 13:04:20 JAS "test program for tdishr" */
-/*  CMS REPLACEMENT HISTORY, Element TDITEST.C */
 #include <string.h>
 #include <stdio.h>
 #include <mdsdescrip.h>
@@ -57,7 +26,14 @@ int main(int argc, char **argv)
   int prefixlen = strlen(expr);
   char line_in[MAXEXPR];
   if (argc > 1)
+    {
 	  in = fopen(argv[1],"r");
+          if (in == (FILE *)0)
+	    {
+               printf("Error opening input file /%s/\n",argv[1]);
+               return 0;
+            }
+    }
   if (argc > 2)
   {
     struct descriptor out_d = {0,DTYPE_T,CLASS_S,0};
