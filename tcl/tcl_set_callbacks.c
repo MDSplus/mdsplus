3ea93412d6c97ef640806e1ff317ca84e6104994
Thomas W. Fredian <twf@psfc.mit.edu>
1999-04-28 18:18:24 +0000

    Add TclSaveOut

--- tcl/tcl_set_callbacks.c
+++ tcl/tcl_set_callbacks.c
@@ -34,6 +34,7 @@
 static void (*ErrorOut) ();
 static void (*TextOut) ();
 static void (*NodeTouched) ();
+static char * saved_output = 0;
 
 	/***************************************************************
 	 * TclSetCallbacks:
@@ -91,3 +92,53 @@ void TclNodeTouched(		/* Returns: void			*/
     if (NodeTouched)
       (*NodeTouched) (nid,type);
    }
+
+static void AppendOut(char *text)
+{
+	char *msg = text ? text : "";
+	int len = strlen(msg);
+	char *old_saved_output = saved_output;
+	if (saved_output)
+	{
+		saved_output = strcpy((char *)malloc(strlen(old_saved_output)+len+2),old_saved_output);
+		free(old_saved_output);
+	}
+	else
+	{
+		saved_output = (char *)malloc(len + 2);
+		saved_output[0] = '\0';
+	}
+	strcat(saved_output,msg);
+	strcat(saved_output,"\n");
+}
+
+void TclSaveOut()
+{
+	if (saved_output)
+	{
+		free(saved_output);
+		saved_output = 0;
+	}
+	TclSetCallbacks(AppendOut,AppendOut,NodeTouched);
+}
+
+int TclOutLen()
+{
+	return saved_output ? strlen(saved_output) : 0;
+}
+
+int TclGetOut(int free_out, int len_out, char *out)
+{
+    int len = 0;
+	if (saved_output)
+	{
+	  len = strlen(saved_output);
+      strncpy(out,saved_output,len_out);
+	  if (free_out)
+	  {
+		  free(saved_output);
+		  saved_output = 0;
+	  }
+	}
+	return len;
+}
