92572c2aa044743c9fec5dfd0255f16220ee2162
Joshua Stillerman <jas@psfc.mit.edu>
1999-08-23 19:12:26 +0000

    use memcpy for unalligned *(short *)

--- mdsshr/MdsSerialize.c
+++ mdsshr/MdsSerialize.c
@@ -496,7 +496,7 @@ static int copy_dx_rec( struct descriptor *in_ptr,char *out_ptr,unsigned int *b_
 	  {
             memcpy(out_ptr,((char *)inp)+offset(inp->pointer),inp->length);
             if (inp->dtype == DTYPE_FUNCTION && inp->length == 2)
-              *(short *)out_ptr = swapshort((char *)out_ptr);
+              memcpy(out_ptr,swapshort((char *)out_ptr),2);
           }
           out_ptr += inp->length;
         }
