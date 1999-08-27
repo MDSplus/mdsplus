2658d84f4ef0523e820cab67a015741dadf0f5f7
Joshua Stillerman <jas@psfc.mit.edu>
1999-08-27 16:18:20 +0000

    More Icons

--- icons/resource.h
+++ icons/resource.h
@@ -4,12 +4,14 @@
 //
 #define IDI_ICON1                       101
 #define IDI_ICON2                       103
+#define IDI_ICON3                       106
+#define IDI_ICON4                       107
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
