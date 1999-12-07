f52af20e1cb1ad516d21c9ed9c5629e566641d99
Thomas W. Fredian <twf@psfc.mit.edu>
1999-12-07 14:35:18 +0000

    Use padova event code

--- wfevent/wfevent.c
+++ wfevent/wfevent.c
@@ -4,44 +4,21 @@
 Program to wait for MDSPlus event from the command line.
 */
 
-main(argc,argv)
-int argc;
-char *argv[];
+void EventOccurred()
 {
+  exit(0);
+}
 
-    char data1[20];
-    char data2[20];
-    char data3[20];
-    char data4[20];
-		int len;
-    if(argc <= 1){
+main(int argc, char **argv)
+{
+    float forever = 1E6;
+    int eventid;
+    if(argc < 2){
         fprintf(stderr,"Usage: %s <event name>\n",argv[0]);
         exit(1);
     }
 
-
-    ipcs_init(NULL);    /*  this is an anonymous process  */
-
-
-		/*mdsq_event(argv[1]);*/
-		/*mdsq_event(argv[1]);*/
-
-		mdsq_event("junk1");
-		mdsq_event("junk2");
-		mdsq_event("junk3");
-		mdsq_event("junk4");
-    len = 20;
-		mdswtevent("junk1",data1,&len);
-		printf("data from event = %s\n",data1);
-		mdswtevent("junk2",data2,&len);
-		printf("data from event = %s\n",data2);
-		mdswtevent("junk3",data3,&len);
-		printf("data from event = %s\n",data3);
-		mdswtevent("junk4",data4,&len);
-		printf("data from event = %s\n",data4);
-		mdsdeq_all();
-		/*mdswtevent(argv[1],data,&len);*/
-
-
+    MDSEventAst(argv[1], EventOccurred, 0, &eventid);
+    LibWait(&forever);
 }
 
