8050280e5fed43d5e74f80002052a7eba2309b7d
Thomas W. Fredian <twf@psfc.mit.edu>
2000-01-11 20:47:59 +0000

    Add optional data

--- setevent/setevent.c
+++ setevent/setevent.c
@@ -1,14 +1,18 @@
-extern int MDSEvent(char *eventname);
+#include <string.h>
+extern int MDSEvent(char *eventname,int len, char *data);
 
 int main(int argc, char **argv)
 {
   int status;
   if (argc < 2)
   {
-    printf("Usage: %s <event>\n",argv[0]);
+    printf("Usage: %s <event> [optional-data-string]\n",argv[0]);
     status = 0;
   }
   else
-    status = MDSEvent(argv[1]);
+  {
+    int len = (int)((argc > 2) ? strlen(argv[2]) : 0);
+    status = MDSEvent(argv[1],len,argv[2]);
+  }
   return(status);
 }    
