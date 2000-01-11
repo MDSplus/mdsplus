48a20f361db39f6dfea81e5e30718eef6467a996
Thomas W. Fredian <twf@psfc.mit.edu>
2000-01-11 20:55:39 +0000

    Add event data

--- wfevent/wfevent.c
+++ wfevent/wfevent.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
 Program to wait for MDSPlus event from the command line.
 */
 
-void EventOccurred()
+static int printdata = 0;
+
+void EventOccurred(void *astprm, int len, char *data)
 {
+  if (printdata)
+  {
+    char *s = strncpy((char *)malloc(len+1),data,len);
+    s[len] = 0;
+    printf("%s\n",s);
+  }
   exit(0);
 }
 
@@ -14,10 +23,10 @@ main(int argc, char **argv)
     float forever = 1E6;
     int eventid;
     if(argc < 2){
-        fprintf(stderr,"Usage: %s <event name>\n",argv[0]);
+        fprintf(stderr,"Usage: %s <event name> [-d]\n",argv[0]);
         exit(1);
     }
-
+    printdata = (argc == 3) && (strcmp(argv[2],"-d") == 0); 
     MDSEventAst(argv[1], EventOccurred, 0, &eventid);
     LibWait(&forever);
 }
