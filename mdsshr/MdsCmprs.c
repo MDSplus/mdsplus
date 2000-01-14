9a3d107283dad266b0a3814f0e7ac0601d5e90b9
Thomas W. Fredian <twf@psfc.mit.edu>
2000-01-14 16:40:51 +0000

    Revert to earlier version until bug discovered

--- mdsshr/MdsCmprs.c
+++ mdsshr/MdsCmprs.c
@@ -1,66 +1,66 @@
-/*	MdsCmprs.C
-	An algorithm to compress data with small differences.
-	Only data less than 32 bits can be packed.
-	The run length may be varied up to the maximum by repeated calls.
+/*      MdsCmprs.C
+        An algorithm to compress data with small differences.
+        Only data less than 32 bits can be packed.
+        The run length may be varied up to the maximum by repeated calls.
 
-	F77: MdsCmprs(nitems, %descr(items), %descr(pack), bit)
-	F77: MdsXpand(nitems, %descr(pack), %descr(items), bit)
+        F77: MdsCmprs(nitems, %descr(items), %descr(pack), bit)
+        F77: MdsXpand(nitems, %descr(pack), %descr(items), bit)
 
-	nitems	is number of items to use.
-	items	is a descriptor of uncompressed data.
-	pack	is a descriptor of compressed array of sufficient size to hold the result.
-	bit	is a modified longword bit offset from the beginning of pack.
-	Error if pack cannot hold all the compressed data.
+        nitems  is number of items to use.
+        items   is a descriptor of uncompressed data.
+        pack    is a descriptor of compressed array of sufficient size to hold the result.
+        bit     is a modified longword bit offset from the beginning of pack.
+        Error if pack cannot hold all the compressed data.
 
-	Each block of information has a header and two data blocks.
-	Each block is independent, it has its own starting value.
-	The header has the number of bits (y) and the count (x) of each data block.
-	Think of the data histogrammed by its significance (y) vs. index (x).
-	Header block: yn & xn-1 & ye-1 & xe
-	Normal block: xn fields of yn each.
-	Exception block: xe fields of ye each.
-	The exception block is absent if there are no exceptions.
-	The normal block is absent if all items are zero.
+        Each block of information has a header and two data blocks.
+        Each block is independent, it has its own starting value.
+        The header has the number of bits (y) and the count (x) of each data block.
+        Think of the data histogrammed by its significance (y) vs. index (x).
+        Header block: yn & xn-1 & ye-1 & xe
+        Normal block: xn fields of yn each.
+        Exception block: xe fields of ye each.
+        The exception block is absent if there are no exceptions.
+        The normal block is absent if all items are zero.
 
-	The input is differenced and these numbers are checked
-	for the number of bits in their representation.
-	The data is optimally divided into normal points and exceptions.
-	The normal points have the differences except those points that
-	are exceptions are marked with the most negative twos complement
-	number for the size of the normal point.
-	Thus non-exceptional points are in a symmetric range about 0.
-	The exception list can approach 31/32 as long as the difference list.
+        The input is differenced and these numbers are checked
+        for the number of bits in their representation.
+        The data is optimally divided into normal points and exceptions.
+        The normal points have the differences except those points that
+        are exceptions are marked with the most negative twos complement
+        number for the size of the normal point.
+        Thus non-exceptional points are in a symmetric range about 0.
+        The exception list can approach 31/32 as long as the difference list.
 
-	Numerical example: items = NINT(256.*SIN(LOG(1..10)))
-	items	= [0,164,228,252,256,250,238,224,207,190]
-	diff	= [0,164, 64, 24,  4, -6,-12,-14,-17,-17]
-	i	= [0,  9,  8,  6,  4,  4,  5,  5,  6,  6]
-	y	= [1,2,3,4,5,6,7,8,9]
-	tally	= [1,0,0,2,2,3,0,1,1]
-	xsum	= [1,1,1,3,5,8,8,9,10]
-	ye = 9; yn = 6; xe = 2; xn = 10
-	for MAXX 1024:
-	pack	= [0x00880246, 0x84620800, 0x4befcb4e, 0x080a] 110 bits
-	.0...8...0...a...4...b...e...f...c...b...4...e...8...4...6...2...0...8...0...0...0...0...8...8...0...2...4...6
-	00100000001010010010111110111111001011010011101000010001100010000010000000000000000000100010000000001001000110
-	.......64......164...-17...-17...-14...-12....-6.....4....24....ex....ex.....0......xe=2.....8....xn-1=9..yn=6
+        Numerical example: items = NINT(256.*SIN(LOG(1..10)))
+        items   = [0,164,228,252,256,250,238,224,207,190]
+        diff    = [0,164, 64, 24,  4, -6,-12,-14,-17,-17]
+        i       = [0,  9,  8,  6,  4,  4,  5,  5,  6,  6]
+        y       = [1,2,3,4,5,6,7,8,9]
+        tally   = [1,0,0,2,2,3,0,1,1]
+        xsum    = [1,1,1,3,5,8,8,9,10]
+        ye = 9; yn = 6; xe = 2; xn = 10
+        for MAXX 1024:
+        pack    = [0x00880246, 0x84620800, 0x4befcb4e, 0x080a] 110 bits
+        .0...8...0...a...4...b...e...f...c...b...4...e...8...4...6...2...0...8...0...0...0...0...8...8...0...2...4...6
+        00100000001010010010111110111111001011010011101000010001100010000010000000000000000000100010000000001001000110
+        .......64......164...-17...-17...-14...-12....-6.....4....24....ex....ex.....0......xe=2.....8....xn-1=9..yn=6
 
-	On VAXstation 3100, time for 1000 items in microseconds/item (1000 trials):
-	dtype:		B	W	L	F	ANINT(F)
-	pk:		na	na	3.2 to 3.5
-	unpk:		na	na	3.0 >=10000 bits required
-	cmprs:		10.8	9.8	8.3	14.6	15.4
-	xpand:		6.0	6.0	5.7	6.7	8.2
-	bits:		2689	2671	2671	22408	17000
-	compress:	11.9	10.8	9.1	16.2	16.8
-	decompress:	6.1	6.1	5.7	6.8	8.3
-	data bytes:	337	334	334	2801	2125
-	data bits:	2696	2672	2672	22408	17000
+        On VAXstation 3100, time for 1000 items in microseconds/item (1000 trials):
+        dtype:          B       W       L       F       ANINT(F)
+        pk:             na      na      3.2 to 3.5
+        unpk:           na      na      3.0 >=10000 bits required
+        cmprs:          10.8    9.8     8.3     14.6    15.4
+        xpand:          6.0     6.0     5.7     6.7     8.2
+        bits:           2689    2671    2671    22408   17000
+        compress:       11.9    10.8    9.1     16.2    16.8
+        decompress:     6.1     6.1     5.7     6.8     8.3
+        data bytes:     337     334     334     2801    2125
+        data bits:      2696    2672    2672    22408   17000
 
-	Ken Klare, LANL P-4	(c)1990
-	Based on ideas of Thomas W. Fredian, MIT (c)1985, the DELTA method.
-	Assumes: 8-bit bytes, 32-bit long, 2's complement
-	MdsPk/MdsUnpk do right-to-left packing/unpacking, not assumed here.
+        Ken Klare, LANL P-4     (c)1990
+        Based on ideas of Thomas W. Fredian, MIT (c)1985, the DELTA method.
+        Assumes: 8-bit bytes, 32-bit long, 2's complement
+        MdsPk/MdsUnpk do right-to-left packing/unpacking, not assumed here.
 */
 
 #include <string.h>
@@ -70,34 +70,88 @@
 static char *cvsrev = "@(#)$RCSfile$ $Revision$ $Date$";
 
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define MAXX 1024		/*length of longest run allowed*/
-#define MAXY 32			/*maximum bits that we can pack*/
-#define BITSX 10		/*number of bits for run length*/
-#define BITSY 6			/*number of bits in y 0-32*/
-#define MASK(bits)  ((unsigned int)0xffffffff >> (32 - bits))
-#define YFIELD(y) ((unsigned int)(y) & MASK(BITSY))
-#define XFIELD(x) ((unsigned int)(x) & MASK(BITSX) << BITSY)
-#define X_AND_Y(x,y) (XFIELD(x) | YFIELD(y))
-#define X_OF_INT(val) (((unsigned int)(val) >> BITSY) & MASK(BITSX))
-#define Y_OF_INT(val) ((unsigned int)(val) & MASK(BITSY))
-#define SWAP_SHORTS(in,out)         ((short *)out)[0] = ((short *)in)[1],((short *)out)[1] = ((short *)in)[0]
+#define MAXX 1024               /*length of longest run allowed*/
+#define MAXY 32                 /*maximum bits that we can pack*/
+#define BITSX 10                /*number of bits for run length*/
+#define BITSY 6                 /*number of bits in y 0-32*/
 
+#if defined(__hpux)
+#pragma HP_ALIGN NOPADDING PUSH
+#elif defined(__sgi)
+#pragma pack(1)
+#elif defined(__GNUC__)
+#pragma pack(1)
+#elif defined(_AIX)
+#pragma options align=packed
+#endif
 
-typedef struct {int l;} *PF;
+#ifdef _big_endian
+typedef union
+{
+  int       l;
+  struct
+  {
+    short     s1,
+              s0;
+  }         s;
+}        *PF;
+
+union Y_X
+{
+  int       dummy;
+  struct
+  {
+    unsigned  :16;
+    unsigned  x:BITSX;
+    unsigned  y:BITSY;
+  }         fields;
+};
+#else
+typedef union
+{
+  int       l;
+  struct
+  {
+    short     s0,
+              s1;
+  }         s;
+}        *PF;
+
+union Y_X
+{
+  int       dummy;
+  struct
+  {
+    unsigned  y:BITSY;
+    unsigned  x:BITSX;
+  }         fields;
+};
+#endif
+
 struct HEADER
 {
-  int n;
-  int e;
+  union Y_X n;
+  union Y_X e;
 };
+
+#if defined(__hpux)
+#pragma HP_ALIGN POP
+#elif defined(__GNUC__)
+#pragma pack()
+#elif defined(__sgi)
+#pragma pack(0)
+#elif defined(_AIX)
+#pragma options align=power
+#endif
 
 static char FIELDSY = BITSY + BITSX;
 static int FIELDSX = 2;
 
 int       MdsCmprs(
-		              int *nitems_ptr,
-		              struct descriptor_a *items_dsc_ptr,
-		              struct descriptor_a *pack_dsc_ptr,
-		              int *bit_ptr)
+                              int *nitems_ptr,
+                              struct descriptor_a *items_dsc_ptr,
+                              struct descriptor_a *pack_dsc_ptr,
+                              int *bit_ptr)
 {
   int       nitems = *nitems_ptr;
   int       step = items_dsc_ptr->length;
@@ -133,7 +187,7 @@ int       MdsCmprs(
   int       diff[MAXX],
               exce[MAXX];
   static int signif[65] = {0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-	  6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7};
+          6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7};
 /***************************
 Text is by the character.
 Otherwise try int or short.
@@ -168,26 +222,26 @@ Do this in runs.
     {
      default:
       if (step == sizeof(int))
-	goto case_LU;
+        goto case_LU;
       if (step == sizeof(short))
-	goto case_WU;
+        goto case_WU;
       goto case_BU;
      case DTYPE_T:
      case DTYPE_BU:
   case_BU:for (; --j >= 0;)
-	*pe++ = *pxuc++;
+        *pe++ = *pxuc++;
       break;
      case DTYPE_B:
       for (; --j >= 0;)
-	*pe++ = *pxc++;
+        *pe++ = *pxc++;
       break;
      case DTYPE_WU:
   case_WU:for (; --j >= 0;)
-	*pe++ = *pxus++;
+        *pe++ = *pxus++;
       break;
      case DTYPE_W:
       for (; --j >= 0;)
-	*pe++ = *pxs++;
+        *pe++ = *pxs++;
       break;
      case DTYPE_L:
      case DTYPE_LU:
@@ -208,7 +262,7 @@ Do this in runs.
      case DTYPE_H:
      case DTYPE_HC:
       for (; --j >= 0;)
-	SWAP_SHORTS(px,pe),++pe,++px;
+        ((PF) pe)->s.s0 = ((PF) px)->s.s1, ((PF) pe)->s.s1 = ((PF) px)->s.s0, ++pe, ++px;
       break;
     }
 
@@ -223,28 +277,28 @@ Do this in runs.
     for (pn = diff, j = xn; --j >= 0; old = *p32++)
     {
       if ((int) (*pn++ = i = *p32 - old) < 0)
-	i = -i;
+        i = -i;
       if ((unsigned int) i <= 64)
-	yy = signif[i];
+        yy = signif[i];
       else
       {
-	yy = 0;
-	if ((unsigned int) i > 0x1000000)
-	{
-	  i = (unsigned int) i >> 24;
-	  yy += 24;
-	}
-	if ((unsigned int) i > 0x1000)
-	{
-	  i = (int) i >> 12;
-	  yy += 12;
-	}
-	if ((unsigned int) i > 0x40)
-	{
-	  i = (int) i >> 6;
-	  yy += 6;
-	}
-	yy += signif[i];
+        yy = 0;
+        if ((unsigned int) i > 0x1000000)
+        {
+          i = (unsigned int) i >> 24;
+          yy += 24;
+        }
+        if ((unsigned int) i > 0x1000)
+        {
+          i = (int) i >> 12;
+          yy += 12;
+        }
+        if ((unsigned int) i > 0x40)
+        {
+          i = (int) i >> 6;
+          yy += 6;
+        }
+        yy += signif[i];
       }
       ++tally[yy];
     }
@@ -278,11 +332,11 @@ Do this in runs.
       best = xn * yy;
       while (--yy > 0)
       {
-	if ((test = xn * yy + *--ptally * ye) < best)
-	{
-	  best = test;
-	  yn = yy;
-	}
+        if ((test = xn * yy + *--ptally * ye) < best)
+        {
+          best = test;
+          yn = yy;
+        }
       }
       xe = tally[yn - 1];
     }
@@ -295,7 +349,7 @@ Do this in runs.
     for (pn = diff, pe = exce, j = xe; --j >= 0;)
     {
       while (*pn <= maxim && *pn > mark)
-	++pn;
+        ++pn;
       *pe++ = *pn;
       *pn++ = mark;
     }
@@ -305,8 +359,12 @@ Do this in runs.
   ******************************/
     if (*bit_ptr + xe * ye + xn * yn > limit)
       return LibSTRTRU;
-    header.n = X_AND_Y(xn - 1,yn);
-    header.e = X_AND_Y(xe,ye - 1);
+    header.n.dummy = 0;
+    header.n.fields.x = xn - 1;
+    header.n.fields.y = yn;
+    header.e.dummy = 0;
+    header.e.fields.x = xe;
+    header.e.fields.y = ye - 1;
     MdsPk((char *) &FIELDSY, &FIELDSX, (int *) ppack, (int *) &header, (int *) bit_ptr);
     yn_c = (char)yn;
     ye_c = (char)ye;
@@ -316,14 +374,14 @@ Do this in runs.
   return 1;
 }
 /*----------------------------------------------------------
-	MdsXpand.C
-	Expand compressed data.
+        MdsXpand.C
+        Expand compressed data.
 */
 int       MdsXpand(
-		              int *nitems_ptr,
-		              struct descriptor_a *pack_dsc_ptr,
-		              struct descriptor_a *items_dsc_ptr,
-		              int *bit_ptr)
+                              int *nitems_ptr,
+                              struct descriptor_a *pack_dsc_ptr,
+                              struct descriptor_a *items_dsc_ptr,
+                              int *bit_ptr)
 {
   int       nitems = *nitems_ptr;
   char     *ppack = pack_dsc_ptr->pointer;
@@ -364,13 +422,13 @@ Note the sign-extended unpacking.
     if (*bit_ptr + 2 * (BITSY + BITSX) > limit)
       break;
     MdsUnpk(&nbits, (int *) &FIELDSX, (int *) ppack, (int *) &header, (int *) bit_ptr);
-    xhead = j = X_OF_INT(header.n) + 1;
+    xhead = j = header.n.fields.x + 1;
     if (j > nitems)
       j = nitems;
     xn = j;
-    yn = -(int)Y_OF_INT(header.n);
-    xe = X_OF_INT(header.e);
-    ye = -(int)Y_OF_INT(header.e) + 1;
+    yn = -(int)header.n.fields.y;
+    xe = header.e.fields.x;
+    ye = -(int)(header.e.fields.y + 1);
     if (*bit_ptr - ye * xe - yn * j > limit)
       break;
     nitems -= j;
@@ -396,9 +454,9 @@ Note the sign-extended unpacking.
     {
      default:
       if (step == sizeof(int))
-	goto case_LU;
+        goto case_LU;
       if (step == sizeof(short))
-	goto case_WU;
+        goto case_WU;
       goto case_BU;
      case DTYPE_T:
      case DTYPE_B:
@@ -437,14 +495,14 @@ Note the sign-extended unpacking.
       pf = (PF) & f;
       pf->l = 0;
       if (xe)
-	for (; --j >= 0;)
-	  if (*pn != mark)
-	    pf->l += *pn++, SWAP_SHORTS(pf,px), ++px;
-	  else
-	    ++pn, pf->l += *pe++, SWAP_SHORTS(pf,px),++px;
+        for (; --j >= 0;)
+          if (*pn != mark)
+            pf->l += *pn++, ((PF) px)->s.s0 = pf->s.s1, ((PF) px)->s.s1 = pf->s.s0, ++px;
+          else
+            ++pn, pf->l += *pe++, ((PF) px)->s.s0 = pf->s.s1, ((PF) px)->s.s1 = pf->s.s0, ++px;
       else
-	for (; --j >= 0;)
-	  pf->l += *pn++, SWAP_SHORTS(pf,px), ++px;
+        for (; --j >= 0;)
+          pf->l += *pn++, ((PF) px)->s.s0 = pf->s.s1, ((PF) px)->s.s1 = pf->s.s0, ++px;
       break;
     }
   }
