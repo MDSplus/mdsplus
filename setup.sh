8bf47d806d82ddc48a79ab5963f7d3fc0b944968
Thomas W. Fredian <twf@psfc.mit.edu>
2000-05-12 14:27:04 +0000

    do not use ENVIRON variable in awk

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ awkcmd='{ if (($3 != "") && ((substr($3,2,1) == ":") || (substr($3,2,1) == ";"))
 awkcmd="$awkcmd"'{ if ($1 == "source") print "" ; '
 awkcmd="$awkcmd"'else if ($1 == ".") print $0 ";"; '
 awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) && (("'$setup_operation'" == "unset"))) print "unset " $1 ";" ; '
-awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) && ((ENVIRON[$1] == "") || (NF < 3))) print $1 "=" $2 "; export " $1 ";" ; '
-awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) && (substr($3,1,1) == "<")) print $1 "=" $2 substr($3,2) "$" $1 "; export " $1 ";" ;'
-awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) && (substr($3,1,1) == ">")) print $1 "=$" $1 substr($3,2) $2 "; export " $1 ";" }'
+awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) &&  (NF < 3)) print $1 "=" $2 "; export " $1 ";" ; '
+awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) && (substr($3,1,1) == "<")) print "if (test \"$" $1 "\" = \"\") then " $1 "=" $2 "; " '
+awkcmd="$awkcmd"'" else " $1 "=" $2 substr($3,2) "$" $1 "; fi; export " $1 ";"  ;'
+awkcmd="$awkcmd"'else if (($1 !~ /^#.*/) && (substr($3,1,1) == ">")) print "if (test \"$" $1 "\" = \"\") then " $1 "=" $2 "; " '
+awkcmd="$awkcmd"'" else " $1 "=$" $1 substr($3,2) $2 "; fi; export " $1 ";" }'
 eval `awk "$awkcmd" $setup_file`
