01ae4bd1b092ee8e3d85c11120a39a922fcc6159
Thomas W. Fredian <twf@psfc.mit.edu>
2000-06-23 13:09:07 +0000

    Add XmdsWidgets.uil

--- include/ipdesc.h
+++ include/ipdesc.h
@@ -1,30 +1,5 @@
-/*  CMS REPLACEMENT HISTORY, Element IPDESC.H */
-/*  *22   12-JAN-1999 16:13:39 JMS "Removed function prototypes for MdsOpen, MdsValue etc. */
-/*  *21   21-APR-1998 11:16:34 TWF "Add MakeDescripWidthLen" */
-/*  *20   17-APR-1998 10:57:54 TWF "Add MakeDescr prototype" */
-/*  *19   17-APR-1998 10:56:53 TWF "Add MakeDescr prototype" */
-/*  *18   14-APR-1998 11:58:54 TWF "Reduce maxdims and return length field" */
-/*  *17   16-OCT-1995 13:23:47 TWF "Cleaner windows version" */
-/*  *16   23-DEC-1994 09:13:03 TWF "Change how events are handled" */
-/*  *15   21-DEC-1994 14:16:20 TWF "Add event can" */
-/*  *14   21-DEC-1994 12:03:53 TWF "Add event can" */
-/*  *13   21-DEC-1994 12:02:55 TWF "Add event can" */
-/*  *12   20-DEC-1994 17:24:56 TWF "support events on unix" */
-/*  *11    7-JUL-1994 12:08:10 TWF "Change _NO_PROTO to something else" */
-/*  *10   14-JUN-1994 09:28:55 TWF "add CRAY" */
-/*  *9    13-JUN-1994 09:52:08 TWF "Add NO_PROTO" */
-/*  *8    10-JUN-1994 13:47:00 TWF "use define with int" */
-/*  *7     9-JUN-1994 16:20:20 TWF "Use int instead of long" */
-/*  *6     9-JUN-1994 16:18:58 TWF "Use int instead of long" */
-/*  *5     2-JUN-1994 12:20:35 TWF "Change GetAnswerInfo to return pointer to data" */
-/*  *4     2-JUN-1994 12:18:36 TWF "Change GetAnswerInfo to return pointer to data" */
-/*  *3     1-JUN-1994 12:14:05 TWF "Add RS6000 support" */
-/*  *2    17-MAY-1994 09:34:55 TWF "Add CDECL" */
-/*  *1    17-MAY-1994 09:33:58 TWF "descriptor structures" */
-/*  CMS REPLACEMENT HISTORY, Element IPDESC.H */
 #ifdef _WIN32
 #include "windows.h"
-//#include "winsock.h"
 #else
 typedef int SOCKET;
 #endif
@@ -47,11 +22,11 @@ typedef int SOCKET;
 #endif
 #define DTYPE_DOUBLE  11
 #define DTYPE_COMPLEX 12
+#define DTYPE_CSTRING 14
 #define DTYPE_EVENT_NOTIFY   99
 #ifndef DTYPE_EVENT
 #define DTYPE_EVENT DTYPE_EVENT_NOTIFY
 #endif
-#define DTYPE_CSTRING 14
 
 struct descrip { char dtype;
                  char ndims;
@@ -71,6 +46,7 @@ extern struct descrip *MakeDescrip();
 extern struct descrip *MakeDescripWithLength();
 extern int   MdsEventAst();
 extern int   MdsEventCan();
+extern int   HostToIp();
 #ifndef MdsLib_H
 extern int   MdsValue();
 extern int   MdsPut();
@@ -90,6 +66,7 @@ extern struct descrip *MakeDescrip(struct descrip *in_descrip, char dtype, char 
 extern struct descrip *MakeDescripWithLength(struct descrip *in_descrip, char dtype, int length, char ndims, int *dims, void *ptr);
 extern int   MdsEventAst(SOCKET sock, char *eventnam, void (*astadr)(), void *astprm, int *eventid);
 extern int   MdsEventCan(SOCKET sock, int eventid);
+extern int HostToIp(char *host, int *addr, short *port);
 #ifndef MdsLib_H
 extern int   MdsValue(SOCKET sock, char *,...);
 extern int   MdsPut(SOCKET sock, char *node, char *expression,...);
@@ -98,4 +75,3 @@ extern int   MdsSetDefault(SOCKET sock, char *node);
 extern int   MdsClose(SOCKET sock);
 #endif
 #endif
-
