ab8b082985cee9203a781525e57326897d7195b5
Thomas W. Fredian <twf@psfc.mit.edu>
2000-09-14 13:33:47 +0000

    Add _int64u

--- include/mdstypes.h
+++ include/mdstypes.h
@@ -6,9 +6,13 @@
 #if (SIZEOF__INT64 != 8)
 #if (SIZEOF_LONG == 8)
 typedef long _int64;
+typedef unsigned long _int64u;
 #elif (SIZEOF_LONG_LONG == 8)
 typedef long long _int64;
+typedef unsigned long long _int64u;
 #endif
+#else
+typedef unsigned _int64 _int64u;
 #endif
 
 #endif
