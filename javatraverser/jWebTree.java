8d86df5c7dbfeeb05261f2bb639ed744085a96c0
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2001-04-19 09:44:38 +0000

    Last version of jWebTree

--- javatraverser/jWebTree.java
+++ javatraverser/jWebTree.java
@@ -9,10 +9,12 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
     int shot;
     String experiment;
     boolean display_wave;
+    boolean is_jscope = false;
     NidData default_nid;
     String default_path;
+    HttpServletResponse response;
     static final String BASEREF = "jWebTree";
-    static final int MAX_DISPLAY_LENGTH = 512;
+    static final int MAX_DISPLAY_LENGTH = 4096;
     static final String FIRST_PLUGIN_DEF = 
         "<OBJECT classid=\"clsid:8AD9C840-044E-11D1-B3E9-00805F499D93\" "+
         " NAME = \"WaveDisplay\" HEIGHT = \"200\" WIDTH = \"600\" ALIGN = \"baseline\"  codebase=\"http://java.sun.com/products/plugin/1.3/jinstall-13-win32.cab#Version=1,3,0,0\">"+"\n"+
@@ -22,6 +24,7 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
         "<PARAM NAME = \"NAME\" VALUE = \"WaveDisplay\" >"+"\n"+
         "<PARAM NAME=\"type\" VALUE=\"application/x-java-applet;version=1.3\">"+"\n"+
         "<PARAM NAME=\"scriptable\" VALUE=\"false\">"+"\n"+
+        "<PARAM NAME = \"AUTENTICATION\" VALUE = \"b2JhcmFuYTpjbHVibGF0aW5v\">"+
         "<PARAM NAME = \"SIGNAL\" VALUE  = \" url = ";
         //mds://150.178.3.80/a/14000/\emra_it
         
@@ -32,10 +35,11 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
         "CODE = \"WaveDisplay.class\"+\"\n"+
         "ARCHIVE = \"WaveDisplay.jar\""+"\n"+ 
         "NAME = \"WaveDisplay\" WIDTH = \"700\" HEIGHT = \"250\" ALIGN = \"baseline\""+"\n"+ 
+        "AUTENTICATION = \"b2JhcmFuYTpjbHVibGF0aW5v\""+
         "SIGNAL=\"url = ";
         //mds:://150.178.3.80/a/14000/\emra_it title = TITOLO "  
     static final String THIRD_PLUGIN_DEF = 
-        "\nscriptable=false pluginspage=\"http://java.sun.com/products/plugin/1.3/plugin-install.html\">"+"\n"+
+        "\"\nscriptable=false pluginspage=\"http://java.sun.com/products/plugin/1.3/plugin-install.html\">"+"\n"+
         "<NOEMBED>"+"\n"+
         "</COMMENT>"+"\n"+
         "No JDK 1.2 support for APPLET!!"+"\n"+
@@ -45,12 +49,25 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
     protected String getPlugin(String path)
     {
         String url, temp;
-        try {
+        if(experiment.equals("jet"))
+        {
+            try{
+                StringTokenizer st = new StringTokenizer(path,":");
+                st.nextToken();
+                st.nextToken();
+                url = "rda://data.jet.uk/"+st.nextToken()+"/"+shot
+                    +"/"+st.nextToken()+"/"+st.nextToken();
+            }catch(Exception exc){return "<h1>Cannot define plugin</h1>";}
+        }
+        else 
+        {
+            try {
                url = "mds://"+InetAddress.getLocalHost().getHostAddress()+"/"+experiment+
                   "/"+shot+"/"+path;
     //        url = "mds://"+"150.178.3.80"+"/"+experiment+
     //           "/"+shot+"/"+path;
-        }catch(Exception exc){return "<h1>Cannot define plugin</h1>";}
+            }catch(Exception exc){return "<h1>Cannot define plugin</h1>";}
+        }
         temp =  FIRST_PLUGIN_DEF+url+SECOND_PLUGIN_DEF+url+THIRD_PLUGIN_DEF;
         return temp;
     }
@@ -64,9 +81,13 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
         try {
             display_wave = request.getParameter("display").equals("true");
         }catch(Exception exc){display_wave = false; }
+        try {
+            is_jscope = request.getParameter("jscope").equals("true");
+        }catch(Exception exc){is_jscope = false; }
         
         int i, curr_shot;
-               
+        
+        this.response = response;      
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html>");
@@ -87,7 +108,10 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
                     tree.close();
                 experiment = curr_experiment;
                 shot = curr_shot;
-                tree = new Database(experiment, shot);
+                if(experiment.equals("jet"))
+                    tree = new Database(experiment, -1);
+                else
+                    tree = new Database(experiment, shot);
                 tree.open();
             }catch(Exception exc) {tree = null; }
         }
@@ -127,7 +151,7 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
                 {
                     NodeInfo node_info = tree.getInfo(sons[i]);
                     out.println("<li><a href="+BASEREF+"?path="+ default_path+"."+node_info.getName().trim()+
-                        "&experiment="+curr_experiment+"&shot="+shot+"&display="+display_wave+">"+
+                        "&experiment="+curr_experiment+"&shot="+shot+"&display="+display_wave+"&jscope="+is_jscope+">"+
                         node_info.getName().trim()+"</a>");
                 }
             }
@@ -137,7 +161,7 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
                 {
                     NodeInfo node_info = tree.getInfo(members[i]);
                     out.println("<li><a href="+BASEREF+"?path="+ default_path+":"+node_info.getName().trim()+
-                        "&experiment="+curr_experiment+"&shot="+shot+"&display="+display_wave+">"+
+                        "&experiment="+curr_experiment+"&shot="+shot+"&display="+display_wave+"&jscope="+is_jscope+">"+
                         node_info.getName().trim()+"</a>");
                 }
             }
@@ -167,31 +191,32 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
             Data data = tree.getData(nid);
             if(data != null)
             {
+                if(data instanceof WithUnitsData)
+                {
+                    try {
+                        String units = ((WithUnitsData)data).getUnits().toString();
+                        out.println("units = " + units+"<br>");
+                        data = ((WithUnitsData)data).getDatum();
+                    }catch(Exception exc) {out.println("Cannot display units: " + exc);}
+                }
+                
                 if(data instanceof ParameterData)
                 {
                     try {
                         String comment = ((ParameterData)data).getHelp().toString();
                         out.println("comment = " + comment+"<br>");
                         data = ((ParameterData)data).getDatum();
-                    }catch(Exception exc) {}
-                }
-                if(data instanceof WithUnitsData)
-                {
-                    try {
-                        String units = ((WithUnitsData)data).getUnits().toString();
-                        out.println("units = " + units+"<br>");
-                        data = ((ParameterData)data).getDatum();
-                    }catch(Exception exc) {}
+                    }catch(Exception exc) {out.println("Cannot display help: " + exc);}
                 }
             
-                else if(info.getLength() <= MAX_DISPLAY_LENGTH)   //display decompiled
+                if(info.getLength() <= MAX_DISPLAY_LENGTH)   //display decompiled
                 {
                     data = convertPaths(data);
                     try {
                         out.println("<p>"+ "expression = "+data.toString() +"<p>");
-                    }catch(Exception exc){}
+                    }catch(Exception exc){out.println("The expression cannot be decompiled: "+exc);}
                 }
-                else if(!display_wave)
+                else if(!display_wave && !is_jscope)
                     out.println("<p>The expression is too long to be displayed<p>");
                 if(!isScalar(data))
                 {
@@ -206,10 +231,22 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
                                 out.println("<hr>");
                                 out.println(getPlugin(info.getFullPath()));
                             }
+                            if(is_jscope)
+                            {
+                                out.println("<hr>");
+                                out.println("<strong>This node can be added to jScope</strong>");
+                            }
+                            
                         }
                     }catch(Exception exc)
                     {
-                        out.println("The expression cannot be evaluated <br>");
+                        if(experiment.equals("jet"))
+                        {
+                            out.println("<hr>");
+                            out.println(getPlugin(info.getFullPath()));
+                        }
+                        else if(!is_jscope)
+                            out.println("The expression cannot be evaluated <br>");
                     }
                 }
             }
@@ -222,7 +259,7 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
         {
             try {
                 return new PathData("<a href="+BASEREF+"?path="+data.toString()+
-                    "&experiment="+experiment+"&shot="+shot+"&display="+display_wave+">"+
+                    "&experiment="+experiment+"&shot="+shot+"&display="+display_wave+"&jscope="+is_jscope+">"+
                     data.toString()+"</a>");
             }catch(Exception exc) {return data; }
         }
@@ -231,7 +268,7 @@ public class jWebTree extends HttpServlet implements SingleThreadModel{
             try {
                 NodeInfo info = tree.getInfo((NidData)data);
                 return new PathData("<a href="+BASEREF+"?path="+info.getFullPath()+
-                    "&experiment="+experiment+"&shot="+shot+"&display="+display_wave+">"+
+                    "&experiment="+experiment+"&shot="+shot+"&display="+display_wave+"&jscope="+is_jscope+">"+
                     info.getFullPath()+"</a>");
             }catch(Exception exc) {return data; }
         }
