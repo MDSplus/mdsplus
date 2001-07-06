a134fcb9c3352637d7d2c1a7cc22d65759a61aed
Thomas W. Fredian <twf@psfc.mit.edu>
2001-07-06 00:45:34 +0000

    Free mem only if non-null

--- mdsshr/librtl.c
+++ mdsshr/librtl.c
@@ -1705,11 +1705,12 @@ static int FindFileEnd(FindFileCtx *ctx)
       closedir(ctx->dir_ptr);
       ctx->dir_ptr = 0;
     }
-    free (ctx->env);
-    free (ctx->file);
+    if (ctx->env != 0) free (ctx->env);
+    if (ctx->file != 0) free (ctx->file);
     for (i=0; i<ctx->num_env; i++)
-      free(ctx->env_strs[i]);
-    free(ctx->env_strs);
+      if (ctx->env_strs[i] != 0)
+        free(ctx->env_strs[i]);
+    if (ctx->env_strs != 0) free(ctx->env_strs);
     free(ctx);
   }
   return 1;
