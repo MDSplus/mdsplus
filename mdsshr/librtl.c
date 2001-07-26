f6b1bebb1ca9fc0052dcd10172714e0ef6cce6e7
Thomas W. Fredian <twf@psfc.mit.edu>
2001-07-26 17:01:27 +0000

    fix compiler error on Solaris

--- mdsshr/librtl.c
+++ mdsshr/librtl.c
@@ -491,10 +491,10 @@ void pthread_unlock_global_np()
 {
   if (!Initialized)
   { 
-#ifdef PTHREAD_MUTEX_INITIALIZER
+#if defined(PTHREAD_MUTEX_INITIALIZER) && defined(PTHREAD_MUTEX_RECURSIVE) 
     pthread_mutexattr_t m_attr;
     pthread_mutexattr_init(&m_attr);
-    pthread_mutexattr_setkind_np(&m_attr,PTHREAD_MUTEX_RECURSIVE_NP);
+    pthread_mutexattr_setkind_np(&m_attr,PTHREAD_MUTEX_RECURSIVE);
     pthread_mutex_init(&GlobalMutex,&m_attr);
 #else 
     pthread_mutex_init(&GlobalMutex,pthread_mutexattr_default);
