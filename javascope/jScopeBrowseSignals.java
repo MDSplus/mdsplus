90b7ef8887d5e0cd9040ca350f2a845bc1be0a7e
John Krom <J.Krom@fz-juelich.de>
2002-05-24 08:19:59 +0000

    Reconstructed the indentation, no code changes.

--- javascope/jScopeBrowseSignals.java
+++ javascope/jScopeBrowseSignals.java
@@ -1,108 +1,103 @@
 /* $Id$ */
- import javax.swing.*; 
- import javax.swing.event.*; 
- import javax.swing.text.*; 
- import javax.swing.text.html.*; 
- import javax.swing.border.*; 
- import javax.swing.colorchooser.*; 
- import javax.swing.filechooser.*; 
- import javax.accessibility.*; 
-  
- import java.awt.*; 
- import java.awt.event.*; 
- import java.beans.*; 
- import java.util.*; 
- import java.io.*; 
- import java.applet.*; 
- import java.net.*; 
- 
- public abstract class jScopeBrowseSignals extends jScopeBrowseUrl
- { 
-  
-     jScopeWaveContainer wave_panel;
-     JButton add_sig;
-     JButton add_sig_shot;
-     String  prev_type = new String("text");
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import javax.swing.text.html.*;
+import javax.swing.border.*;
+import javax.swing.colorchooser.*;
+import javax.swing.filechooser.*;
+import javax.accessibility.*;
 
-     public jScopeBrowseSignals()
-     { 
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+import java.util.*;
+import java.io.*;
+import java.applet.*;
+import java.net.*;
+
+public abstract class jScopeBrowseSignals extends jScopeBrowseUrl
+{
+    jScopeWaveContainer wave_panel;
+    JButton add_sig;
+    JButton add_sig_shot;
+    String  prev_type = new String("text");
+
+    public jScopeBrowseSignals()
+    {
         super(null);
-  
+
         add_sig = new JButton("Add signal");
         add_sig.setSelected(true);
         p.add(add_sig);
         add_sig.addActionListener(new ActionListener()
-	        {
-	            public void actionPerformed(ActionEvent e)
+            {
+                public void actionPerformed(ActionEvent e)
                 {
                     addSignal(false);
                 }
-	        }
-	    );
- 
+            });
+
         add_sig_shot = new JButton("Add signal & shot");
         add_sig_shot.setSelected(true);
         p.add(add_sig_shot);
         add_sig_shot.addActionListener(new ActionListener()
-	        {
-	            public void actionPerformed(ActionEvent e)
+            {
+                public void actionPerformed(ActionEvent e)
                 {
                     addSignal(true);
                 }
-	        }
-	    );
-                  
+            });
+
         pack();
- 	    setSize(480,700);
-      
-     }
-     
-     abstract protected String getSignal(String url);
-     abstract protected String getShot();
-     abstract protected String getTree();
-     abstract protected String getServerAddr();
-     
-     protected void setPage(URL url) throws IOException
-     {
+        setSize(480,700);
+    }
+
+    abstract protected String getSignal(String url);
+    abstract protected String getShot();
+    abstract protected String getTree();
+    abstract protected String getServerAddr();
+
+    protected void setPage(URL url) throws IOException
+    {
         super.setPage(url);
         if(prev_type == null || prev_type.equals(mime_type))
-            return;
-            
+          return;
+
         prev_type = mime_type;
- 		if(mime_type.indexOf("text") != -1)
- 		{
+        if(mime_type.indexOf("text") != -1)
+        {
             add_sig.setText("Add signal");
             add_sig.setEnabled(true);
-            add_sig_shot.setText("Add signal & shot");            
+            add_sig_shot.setText("Add signal & shot");
         }
         else
         {
             add_sig.setText("Add frames");
             add_sig.setEnabled(false);
-            add_sig_shot.setText("Add frames & shot");            
+            add_sig_shot.setText("Add frames & shot");
         }
-     }
-     
-     
-     public void addSignal(boolean with_shot)
-     {
+    }
+
+
+    public void addSignal(boolean with_shot)
+    {
         if(wave_panel != null)
         {
             URL u = (URL)url_list.elementAt(curr_url);
             String url_name = (u.toString());
-            String sig_path = getSignal(url_name);            
+            String sig_path = getSignal(url_name);
             String shot = (with_shot) ? getShot() : null;
-            if(sig_path != null) 
+            if(sig_path != null)
             {
                 boolean is_image = (mime_type.indexOf("image") != -1);
                 wave_panel.AddSignal(getTree(), shot, "", sig_path, true, is_image);
             }
         }
-     }
-     
-     public void setWaveContainer(jScopeWaveContainer wave_panel)
-     {
+    }
+
+    public void setWaveContainer(jScopeWaveContainer wave_panel)
+    {
         this.wave_panel = wave_panel;
-     }
- } 
- 
+    }
+}
