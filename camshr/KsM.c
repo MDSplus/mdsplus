025006cc9129f322d25c89fc89c1575eab6d2322
Thomas W. Fredian <twf@psfc.mit.edu>
2003-05-13 20:24:03 +0000

    get esr for status

--- camshr/KsM.c
+++ camshr/KsM.c
@@ -39,6 +39,7 @@ static int KsMultiIo(
   RequestSenseData sense;
   unsigned char sb_out_len;
   unsigned int transfer_len;
+  unsigned int dummy;
   int enhanced;
   int online;
   
@@ -76,9 +77,34 @@ static int KsMultiIo(
   direction = ((F & 0x08) == 0) ? ((F < 8) ? 1 : 2) : 0;
   
   // talk to the physical device
+  scsi_lock( scsiDevice, 1);
   status = scsi_io( scsiDevice, direction, Command, sizeof(Command),
                     Data, xfer_len.l, 
-                    (char *)&sense, sizeof(sense), &sb_out_len,&transfer_len);
+                    0, 0, &sb_out_len,&dummy);
+  Command[0] = OpCodeRegisterAccess;
+  Command[1] = 0;
+  Command[2] = 0x01;
+  Command[3] = 0x80;
+  Command[4] = 1;
+  Command[5] = 0;
+  status = scsi_io( scsiDevice, 1, Command, 6,
+                    (char *)&sense.u2.esr, sizeof(sense.u2.esr), 
+                    0, 0, &sb_out_len,&dummy);
+  Command[2] = 0;
+  Command[3] = 0;
+  Command[4] = 0;
+  status = scsi_io( scsiDevice, 1, Command, 6,
+                    (char *)&transfer_len, 1, 
+                    0, 0, &sb_out_len,&dummy);
+  Command[3] = 2;
+  status = scsi_io( scsiDevice, 1, Command, 6,
+                    ((char *)&transfer_len)+1, 1, 
+                    0, 0, &sb_out_len,&dummy);
+  Command[3] = 0x1A;
+  status = scsi_io( scsiDevice, 1, Command, 6,
+                    ((char *)&transfer_len)+2, 1, 
+                    0, 0, &sb_out_len,&dummy);
+  scsi_lock (scsiDevice, 0);
   LastIosb.bytcnt = (unsigned short)(transfer_len & 0xFFFF);
   LastIosb.lbytcnt = (unsigned short)(transfer_len >> 16);
   status = KsTranslateIosb(&sense,status);
--- camshr/KsS.c
+++ camshr/KsS.c
@@ -34,6 +34,7 @@ static int KsSingleIo(
   unsigned int transfer_len;
   int enhanced;
   int online;
+  int dummy;
   
   if( MSGLVL(FUNCTION_NAME) )
     printf( "%s()\n", KS_ROUTINE_NAME );
@@ -63,9 +64,20 @@ static int KsSingleIo(
   direction = ((F & 0x08) == 0) ? ((F < 8) ? 1 : 2) : 0;
   
   // talk to the physical device
+  scsi_lock( scsiDevice, 1);
   status = scsi_io( scsiDevice, direction, Command, sizeof(Command),
                     Data, direction ? ((Mem == 16) ? 2 : 4) : 0, 
                     (char *)&sense, sizeof(sense), &sb_out_len,&transfer_len);
+  Command[0] = OpCodeRegisterAccess;
+  Command[1] = 0;
+  Command[2] = 0x01;
+  Command[3] = 0x80;
+  Command[4] = 1;
+  Command[5] = 0;
+  status = scsi_io( scsiDevice, 1, Command, 6,
+                    (char *)&sense.u2.esr, sizeof(sense.u2.esr), 
+                    0, 0, &sb_out_len,&dummy);
+  scsi_lock( scsiDevice, 0);
   LastIosb.bytcnt = (unsigned short)(transfer_len & 0xFFFF);
   status = KsTranslateIosb(&sense,status);
   if (iosb) *iosb=LastIosb;
