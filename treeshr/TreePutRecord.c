1220960986645cf9777134489da4e1d8132946dd
Thomas W. Fredian <twf@psfc.mit.edu>
2003-06-02 16:31:49 +0000

    close old get fd on datafile after opening it for write

--- treeshr/TreePutRecord.c
+++ treeshr/TreePutRecord.c
@@ -320,7 +320,6 @@ static int FixupNid(NID *nid, unsigned char *tree, struct descriptor *path)
       }
     
     path_reference = 1;
-    status = 1;
   }
   else
     nid_reference = 1;
@@ -345,6 +344,7 @@ int TreeOpenDatafileW(TREE_INFO *info, int *stv_ptr, int tmpfile)
   }
   if (status & 1)
   {
+    int old_get = df_ptr->get; 
     size_t len = strlen(info->filespec)-4;
     char *filename = strncpy(malloc(len+20),info->filespec,len);
     filename[len]='\0';
@@ -352,7 +352,9 @@ int TreeOpenDatafileW(TREE_INFO *info, int *stv_ptr, int tmpfile)
     df_ptr->get = MDS_IO_OPEN(filename,tmpfile ? O_RDWR | O_CREAT | O_TRUNC : O_RDONLY, 0664);
     status = (df_ptr->get == -1) ? TreeFAILURE : TreeNORMAL;
     if (df_ptr->get == -1)
-      df_ptr->get = 0;
+      df_ptr->get = old_get;
+    else if (df_ptr->get > 0)
+      MDS_IO_CLOSE(old_get);
     if (status & 1)
     {
       df_ptr->put = MDS_IO_OPEN(filename,O_RDWR, 0);
