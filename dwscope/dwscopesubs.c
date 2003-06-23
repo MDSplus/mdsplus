b1b4f8611174a3ff37216dd3613e6b376f300399
Thomas W. Fredian <twf@psfc.mit.edu>
2003-06-23 15:31:35 +0000

    oops, revert last change. Problem was in cw_wvedit.

--- dwscope/dwscopesubs.c
+++ dwscope/dwscopesubs.c
@@ -485,8 +485,8 @@ Boolean   ConvertWaveToSelection(Widget w, String prefix, WaveInfo *wave, Atom t
     {
       *type = target;
       *value = (String)memcpy(XtMalloc(count * sizeof(float)),x->addr,count * sizeof(float));
-      *length = count * sizeof(float);
-      *format = 8;
+      *length = count;
+      *format = 8 * sizeof(float);
       status = 1;
     }
   }
@@ -499,8 +499,8 @@ Boolean   ConvertWaveToSelection(Widget w, String prefix, WaveInfo *wave, Atom t
     {
       *type = target;
       *value = (String)memcpy(XtMalloc(count * sizeof(float)),y->addr,count * sizeof(float));
-      *length = count * sizeof(float);
-      *format = 8;
+      *length = count;
+      *format = 8 * sizeof(float);
       status = 1;
     }
   }
