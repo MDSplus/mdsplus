fbffe727bc529e932aff5aa0e359b0901d2a18dc
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2003-11-25 11:55:09 +0000

    Fixes

--- javatraverser/T2ControlSetup.java
+++ javatraverser/T2ControlSetup.java
@@ -53,7 +53,7 @@ public class T2ControlSetup extends DeviceSetup
 		}
 		deviceChoice1.setLabelString("Initial Control");
 		deviceChoice1.setConvert(true);
-		deviceChoice1.setChoiceIntValues(new int[] {});
+		deviceChoice1.setChoiceIntValues(new int[] {(int)1,(int)2,(int)3,(int)4,(int)5});
 		getContentPane().add(deviceChoice1);
 		deviceChoice1.setBounds(12,156,276,40);
 		deviceChoice2.setOffsetNid(7);
@@ -68,7 +68,7 @@ public class T2ControlSetup extends DeviceSetup
 		}
 		deviceChoice2.setLabelString("Triggered Control");
 		deviceChoice2.setConvert(true);
-		deviceChoice2.setChoiceIntValues(new int[] {});
+		deviceChoice2.setChoiceIntValues(new int[] {(int)1,(int)2,(int)3,(int)4,(int)5});
 		getContentPane().add(deviceChoice2);
 		deviceChoice2.setBounds(312,156,300,40);
 		deviceField4.setNumCols(4);
