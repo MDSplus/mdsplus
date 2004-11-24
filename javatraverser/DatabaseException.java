cfb8ba95e452d836dd00d199787555b1cdc50930
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2004-11-24 16:45:35 +0000

    Report status in Database Exception

--- javatraverser/DatabaseException.java
+++ javatraverser/DatabaseException.java
@@ -1,8 +1,31 @@
 //package jTraverser;
+import java.util.*;
+class DatabaseException
+    extends Exception
+{
+    int status = 0;
+    static String getMsg(String message)
+    {
+        StringTokenizer st = new StringTokenizer(message, ":");
+        String statusStr = "";
+        if(st.countTokens() > 0)
+            statusStr = st.nextToken();
+        return message.substring(statusStr.length() + 1);
 
-class DatabaseException extends Exception {
+    }
+    private void setStatus(String message)
+    {
+        try {
+            StringTokenizer st = new StringTokenizer(message, ":");
+            status = Integer.parseInt(st.nextToken());
+        }catch(Exception exc){status = 0;}
+    }
+
     public DatabaseException(String message)
     {
-	super(message);
+        super(getMsg(message));
+        setStatus(message);
     }
+
+    public int getStatus(){return status;}
 }
