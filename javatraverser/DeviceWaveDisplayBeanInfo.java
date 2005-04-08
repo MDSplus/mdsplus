f87bd6c89eff1c740edb8312b93f8b6962af49be
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2005-04-08 15:34:25 +0000

    New Bean for waveform display

---
+++ javatraverser/DeviceWaveDisplayBeanInfo.java
@@ -0,0 +1,37 @@
+import java.beans.*;
+import java.awt.*;
+
+public class DeviceWaveDisplayBeanInfo extends SimpleBeanInfo
+{
+    public Image getIcon(int kind)
+    {
+        return loadImage("DeviceWaveDisplay.gif");
+    }
+
+    public BeanDescriptor getBeanDescriptor()
+    {
+        return new BeanDescriptor(DeviceWaveDisplay.class, DeviceWaveDisplayCustomizer.class);
+    }
+    public PropertyDescriptor property(String name, String description)
+    throws IntrospectionException
+    {
+        PropertyDescriptor p = new PropertyDescriptor(name, DeviceChannel.class);
+        p.setShortDescription(description);
+        return p;
+    }
+    public PropertyDescriptor [] getPropertyDescriptors()
+    {
+        try {
+            PropertyDescriptor[] props = {
+                property("offsetNid", "Offset nid"),
+                property("prefHeight", "Preferred height")
+            };
+            return props;
+        }catch(IntrospectionException e)
+        {
+            System.out.println("DeviceChannel: property exception " + e);
+            return super.getPropertyDescriptors();
+        }
+    }
+
+}
---
+++ javatraverser/DeviceWaveDisplayCustomizer.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.beans.*;
+import java.awt.*;
+import java.lang.reflect.*;
+import java.awt.event.*;
+
+public class DeviceWaveDisplayCustomizer extends DeviceCustomizer implements Customizer
+{
+    DeviceWaveDisplay bean = null;
+    Object obj;
+    PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+    Choice nids;
+    Button doneButton;
+
+    public DeviceWaveDisplayCustomizer()
+    {
+    }
+    public void setObject(Object o)
+    {
+
+        bean = (DeviceWaveDisplay)o;
+
+        setLayout(new BorderLayout());
+        Panel jp = new Panel();
+        jp.add(new Label("Offset nid: "));
+        jp.add(nids = new Choice());
+
+        String names[] = getDeviceFields();
+
+        if(names != null)
+        for(int i = 0; i < names.length; i++)
+            nids.addItem(names[i]);
+        int offsetNid = bean.getOffsetNid();
+        if(offsetNid > 0) offsetNid--;
+        nids.select(offsetNid);
+        add(jp, "Center");
+        jp = new Panel();
+        jp.add(doneButton = new Button("Apply"));
+        doneButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e)
+        {
+            if(bean == null) return;
+            int oldOffsetNid = bean.getOffsetNid();
+            bean.setOffsetNid(nids.getSelectedIndex() + 1);
+            listeners.firePropertyChange("offsetNid", oldOffsetNid, bean.getOffsetNid());
+            DeviceWaveDisplayCustomizer.this.repaint();
+         }
+      });
+      add(jp,"South");
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener l)
+    {
+        listeners.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l)
+    {
+        listeners.removePropertyChangeListener(l);
+    }
+  }
+
