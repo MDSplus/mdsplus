7fc5f2e738cbb76f87e7a7ba8dcdf443d4a313d6
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2005-11-18 09:22:12 +0000

    Waveforms set-up for toroidal axisymmetric control

---
+++ javatraverser/RFXAxiToroidalControlSetup.java
@@ -0,0 +1,63 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class RFXAxiToroidalControlSetup extends DeviceSetup
+{
+    BorderLayout borderLayout1 = new BorderLayout();
+    DeviceButtons deviceButtons1 = new DeviceButtons();
+    JTabbedPane jTabbedPane1 = new JTabbedPane();
+    JPanel jPanel1 = new JPanel();
+    DeviceField deviceField1 = new DeviceField();
+    DeviceWave deviceWave1 = new DeviceWave();
+    DeviceWave deviceWave2 = new DeviceWave();
+    public RFXAxiToroidalControlSetup()
+    {
+        try
+        {
+          jbInit();
+        }
+        catch(Exception e)
+        {
+          e.printStackTrace();
+        }
+    }
+    private void jbInit() throws Exception
+    {
+      this.setWidth(471);
+      this.setHeight(471);
+      this.setDeviceType("RFXAxiToroidalControl");
+      this.setDeviceProvider("localhost");
+      this.setDeviceTitle("Toroidal Axisymmetric Control Setup");
+      this.getContentPane().setLayout(borderLayout1);
+      deviceField1.setOffsetNid(1);
+      deviceField1.setTextOnly(true);
+      deviceField1.setLabelString("Comment: ");
+      deviceField1.setNumCols(30);
+      deviceField1.setIdentifier("");
+      deviceWave1.setOffsetNid(3);
+      deviceWave1.setMinYVisible(true);
+      deviceWave1.setIdentifier("");
+      deviceWave1.setUpdateExpression("");
+      deviceWave2.setOffsetNid(9);
+      deviceWave2.setMinYVisible(true);
+      deviceWave2.setIdentifier("");
+      deviceWave2.setUpdateExpression("");
+      this.getContentPane().add(deviceButtons1, BorderLayout.SOUTH);
+      this.getContentPane().add(jTabbedPane1, BorderLayout.CENTER);
+      jTabbedPane1.add(deviceWave1, "deviceWave1");
+      jTabbedPane1.add(deviceWave2, "deviceWave2");
+      this.getContentPane().add(jPanel1, BorderLayout.NORTH);
+      jPanel1.add(deviceField1, null);
+    }
+
+}
