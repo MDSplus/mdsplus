65755a0d037bcb6a4dd9374009dfccb6a400cd49
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2006-04-19 14:01:48 +0000

    Added "diagonal" keyword

--- javatraverser/Convert.java
+++ javatraverser/Convert.java
@@ -21,18 +21,36 @@ class Convert
     {
 	System.out.println(path);
     	Database rfx = new Database("rfx", -1);
-	try {
-	    rfx.open();
-	    float []data = new float[192*192];
-	    BufferedReader br = new BufferedReader(new FileReader(fileName));
+	float [] data = new float[192*192];
+	if(fileName.equalsIgnoreCase("diagonal"))
+	{
 	    for(int i = 0; i < 192; i++)
 	    {
-	    	System.out.println(i);
-	    	String line = br.readLine();
-		StringTokenizer st = new StringTokenizer(line, "[], ");
-		for(int j = 0; j < 192; j++)
-		    data[192*i+j] = Float.parseFloat(st.nextToken());
+	    	for(int j = 0; j < 192; j++)
+		{
+		    if(i == j)
+		    	data[192*i+j] = 1;
+		    else
+		    	data[192*i+j] = 0;
+		}
 	    }
+	}
+	else
+	{
+	    try {
+	    	BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    	for(int i = 0; i < 192; i++)
+	    	{
+	    	    System.out.println(i);
+	    	    String line = br.readLine();
+		    StringTokenizer st = new StringTokenizer(line, "[], ");
+		    for(int j = 0; j < 192; j++)
+		    	data[192*i+j] = Float.parseFloat(st.nextToken());
+	    	}
+	    }catch(Exception exc){System.err.println(exc);} 	
+	 }
+	 try {
+	    rfx.open();
 	    NidData nid = rfx.resolve(new PathData(path), 0);
 	    FloatArray array = new FloatArray(data);
 	    rfx.putData(nid, array, 0);
