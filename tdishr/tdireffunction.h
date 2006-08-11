5732d0ae80743e15f4e09aeffc9927020abedf34
Joshua Stillerman <jas@psfc.mit.edu>
2006-08-11 14:10:13 +0000

    f3 pointer must be to a routine returning an address

--- tdishr/tdireffunction.h
+++ tdishr/tdireffunction.h
@@ -5,11 +5,13 @@
 	Ken Klare, LANL CTR-10	(c)1989
 */
 #define TdiFUNCTION_DEF
+#include <mdstypes.h>
+#include <config.h>
 struct	TdiFunctionStruct {
 	char	*name;		/*string to compile/decompile	*/
 	int	(*f1)();	/*routine to check arguments	*/
 	int	(*f2)();	/*routine to set conversions	*/
-	int	(*f3)();	/*routine to do operation	*/
+	_pointer_int  (*f3)();	/*routine to do operation	*/
 	unsigned char i1;	/*input minimum cat		*/
 	unsigned char i2;	/*input maximum cat		*/
 	unsigned char o1;	/*output minimum cat		*/
