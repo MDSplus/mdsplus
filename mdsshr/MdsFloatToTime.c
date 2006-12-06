dfbc829149674761c128a48f85a4c084d4e51868
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2006-12-06 14:10:52 +0000

    added MdsFloatToTime, converting float time values into 64 bit integer preserving monotonicity

---
+++ mdsshr/MdsFloatToTime.c
@@ -0,0 +1,47 @@
+#include <mdsdescrip.h>
+#include <mdstypes.h>
+
+
+//Conversion to Float/Doublee to 64 bit int.
+
+
+int MdsFloatToTime(struct descriptor *floatD, _int64u *retInt64)
+{
+	double doubleVal;
+	_int64 currInt64_1, currInt64_2;
+
+	if(floatD->class != CLASS_S || (floatD->dtype != DTYPE_FLOAT && floatD->dtype != DTYPE_DOUBLE))
+		return 0;
+ //Invalid time format
+ 	
+	if(floatD->dtype == DTYPE_FLOAT)
+		doubleVal = *(float *)floatD->pointer;
+	else
+		doubleVal = *(double *)floatD->pointer;
+
+	if(doubleVal >= 0)
+	{
+		//To preserve monotonicy it suffices to complement the sign bit (most significative)
+		*retInt64 = *(_int64u *)&doubleVal;
+		*retInt64 |= 0x8000000000000000LL;
+	}
+	else
+	{
+		//It is necessary to operate on exponent and mantissa
+		currInt64_1 = *(_int64 *)&doubleVal;
+		//Change Mantissa
+		currInt64_1 &= 0x000fffffffffffffLL;
+		currInt64_2 =  0x000fffffffffffffLL;
+		*retInt64 = (_int64u)(currInt64_2 - currInt64_1);
+		//Change exponent
+		currInt64_1 = *(_int64 *)&doubleVal;
+		currInt64_1 &= 0x7ff0000000000000LL;
+		currInt64_2 =  0x7ff0000000000000LL;
+	//???	currInt64_1 = currInt64_1 - currInt64_2;
+		currInt64_1 = (_int64u)(currInt64_2 - currInt64_1);
+
+
+		*retInt64 |= currInt64_1;
+	}
+	return 1;
+}
