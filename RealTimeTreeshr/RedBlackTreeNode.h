c1c928848884dea1229913c2d82a349a5715d641
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2006-12-06 17:48:51 +0000

    The cache management sources are now in cacheshr

--- RealTimeTreeshr/RedBlackTreeNode.h
+++
@@ -1,8 +0,0 @@
-class RedBlackTreeNode
-{
-public:
-	const int RED = 0, BLACK = 1;
-	int nid;
-	RedBlackTreeNode *left, *right;
-	char color;
-}
--- RealTimeTreeshr/Runnable.h
+++
@@ -1,9 +0,0 @@
-#ifndef RUNNABLE
-#define RUNNABLE
-class Runnable
-{
-public: 
-	virtual void run() = 0;
-};
-
-#endif
