adbd526a5803e99305fd7a7b068d1040439ced1a
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2007-01-31 21:04:26 +0000

    Add MdsTimeToDouble

--- mdsshr/MdsFloatToTime.c
+++ mdsshr/MdsFloatToTime.c
@@ -1,6 +1,6 @@
 #include <mdsdescrip.h>
 #include <mdstypes.h>
-
+#include <string.h>
 
 //Conversion to Float/Doublee to 64 bit int.
 
@@ -47,6 +47,35 @@ int MdsFloatToTime(struct descriptor *floatD, _int64u *retInt64)
 	return 1;
 }
 
+
+
+int MdsTimeToDouble(_int64u in64, double *retDouble)
+{
+	_int64 currInt64_1, currInt64_2, retInt64;
+
+
+	if(in64 & 0x8000000000000000) //If most significant bit set, it is a positive number
+	{
+		currInt64_1 = in64 & 0x7FFFFFFFFFFFFFFF;
+		memcpy(retDouble, &currInt64_1, 8);
+		return 1;
+	}
+	//Otherwise need to operate on mantissa and exponent
+	currInt64_1 = in64 & 0x000fffffffffffff;
+	currInt64_2 =  0x000fffffffffffff;
+	retInt64 = (_int64u)(currInt64_2 - currInt64_1);
+
+	currInt64_1 = in64 & 0x7ff0000000000000;
+	currInt64_2 =  0x7ff0000000000000;
+	currInt64_1 = (_int64u)(currInt64_2 - currInt64_1);
+
+	retInt64 |= currInt64_1;
+	retInt64 |= 0x8000000000000000;
+	memcpy(retDouble, &retInt64, 8);
+	return 1;
+}
+
+
 #else
 int MdsFloatToTime(struct descriptor *floatD, _int64u *retInt64)
 {
@@ -88,4 +117,34 @@ int MdsFloatToTime(struct descriptor *floatD, _int64u *retInt64)
 	}
 	return 1;
 }
+
+
+int MdsTimeToDouble(_int64u in64, double *retDouble)
+{
+	double doubleVal;
+	_int64 currInt64_1, currInt64_2, retInt64;
+
+
+	if(in64 & 0x8000000000000000LL) //If most significant bit set, it is a positive number
+	{
+		currInt64_1 = in64 & 0x7FFFFFFFFFFFFFFFLL;
+		memcpy(retDouble, currInt64_1, 8);
+		return 1;
+	}
+	//Otherwise need to operate on mantissa and exponent
+	currInt64_1 = in64 & 0x000fffffffffffffLL;
+	currInt64_2 =  0x000fffffffffffffLL;
+	retInt64 = (_int64u)(currInt64_2 - currInt64_1);
+
+	currInt64_1 = in64 & 0x7ff0000000000000LL;
+	currInt64_2 =  0x7ff0000000000000LL;
+	currInt64_1 = (_int64u)(currInt64_2 - currInt64_1);
+
+	retInt64 |= currInt64_1;
+	retInt64 |= 0x8000000000000000LL;
+	memcpy(retDouble, retInt64, 8);
+	return 1;
+}
+
+
 #endif
