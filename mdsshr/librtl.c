b9cc3e9014db7e4bd4c03d3a98b4df75e5a30418
Thomas W. Fredian <twf@psfc.mit.edu>
2007-02-12 19:42:40 +0000

    fix to handle larger arrays

--- mdsshr/librtl.c
+++ mdsshr/librtl.c
@@ -1314,7 +1314,7 @@ int LibResetVmZone(ZoneList **zone)
 }
 
 
-int LibFreeVm(int *len, void **vm, ZoneList **zone)
+int LibFreeVm(unsigned int *len, void **vm, ZoneList **zone)
 {
   VmList *list = NULL;
   if (zone != NULL) {
@@ -1344,11 +1344,12 @@ int libfreevm(int *len, void **vm, ZoneList **zone)
   return(LibFreeVm(len, vm, zone));
 }
 
-int LibGetVm(int *len, void **vm, ZoneList **zone)
+int LibGetVm(unsigned int *len, void **vm, ZoneList **zone)
 {
   *vm = malloc(*len);
-  if (*vm == NULL)
+  if (*vm == NULL) {
     printf("Insufficient virtual memory\n");
+  }
   if (zone != NULL) {
     VmList *list = malloc(sizeof(VmList));
     list->ptr = *vm;
@@ -1362,7 +1363,7 @@ int LibGetVm(int *len, void **vm, ZoneList **zone)
   return (*vm != NULL);
 }
 
-int libgetvm_(int *len, void **vm, ZoneList **zone)
+int libgetvm_(unsigned int *len, void **vm, ZoneList **zone)
 {
   return(LibGetVm(len, vm, zone));
 }
