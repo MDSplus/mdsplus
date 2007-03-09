d8977bcc27722a7e59d18ea244b02289b3ecc67f
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2007-03-09 12:25:52 +0000

    Fixes for limits and memory allocation

--- xtreeshr/XTreeDefaultResample.c
+++ xtreeshr/XTreeDefaultResample.c
@@ -17,6 +17,7 @@ extern int TdiEvaluate();
 extern int XTreeConvertToLongTime(struct descriptor *timeD, _int64u *converted);
 extern int XTreeConvertToLongDelta(struct descriptor *deltaD, _int64u *converted);
 
+
 static void printDecompiled(struct descriptor *inD)
 {
 	int status;
@@ -44,23 +45,40 @@ static void printDecompiled(struct descriptor *inD)
 //64 bit time-based resampling function. It is assumed here that the 54 bit representation of time is the count
 //of a given, fixed amount of time, starting from a given time in the past
 //The closest point is selected as representative for a given time
-static void resample(_int64u start, _int64u end, _int64u delta, _int64u *timebase, int timebaseSamples, 
-					int numDims, int *dims, char *data, int dataSize, char *outData, _int64u *outDim, int *retSamples)
+static void resample(_int64u start, _int64u end, _int64u delta, _int64u *inTimebase, int inTimebaseSamples, 
+					int numDims, int *dims, char *inData, int dataSize, char *outData, _int64u *outDim, int *retSamples)
 {
-	int i, timebaseIdx, outIdx, itemSize, outSamples;
+	int i, timebaseIdx, outIdx, itemSize, outSamples, startIdx, timebaseSamples;
 	_int64u refTime, delta1, delta2;
+	_int64u *timebase;
+	char *data;
+
 
 	itemSize = dataSize;
-	for(i = 0; i < numDims - 1; i++)
+	for(i = 0; i < numDims-1; i++)
 		itemSize *= dims[i];
 
 
-	if(start < timebase[0])
-		start = timebase[0];
-	if(end > timebase[timebaseSamples - 1])
-		end = timebase[timebaseSamples - 1];
+	if(start < inTimebase[0])
+		start = inTimebase[0];
+	if(end > inTimebase[inTimebaseSamples - 1])
+		end = inTimebase[inTimebaseSamples - 1];
 
 	timebaseIdx = outIdx = outSamples = 0;
+
+	
+	for(startIdx = 0; startIdx < inTimebaseSamples && inTimebase[startIdx] < start; startIdx++);
+
+	if(startIdx == inTimebaseSamples) //Not possible in any case
+	{
+		*retSamples = 0;
+		return;
+	}
+	timebase = &inTimebase[startIdx];
+	timebaseSamples  = inTimebaseSamples - startIdx;
+	data = &inData[startIdx * itemSize];
+
+
 	refTime = start;
 	if(delta)
 	{
@@ -167,31 +185,20 @@ static _int64u *convertTimebaseToInt64(struct descriptor_signal *inSignalD, int 
 }
 
 
-
-EXPORT int XTreeDefaultResample(struct descriptor_signal *inSignalD, struct descriptor *startD, struct descriptor *endD, 
-						 struct descriptor *deltaD, struct descriptor_xd *outSignalXd)
+//Handle resampling in when dimension is specified as a range
+static int rangeResample(struct descriptor *startD, struct descriptor *endD, struct descriptor *deltaD, 
+			ARRAY_COEFF(char *, 256) *dataD, struct descriptor_range *rangeD, struct descriptor_xd *outSignalXd)
 {
-	char resampleExpr[64];
-	struct descriptor resampleExprD = {0, DTYPE_T, CLASS_S, resampleExpr};
-	struct descriptor_a *arrayD;
-	char *shapeExpr = "SHAPE(DATA($1))";
-	struct descriptor shapeExprD = {strlen(shapeExpr), DTYPE_T, CLASS_S, shapeExpr};
+	int dataSize, numOutSamples, status, i, memSize, currOffset, numInSamples, totInSamples;
+	_int64 start64, end64, delta64, rangeStart64, rangeEnd64, rangeDelta64, currTime64, inTime64;
+	char *signalExpr = "MAKE_SIGNAL($1,,MAKE_RANGE(MAX($2, $3), MIN($4, $5), $6))";
+	struct descriptor signalExprD = {strlen(signalExpr), DTYPE_T, CLASS_S, signalExpr};
 
-	_int64u start64, end64, delta64, *timebase64, *outDim;
-	float *timebaseFloat;
-	int *dims;
-	int numDims;
-	int numTimebaseSamples;
-	int outSamples, itemSize;
-	int isFloat = 0;
-	int status, i;
-	char *outData;
-	struct descriptor_a *dataD;
-	EMPTYXD(shapeXd);
-
+	char *currDataPtr, *outDataPtr, *currOutDataPtr;
 	DESCRIPTOR_A_COEFF(outDataArray, 0, 0, 0, 255, 0); 
-	DESCRIPTOR_A(outDimArray, 0, 0, 0, 0);
-	DESCRIPTOR_SIGNAL_1(outSignalD, &outDataArray, 0, &outDimArray);
+
+printf("RANGE RESAMPLE\n");
+
 
 	if(startD)
 	{
@@ -211,6 +218,129 @@ EXPORT int XTreeDefaultResample(struct descriptor_signal *inSignalD, struct desc
 		if(!(status & 1))
 			return status;
 	}
+	//Range descriptor must be fully specified
+	if(!rangeD->begin || !rangeD->ending || !rangeD->deltaval)
+		return 0;
+		
+	status = XTreeConvertToLongTime(rangeD->begin, &rangeStart64);
+	if(!(status & 1))
+		return status;
+	status = XTreeConvertToLongTime(rangeD->ending, &rangeEnd64);
+	if(!(status & 1))
+		return status;
+	status = XTreeConvertToLongDelta(rangeD->deltaval, &rangeDelta64);
+	if(!(status & 1))
+		return status;
+
+	dataSize = dataD->length;
+	for(i = 0; i < dataD->dimct-1; i++)
+		dataSize *= dataD->m[i];
+
+	totInSamples = dataD->arsize/dataD->length;
+	if(!startD || start64 < rangeStart64)
+		start64 = rangeStart64;
+	if(!endD || end64 > rangeEnd64)
+		end64 = rangeEnd64;
+	if(!deltaD)
+		delta64 = rangeDelta64;
+
+	currDataPtr = dataD->pointer;
+	currDataPtr += (dataSize * (start64 - rangeStart64)/rangeDelta64);
+	currTime64 = inTime64 = start64;
+	memSize = dataSize * ((end64 - start64)/delta64 + 1);
+	currOutDataPtr = outDataPtr = malloc(memSize);
+	numOutSamples = numInSamples = 0;
+	currOffset = 0;
+	while(numInSamples < totInSamples && currTime64 < end64)
+	{
+		memcpy(currOutDataPtr, currDataPtr, dataSize);
+		currOutDataPtr += dataSize;
+		currOffset += dataSize;
+		currTime64 += delta64;
+		while(numInSamples < totInSamples && inTime64 < currTime64)
+		{
+			inTime64 += rangeDelta64;
+			currDataPtr += dataSize;
+			numInSamples++;
+		}
+		numOutSamples++;
+	}
+	outDataArray.length = dataD->length;
+	outDataArray.dtype = dataD->dtype;
+	outDataArray.pointer = outDataPtr;
+	outDataArray.arsize = dataSize*numOutSamples;
+	outDataArray.dimct = dataD->dimct;
+	for(i = 0; i < dataD->dimct-1; i++)
+	{
+		outDataArray.m[i] = dataD->m[i];
+	}
+	outDataArray.m[dataD->dimct - 1] = numOutSamples;
+
+//Build out signal descriptor, keeping the same data type as passed
+	status = TdiCompile(&signalExprD, &outDataArray, startD, rangeD->begin, endD, rangeD->ending, 
+		deltaD, outSignalXd MDS_END_ARG);
+	if((status & 1))
+		status = TdiEvaluate(outSignalXd, outSignalXd MDS_END_ARG);
+	free(outDataPtr);
+	return status;
+}
+
+
+
+
+
+
+EXPORT int XTreeDefaultResample(struct descriptor_signal *inSignalD, struct descriptor *startD, struct descriptor *endD, 
+						 struct descriptor *deltaD, struct descriptor_xd *outSignalXd)
+{
+	char resampleExpr[64];
+	struct descriptor resampleExprD = {0, DTYPE_T, CLASS_S, resampleExpr};
+	struct descriptor_a *arrayD;
+	char *shapeExpr = "SHAPE(DATA($1))";
+	struct descriptor shapeExprD = {strlen(shapeExpr), DTYPE_T, CLASS_S, shapeExpr};
+
+	_int64u start64, end64, delta64, *timebase64, *outDim;
+	float *timebaseFloat;
+	int *dims;
+	int numDims;
+	int numTimebaseSamples;
+	int outSamples, itemSize;
+	int isFloat = 0;
+	int status, i, currIdx;
+	char *outData;
+	struct descriptor_a *dataD;
+	EMPTYXD(shapeXd);
+
+	DESCRIPTOR_A_COEFF(outDataArray, 0, 0, 0, 255, 0); 
+	DESCRIPTOR_A(outDimArray, 0, 0, 0, 0);
+	DESCRIPTOR_SIGNAL_1(outSignalD, &outDataArray, 0, &outDimArray);
+
+	if(inSignalD->dimensions[0]->class == CLASS_R && inSignalD->dimensions[0]->dtype == DTYPE_RANGE && 
+		((struct descriptor_range *)inSignalD->dimensions[0])->begin && 
+		((struct descriptor_range *)inSignalD->dimensions[0])->ending && 
+		((struct descriptor_range *)inSignalD->dimensions[0])->deltaval)
+
+		return rangeResample(startD, endD, deltaD, inSignalD->data, inSignalD->dimensions[0], outSignalXd); 
+
+	if(startD)
+	{
+		status = XTreeConvertToLongTime(startD, &start64);
+		if(!(status & 1))
+			return status;
+	}
+	if(endD)
+	{
+		status = XTreeConvertToLongTime(endD, &end64);
+		if(!(status & 1))
+			return status;
+	}
+	if(deltaD)
+	{
+		status = XTreeConvertToLongDelta(deltaD, &delta64);
+		if(!(status & 1))
+			return status;
+	}
+
 
 //This version handles only 64 bit time format
 	if(inSignalD->dimensions[0]->dtype != DTYPE_Q && inSignalD->dimensions[0]->dtype != DTYPE_QU)
@@ -238,15 +368,20 @@ EXPORT int XTreeDefaultResample(struct descriptor_signal *inSignalD, struct desc
 	arrayD = (struct descriptor_a *)shapeXd.pointer;
 	numDims = arrayD->arsize/arrayD->length;
 	dims = (int *)arrayD->pointer;
-	//Make sure enough room is allocated
-	if(!deltaD)
-		outSamples = dataD->arsize / dataD->length;
-	else 
-		outSamples = (end64 - start64)/delta64 + 1;
-
 	itemSize = dataD->length;
 	for(i = 0; i < numDims-1; i++)
 		itemSize *= dims[i];
+	//Make sure enough room is allocated
+	if(!deltaD)
+	{
+		//Count number of copied samples
+		for(currIdx = 0; currIdx < numTimebaseSamples && timebase64[currIdx] < start64; currIdx++);
+		for(outSamples = 0; currIdx < numTimebaseSamples && timebase64[currIdx] < end64; currIdx++, outSamples++);
+		outSamples++;
+	}
+	else 
+		outSamples = (end64 - start64)/delta64 + 1;
+
 	outData = malloc(outSamples * itemSize);
 	outDim = malloc(outSamples * 8);
 
@@ -264,7 +399,7 @@ EXPORT int XTreeDefaultResample(struct descriptor_signal *inSignalD, struct desc
 	{
 		outDataArray.m[i] = dims[i];
 	}
-	outDataArray.m[numDims - 1] = outSamples;
+	outDataArray.m[numDims-1] = outSamples;
 
 	//If originally float, convert  dimension to float
 	if(isFloat)
