a81aed48eb958e39a1af7c03076692f4d09c14ae
Thomas W. Fredian <twf@psfc.mit.edu>
2009-03-17 16:21:25 +0000

    New version from Basil

--- tdic/tdic.c
+++ tdic/tdic.c
@@ -1,3 +1,7 @@
+#define DYNreadline "libreadline.so"
+#define DYNcurses   "libcurses.so"
+#define DYNTdiShr "libTdiShr.so"
+
 #include <string.h>
 #include <stdio.h>
 #include <mdsdescrip.h>
@@ -13,18 +17,93 @@
 #include <readline/history.h>
 #endif
 
+static void *TDIhandle = NULL;
+#ifdef DYNTdiShr
+#include <dlfcn.h>
+int (*BTdiExecute)() = NULL;
+#else
 extern int TdiExecute();
+#define BTdiExecute TdiExecute
+#endif
+
+static void *READhandle = NULL;
+#ifdef DYNreadline
+#include <dlfcn.h>
+void (*Busing_history) PARAMS((void)) = NULL;
+int (*Bread_history) PARAMS((const char *)) = NULL;
+int (*Bwrite_history) PARAMS((const char *)) = NULL;
+void (*Badd_history) PARAMS((const char *)) = NULL;
+int (*Bhistory_truncate_file) PARAMS((const char *, int)) = NULL;
+char *(*Breadline) PARAMS((const char *)) = NULL;;
+#else
+#define Busing_history using_history
+#define Bread_history read_history
+#define Bwrite_history write_history
+#define Badd_history add_history
+#define Bhistory_truncate_file history_truncate_file
+#define Breadline readline
+#endif
+
+static void *CURSEShandle = NULL;
+#define HFILE "/.tdic"
+
 char *bfgets(char *s, int size, FILE *stream, char *prompt);   /* fgets replacement */
 #define PROMPT "TDI> "
 #define MDSPATH "MDS_PATH"
 
 static void tdiputs(char *line);
 
-int main(int argc, char **argv)
-{
+/* Lookup up entry point in shareable */
+void *dlsymget(void *handle, char *sym) {
+  char *error;
+  void *ret=NULL;
+  ret = dlsym(handle,sym);
+  dlerror();		/* Clear any existing error */
+  if ((error = dlerror()) != NULL)  {
+      fprintf (stderr, "%s\n", error);
+      exit(1);
+  }
+  return(ret);
+}
+
+int main(int argc, char **argv) {
    FILE *in = stdin;
    int status;
    char temp[MAXEXPR];		       /* temp strings for command */
+   char hfile[32];
+
+#ifdef DYNTdiShr
+    if(TDIhandle == NULL) {
+	TDIhandle = dlopen(DYNTdiShr, RTLD_LAZY);
+	if(!TDIhandle) {
+	 fprintf (stderr, "%s\n", dlerror());
+	 exit(1);
+	}
+    *(void **)(&BTdiExecute)	= dlsymget(TDIhandle, "TdiExecute");
+   }
+#endif
+
+#ifdef DYNreadline
+    if(READhandle == NULL) {
+        CURSEShandle = dlopen(DYNcurses, RTLD_NOW | RTLD_GLOBAL);
+	READhandle = dlopen(DYNreadline, RTLD_LAZY);
+//	READhandle = dlopen(DYNreadline, RTLD_NOW);
+//	READhandle = dlopen(DYNreadline, RTLD_NOW | RTLD_GLOBAL);
+	if(!READhandle) {
+	 fprintf (stderr, "%s\n", dlerror());
+	 exit(1);
+	}
+    *(void **)(&Busing_history)		= dlsymget(READhandle, "using_history");
+    *(void **)(&Bread_history)		= dlsymget(READhandle, "read_history");
+    *(void **)(&Bwrite_history)		= dlsymget(READhandle, "write_history");
+    *(void **)(&Badd_history)		= dlsymget(READhandle, "add_history");
+    *(void **)(&Bhistory_truncate_file) = dlsymget(READhandle, "history_truncate_file");
+    *(void **)(&Breadline)		= dlsymget(READhandle, "readline");
+   }
+#endif
+   strcpy(hfile,getenv("HOME"));
+   strcat(hfile,HFILE);
+   Busing_history();
 /*
   static char expr[MAXEXPR] = "WRITE(_OUTPUT_UNIT,`DECOMPILE(`";
 */
@@ -37,6 +116,8 @@ int main(int argc, char **argv)
    static DESCRIPTOR(reset_output_unit,"PUBLIC _OUTPUT_UNIT=$");
    int prefixlen = strlen(expr);
    char line_in[MAXEXPR];
+   char last_line[MAXEXPR];
+   *last_line=0;
 /* get input from file */
    if (argc > 1) {
       in = fopen(argv[1],"r");
@@ -50,16 +131,18 @@ int main(int argc, char **argv)
       struct descriptor out_d = {0,DTYPE_T,CLASS_S,0};
       out_d.length = (unsigned short)strlen(argv[2]);
       out_d.pointer = argv[2];
-      TdiExecute(&out_unit_other,&out_d,&output_unit MDS_END_ARG);
+      BTdiExecute(&out_unit_other,&out_d,&output_unit MDS_END_ARG);
    }
    else
-     TdiExecute(&out_unit_stdout, &output_unit MDS_END_ARG);
+     BTdiExecute(&out_unit_stdout, &output_unit MDS_END_ARG);
+/* get history loaded */
+   Bread_history(hfile);
 /* main loop to get characters */
    while(bfgets(line_in,MAXEXPR,in,PROMPT) != NULL) {
       int comment = line_in[0] == '%'; /* is a comment */
       int len = strlen(line_in);       /* get first line of command */
       if (!comment) {
-	 TdiExecute(&reset_output_unit,&output_unit, &ans MDS_END_ARG);
+	 BTdiExecute(&reset_output_unit,&output_unit, &ans MDS_END_ARG);
 /*	 tdiputs(line_in);*/
       }
 /* if a continuation, keep getting the rest of the command */
@@ -71,9 +154,32 @@ int main(int argc, char **argv)
       if (!comment) {
 	 static DESCRIPTOR(error_out,"_MSG=DEBUG(0),DEBUG(4),WRITE($,_MSG)");
 	 static DESCRIPTOR(clear_errors,"WRITE($,DECOMPILE($)),DEBUG(4)");
+#ifdef OLDHIST
+	if(!strcmp(line_in,last_line) || (*line_in == ' ')) {
+/*		printf("Removing [%d]\n",Bwhere_history());*/
+		HIST_ENTRY *entry = Bremove_history(Bwhere_history());
+		if(entry) {
+		  free (entry->line);
+		  free (entry);
+		}
+	} else
+		strcpy(last_line,line_in);
+#else
+	if(*line_in && (*line_in != ' ') && strcmp(line_in,last_line)) {
+	    Badd_history(line_in);
+	    strcpy(last_line,line_in);
+	}
+#endif
 /* Check the special TDIC options */
-	 if(!strcmp(line_in,"exit") || !strcmp(line_in,"quit"))
-	   exit(1);
+	 if(!strcmp(line_in,"exit") || !strcmp(line_in,"quit")) {
+		Bwrite_history(hfile);
+		if(getenv("HISTFILESIZE")) {
+		  int i;
+		  sscanf(getenv("HISTFILESIZE"),"%d",&i);
+		  Bhistory_truncate_file(hfile,i);
+		}
+		exit(1);
+		}
 	 else if(*line_in == '!') {
 	    *line_in = ' ';			/* replace by a space */
 	    system(line_in);
@@ -194,13 +300,13 @@ int main(int argc, char **argv)
       expr[expr_dsc.length++] = ')';
       expr[expr_dsc.length++] = ')';
 */
-	 status = TdiExecute(&expr_dsc, &ans MDS_END_ARG);
+	 status = BTdiExecute(&expr_dsc, &ans MDS_END_ARG);
 	 if (status & 1)  {
 	    if(!comment)
-	      TdiExecute(&clear_errors , &output_unit, &ans, &ans MDS_END_ARG);
+	      BTdiExecute(&clear_errors , &output_unit, &ans, &ans MDS_END_ARG);
 	 }
 	 else
-	   TdiExecute(&error_out, &output_unit, &ans MDS_END_ARG);
+	   BTdiExecute(&error_out, &output_unit, &ans MDS_END_ARG);
       }
       }
    }
@@ -215,31 +321,31 @@ static void tdiputs(char *line)
    line_d.length = (unsigned short)strlen(line);
    line_d.pointer = line;
    if (line[line_d.length-1]=='\n') line_d.length--;
-   TdiExecute(&write_it,&line_d,&ans MDS_END_ARG);
+   BTdiExecute(&write_it,&line_d,&ans MDS_END_ARG);
 }
 #ifndef HAVE_READLINE_READLINE_H
 /* Routine to replace fgets using readline on stdin */
-static char *readline(char *prompt)
-{
+static char *Breadline(char *prompt) {
   if (prompt) printf("%s ",prompt);
   return fgets(malloc(1024),1023,stdin);
 }
 
-static void add_history(char *string){return;}
+static void Badd_history(char *string){return;}
 #endif
-char *bfgets(char *s, int size, FILE *stream, char *prompt)
-{
+char *bfgets(char *s, int size, FILE *stream, char *prompt) {
    char *rep;
    int n;
 /* if not stdin, read from file */
    if(stream == stdin) {
-      rep = readline(prompt);
-      strncpy(s,rep,size-1);	       /* Copy respecting the size limit */
-      if (rep && *rep)		       /* Add to history if interesting */
-      	add_history (rep);
+      rep = Breadline(prompt);
+      strncpy(s,rep?rep:"",size-1);		/* Copy respecting the size limit */
+#ifdef OLDHIST
+      if (rep && *rep)			/* Add to history if interesting */
+      	Badd_history(rep);
+#endif
       if(rep)
       	free(rep);
-      s[size-1]='\0';		       /* null terminate if necessary */
+      s[size-1]='\0';			/* null terminate if necessary */
       return(s);
    } else
      return(fgets(s,size,stream));
