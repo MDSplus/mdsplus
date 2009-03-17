a16962503a2469eb4c9c358918961263c63a4f44
Thomas W. Fredian <twf@psfc.mit.edu>
2009-03-17 17:36:04 +0000

    Fix main document prefix

--- mdsobjects/python/makedoc.sh
+++ mdsobjects/python/makedoc.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+thisdir=`pwd`
 if [ -z "$1" ]
 then
-  dir="`pwd`/doc"
+  dir="${thisdir}/doc"
 else
   dir="$1"
 fi
-mkdir MDSplus
-cd MDSplus
-ln -sf ../*.py .
+mkdir /tmp/MDSplus
+cd /tmp/MDSplus
+ln -sf ${thisdir}/*.py .
 epydoc -o $dir --name=MDSplus --docformat=epytext --css=grayscale apd.py array.py compound.py ident.py mdsdata.py scalar.py tree.py treenode.py
 cd ..
-rm -Rf MDSplus
+rm -Rf /tmp/MDSplus
 
