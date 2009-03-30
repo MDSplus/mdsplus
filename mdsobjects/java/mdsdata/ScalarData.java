c3fe06beae01dab042315b4e7ae0ce6327132a67
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2009-03-30 11:48:57 +0000

    New Java EA - generated mdsobjects java implementation

--- mdsobjects/java/mdsdata/ScalarData.java
+++
@@ -1,7 +0,0 @@
-package mdsdata;
-
-public class ScalarData extends Data
-{
-    public ScalarData() {clazz = Data.CLASS_S;}
-    public boolean isScalar() { return true; }
-}
--- mdsobjects/java/mdstree/DataStreamProducer.java
+++
@@ -1,8 +0,0 @@
-package mdstree;
-
-public interface DataStreamProducer
-{
-    public void addDataStreamConsumer(DataStreamConsumer consumer) throws TreeException;
-    public void removeDataStreamConsumer(DataStreamConsumer consumer) throws TreeException;
-}
-
