07537fcc0222f817fc6d8560b18ae27aa66e0e9a
Thomas W. Fredian <twf@psfc.mit.edu>
2009-04-13 14:29:27 +0000

    Do not close fd 0. Instead set channel to -2 and check for it. Also check tree read length

--- treeshr/TreeOpen.c
+++ treeshr/TreeOpen.c
@@ -1020,9 +1020,7 @@ static int MapFile(int fd, TREE_INFO *info, int edit_flag, int nomap)
   {
     if (nomap)
     {
-      MDS_IO_READ(fd,(void *)info->section_addr[0], 512 * info->alq);
-      //      MDS_IO_CLOSE(fd);
-      status = 1;
+		status = (MDS_IO_READ(fd,(void *)info->section_addr[0], 512 * info->alq) == (512*info->alq)) ? TreeNORMAL : TreeFAILURE;
     }
 #if (!defined (HAVE_WINDOWS_H) && !defined(HAVE_VXWORKS_H))
     else
@@ -1299,7 +1297,7 @@ int       _TreeOpenNew(void **dbid, char *tree_in, int shot_in)
         {
           char *resnam = 0;
           MDS_IO_CLOSE(fd);
-          info->channel=0;
+          info->channel=-2;
           fd = OpenOne(info, tree, (*dblist)->shotid, TREE_NCIFILE_TYPE, 1, &resnam, 0, 0);
           if (resnam)
             free(resnam);
