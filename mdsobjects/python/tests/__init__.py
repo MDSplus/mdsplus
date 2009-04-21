793e6d02944c835b597b0988836bbeae99d4fb19
Thomas W. Fredian <twf@psfc.mit.edu>
2009-04-21 18:37:29 +0000

    Use subdirectory for test trees and use setenv to define environment vars

--- mdsobjects/python/tests/__init__.py
+++ mdsobjects/python/tests/__init__.py
@@ -10,7 +10,9 @@ import treeUnitTest
 import threadsUnitTest
 import dataUnitTest
 import os
+import time
 import warnings
+from mdsdata import Data
 
 class cleanup(TestCase):
     dir=None
@@ -20,19 +22,32 @@ class cleanup(TestCase):
             dir=cleanup.dir
             for i in os.walk(dir,False):
                 for f in i[2]:
-                    os.remove(i[0]+os.sep+f)
+                    try:
+                      os.remove(i[0]+os.sep+f)
+                    except Exception,e:
+                      print e
                 for d in i[1]:
-                    os.rmdir(i[0]+os.sep+d)
-            os.rmdir(dir)
+                    try:
+                      os.rmdir(i[0]+os.sep+d)
+                    except Exception,e:
+                      print e
+            try:
+              os.rmdir(dir)
+            except Exception,e:
+             print e
         return
 
 def test_all(*arg):
     warnings.filterwarnings("ignore","tmpnam",RuntimeWarning,__name__)
-    dir=os.tmpnam()
+    dir='./pytree'
+    print "Creating trees in %s" % (dir,)
     cleanup.dir=dir
-    os.mkdir(dir)
-    os.putenv('pytree_path',dir)
-    os.putenv('pytreesub_path',dir)
+    try:
+      os.mkdir(dir)
+    except:
+      pass
+    Data.execute('setenv("pytree_path='+dir+'")')
+    Data.execute('setenv("pytreesub_path='+dir+'")')
     tests=list()
     tests.append(treeUnitTest.suite())
     tests.append(threadsUnitTest.suite())
