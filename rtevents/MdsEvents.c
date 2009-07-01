1fce9414b407b83cf423870e653e2b492fbabab4
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2009-07-01 16:01:05 +0000

    Integrate RT Events in mdsip

---
+++ rtevents/MdsEvents.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <mdsshr.h>
+#include <ipdesc.h>
+#include <mdsdescrip.h>
+#include <strroutines.h>
+#include <string.h>
+#include "SystemSpecific.h"
+extern char *TranslateLogical(char *);
+
+extern void *EventAddListener(char *name,  void (*callback)(char *, char *, int, char, int retSize, char *retData, int type));
+extern void *EventAddListenerGlobal(char *name,  void (*callback)(char *, char *, int, char, int retSize, char *retData, int type));
+extern void EventRemoveListener(void *eventHandler);
+extern int EventTrigger(char *name, char *buf, int size);
+extern int EventTriggerAndWait(char *name, char *buf, int size);
+extern void EventClean();
+extern void EventReset();
+
+////Structures used to record for each event the list of IP addresses for that event
+#define MAX_IP 256
+#define MAX_LINE 1024
+struct EventDescr 
+{
+	char *eventName;
+	int numIp;
+	SOCKET ids[MAX_IP];
+	struct EventDescr *nxt;
+};
+
+//mdseventip.host format:
+//<event name> : <ip address> [, <ip address>]*
+
+//Event descriptor head
+static struct EventDescr *eventDescrHead = 0;
+
+
+static void configure()
+{
+	char *confFilename;
+	char line[MAX_LINE];
+	FILE *confF;
+	char *tok;
+	char currAddr[512];
+	char *addrPtr, *addrPtr1;
+	struct EventDescr *currEventDescr;
+	int i;
+	if(eventDescrHead) return;
+
+
+	confFilename = TranslateLogical("mdsevent_conf");
+	confF = fopen(confFilename, "r");
+	if(!confF) return;
+
+
+	while(1)
+	{
+		if(!fgets(line, MAX_LINE, confF)) break;
+		tok = strtok(line, ":");
+		if(!tok) continue;
+		currEventDescr = (struct EventDescr *)malloc(sizeof(struct EventDescr));
+		currEventDescr->eventName = malloc(strlen(tok) + 1);
+		strcpy(currEventDescr->eventName, tok);
+		currEventDescr->numIp = 0;
+		for(i = 0; i < MAX_IP; i++)
+		{
+			tok = strtok(NULL, ",");
+			if(!tok) break;
+			strcpy(currAddr, tok);
+			for(addrPtr = currAddr;  *addrPtr == ' '; addrPtr++);
+			if(!addrPtr) break;
+			for(addrPtr1 = addrPtr; *addrPtr1 && *addrPtr1 != ' '; addrPtr1++);
+			*addrPtr1 = 0;
+			currEventDescr->ids[currEventDescr->numIp] = ConnectToMds(addrPtr);
+			if(currEventDescr->ids[currEventDescr->numIp]) 
+				currEventDescr->numIp++;
+		}
+		currEventDescr->nxt = eventDescrHead;
+		eventDescrHead = currEventDescr;
+	}
+	fclose(confF);
+}
+
+
+
+static void propagateEvent(char *name, char *buf, int size, char wait)
+{
+	struct EventDescr *currEventDescr;
+	int i;
+	char *expr;
+	struct descriptor candidateD = {0, DTYPE_T, CLASS_S, 0};
+	struct descriptor patternD = {0, DTYPE_T, CLASS_S, 0};
+	struct descrip ansarg;
+	struct descrip bufD;
+
+	configure();
+
+	bufD.dtype = DTYPE_BU;
+	bufD.ndims = 1;
+	bufD.dims[0] = size;
+	bufD.length = 1;
+	bufD.ptr = buf;
+
+	expr = malloc(strlen(name) + 512);
+	if(wait)
+		sprintf(expr, "rtevents->EventTriggerAndWait(\"%s\", $1, val(%d))", name, size);
+	else
+		sprintf(expr, "rtevents->EventTrigger(\"%s\", $1, val(%d))", name, size);
+
+	candidateD.length = strlen(name);
+	candidateD.pointer = name;
+	for(currEventDescr = eventDescrHead; currEventDescr; currEventDescr = currEventDescr->nxt)
+	{
+		patternD.length = strlen(currEventDescr->eventName);
+		patternD.pointer = currEventDescr->eventName;
+		if(!StrMatchWild(&candidateD, &patternD))
+			continue;
+		for(i = 0; i < currEventDescr->numIp; i++)
+		{
+			memset(&ansarg, 0, sizeof(ansarg));
+			MdsValue(currEventDescr->ids[i], expr, &bufD, &ansarg, NULL);
+		}
+	}
+	free(expr);
+}
+
+
+
+EXPORT void *MdsEventAddListener(char *name,  void (*callback)(char *, char *, int))
+{
+	return EventAddListener(name,  (void (*)(char *, char *, int, char, int retSize, char *retData, int type))callback);
+}
+
+EXPORT int MdsEventTrigger(char *name, char *buf, int size)
+{
+	propagateEvent(name, buf, size, 0);
+	return EventTrigger(name, buf, size);
+}
+
+EXPORT int MdsEventTriggerAndWait(char *name, char *buf, int size)
+{
+	propagateEvent(name, buf, size, 1);
+	return EventTriggerAndWait(name, buf, size);
+}
--- rtevents/rtevents.dsp
+++ rtevents/rtevents.dsp
@@ -68,7 +68,7 @@ LINK32=link.exe
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "RTEVENTS_EXPORTS" /YX /FD /GZ /c
-# ADD CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "HAVE_WINDOWS_H" /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "RTEVENTS_EXPORTS" /FD /GZ /c
+# ADD CPP /nologo /MDd /W3 /Gm /GX /ZI /Od /I "../include" /D "HAVE_WINDOWS_H" /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "RTEVENTS_EXPORTS" /FD /GZ /c
 # SUBTRACT CPP /YX
 # ADD BASE MTL /nologo /D "_DEBUG" /mktyplib203 /win32
 # ADD MTL /nologo /D "_DEBUG" /mktyplib203 /win32
@@ -79,7 +79,8 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /debug /machine:I386 /pdbtype:sept
-# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib ws2_32.lib /nologo /dll /debug /machine:I386 /out:"../Debug/rtevents.dll" /pdbtype:sept
+# ADD LINK32 ..\mdsshr\Debug\mdsshr.lib ..\mdstcpip\Debug\mdsipshr.lib  kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib wsock32.lib /nologo /dll /debug /machine:I386 /out:"../Debug/rtevents.dll" /pdbtype:sept
+# SUBTRACT LINK32 /incremental:no
 
 !ENDIF 
 
@@ -105,6 +106,10 @@ SOURCE=.\EventHandler.cpp
 # Begin Source File
 
 SOURCE=.\EventManager.cpp
+# End Source File
+# Begin Source File
+
+SOURCE=.\MdsEvents.c
 # End Source File
 # Begin Source File
 
