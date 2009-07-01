0c7bec8a1acc14cf26856694a391513e5dd32228
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2009-07-01 17:04:38 +0000

    Prototypes for MDSplus interface to RTEvents

---
+++ include/rtevents.h
@@ -0,0 +1,3 @@
+extern void *MdsEventAddListener(char *name,  void (*callback)(char *, char *, int));
+extern int MdsEventTrigger(char *name, char *buf, int size);
+extern int MdsEventTriggerAndWait(char *name, char *buf, int size);
