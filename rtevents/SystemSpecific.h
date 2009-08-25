4a7651cbe314c2081cae6ede1c4733fe13a90b66
Thomas W. Fredian <twf@psfc.mit.edu>
2009-08-25 21:03:16 +0000

    Fix for linux to be compatible with vs2008 fixes

--- rtevents/SystemSpecific.h
+++ rtevents/SystemSpecific.h
@@ -1,12 +1,12 @@
 #ifndef _SYSTEM_SPECIFIC_H
 #define SYSTEM_SPECIFIC_H
 
+#include <mdstypes.h>
+
 #ifdef HAVE_WINDOWS_H
 #define EXPORT __declspec(dllexport)
 #else
 #define EXPORT
-typedef long long _int64;
-typedef unsigned long long _int64u;
 #endif
 
 #endif
