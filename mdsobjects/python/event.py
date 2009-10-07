af6c7ae9f304e4502e7e9e0b9f93b05b8018183e
Thomas W. Fredian <twf@psfc.mit.edu>
2009-10-07 18:40:22 +0000

    New methods

--- mdsobjects/python/event.py
+++ mdsobjects/python/event.py
@@ -7,6 +7,9 @@ import time
 class Event(Thread):
     """Thread to wait for event"""
 
+    def __del__(self):
+        self.__active__=True
+
     def __init__(self,event):
         """Saves event name and starts wfevent thread
         @param event: name of event to monitor
@@ -14,6 +17,7 @@ class Event(Thread):
         """
         self.event=event
         Thread.__init__(self)
+        self.__active__=True
         self.start()
 
     def __getattribute__(self,name):
@@ -24,12 +28,42 @@ class Event(Thread):
 
     def _event_run(self):
         from _mdsshr import MDSWfevent
-        while True:
-            self.data=MDSWfevent(self.event)
+        while self.__active__:
+            self.raw=MDSWfevent(self.event)
             self.time=time.time()
             self.__class__.run(self)
 
-    def setevent(event,buffer):
+    def getData(self):
+        """Return data transfered with the event.
+        @rtype: Data
+        """
+        from mdsarray import makeArray
+        return makeArray(self.getRaw()).deserialize()
+
+    def getRaw(self):
+        """Return raw data transfered with the event.
+        @rtype: numpy.uint8
+        """
+        return self.raw
+
+    def getTime(self):
+        """Return time of event in seconds since epoch
+        rtype: float
+        """
+        return self.time
+        
+
+    def setevent(event,data):
+        """Issue an MDSplus event
+        @param event: event name
+        @type event: str
+        @param data: data to pass with event
+        @type data: Data
+        """
+        Event.seteventRaw(event,data.serialize())
+    setevent=staticmethod(setevent)
+    
+    def seteventRaw(event,buffer):
         """Issue an MDSplus event
         @param event: event name
         @type event: str
@@ -38,4 +72,5 @@ class Event(Thread):
         """
         from _mdsshr import MDSEvent
         MDSEvent(event,buffer)
-    setevent=staticmethod(setevent)
+    seteventRaw=staticmethod(seteventRaw)
+
