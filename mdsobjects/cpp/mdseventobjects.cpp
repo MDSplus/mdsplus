6485db775fd6d053251f388f0d2bb222276c50a0
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2009-12-09 16:07:50 +0000

    Added support for reliable events

--- mdsobjects/cpp/mdseventobjects.cpp
+++ mdsobjects/cpp/mdseventobjects.cpp
@@ -16,6 +16,10 @@ extern "C" {
 	int MDSEventAst(char *eventNameIn, void (*astadr)(void *,int,char *), void *astprm, int *eventid);
 	int MDSEventCan(int id);
 	int MDSEvent(char *eventNameIn, int bufLen, char *buf);
+	void *MdsEventAddListener(char *name,  void (*callback)(char *, char *, int, void *), void *callbackArg);
+	void MdsEventRemoveListener(void *eventId);
+	int MdsEventTrigger(char *name, char *buf, int size);
+	int MdsEventTriggerAndWait(char *name, char *buf, int size);
 }
 
 
@@ -29,8 +33,27 @@ extern "C" void eventAst(void *arg, int len, char *buf)
 	ev->eventTime = convertAsciiToTime("now");
 	ev->run();
 }
+extern "C" void reventAst(char *evname, char *buf, int len, void *arg)
+{
+	eventAst(arg, len, buf);
+}
 	
-
+void Event::connectToEvents()
+{
+	MDSEventAst(eventName, eventAst, this, &eventId);
+}
+void Event::disconnectFromEvents()
+{
+	MDSEventCan(eventId);
+}
+void REvent::connectToEvents()
+{
+	reventId = MdsEventAddListener(eventName,  reventAst, this);
+}
+void REvent::disconnectFromEvents()
+{
+	MdsEventRemoveListener(reventId);
+}
 
 Event::Event(char *evName)
 {
@@ -40,8 +63,19 @@ Event::Event(char *evName)
 	eventName = new char[strlen(evName) + 1];
 	strcpy(eventName, evName);
 	eventId = -1;
-	MDSEventAst(evName, eventAst, this, &eventId);
+	connectToEvents();
 }
+REvent::REvent(char *evName)
+{
+	eventData = 0;
+	eventBufSize = 0;
+	eventBuf = 0;
+	eventName = new char[strlen(evName) + 1];
+	strcpy(eventName, evName);
+	eventId = -1;
+	connectToEvents();
+}
+
 
 Event::~Event()
 {
@@ -49,16 +83,38 @@ Event::~Event()
 		deleteData(eventData);
 	delete [] eventName;
 	if(eventId != -1)
-		MDSEventCan(eventId);
+		disconnectFromEvents();
+		
 }
 
-void Event::setevent(char *evName, Data *evData)
+
+
+
+
+void Event::setEvent(char *evName, Data *evData)
 {
 	int bufLen;
 	char *buf = evData->serialize(&bufLen);
 	MDSEvent(evName, bufLen, buf);
 }
-void Event::seteventRaw(char *evName, int bufLen, char *buf)
+void Event::setEventRaw(char *evName, int bufLen, char *buf)
 {
 	MDSEvent(evName, bufLen, buf);
+}
+
+void REvent::setEvent(char *evName, Data *evData)
+{
+	int bufLen;
+	char *buf = evData->serialize(&bufLen);
+	MdsEventTrigger(evName, buf, bufLen);
+}
+void REvent::setEventAndWait(char *evName, Data *evData)
+{
+	int bufLen;
+	char *buf = evData->serialize(&bufLen);
+	MdsEventTriggerAndWait(evName, buf, bufLen);
+}
+void REvent::setEventRawAndWait(char *evName, int bufLen, char *buf)
+{
+	MdsEventTriggerAndWait(evName, buf, bufLen);
 }
