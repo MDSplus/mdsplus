a64c1608e4d635a9750f63bd032bdde822f1c358
Joshua Stillerman <jas@psfc.mit.edu>
2010-03-09 19:39:37 +0000

    adding dt196b and dt216b devices (python)

--- tdi/MitDevices/__init__.py
+++ tdi/MitDevices/__init__.py
@@ -14,3 +14,8 @@ from cp7452 import CP7452
 from dtao32 import DTAO32
 from dtdo32 import DTDO32
 from dt132  import DT132
+from dt216b import DT216B
+from dt196b import DT196B
+import acq200
+import transport
+import dt100
