a135ed1390ee013a1b3488d65c45fa0737048aeb
Joshua Stillerman <jas@psfc.mit.edu>
2010-09-14 15:13:12 +0000

    shell script to be run on the cards in the /etc/postshot.d  if they are going to use the storeftp method.
    This file must be installed on the cards in a directory that is in ~root's
    path.  For example /usr/local/bin/

---
+++ tdi/MitDevices/storeftp.sh
@@ -0,0 +1,64 @@
+xmlstart() {
+echo "<?xml version=\"1.0\"?>"
+echo "<marshal>"
+echo "<dictionary id=\"i2\">"
+}
+xmlfinish() {
+echo "</dictionary>"
+echo "</marshal>"
+}
+
+xmlcmd() {
+  cmd=$1
+  echo "<string>$cmd</string>"
+  echo "<string>`$cmd`</string>"
+}
+xmlacqcmd() {
+  cmd=$1
+  echo "<string>$cmd</string>"
+  echo "<string>`acqcmd $cmd`</string>"
+}
+
+xmlstart
+xmlacqcmd getNumSamples
+xmlacqcmd getChannelMask
+xmlacqcmd getInternalClock
+xmlcmd date
+xmlcmd hostname
+xmlcmd 'sysmon -T 0'
+xmlcmd 'sysmon -T 1'
+xmlcmd get.channelMask
+xmlcmd get.channel_mask
+xmlcmd get.d-tacq.release
+xmlcmd get.event0
+xmlcmd get.event1
+xmlcmd get.extClk 
+xmlcmd get.ext_clk
+xmlcmd get.int_clk_src
+xmlcmd get.modelspec
+xmlcmd get.numChannels
+xmlcmd get.pulse_number
+xmlcmd get.trig
+xmlcmd get.vin
+xmlfinish
+tree=$1
+shot=$2
+path=$3
+host=192.168.0.254
+settings.sh > /tmp/settings.xml
+echo $tree $shot $path > /tmp/$tree.$shot.$path
+ftp $host <<EOF
+cd pub
+put /tmp/$tree.$shot.$path triggers/$tree.$shot.$path 
+ls mkdir-$tree
+cd $tree
+ls mkdir-$shot
+cd $shot
+ls mkdir-$path
+cd $path
+lcd /tmp
+put settings.xml
+lcd /dev/acq200/data
+prompt
+mput [0-9][0-9]
+EOF
