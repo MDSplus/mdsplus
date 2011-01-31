eda6d6cbe2673a1e3c900753c4354a31c37784ee
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2011-01-31 17:07:11 +0000

    Fields added

--- javadevices/CAENDT5720Setup.java
+++ javadevices/CAENDT5720Setup.java
@@ -31,9 +31,13 @@ public class CAENDT5720Setup extends DeviceSetup {
 
         deviceButtons1 = new DeviceButtons();
         jPanel1 = new javax.swing.JPanel();
+        jPanel26 = new javax.swing.JPanel();
         deviceField1 = new DeviceField();
         deviceField3 = new DeviceField();
         deviceDispatch1 = new DeviceDispatch();
+        jPanel27 = new javax.swing.JPanel();
+        deviceChoice15 = new DeviceChoice();
+        deviceField2 = new DeviceField();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
@@ -96,23 +100,41 @@ public class CAENDT5720Setup extends DeviceSetup {
         setDeviceProvider("localhost");
         setDeviceTitle("CAEN DT5720");
         setDeviceType("CAENDT5720");
-        setHeight(600);
+        setHeight(650);
         setWidth(700);
         getContentPane().add(deviceButtons1, java.awt.BorderLayout.PAGE_END);
+
+        jPanel1.setLayout(new java.awt.GridLayout(2, 1));
 
         deviceField1.setIdentifier("");
         deviceField1.setLabelString("Comment");
         deviceField1.setNumCols(15);
         deviceField1.setOffsetNid(2);
         deviceField1.setTextOnly(true);
-        jPanel1.add(deviceField1);
+        jPanel26.add(deviceField1);
 
         deviceField3.setIdentifier("");
         deviceField3.setLabelString("Board Id");
         deviceField3.setNumCols(4);
         deviceField3.setOffsetNid(1);
-        jPanel1.add(deviceField3);
-        jPanel1.add(deviceDispatch1);
+        jPanel26.add(deviceField3);
+        jPanel26.add(deviceDispatch1);
+
+        jPanel1.add(jPanel26);
+
+        deviceChoice15.setChoiceItems(new String[] {"TRANSIENT RECORDER", "CONTINUOUS SAMPLING"});
+        deviceChoice15.setIdentifier("");
+        deviceChoice15.setLabelString("Acquisiton mode: ");
+        deviceChoice15.setOffsetNid(16);
+        deviceChoice15.setUpdateIdentifier("");
+        jPanel27.add(deviceChoice15);
+
+        deviceField2.setIdentifier("");
+        deviceField2.setLabelString("Events per IRQ:");
+        deviceField2.setOffsetNid(17);
+        jPanel27.add(deviceField2);
+
+        jPanel1.add(jPanel27);
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_START);
 
@@ -134,7 +156,7 @@ public class CAENDT5720Setup extends DeviceSetup {
 
         deviceField5.setIdentifier("");
         deviceField5.setLabelString("Clock Source: ");
-        deviceField5.setNumCols(20);
+        deviceField5.setNumCols(30);
         deviceField5.setOffsetNid(8);
         jPanel11.add(deviceField5);
 
@@ -225,7 +247,7 @@ public class CAENDT5720Setup extends DeviceSetup {
 
         deviceField4.setIdentifier("");
         deviceField4.setLabelString("Trig Time: ");
-        deviceField4.setNumCols(20);
+        deviceField4.setNumCols(30);
         deviceField4.setOffsetNid(6);
         jPanel9.add(deviceField4);
 
@@ -241,32 +263,32 @@ public class CAENDT5720Setup extends DeviceSetup {
         deviceChoice7.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice7.setIdentifier("");
         deviceChoice7.setLabelString("State");
-        deviceChoice7.setOffsetNid(17);
+        deviceChoice7.setOffsetNid(19);
         deviceChoice7.setUpdateIdentifier("");
         jPanel15.add(deviceChoice7);
 
         deviceChoice8.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice8.setIdentifier("");
         deviceChoice8.setLabelString("Trig State: ");
-        deviceChoice8.setOffsetNid(18);
+        deviceChoice8.setOffsetNid(20);
         deviceChoice8.setUpdateIdentifier("");
         jPanel15.add(deviceChoice8);
 
         deviceField11.setIdentifier("");
         deviceField11.setLabelString("DAC Offset: ");
-        deviceField11.setOffsetNid(20);
+        deviceField11.setOffsetNid(22);
         jPanel15.add(deviceField11);
 
         jPanel14.add(jPanel15);
 
         deviceField12.setIdentifier("");
         deviceField12.setLabelString("Trig Thresh. Lev.:");
-        deviceField12.setOffsetNid(21);
+        deviceField12.setOffsetNid(23);
         jPanel16.add(deviceField12);
 
         deviceField13.setIdentifier("");
         deviceField13.setLabelString("Trig Tresh. Samples:");
-        deviceField13.setOffsetNid(30);
+        deviceField13.setOffsetNid(33);
         jPanel16.add(deviceField13);
 
         jPanel14.add(jPanel16);
@@ -278,32 +300,32 @@ public class CAENDT5720Setup extends DeviceSetup {
         deviceChoice9.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice9.setIdentifier("");
         deviceChoice9.setLabelString("State");
-        deviceChoice9.setOffsetNid(25);
+        deviceChoice9.setOffsetNid(28);
         deviceChoice9.setUpdateIdentifier("");
         jPanel18.add(deviceChoice9);
 
         deviceChoice10.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice10.setIdentifier("");
         deviceChoice10.setLabelString("Trig State: ");
-        deviceChoice10.setOffsetNid(26);
+        deviceChoice10.setOffsetNid(29);
         deviceChoice10.setUpdateIdentifier("");
         jPanel18.add(deviceChoice10);
 
         deviceField14.setIdentifier("");
         deviceField14.setLabelString("DAC Offset: ");
-        deviceField14.setOffsetNid(28);
+        deviceField14.setOffsetNid(31);
         jPanel18.add(deviceField14);
 
         jPanel17.add(jPanel18);
 
         deviceField15.setIdentifier("");
         deviceField15.setLabelString("Trig Thresh. Lev.:");
-        deviceField15.setOffsetNid(29);
+        deviceField15.setOffsetNid(32);
         jPanel19.add(deviceField15);
 
         deviceField16.setIdentifier("");
         deviceField16.setLabelString("Trig Tresh. Samples:");
-        deviceField16.setOffsetNid(30);
+        deviceField16.setOffsetNid(33);
         jPanel19.add(deviceField16);
 
         jPanel17.add(jPanel19);
@@ -315,32 +337,32 @@ public class CAENDT5720Setup extends DeviceSetup {
         deviceChoice11.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice11.setIdentifier("");
         deviceChoice11.setLabelString("State");
-        deviceChoice11.setOffsetNid(33);
+        deviceChoice11.setOffsetNid(37);
         deviceChoice11.setUpdateIdentifier("");
         jPanel21.add(deviceChoice11);
 
         deviceChoice12.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice12.setIdentifier("");
         deviceChoice12.setLabelString("Trig State: ");
-        deviceChoice12.setOffsetNid(34);
+        deviceChoice12.setOffsetNid(38);
         deviceChoice12.setUpdateIdentifier("");
         jPanel21.add(deviceChoice12);
 
         deviceField17.setIdentifier("");
         deviceField17.setLabelString("DAC Offset: ");
-        deviceField17.setOffsetNid(36);
+        deviceField17.setOffsetNid(40);
         jPanel21.add(deviceField17);
 
         jPanel20.add(jPanel21);
 
         deviceField18.setIdentifier("");
         deviceField18.setLabelString("Trig Thresh. Lev.:");
-        deviceField18.setOffsetNid(37);
+        deviceField18.setOffsetNid(41);
         jPanel22.add(deviceField18);
 
         deviceField19.setIdentifier("");
         deviceField19.setLabelString("Trig Tresh. Samples:");
-        deviceField19.setOffsetNid(38);
+        deviceField19.setOffsetNid(42);
         jPanel22.add(deviceField19);
 
         jPanel20.add(jPanel22);
@@ -352,32 +374,32 @@ public class CAENDT5720Setup extends DeviceSetup {
         deviceChoice13.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice13.setIdentifier("");
         deviceChoice13.setLabelString("State");
-        deviceChoice13.setOffsetNid(41);
+        deviceChoice13.setOffsetNid(46);
         deviceChoice13.setUpdateIdentifier("");
         jPanel24.add(deviceChoice13);
 
         deviceChoice14.setChoiceItems(new String[] {"ENABLED", "DISABLED"});
         deviceChoice14.setIdentifier("");
         deviceChoice14.setLabelString("Trig State: ");
-        deviceChoice14.setOffsetNid(42);
+        deviceChoice14.setOffsetNid(47);
         deviceChoice14.setUpdateIdentifier("");
         jPanel24.add(deviceChoice14);
 
         deviceField20.setIdentifier("");
         deviceField20.setLabelString("DAC Offset: ");
-        deviceField20.setOffsetNid(36);
+        deviceField20.setOffsetNid(49);
         jPanel24.add(deviceField20);
 
         jPanel23.add(jPanel24);
 
         deviceField21.setIdentifier("");
         deviceField21.setLabelString("Trig Thresh. Lev.:");
-        deviceField21.setOffsetNid(45);
+        deviceField21.setOffsetNid(50);
         jPanel25.add(deviceField21);
 
         deviceField22.setIdentifier("");
         deviceField22.setLabelString("Trig Tresh. Samples:");
-        deviceField22.setOffsetNid(46);
+        deviceField22.setOffsetNid(51);
         jPanel25.add(deviceField22);
 
         jPanel23.add(jPanel25);
@@ -400,6 +422,7 @@ public class CAENDT5720Setup extends DeviceSetup {
     private DeviceChoice deviceChoice12;
     private DeviceChoice deviceChoice13;
     private DeviceChoice deviceChoice14;
+    private DeviceChoice deviceChoice15;
     private DeviceChoice deviceChoice2;
     private DeviceChoice deviceChoice3;
     private DeviceChoice deviceChoice4;
@@ -420,6 +443,7 @@ public class CAENDT5720Setup extends DeviceSetup {
     private DeviceField deviceField17;
     private DeviceField deviceField18;
     private DeviceField deviceField19;
+    private DeviceField deviceField2;
     private DeviceField deviceField20;
     private DeviceField deviceField21;
     private DeviceField deviceField22;
@@ -448,6 +472,8 @@ public class CAENDT5720Setup extends DeviceSetup {
     private javax.swing.JPanel jPanel23;
     private javax.swing.JPanel jPanel24;
     private javax.swing.JPanel jPanel25;
+    private javax.swing.JPanel jPanel26;
+    private javax.swing.JPanel jPanel27;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
