8907519143c136d45e90a9078d390490d7c2162b
Thomas W. Fredian <twf@psfc.mit.edu>
2011-03-11 14:18:09 +0000

    cast void ptr to char *

--- mdstcpip/t.c
+++ mdstcpip/t.c
@@ -25,7 +25,7 @@ int main( int argc, void **argv)
       val = val + (float)1.;
     }
     else
-      printf("%s\n",ans.ptr);
+      printf("%s\n",(char *)ans.ptr);
     if (ans.ptr) 
     {
       free(ans.ptr);
@@ -37,7 +37,7 @@ int main( int argc, void **argv)
     if (MdsValue(sock,"42.0",&ans,0) & 1)
       printf("%g\n",*(float *)ans.ptr);
     else
-      printf("%s\n",ans.ptr);
+      printf("%s\n",(char *)ans.ptr);
     if (ans.ptr) free(ans.ptr);
   }
   return 1;
