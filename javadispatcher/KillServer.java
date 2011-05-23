ea54fbf8fb11b143da9979ae63f44bf08446c94c
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2011-05-23 12:33:20 +0000

    fix

--- javadispatcher/KillServer.java
+++ javadispatcher/KillServer.java
@@ -13,18 +13,29 @@ public class KillServer extends MdsConnection
   {
     String serverIp = "";
     int serverPort = 0;
+	  
+
     try {
       serverIp = args[0];
       serverPort = Integer.parseInt(args[1]);
-    }catch(Exception exc)
+    }
+	catch(Exception exc)
     {
       System.err.println("Usage: java KillServer <ip>, <port>");
       System.exit(0);
     }
+
     KillServer killServer = new KillServer(serverIp + ":"+serverPort);
     killServer.ConnectToMds(false);
-    killServer.MdsValue("kill", new Vector());
 
+    killServer.MdsValue("kill", new Vector(), false);
+
+	
+	killServer.DisconnectFromMds();
+
+	     
+	System.exit(0);
+ 
   }
 
 
