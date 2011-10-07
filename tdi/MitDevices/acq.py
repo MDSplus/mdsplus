8338b5d136cefe9982cbf7c9179c404a7824ccb2
Joshua Stillerman <jas@psfc.mit.edu>
2011-10-07 20:50:23 +0000

    new acq device support based on acq super class methods:   initftp([autostore])   store([nochecks])   getstate   acqcmd(acq-command-string)   acq2sh(acq-shell-command-string)   trigger   wait
      default actions should be
      (method constructor does not currently take argument)
        initftp('autostore')
        wait()

---
+++ tdi/MitDevices/acq.py
@@ -0,0 +1,299 @@
+import time
+import os
+import numpy
+import array
+import ftplib
+import tempfile
+from xml.marshal.generic import dumps, loads, load 
+
+import MDSplus
+from MitDevices import acq200, transport
+
+
+class ACQ(MDSplus.Device):
+    """
+    Abstract class to subclass the d-tacq acqxxx device types.  Contains
+    members and methods that all of the acqxxx  devices share:
+
+
+    members:
+      parts - the shared parts.  This should have the channels and the actions appended to it
+      actions - the actions.  This gets appended after the channels
+
+    methods:
+      debugging() - is debugging enabled.  Controlled by environment variable DEBUG_DEVICES
+
+    
+    """
+    
+    acq_parts=[
+        {'path':':NODE','type':'text','value':'192.168.0.254'},
+        {'path':':COMMENT','type':'text'},
+        ]
+
+    for i in range(6):
+        acq_parts.append({'path':':DI%1.1d'%(i,),'type':'axis','options':('no_write_shot',)})
+        acq_parts.append({'path':':DI%1.1d:WIRE'%(i,),'type':'text','options':('no_write_shot',)})
+        acq_parts.append({'path':':DI%1.1d:BUS'%(i,),'type':'text','options':('no_write_shot',)})
+
+
+    parts2=[
+        {'path':':INT_CLOCK','type':'axis','options':('no_write_model','write_once')},       
+        {'path':':TRIG_SRC','type':'numeric','valueExpr':'head.di3','options':('no_write_shot',)},
+        {'path':':TRIG_EDGE','type':'text','value':'rising','options':('no_write_shot',)},
+        {'path':':CLOCK_SRC','type':'numeric','valueExpr':'head.int_clock','options':('no_write_shot',)},
+        {'path':':CLOCK_DIV','type':'numeric','value':1,'options':('no_write_shot',)},
+        {'path':':CLOCK_EDGE','type':'text','value':'rising','options':('no_write_shot',)},
+        {'path':':CLOCK_FREQ','type':'numeric','value':1000000,'options':('no_write_shot',)},
+        {'path':':CLOCK_OUT','type':'numeric','valueExpr':'head.di2','options':('no_write_shot',)},
+
+        {'path':':DAQ_MEM','type':'numeric','value':512,'options':('no_write_shot',)},
+        {'path':':ACTIVE_CHAN','type':'numeric','value':16,'options':('no_write_shot',)},
+        {'path':':POST_TRIG','type':'numeric','value':128,'options':('no_write_shot',)},
+        {'path':':PRE_TRIG','type':'numeric','value':0,'options':('no_write_shot',)},
+        {'path':':RANGES','type':'numeric','options':('write_once',)},
+        {'path':':STATUS_CMDS','type':'text','value':MDSplus.makeArray(['cat /proc/cmdline', 'get.d-tacq.release']),'options':('no_write_shot',)},
+        {'path':':BOARD_STATUS','type':'signal','options':('write_shot',)},
+        {'path':':CLOCK','type':'axis','options':('no_write_model','write_once')},
+        ]
+    acq_parts.extend(parts2)
+    del parts2
+
+    action_parts=[
+        {'path':':INIT_ACTION','type':'action',
+         'valueExpr':"Action(Dispatch('CAMAC_SERVER','INIT',50,None),Method(None,'INITFTP',head))",
+         'options':('no_write_shot',)},
+        {'path':':STORE_ACTION','type':'action',
+         'valueExpr':"Action(Dispatch('CAMAC_SERVER','STORE',50,None),Method(None,'WAITFTP',head))",
+         'options':('no_write_shot',)}]
+
+
+    debug=None
+
+    wires = [ 'fpga','mezz','rio','pxi','lemo', 'none', 'fpga pxi', ' ']
+    
+
+    def debugging(self):
+	if self.debug == None :
+            self.debug=os.getenv("DEBUG_DEVICES")
+	return(self.debug)
+
+    def getPreTrig(self,str) :
+	parts = str.split('=')
+        pre_trig = int(parts[2].split(' ')[0])
+	return pre_trig
+
+    def getPostTrig(self,str) :
+        parts = str.split('=')
+        post_trig = int(parts[3].split(' ')[0])
+        return post_trig
+
+    def getBoardIp(self):
+        boardip=str(self.node.record)
+        if len(boardip) == 0 :
+            raise Exception, "boardid record empty"
+        return boardip
+
+    def readRawData(self, chan, pre, start, end, inc) :
+        import pycurl
+        import cStringIO
+        try:
+            if self.debugging():
+                print "starting readRawData\n"
+            c=pycurl.Curl()
+            c.setopt(pycurl.HTTPHEADER,["Accept:"])
+            buf=cStringIO.StringIO()
+            c.setopt(pycurl.URL,"http://%s/dev/acq200/data/%02d" % (self.getBoardIp(), chan,))
+            c.setopt(pycurl.WRITEFUNCTION,buf.write)
+            c.perform()
+            binValues = array.array('H')
+            binValues.fromstring(buf.getvalue())
+            if inc == 1:
+                ans = numpy.array(binValues[pre+start:end-start-1], dtype=numpy.int16)
+            else :
+                ans = numpy.array(binValues[pre+start:end-start-1:inc], dtype=numpy.int16)
+        except Exception,e :
+           print "readRawData - %s" % e
+           raise e
+        return ans
+
+
+    def timeoutHandler(self,sig,stack):
+        raise Exception("Timeout occurred")
+        
+    def getBoardState(self):
+        """Get the current state"""
+        import socket,signal
+        s=socket.socket()
+        state="Unknown"
+        try:
+            signal.signal(signal.SIGALRM,self.timeoutHandler)
+            signal.alarm(60)
+            s.connect((self.getBoardIp(),54545))
+            state=s.recv(100)[0:-1]
+        except Exception,e:
+            print "Error getting board state: %s" % (str(e),)
+        signal.alarm(0)
+        s.close()
+        return state
+                  
+    def getMyIp(self):
+        import socket
+        icmp = socket.getprotobyname("icmp")
+
+        s=socket.socket()
+	try:
+            s.connect((self.getBoardIp()),54545)
+            hostip = s.getsockname()[0]
+	except Exception,e:
+	    hostip = ""
+	    print "could not connect to board to verify local IP address\n%s\n" %(e,)
+	return hostip
+
+    def loadSettings(self):
+        settingsfd = tempfile.TemporaryFile()
+        ftp = ftplib.FTP(self.getBoardIp())
+        ftp.login('dt100', 'dt100')
+        ftp.retrlines("RETR /tmp/settings.xml", lambda s, w=settingsfd.write: w(s+"\n"))
+        settingsfd.seek(0,0)
+        if self.debugging():
+            print "got the settings\n"
+            
+        try :
+            settings = load(settingsfd)
+        except Exception,e:
+            settingsfd.close()
+	    raise Exception, "Could not parse XML settings\n%s" %(e,)
+        settingsfd.close()
+        if self.debugging():
+            print "The settings are loaded\n"
+        return settings
+    
+    def doInit(self,fd):
+        """Tell the board to arm"""
+        import socket,signal
+        if self.debugging():
+	    print "starting doInit"
+        status=1
+        try:
+            ftp = ftplib.FTP(self.getBoardIp())
+            ftp.login('dt100','dt100')
+            ftp.storlines("STOR /tmp/initialize",fd)
+        except Exception,e:
+            print "Error sending arm commands via ftp to %s\n%s\n" % (self.getBoardIp(), e,)
+            return
+        s=socket.socket()
+        try:
+            signal.signal(signal.SIGALRM,self.timeoutHandler)
+            signal.alarm(15)
+            s.connect((self.getBoardIp(),54546))
+            s.send("initialize")
+        except Exception,e:
+            status=0
+            print "Error sending doInit: %s" % (str(e),)
+        signal.alarm(0)
+        s.close()
+        if self.debugging():
+            print "finishing doInit"
+        return status
+
+    def waitftp(self, arg) :
+        """Wait for board to finish digitizing and ftp'ing data to host"""
+        state = self.getBoardState()
+        if state == 'ARMED' or state == 'RUN':
+            return 662470754
+            raise Exception, "device Not triggered"
+        for chan in range(int(self.active_chan), 0, -1):
+            chan_node = self.__getattr__('input_%2.2d' % (chan,))
+            if chan_node.on :
+                max_chan = chan_node
+		break
+        tries = 0
+        while tries < 60 :
+            if max_chan.rlength > 0:
+                break
+            time.sleep(3)
+            tries = tries+1
+        if tries == 60:
+            print "Triggered, but data not stored !"
+            return 0
+
+        return 1
+    WAITFTP=waitftp
+
+    def trigger(self, arg):
+        if self.debugging():
+            print "starting trigger"
+        boardip = self.getBoardIp()
+        try:
+            trig_src=self.trig_src.record.getOriginalPartName().getString()[1:]
+        except:
+            print "could not read trigger source"
+            return 0
+        if self.debugging() :
+            print "executing trigger on board %s, trig_src is %s."% (boardip, trig_src,)
+        trig_src = trig_src[2:]
+        
+        try:
+            UUT = acq200.Acq200(transport.factory(boardip))
+        except:
+            print "could not connect to the board %s"% (boardip,)
+        try:
+            route = UUT.uut.acq2sh('get.route D%s'%(trig_src,))
+            d1 = UUT.uut.acq2sh('set.route d%s in fpga out'%(trig_src,))
+            d2 = UUT.uut.acq2sh('set.dtacq dio_bit %s P'%(trig_src,))
+            d3 = UUT.uut.acq2sh('set.route %s' %(route,))
+            d4 = UUT.uut.acq2sh('set.dtacq dio_bit %s -'%(trig_src,))
+            
+        except:
+            print "could not send command to the board"
+        if self.debugging():
+            print "got back: %s\n" % (route,)
+	    print "     and: %s\n" % (d1,)
+	    print "     and: %s\n" % (d2,)
+	    print "     and: %s\n" % (d3,)
+	    print "     and: %s\n" % (d4,)
+        return 1
+    TRIGGER=trigger
+
+    def acqcmd(self, arg):
+        boardip = self.getBoardIp()
+        
+        try:
+            UUT = acq200.Acq200(transport.factory(boardip))
+        except:
+            print "could not connect to the board %s"% (boardip,)
+        try:
+            a = UUT.uut.acqcmd(str(arg))
+        except:
+            print "could not send %s to the board" %(str(arg),)
+            return 0
+        print "%s  %s -> %s"%(boardip, arg, a,)
+        return 1
+    ACQCMD=acqcmd
+
+    def acq2sh(self, arg):
+        boardip = self.getBoardIp()
+        
+        try:
+            UUT = acq200.Acq200(transport.factory(boardip))
+        except:
+            print "could not connect to the board %s"% (boardip,)
+        try:
+            a = UUT.uut.acq2sh(str(arg))
+        except:
+            print "could not send %s to the board" %(str(arg),)
+            return 0
+        print "%s  %s -> %s"%(boardip, arg, a,)
+        return 1
+    ACQ2SH=acq2sh
+
+    def getnumsamples(self, arg):
+        self.acqcmd('getNumSamples')
+        return 1
+    GETNUMSAMPLES=getnumsamples
+    
+    def getstate(self, arg):
+        self.acqcmd('getState')
+        return 1
+    GETSTATE=getstate
