e4d2ce9604aed54b69d5e8392422103589e75c4b
Thomas W. Fredian <twf@psfc.mit.edu>
2012-02-17 13:30:56 +0000

    clean up after build

--- devscripts/pkg_rpms.py
+++ devscripts/pkg_rpms.py
@@ -136,19 +136,19 @@ def makeRpmsCommand(args):
                 for line in c:
                     print line
                 print "================================="
-            else:
-                try:
-                    rpmfile="%s/RPMS/x86_64/mdsplus%s-%s-%s-%s.%s.x86_64.rpm" % (WORKSPACE,rpmflavor,pkg,VERSION,updates[pkg]['Release'],DIST)
-                    os.stat(rpmfile)
-                except Exception,e:
-                    print "%s missing. Rebuilding." % (rpmfile,)
-                    updates[pkg]['Update']=True
-                try:
-                    rpmfile="%s/RPMS/i686/mdsplus%s-%s-%s-%s.%s.i686.rpm" % (WORKSPACE,rpmflavor,pkg,VERSION,updates[pkg]['Release'],DIST)
-                    os.stat(rpmfile)
-                except Exception,e:
-                    print "%s missing. Rebuilding." % (rpmfile,)
-                    updates[pkg]['Update']=True
+#            else:
+#                try:
+#                    rpmfile="%s/RPMS/x86_64/mdsplus%s-%s-%s-%s.%s.x86_64.rpm" % (WORKSPACE,rpmflavor,pkg,VERSION,updates[pkg]['Release'],DIST)
+#                    os.stat(rpmfile)
+#                except Exception,e:
+#                    print "%s missing. Rebuilding." % (rpmfile,)
+#                    updates[pkg]['Update']=True
+#                try:
+#                    rpmfile="%s/RPMS/i686/mdsplus%s-%s-%s-%s.%s.i686.rpm" % (WORKSPACE,rpmflavor,pkg,VERSION,updates[pkg]['Release'],DIST)
+#                    os.stat(rpmfile)
+#                except Exception,e:
+#                    print "%s missing. Rebuilding." % (rpmfile,)
+#                    updates[pkg]['Update']=True
                 
         if updates[pkg]['Update']:
             need_to_build=True
@@ -232,4 +232,10 @@ def makeRpmsCommand(args):
     if status in ('ok','skip'):
         
         p=subprocess.Popen('rsync -a RPMS %s;rsync -a SOURCES %s;rsync -a EGGS %s' % (DISTPATH,DISTPATH,DISTPATH,DISTPATH),shell=True,cwd=WORKSPACE)
-        sys.exit(p.wait())
+        pstat=p.wait()
+        if pstat != 0:
+	  print "Error copying files to final destination. Does the directory %s exist and is it writable by the account used by this hudson node?" % (DISTPATH,)
+        else:
+          p=subprocess.Popen('rm -Rf RPMS SOURCES EGGS',shell=True,cwd=WORKSPACE)
+          pstat=p.wait()
+        sys.exit(pstat)
