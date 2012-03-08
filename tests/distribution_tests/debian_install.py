7c31c4cfea0fdb198622078b8e9938f123921680
Thomas W. Fredian <twf@psfc.mit.edu>
2012-03-08 20:22:19 +0000

    Add distribution tests

---
+++ tests/distribution_tests/debian_install.py
@@ -0,0 +1,39 @@
+import subprocess
+from pkg_utils import getPackages
+def debian_install(WORKSPACE,FLAVOR):
+  print "Testing package installation"
+  p=subprocess.Popen('sudo /usr/bin/apt-get autoremove -y "mdsplus*"',stdout=subprocess.PIPE,shell=True)
+  p.wait()
+  p=subprocess.Popen('echo "deb http://www.mdsplus.org/dist/Ubuntu11/repo MDSplus %s" >> /etc/apt/sources.list.d/mdsplus.list;' % (FLAVOR,) +\
+   'sudo /usr/bin/apt-get update 2>&1',stdout=subprocess.PIPE,shell=True)
+  if p.wait() != 0:
+    print p.stdout.read()
+    print "Error doing apt-get update command"
+    sys.exit(1)
+  else:
+    print "Successfully did apt-get update command"
+  if FLAVOR=='stable':
+	flav=""
+  else:
+        flav="-"+FLAVOR
+  pkgs=getPackages()
+  pkgs.append('mdsplus')
+  for pkg in pkgs:
+    if pkg == 'mdsplus':
+      package='mdsplus%s' % (flav,)
+    else:
+      package='mdsplus%s-%s' % (flav,pkg)
+    p=subprocess.Popen('sudo /usr/bin/apt-get install -y %s' % (package),stdout=subprocess.PIPE,shell=True)
+    if p.wait() != 0:
+      print p.stdout.read()
+      print "Error installing package %s" % (package,)
+      sys.exit(1)
+    else:
+      print "Successfully installed package %s" % (package,)
+    p=subprocess.Popen('sudo /usr/bin/apt-get autoremove -y %s' % (package,),stdout=subprocess.PIPE.shell=True)
+    if p.wait() != 0:
+      print p.stdout.read()
+      print "Error removing package %s" % (package,)
+      sys.exit(1)
+    else:
+      print "Successfully removed package %s" % (package,)
---
+++ tests/distribution_tests/test_all.py
@@ -0,0 +1,3 @@
+def test_debian(WORKSPACE,FLAVOR):
+  from debian_install import debian_install
+  debian_install(WORKSPACE,FLAVOR)
