f7b3f4df7478ee6e1431ea486fa3bd2f087b6ccc
Thomas W. Fredian <twf@psfc.mit.edu>
2012-03-20 18:11:23 +0000

    Add more tests

--- tests/distribution_tests/debian_install.py
+++ tests/distribution_tests/debian_install.py
@@ -1,5 +1,40 @@
 import subprocess
 from pkg_utils import getPackages
+
+def debian_install(pkg,FLAVOR):
+  if FLAVOR=='stable':
+	flav=""
+  else:
+        flav="-"+FLAVOR
+  if pkg == 'mdsplus':
+    package='mdsplus%s' % (flav,)
+  else:
+    package='mdsplus%s-%s' % (flav,pkg)
+  p=subprocess.Popen('sudo /usr/bin/apt-get install -y %s' % (package),stdout=subprocess.PIPE,shell=True)
+  if p.wait() != 0:
+    print p.stdout.read()
+    print "Error installing package %s" % (package,)
+    sys.exit(1)
+  else:
+    print "Successfully installed package %s" % (package,)
+
+def debian_remove(pkg,FLAVOR):
+  if FLAVOR=='stable':
+	flav=""
+  else:
+        flav="-"+FLAVOR
+  if pkg == 'mdsplus':
+    package='mdsplus%s' % (flav,)
+  else:
+    package='mdsplus%s-%s' % (flav,pkg)
+  p=subprocess.Popen('sudo /usr/bin/apt-get autoremove -y %s' % (package,),stdout=subprocess.PIPE,shell=True)
+  if p.wait() != 0:
+    print p.stdout.read()
+    print "Error removing package %s" % (package,)
+    sys.exit(1)
+  else:
+    print "Successfully removed package %s" % (package,)
+
 def debian_install_tests(WORKSPACE,FLAVOR):
   print "Testing package installation"
   p=subprocess.Popen('sudo /usr/bin/apt-get autoremove -y "mdsplus*"',stdout=subprocess.PIPE,shell=True)
@@ -11,28 +46,8 @@ def debian_install_tests(WORKSPACE,FLAVOR):
     sys.exit(1)
   else:
     print "Successfully did apt-get update command"
-  if FLAVOR=='stable':
-	flav=""
-  else:
-        flav="-"+FLAVOR
   pkgs=getPackages()
   pkgs.append('mdsplus')
   for pkg in pkgs:
-    if pkg == 'mdsplus':
-      package='mdsplus%s' % (flav,)
-    else:
-      package='mdsplus%s-%s' % (flav,pkg)
-    p=subprocess.Popen('sudo /usr/bin/apt-get install -y %s' % (package),stdout=subprocess.PIPE,shell=True)
-    if p.wait() != 0:
-      print p.stdout.read()
-      print "Error installing package %s" % (package,)
-      sys.exit(1)
-    else:
-      print "Successfully installed package %s" % (package,)
-    p=subprocess.Popen('sudo /usr/bin/apt-get autoremove -y %s' % (package,),stdout=subprocess.PIPE,shell=True)
-    if p.wait() != 0:
-      print p.stdout.read()
-      print "Error removing package %s" % (package,)
-      sys.exit(1)
-    else:
-      print "Successfully removed package %s" % (package,)
+    debian_install(pkg,FLAVOR)
+    debian_remove(pkg,FLAVOR)
--- tests/distribution_tests/test_all.py
+++ tests/distribution_tests/test_all.py
@@ -1,3 +1,21 @@
+import sys
+
+def python_test():
+  import MDSplus
+  import sys
+  sys.path.insert(0,MDSplus.__path__[0])
+  from unittest import TextTestRunner
+  from MDSplus.tests import test_all
+  t=TextTestRunner()
+  result=t.run(test_all())
+  return result.wasSuccessful()
+
 def test_debian(WORKSPACE,FLAVOR):
-  from debian_install import debian_install_tests
+  from debian_install import debian_install_tests,debian_install,debian_remove
   debian_install_tests(WORKSPACE,FLAVOR)
+  debian_install('python',FLAVOR)
+  ok=python_test()
+  debian_remove('python',FLAVOR)
+  if not ok:
+    sys.exit(1)
+ 
