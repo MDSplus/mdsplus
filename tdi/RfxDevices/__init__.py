d5b1dd8b3a8293d9669eaef712061c49f0fef885
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2012-04-03 15:38:43 +0000

    Added MARTe devices

--- tdi/RfxDevices/__init__.py
+++ tdi/RfxDevices/__init__.py
@@ -15,11 +15,11 @@ from DEMOADC import DEMOADC
 from DIO4 import DIO4
 from SIS3820 import SIS3820
 from FEMTO import FEMTO
+from MARTE_GENERIC import MARTE_GENERIC
 from MARTE import MARTE
+from MARTE_MHD_BT import MARTE_MHD_BT
 from MARTE_MHD_BR import MARTE_MHD_BR
 from MARTE_MHD_I import MARTE_MHD_I
-from MARTE_MHD_BT import MARTE_MHD_BT
-from MARTE_MHD_CTRL import MARTE_MHD_CTRL
 from SPIDER_SM import SPIDER_SM
 from ZELOS2150GV import ZELOS2150GV
 from NI6259AI import NI6259AI
