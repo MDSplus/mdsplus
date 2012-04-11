4f1dd4b8dde2a48942080f316c1e59cdee5c5f86
Thomas W. Fredian <twf@psfc.mit.edu>
2012-04-11 17:49:03 +0000

    Fix install

--- devscripts/pkg_solaris.py
+++ devscripts/pkg_solaris.py
@@ -134,8 +134,8 @@ def makeSolarisPkgsCommand(args):
         cmd=cmd+'--exec-prefix=%s --prefix=%s CFLAGS="-m64" FFLAGS="-m64";' % (prefix64,prefix64)
         cmd=cmd+'if (! make ); then exit 1; fi; if (! make install); then exit 1; fi;'
         cmd=cmd+'mv %s/lib %s/lib64; mv %s/bin %s/bin64;' % (prefix64,prefix64,prefix64,prefix64)
-        cmd=cmd+'rsync -a %s/lib %s/lib32;' % (prefix32,prefix64)
-        cmd=cmd+'rsync -a %s/bin %s/bin64;' % (prefix32,prefix64)
+        cmd=cmd+'rsync -a %s/lib/* %s/lib32;' % (prefix32,prefix64)
+        cmd=cmd+'rsync -a %s/bin/* %s/bin32;' % (prefix32,prefix64)
         cmd=cmd+'rsync -a %s/java %s/;' % (prefix32,prefix64)
         sys.stdout.flush()
         p=subprocess.Popen(cmd,shell=True,cwd=os.getcwd())
