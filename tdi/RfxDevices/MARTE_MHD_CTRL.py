c7981e955b495b8f51b904eda43b7b4b16afa9a3
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2012-04-20 07:39:38 +0000

    Fix MARTe MHD_CTRL device

--- tdi/RfxDevices/MARTE_MHD_CTRL.py
+++ tdi/RfxDevices/MARTE_MHD_CTRL.py
@@ -6,48 +6,61 @@ import MARTE_GENERIC
 class MARTE_MHD_CTRL(MARTE_GENERIC.MARTE_GENERIC):
     print 'MARTe MhdCtrl'
     parNames = ['cleaningEnable', 'extrapolationEnable', 'reconfigurationEnable', 'dynamicDecouplerEnable', 'staticDecouplerEnable', 'applyCleaning', 'extrapolationRadius', 
-    'rgM_1','rgM_2','rgM_3','rgM_4','rgM_5','rgM_6','rgM_7','rgM_8', 
-    'rgN_1','rgN_2','rgN_3','rgN_4','rgN_5','rgN_6','rgN_7','rgN_8',
-    'rgFreq_1','rgFreq_2','rgFreq_3','rgFreq_4','rgFreq_5','rgFreq_6','rgFreq_7','rgFreq_8',
-    'rgAmpl_1','rgAmpl_2','rgAmpl_3','rgAmpl_4','rgAmpl_5','rgAmpl_6','rgAmpl_7','rgAmpl_8',
-    'rgPhase_1','rgPhase_2','rgPhase_3','rgPhase_4','rgPhase_5','rgPhase_6','rgPhase_7','rgPhase_8',
-    'rgTStart_1','rgTStart_2','rgTStart_3','rgTStart_4','rgTStart_5','rgTStart_6','rgTStart_7','rgTStart_8',
-    'rgRiseTime_1','rgRiseTime_2','rgRiseTime_3','rgRiseTime_4','rgRiseTime_5','rgRiseTime_6','rgRiseTime_7','rgRiseTime_8',
-    'rgTEnd_1','rgTEnd_2','rgTEnd_3','rgTEnd_4','rgTEnd_5','rgTEnd_6','rgTEnd_7','rgTEnd_8',
-    'rgFallTime_1','rgFallTime_2','rgFallTime_3','rgFallTime_4','rgFallTime_5','rgFallTime_6','rgFallTime_7','rgFallTime_8',
-    'rgThreshold_1','rgThreshold_2','rgThreshold_3','rgThreshold_4','rgThreshold_5','rgThreshold_6','rgThreshold_7','rgThreshold_8',
-    'rgIsRelative_1','rgIsRelative_2','rgIsRelative_3','rgIsRelative_4','rgIsRelative_5','rgIsRelative_6','rgIsRelative_7','rgIsRelative_8',
-    'ffgM_1','ffgM_2','ffgM_3','ffgM_4','ffgM_5','ffgM_6','ffgM_7','ffgM_8',
-    'ffgN_1','ffgN_2','ffgN_3','ffgN_4','ffgN_5','ffgN_6','ffgN_7','ffgN_8',
-    'ffgFreq_1','ffgFreq_2','ffgFreq_3','ffgFreq_4','ffgFreq_5','ffgFreq_6','ffgFreq_7','ffgFreq_8',
-    'ffgAmpl_1','ffgAmpl_2','ffgAmpl_3','ffgAmpl_4','ffgAmpl_5','ffgAmpl_6','ffgAmpl_7','ffgAmpl_8',
-    'ffgPhase_1','ffgPhase_2','ffgPhase_3','ffgPhase_4','ffgPhase_5','ffgPhase_6','ffgPhase_7','ffgPhase_8',
-    'ffgTStart_1','ffgTStart_2','ffgTStart_3','ffgTStart_4','ffgTStart_5','ffgTStart_6','ffgTStart_7','ffgTStart_8',
-    'ffgRiseTime_1','ffgRiseTime_2','ffgRiseTime_3','ffgRiseTime_4','ffgRiseTime_5','ffgRiseTime_6','ffgRiseTime_7','ffgRiseTime_8',
-    'ffgTEnd_1','ffgTEnd_2','ffgTEnd_3','ffgTEnd_4','ffgTEnd_5','ffgTEnd_6','ffgTEnd_7','ffgTEnd_8',
-    'ffgFallTime_1','ffgFallTime_2','ffgFallTime_3','ffgFallTime_4','ffgFallTime_5','ffgFallTime_6','ffgFallTime_7','ffgFallTime_8',
-    'ffgThreshold_1','ffgThreshold_2','ffgThreshold_3','ffgThreshold_4','ffgThreshold_5','ffgThreshold_6','ffgThreshold_7','ffgThreshold_8',
-    'ffgIsRelative_1','ffgIsRelative_2','ffgIsRelative_3','ffgIsRelative_4','ffgIsRelative_5','ffgIsRelative_6','ffgIsRelative_7','ffgIsRelative_8',
-    'horFieldControlEnable_1','horFieldControlEnable_2','horFieldControlEnable_3','horFieldControlEnable_4','horFieldControlEnable_5','horFieldControlEnable_6','horFieldControlEnable_7','horFieldControlEnable_8',
-    'kpAmplitudes_1','kpAmplitudes_2','kpAmplitudes_3','kpAmplitudes_4','kpAmplitudes_5','kpAmplitudes_6','kpAmplitudes_7','kpAmplitudes_8',
-    'kpPhases_1','kpPhases_2','kpPhases_3','kpPhases_4','kpPhases_5','kpPhases_6','kpPhases_7','kpPhases_8',
-    'ki_1','ki_2','ki_3','ki_4','ki_5','ki_6','ki_7','ki_8',
-    'kd_1','kd_2','kd_3','kd_4','kd_5','kd_6','kd_7','kd_8',
-    'tStart_1','tStart_2','tStart_3','tStart_4','tStart_5','tStart_6','tStart_7','tStart_8',
-    'tEnd_1','tEnd_2','tEnd_3','tEnd_4','tEnd_5','tEnd_6','tEnd_7','tEnd_8',
-    'cutoffFreq_1','cutoffFreq_2','cutoffFreq_3','cutoffFreq_4','cutoffFreq_5','cutoffFreq_6','cutoffFreq_7','cutoffFreq_8',
-    'vsKpAmplitudes_1','vsKpAmplitudes_2','vsKpAmplitudes_3','vsKpAmplitudes_4','vsKpAmplitudes_5','vsKpAmplitudes_6','vsKpAmplitudes_7','vsKpAmplitudes_8',
-    'vsKpPhases_1','vsKpPhases_2','vsKpPhases_3','vsKpPhases_4','vsKpPhases_5','vsKpPhases_6','vsKpPhases_7','vsKpPhases_8',
-    'vsKp_1','vsKp_2','vsKp_3','vsKp_4','vsKp_5','vsKp_6','vsKp_7','vsKp_8',
-    'vsKi_1','vsKi_2','vsKi_3','vsKi_4','vsKi_5','vsKi_6','vsKi_7','vsKi_8',
-    'vsKd_1','vsKd_2','vsKd_3','vsKd_4','vsKd_5','vsKd_6','vsKd_7','vsKd_8',
-    'vsTStart_1','vsTStart_2','vsTStart_3','vsTStart_4','vsTStart_5','vsTStart_6','vsTStart_7','vsTStart_8',
-    'vsTEnd_1','vsTEnd_2','vsTEnd_3','vsTEnd_4','vsTEnd_5','vsTEnd_6','vsTEnd_7','vsTEnd_8',
-    'vsCutoffFreq_1','vsCutoffFreq_2','vsCutoffFreq_3','vsCutoffFreq_4','vsCutoffFreq_5','vsCutoffFreq_6','vsCutoffFreq_7','vsCutoffFreq_8',
+    'rgM_1', 'rgN_1', 'rgFreq_1', 'rgAmpl_1','rgPhase_1','rgTStart_1','rgRiseTime_1','rgTEnd_1','rgFallTime_1','rgThreshold_1','rgIsRelative_1',
+    'rgM_2', 'rgN_2', 'rgFreq_2', 'rgAmpl_2','rgPhase_2','rgTStart_2','rgRiseTime_2','rgTEnd_2','rgFallTime_2','rgThreshold_2','rgIsRelative_2',
+    'rgM_3', 'rgN_3', 'rgFreq_3', 'rgAmpl_3','rgPhase_3','rgTStart_3','rgRiseTime_3','rgTEnd_3','rgFallTime_3','rgThreshold_3','rgIsRelative_3',
+    'rgM_4', 'rgN_4', 'rgFreq_4', 'rgAmpl_4','rgPhase_4','rgTStart_4','rgRiseTime_4','rgTEnd_4','rgFallTime_4','rgThreshold_4','rgIsRelative_4',
+    'rgM_5', 'rgN_5', 'rgFreq_5', 'rgAmpl_5','rgPhase_5','rgTStart_5','rgRiseTime_5','rgTEnd_5','rgFallTime_5','rgThreshold_5','rgIsRelative_5',
+    'rgM_6', 'rgN_6', 'rgFreq_6', 'rgAmpl_6','rgPhase_6','rgTStart_6','rgRiseTime_6','rgTEnd_6','rgFallTime_6','rgThreshold_6','rgIsRelative_6',
+    'rgM_7', 'rgN_7', 'rgFreq_7', 'rgAmpl_7','rgPhase_7','rgTStart_7','rgRiseTime_7','rgTEnd_7','rgFallTime_7','rgThreshold_7','rgIsRelative_7',
+    'rgM_8', 'rgN_8', 'rgFreq_8', 'rgAmpl_8','rgPhase_8','rgTStart_8','rgRiseTime_8','rgTEnd_8','rgFallTime_8','rgThreshold_8','rgIsRelative_8',
+    'ffM_1', 'ffN_1', 'ffFreq_1', 'ffAmpl_1','ffPhase_1','ffTStart_1','ffRiseTime_1','ffTEnd_1','ffFallTime_1','ffThreshold_1','ffIsRelative_1',
+    'ffM_2', 'ffN_2', 'ffFreq_2', 'ffAmpl_2','ffPhase_2','ffTStart_2','ffRiseTime_2','ffTEnd_2','ffFallTime_2','ffThreshold_2','ffIsRelative_2',
+    'ffM_3', 'ffN_3', 'ffFreq_3', 'ffAmpl_3','ffPhase_3','ffTStart_3','ffRiseTime_3','ffTEnd_3','ffFallTime_3','ffThreshold_3','ffIsRelative_3',
+    'ffM_4', 'ffN_4', 'ffFreq_4', 'ffAmpl_4','ffPhase_4','ffTStart_4','ffRiseTime_4','ffTEnd_4','ffFallTime_4','ffThreshold_4','ffIsRelative_4',
+    'ffM_5', 'ffN_5', 'ffFreq_5', 'ffAmpl_5','ffPhase_5','ffTStart_5','ffRiseTime_5','ffTEnd_5','ffFallTime_5','ffThreshold_5','ffIsRelative_5',
+    'ffM_6', 'ffN_6', 'ffFreq_6', 'ffAmpl_6','ffPhase_6','ffTStart_6','ffRiseTime_6','ffTEnd_6','ffFallTime_6','ffThreshold_6','ffIsRelative_6',
+    'ffM_7', 'ffN_7', 'ffFreq_7', 'ffAmpl_7','ffPhase_7','ffTStart_7','ffRiseTime_7','ffTEnd_7','ffFallTime_7','ffThreshold_7','ffIsRelative_7',
+    'ffM_8', 'ffN_8', 'ffFreq_8', 'ffAmpl_8','ffPhase_8','ffTStart_8','ffRiseTime_8','ffTEnd_8','ffFallTime_8','ffThreshold_8','ffIsRelative_8',
+    'horFieldControlEnable_1', 'kpAmplitudes_1','kpPhases_1','ki_1','kd_1','tStart_1','tEnd_1','cutoffFreq_1',
+    'horFieldControlEnable_2', 'kpAmplitudes_2','kpPhases_2','ki_2','kd_2','tStart_2','tEnd_2','cutoffFreq_2',
+    'horFieldControlEnable_3', 'kpAmplitudes_3','kpPhases_3','ki_3','kd_3','tStart_3','tEnd_3','cutoffFreq_3',
+    'horFieldControlEnable_4', 'kpAmplitudes_4','kpPhases_4','ki_4','kd_4','tStart_4','tEnd_4','cutoffFreq_4',
+    'horFieldControlEnable_5', 'kpAmplitudes_5','kpPhases_5','ki_5','kd_5','tStart_5','tEnd_5','cutoffFreq_5',
+    'horFieldControlEnable_6', 'kpAmplitudes_6','kpPhases_6','ki_6','kd_6','tStart_6','tEnd_6','cutoffFreq_6',
+    'horFieldControlEnable_7', 'kpAmplitudes_7','kpPhases_7','ki_7','kd_7','tStart_7','tEnd_7','cutoffFreq_7',
+    'horFieldControlEnable_8', 'kpAmplitudes_8','kpPhases_8','ki_8','kd_8','tStart_8','tEnd_8','cutoffFreq_8',
+    'vsKpAmplitudes_1','vsKpPhases_1','vsKi_1','vsKd_1','vsTStart_1','vsTEnd_1','vsCutoffFreq_1',
+    'vsKpAmplitudes_2','vsKpPhases_2','vsKi_2','vsKd_2','vsTStart_2','vsTEnd_2','vsCutoffFreq_2',
+    'vsKpAmplitudes_3','vsKpPhases_3','vsKi_3','vsKd_3','vsTStart_3','vsTEnd_3','vsCutoffFreq_3',
+    'vsKpAmplitudes_4','vsKpPhases_4','vsKi_4','vsKd_4','vsTStart_4','vsTEnd_4','vsCutoffFreq_4',
+    'vsKpAmplitudes_5','vsKpPhases_5','vsKi_5','vsKd_5','vsTStart_5','vsTEnd_5','vsCutoffFreq_5',
+    'vsKpAmplitudes_6','vsKpPhases_6','vsKi_6','vsKd_6','vsTStart_6','vsTEnd_6','vsCutoffFreq_6',
+    'vsKpAmplitudes_7','vsKpPhases_7','vsKi_7','vsKd_7','vsTStart_7','vsTEnd_7','vsCutoffFreq_7',
+    'vsKpAmplitudes_8','vsKpPhases_8','vsKi_8','vsKd_8','vsTStart_8','vsTEnd_8','vsCutoffFreq_8',
     'staticDecoupler']
     parValues = [0,0,0,0,0,Data.compile('zero(192, 0)'), 0]
-    for i in range(len(parNames) - 8):
+    for i in range(88):  #References
+      parValues.append(0)
+    for i in range(88):  #FF References
+      parValues.append(0)
+    for i in range(8):  #Mode Controller
+      parValues.append(0)
       parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(0)
+      parValues.append(0)
+      parValues.append(0)
+    for i in range(8):  #VS Controller
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(Data.compile('zero(192, 0.)'))
+      parValues.append(0)
+      parValues.append(0)
+      parValues.append(0)
     parValues.append(Data.compile('diagonal(zero(192,0.)+1)'))
     parts = []
     for i in range(len(MARTE_GENERIC.MARTE_GENERIC.parts)):
@@ -55,17 +68,12 @@ class MARTE_MHD_CTRL(MARTE_GENERIC.MARTE_GENERIC):
     parts.append({'path':'.PARAMS', 'type':'structure'})
     parts.append({'path':'.PARAMS:NUM_ACTIVE', 'type':'numeric', 'value':len(parNames)})
     for i in range(len(parNames)):
-      print 'MARTe mhd_bt', i, 1
+#      print 'MARTe mhd_bt', i, 1, parNames[i]
       parts.append({'path':'.PARAMS:PAR_%03d'%(i+1), 'type':'structure'})
-      print 'MARTe mhd_bt', i, 2
       parts.append({'path':'.PARAMS:PAR_%03d:DESCRIPTION'%(i+1), 'type':'text'})
-      print 'MARTe mhd_bt', i, 3
       parts.append({'path':'.PARAMS:PAR_%03d:NAME'%(i+1), 'type':'text', 'value':parNames[i]})
-      print 'MARTe mhd_bt', i, 4
       parts.append({'path':'.PARAMS:PAR_%03d:TYPE'%(i+1), 'type':'text'})
-      print 'MARTe mhd_bt', i, 5
       parts.append({'path':'.PARAMS:PAR_%03d:DIMS'%(i+1), 'type':'numeric'})
-      print 'MARTe mhd_bt', i, 6
       parts.append({'path':'.PARAMS:PAR_%03d:DATA'%(i+1), 'type':'numeric','value':parValues[i]})
 
     for i in range(len(parNames), 512):
