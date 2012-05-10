e397f56b97a82c426fb0abdd8ba297173106d0d0
Thomas W. Fredian <twf@psfc.mit.edu>
2012-05-10 20:11:48 +0000

    Fix when PARAMS not defined

--- tdic/tdic.c
+++ tdic/tdic.c
@@ -29,6 +29,9 @@ extern int TdiExecute();
 static void *READhandle = NULL;
 #ifdef DYNreadline
 #include <dlfcn.h>
+#ifndef PARAMS
+#define PARAMS(gub) gub
+#endif
 void (*Busing_history) PARAMS((void)) = NULL;
 int (*Bread_history) PARAMS((const char *)) = NULL;
 int (*Bwrite_history) PARAMS((const char *)) = NULL;
