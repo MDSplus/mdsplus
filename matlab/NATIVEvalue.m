21ea9f708687e992851e467912bb09b6881f7cfa
Joshua Stillerman <jas@psfc.mit.edu>
2012-09-18 15:44:19 +0000

    fix 1d array case (found on mac)

--- matlab/NATIVEvalue.m
+++ matlab/NATIVEvalue.m
@@ -1,8 +1,8 @@
 function  result = NATIVEvalue( mdsthing )
 %NATIVEvalue - returns a Native Matlab object from an MDSplus object
-%   
+%
 % This Function takes an argument that is one of native scalar, native
-% array, mdsplus scalar, or mdsplus array.  It returns a Native matlab 
+% array, mdsplus scalar, or mdsplus array.  It returns a Native matlab
 % object of the corresponding type and shape.
 %
 % syntax:
@@ -13,25 +13,30 @@ if  isa(mdsthing, 'MDSplus.Data') == 0
     result = mdsthing;
 else
     if isa (mdsthing, 'MDSplus.Array')
+       shape = mdsthing.getShape';
+       sz = size(shape);
+       if length(sz) == 2 && sz(1) == 1 && sz(2) == 1
+           shape = [1,shape];
+       end
         switch class(mdsthing)
             case 'MDSplus.Int64Array'
                 fprintf('it is an int64 array\n');
-                result = reshape(mdsthing.getLongArray, mdsthing.getShape');
+                result = reshape(mdsthing.getLongArray, shape);
             case 'MDSplus.Int32Array'
                 fprintf('it is an int32 array\n');
-                result = reshape(mdsthing.getIntArray, mdsthing.getShape');
+                result = reshape(mdsthing.getIntArray, shape);
             case 'MDSplus.Int16Array'
                 fprintf('itis an int16 Array\n');
-                result = reshape(mdsthing.getShortArray, mdsthing.getShape');
+                result = reshape(mdsthing.getShortArray, shape);
             case 'MDSplus.Int8Array'
                 fprintf('itis an int8 Array\n');
-                result = reshape(mdsthing.getByteArray, mdsthing.getShape');
+                result = reshape(mdsthing.getByteArray, shape);
             case 'MDSplus.Float64Array'
                 fprintf('it is a float64array\n');
-                result = reshape(mdsthing.getDoubleArray,  mdsthing.getShape');
+                result = reshape(mdsthing.getDoubleArray,  shape);
             case 'MDSplus.Float32Array'
                 fprintf('it is a float32array\n');
-                result = reshape(mdsthing.getFloatArray,  mdsthing.getShape');
+                result = reshape(mdsthing.getFloatArray,  shape);
             otherwise
                 throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
         end
@@ -61,10 +66,9 @@ else
             otherwise
                 throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
         end
-        
+
     else
         throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
-        
+
     end
 end
-
