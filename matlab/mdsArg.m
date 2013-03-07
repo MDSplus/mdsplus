13d38d35611e5f2a31c0630ad1e57baf11c14f13
Joshua Stillerman <jas@psfc.mit.edu>
2013-03-07 15:19:16 +0000

    a different spelling (case) as per request

---
+++ matlab/mdsArg.m
@@ -0,0 +1,52 @@
+function  result = mdsArg( thing )
+%mdsArg - returns an object that can be passed to MDSplus.putData
+%
+% This function returns an MDSplus object representation of its input
+% argument.  This way MDSplus.node.putData  can operate on and preserve
+% the type and shape of its input arguments.
+%
+% syntax:
+%   node.putData(mdsArg(matlab-native-object))
+%
+import MDSplus.*;
+if size(thing) == [1,1]
+    switch class(thing)
+        case 'double'
+            result = Float64(thing);
+        case 'single'
+            result = Float32(thing);
+        case 'int64'
+            result = Int64(thing);
+        case 'int32'
+            result = Int32(thing);
+        case 'int16'
+            result = Int16(thing);
+        case 'int8'
+            result = Int8(thing);
+        case 'char'
+            result = String(thing);
+        otherwise
+            result = thing;
+    end
+else
+    sz = size(thing);
+    switch class(thing)
+        case 'double'
+            result = Float64Array(reshape(thing,[],1),sz(sz > 1));
+        case 'single'
+            result = Float32Array(reshape(thing,[],1),sz(sz > 1));
+        case 'int64'
+            result = Int64Array(reshape(thing,[],1),sz(sz > 1));
+        case 'int32'
+             result = Int32Array(reshape(thing,[],1),sz(sz > 1));
+        case 'int16'
+            result = Int16Array(reshape(thing,[],1),sz(sz > 1));
+        case 'int8'
+            result = Int8Array(reshape(thing,[],1),sz(sz > 1));
+        case 'char'
+            result = String(thing);
+        otherwise
+            result = thing;
+    end
+end
+
---
+++ matlab/nativeValue.m
@@ -0,0 +1,63 @@
+function  result = nativeValue( mdsthing )
+%nativeValue - returns a Native Matlab object from an MDSplus object
+%   
+% This Function takes an argument that is one of native scalar, native
+% array, mdsplus scalar, or mdsplus array.  It returns a Native matlab 
+% object of the corresponding type and shape.
+%
+% syntax:
+%    answer = nativeValue(mdsnode.getData)
+%
+import MDSplus.*;
+if  isa(mdsthing, 'MDSplus.Data') == 0
+    result = mdsthing;
+else
+    if isa (mdsthing, 'MDSplus.Array')
+        shape = mdsthing.getShape;
+        if numel(shape) == 1
+            shape = [1, shape];
+        else
+            shape = shape';
+        end
+            
+        switch class(mdsthing)
+            case 'MDSplus.Int64Array'
+                result = reshape(mdsthing.getLongArray, shape);
+            case 'MDSplus.Int32Array'
+                result = reshape(mdsthing.getIntArray, shape);
+            case 'MDSplus.Int16Array'
+                result = reshape(mdsthing.getShortArray, shape);
+            case 'MDSplus.Int8Array'
+                result = reshape(mdsthing.getByteArray, shape);
+            case 'MDSplus.Float64Array'
+                result = reshape(mdsthing.getDoubleArray, shape);
+            case 'MDSplus.Float32Array'
+                result = reshape(mdsthing.getFloatArray,  shape);
+            otherwise
+                throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
+        end
+    elseif isa (mdsthing, 'MDSplus.Scalar')
+        switch class(mdsthing)
+            case 'MDSplus.Int64'
+                result = mdsthing.getLong;
+            case 'MDSplus.Int32'
+                result = mdsthing.getInt;
+            case 'MDSplus.Int16'
+                result = mdsthing.getShort;
+            case 'MDSplus.Int8'
+                result = mdsthing.getByte;
+            case 'MDSplus.Float64'
+                result = mdsthing.getDouble;
+            case 'MDSplus.Float32'
+                result = mdsthing.getFloat;
+            case 'MDSplus.String'
+                result = mdsthing.getString;
+            otherwise
+                throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
+        end
+        
+    else
+        throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
+        
+    end
+end
