519b2f3e40fec881ef667d67370192ad5f681e91
Thomas W. Fredian <twf@psfc.mit.edu>
2013-03-28 19:19:01 +0000

    Handle bounded compressed arrays on 64-bit platforms correctly (very subtle)

--- tdishr/TdiGetData.c
+++ tdishr/TdiGetData.c
@@ -79,8 +79,9 @@ int	in_size, out_size, dimct, status = 1;
 			/*******************************
 			For CA it is a relative pointer.
 			*******************************/
-			if (in_ptr->class == CLASS_CA) 
-                          arr.a0 = pout->pointer + ((char *)arr.a0 - (char *)0);
+			if (in_ptr->class == CLASS_CA) { 
+                          arr.a0 = pout->pointer + (((char *)arr.a0 - (char *)0) & 0xffff);
+                        }
 			else arr.a0 = pout->pointer + (arr.a0 - arr.pointer);
 		}
 		arr.pointer = pout->pointer;
