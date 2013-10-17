bd1592c81214d2f975d0ac1034f63ec0f86cd0a5
Thomas W. Fredian <twf@psfc.mit.edu>
2013-10-17 17:19:57 +0000

    Do not use mdsplus_sudo script

--- devscripts/pkg_macosx.py
+++ devscripts/pkg_macosx.py
@@ -47,13 +47,13 @@ def makeMacosxPkgCommand(args):
 	     './configure --enable-mdsip_connections --prefix=%s/build/mdsplus --exec-prefix=%s/build/mdsplus --with-idl=/Applications/itt/idl/idl' % (WORKSPACE,WORKSPACE,),
              "configure failed")
        shell(mdsplusdir,'rm -f lib/*.a; make clean; make','make failed')
-       shell(mdsplusdir,'sudo /usr/bin/mdsplus_sudo remove ../build','remove build failed')
+       shell(mdsplusdir,'sudo rm -Rf ../build','remove build failed')
        shell(mdsplusdir,'env MDSPLUS_VERSION="%s%s.%d" make install' % (pyflavor,VERSION,release),'make install failed')
        shell(mdsplusdir,'cd ../build/mdsplus/mdsobjects/python; python setup.py bdist_egg version=%s%s.%d' % (pyflavor,VERSION,release),'python bdist_egg failed')
        shell(mdsplusdir,'rsync -a ../build/mdsplus/mdsobjects/python/dist/*.egg %s/%s/EGGS/' % (args[2],FLAVOR),'Failed to copy eggs to destination')
-       shell(mdsplusdir,'sudo /usr/bin/mdsplus_sudo chown ../build','chown failed')
+       shell(mdsplusdir,'sudo chown -R root:admin ../build','chown failed')
        shell(mdsplusdir,'rm -f %s; /Developer/usr/bin/packagemaker --title "MDSplus%s" --version "%s.%d" --scripts %s/build/mdsplus/scripts --install-to "/usr/local" --target "10.5" -r %s/build -v -i "MDSplus%s" -o %s' % (pkgfile,pkgflavor,VERSION,release,WORKSPACE,WORKSPACE,pkgflavor,pkgfile),'Building package failed')
-       shell(mdsplusdir,'sudo /usr/bin/mdsplus_sudo remove ../build','remove build failed')
+       shell(mdsplusdir,'sudo rm -Rf ../build','remove build failed')
        print "%s, Setup kit build completed" % (str(datetime.datetime.now()))
        if need_to_tag:
             print "Tag all modules for this release. This can take a while!"
