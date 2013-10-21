de396b1c18cdbb42ab0a9285f88bfa2763ff0529
Thomas W. Fredian <twf@psfc.mit.edu>
2013-10-21 13:20:27 +0000

    Fix use of stdint and size of pointer on windows

--- include/mdstypes.h
+++ include/mdstypes.h
@@ -10,10 +10,14 @@
 typedef int64_t _int64;
 typedef uint64_t _int64u;
 #else
+#ifdef HAVE_WINDOWS_H
+#include <msc_stdint.h>
+#else
 
 #ifdef HAVE_VXWORKS_H
 typedef long long _int64;
 typedef unsigned long long _int64u;
+typedef int intptr_t
 #else
 
 #if (SIZEOF__INT64 != 8)
@@ -28,6 +32,8 @@ typedef unsigned long long _int64u;
 typedef unsigned _int64 _int64u;
 #endif
 #endif
+typedef  _int64 int64_t
+typedef  _int64u uint64_t
 #endif /*vxWorks*/
-
+#endif
 #endif
