ab863d314fea58afd6361d176d8ef34cfc783988
Thomas W. Fredian <twf@psfc.mit.edu>
2013-10-23 20:02:55 +0000

    Use treeput instead of remote mdsput

--- matlab/mdsput.m
+++ matlab/mdsput.m
@@ -2,7 +2,7 @@ function [ status ] = mdsput( node, expression, varargin)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-   expr = strcat('mdsput($, $', repmat(',$', 1,size(varargin, 2)),')');
+   expr = strcat('treeput($, $', repmat(',$', 1,size(varargin, 2)),')');
    status = mdsvalue(expr, node, expression, varargin{:});
 
 end
