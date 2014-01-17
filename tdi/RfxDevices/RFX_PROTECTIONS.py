0f8b2578fbae9e7d20f2fd10a6fb9cd845f4b1af
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2014-01-17 10:35:10 +0000

    Add I2t F coils Limits

--- tdi/RfxDevices/RFX_PROTECTIONS.py
+++ tdi/RfxDevices/RFX_PROTECTIONS.py
@@ -14,5 +14,15 @@ class RFX_PROTECTIONS(Device):
       {'path':':MAX_BR_TIM_F', 'type':'numeric'},
       {'path':':MAX_PR_CURR', 'type':'numeric'},
       {'path':':MAX_PR_I2T', 'type':'numeric'},
-      {'path':':MAX_TOR_I2T', 'type':'numeric'}]
+      {'path':':MAX_TOR_I2T', 'type':'numeric'},
+
+      {'path':'.FCOIL_R_I2T', 'type':'structure'},
+      {'path':'.FCOIL_R_I2T:I2T_R1', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R2', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R3', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R4', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R5', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R6', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R7', 'type':'numeric'},
+      {'path':'.FCOIL_R_I2T:I2T_R8', 'type':'numeric'}]
     
