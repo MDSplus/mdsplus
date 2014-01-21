1eb934aac11326bc66c6786b5810001346a3a804
Joshua Stillerman <jas@psfc.mit.edu>
2014-01-21 21:35:21 +0000

    make the expression optional.  So mdsput('node', variable) will work and be the equivalent to mdsput('node', '$', variable)

--- matlab/mdsput.m
+++ matlab/mdsput.m
@@ -1,9 +1,11 @@
 function [ status ] = mdsput( node, expression, varargin)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
-   expr = strcat('treeput($, $', repmat(',$', 1,size(varargin, 2)),')');
-   status = mdsvalue(expr, node, expression, varargin{:});
-
+   if nargin == 2
+     status = mdsput(node, '$', expression)
+   else
+     expr = strcat('treeput($, $', repmat(',$', 1,size(varargin, 2)),')');
+     status = mdsvalue(expr, node, expression, varargin{:});
+   end
 end
 
