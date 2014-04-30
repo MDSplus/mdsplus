1e61c006913f90622224bc055af003864bab1da4
Keith Erickson <kerickso@pppl.gov>
2014-04-30 00:40:44 +0000

    Amplify MdsTimeout interface to use timespecs natively internally.  Change naming convention to match the internal c struct.  Add a get function to differentiate between the underlying duration and the timepoint of epoch + duration.

--- include/Timeout.h
+++ include/Timeout.h
@@ -1,24 +1,47 @@
 #ifndef TIMEOUT_H_
 #define TIMEOUT_H_
 #include "SystemSpecific.h"
+
+#include <time.h>
+
 class EXPORT MdsTimeout
 {
-	long secs;
-	long nanoSecs;
 public:
-	MdsTimeout(long secs, long nanoSecs)
-	{
-		this->secs = secs + nanoSecs / 1000000000;	
-		this->nanoSecs = nanoSecs % 1000000000;
+	MdsTimeout(long sec, long nSec) {
+		set(sec + nSec / 1000000000, nSec % 1000000000);
 	}
-	MdsTimeout(long milliSecs)
-	{
-		this->secs = milliSecs / 1000;	
-		this->nanoSecs = (milliSecs % 1000) * 1000000;
+
+	MdsTimeout(long milliSecs) {
+		set(milliSecs / 1000, (milliSecs % 1000) * 1000000);
 	}
-	long getSecs(){return secs;}	
-	long getNanoSecs(){return nanoSecs;}
-	long getTotMilliSecs(){return secs * 1000 + nanoSecs / 1000000;}
+
+	timespec getFutureTime() {
+		timespec result;
+		if(clock_gettime(CLOCK_REALTIME, &result) < 0) {
+			// FIXME: error
+		}
+		result.tv_sec += ts.tv_sec;
+		result.tv_nsec += ts.tv_nsec;
+		if (result.tv_nsec >= 1000000000L) {
+			++result.tv_sec;
+			result.tv_nsec -= 1000000000L;
+		}
+		return result;
+	}
+
+	timespec getDuration() { return ts; }
+
+	long getSecs() { return ts.tv_sec; }
+	long getNanoSecs() { return ts.tv_nsec; }
+	long getTotMilliSecs() { return ts.tv_sec * 1000 + ts.tv_nsec / 1000000; }
+
+private:
+	timespec ts;
+
+	void set(long sec, long nSec) {
+		ts.tv_sec = sec;
+		ts.tv_nsec = nSec;
+	}
 };
 
 
