d0ddbb7d4c45eae2ca2d02bb416e83f425beefd3
Keith Erickson <kerickso@pppl.gov>
2014-05-03 22:56:18 +0000

    A new C++ header-only mutex object with accompanying guard object that can be used on windows or any system with pthreads.

---
+++ include/mdsplus/Mutex.hpp
@@ -0,0 +1,68 @@
+/*
+ * Mutex.hpp
+ *
+ *  Created on: May 3, 2014
+ *      Author: kerickso
+ */
+
+#ifndef MDS_MUTEX_HPP_
+#define MDS_MUTEX_HPP_
+
+#include <mdsplus/mdsplus.h>
+#if defined (MDS_WINDOWS)
+#include <windows.h>
+#elif defined (MDS_PTHREAD)
+#include <pthread.h>
+#endif
+
+namespace MDSplus  {
+
+class Mutex {
+public:
+	Mutex() {
+		_create();
+	}
+
+	~Mutex() {
+		unlock();
+		_destroy();
+	}
+
+	void lock() { _lock(); }
+
+	void unlock() { _unlock(); }
+
+private:
+#if defined (MDS_PTHREAD)
+	typedef pthread_mutex_t Mutex_t;
+	void _create() { pthread_mutex_init(&mutex, NULL); }
+	void _lock() { pthread_mutex_lock(&mutex); }
+	void _unlock() { pthread_mutex_unlock(&mutex); }
+	void _destroy() { pthread_mutex_destroy(&mutex); }
+#elif defined (MDS_WINDOWS)
+	typedef HANDLE Mutex_t;
+	void _create() { mutex = CreateMutex(NULL, FALSE, NULL);
+	void _lock() { WaitForSingleObject(mutex, INFINITE); }
+	void _unlock() { ReleaseMutex(mutex); }
+	void _destroy() { CloseHandle(mutex); }
+#endif
+
+	Mutex_t mutex;
+};
+
+class MutexGuard {
+public:
+	MutexGuard(Mutex & mutex): mutex(mutex) {
+		mutex.lock();
+	}
+
+	~MutexGuard() {
+		mutex.unlock;
+	}
+
+private:
+	Mutex & mutex;
+
+};
+
+#endif /* MDS_MUTEX_HPP_ */
