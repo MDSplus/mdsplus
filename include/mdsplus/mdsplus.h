cec7b761d2615d5bc875cd1a190e482c8cce5178
Keith Erickson <kerickso@pppl.gov>
2014-05-03 22:10:17 +0000

    Optimize system checking, end after first hit

--- include/mdsplus/mdsplus.h
+++ include/mdsplus/mdsplus.h
@@ -1,19 +1,20 @@
 #ifndef MDSPLUS_MDSPLUS_H
 #define MDSPLUS_MDSPLUS_H
 
-#ifdef _WIN32
+#if defined (_WIN32)
 #define MDS_WINDOWS
 #define EXPORT __declspec(dllexport)
+
+#elif defined (__APPLE__)
+#define MDS_MAC
+#define EXPORT
+
+#elif defined (__gnu_linux__)
+#define EXPORT
+#define MDS_LINUX
+
 #else
 #define EXPORT
-#endif
-
-#ifdef __APPLE__
-#define MDS_MAC
-#endif
-
-#ifdef __gnu_linux__
-#define MDS_LINUX
 #endif
 
 #endif
