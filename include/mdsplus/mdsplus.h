b412ff81ce9db42f00542ed0c59142edc6a13085
Keith Erickson <kerickso@pppl.gov>
2014-05-03 21:52:35 +0000

    Create preproc macros for different systems in the MDS namespace

--- include/mdsplus/mdsplus.h
+++ include/mdsplus/mdsplus.h
@@ -2,9 +2,18 @@
 #define MDSPLUS_MDSPLUS_H
 
 #ifdef _WIN32
+#define MDS_WINDOWS
 #define EXPORT __declspec(dllexport)
 #else
 #define EXPORT
+#endif
+
+#ifdef __APPLE__
+#define MDS_MAC
+#endif
+
+#ifdef __gnu_linux__
+#define MDS_LINUX
 #endif
 
 #endif
