3a490f66e8f5e228e25c590020eeba1a8aa4f5ac
Keith Erickson <kerickso@pppl.gov>
2014-05-14 16:23:52 +0000

    Fix mac bug, . vs ->

--- include/mdsplus/ConditionVar.hpp
+++ include/mdsplus/ConditionVar.hpp
@@ -30,8 +30,8 @@ static void clock_gettime(int dummyClock, timespec * ts) {
 	clock_get_time(cclock, &mts);
 	mach_port_deallocate(mach_task_self(), cclock);
 
-	ts.tv_sec = mts.tv_sec;
-	ts.tv_nsec = mts.tv_nsec;
+	ts->tv_sec = mts.tv_sec;
+	ts->tv_nsec = mts.tv_nsec;
 }
 #endif
 
