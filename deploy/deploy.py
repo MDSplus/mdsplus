b25be262e896c5af2426a9d1a41751918415a88e
Thomas W. Fredian <twf@psfc.mit.edu>
2014-06-02 12:20:44 +0000

    Add new deploy system

---
+++ deploy/deploy.py
@@ -0,0 +1,66 @@
+import subprocess,os,sys
+__doc__="""Deploy installation packages
+
+   Format:
+      python deploy.py flavor major minor release
+
+   Where:
+      flavor is one of alpha,beta or stable
+      major is first number in version (i.e. in release 6.2-42, major is 6)
+      minor is second number in version (i.e. in release 6.2.42, minor is 2)
+      release is third number in version (i.e. in release 6.2-42, release is 42)
+
+   Deploy does the following:
+
+   * Determine platform and import platform specific packaging class instance.
+   * Check if installation packages for this flavor and version already exits in the
+     repository and just exit if they do.
+   * Build new installation packages.
+   * Test new installation packages.
+   * Deploy installation packages to repository.
+
+  NOTE: SRCDIR environment variable designating the location of the source tarballs
+        must be defined!
+  NOTE: DIST environment variable designating the platform (i.e. el6,fc20,Ubuntu12)
+        must be defined!
+
+"""
+def flushPrint(text):
+  print(text)
+  sys.stdout.flush()
+
+if __name__ == "__main__":
+  if "SRCDIR" not in os.environ:
+    flushPrint("SRCDIR not defined!")
+    flushPrint(__doc__)
+    sys.exit(1)
+
+  if "DIST" not in os.environ:
+    flushPrint("DIST not defined!")
+    flushPrint(__doc__)
+    sys.exit(1)
+
+  if len(sys.argv) != 5:
+    flushPrint("Invalid argument list")
+    flushPrint(__doc__)
+    sys.exit(1)
+
+  info={'flavor':sys.argv[1],'major':int(sys.argv[2]),'minor':int(sys.argv[3]),'release':int(sys.argv[4]),'dist':os.environ['DIST']}
+  if info['flavor']=='stable':
+    info['rflavor']=""
+  else:
+    info['rflavor']="-"+info['flavor']
+
+  if os.environ['DIST'].startswith('el') or os.environ['DIST'].startswith('fc'):
+    module=__import__('rpms',globals())
+  elif os.environ['DIST'].startswith('Ubuntu'):
+    module=__import__('rpms',globals())
+  elif os.environ['DIST'].startswith('solaris'):
+    module=__import__('solaris',globals())
+  elif os.environ['DIST'].startswith('win'):
+    module=__import__('windows',globals())
+  InstallationPackage=module.InstallationPackage(info)
+  if not InstallationPackage.exists():
+    InstallationPackage.build()
+    InstallationPackage.test()
+    InstallationPackage.deploy()
---
+++ deploy/rpms.py
@@ -0,0 +1,133 @@
+import subprocess,os,sys,pexpect
+
+def _packages():
+    """Get package list from rpm spec file"""
+    pkgs=list()
+    f=open('rpm.spec','r')
+    lines=f.readlines()
+    f.close()
+    for line in lines:
+        if line.startswith('%package'):
+            pkgs.append(line.split()[1])
+    return pkgs
+
+class InstallationPackage(object):
+    """Provides exists,build,test and deploy methods"""
+    def __init__(self,info):
+        self.info=info
+        self.info['workspace']=os.environ['WORKSPACE']
+
+    def exists(self):
+        """Check to see if rpms for this release do not already exist."""
+        for pkg in _packages():
+            self.info['package']=pkg
+            found=list()
+            for arch in ('i686','noarch','x86_64'):
+                self.info['arch']=arch
+                rpm='/repository/%(dist)s/%(flavor)s/RPMS/%(arch)s/mdsplus%(rflavor)s-%(package)s-%(major)d.%(minor)d-%(release)d.%(dist)s.%(arch)s.rpm' % self.info
+            try:
+                os.stat(rpm)
+                found.append(arch)
+            except:
+                pass
+        if not ((len(found) == 1 and found[0]=='noarch') or (len(found) == 2 and found[0]=='i686' and found[1]=='x86_64')):
+            return False
+        return True
+
+
+    def build(self):
+        """Build rpms using rpmbuild"""
+        if subprocess.Popen("""
+mkdir -p %(workspace)s/%(flavor)s/{BUILD,RPMS,SPECS,SRPMS} && \
+ln -sf $SRCDIR %(workspace)s/%(flavor)s/SOURCES && \
+rpmbuild -bb  \
+  -D '_topdir %(workspace)s/%(flavor)s' \
+  -D 'version %(major)d.%(minor)d' \
+  -D 'release_num %(release)d' \
+  -D 'rflavor %(rflavor)s' \
+  -D 'flavor %(flavor)s' rpm.spec && \
+rpmbuild -bb  \
+  -D '_topdir %(workspace)s/%(flavor)s' \
+  -D 'version %(major)d.%(minor)d' \
+  -D 'release_num %(release)d' \
+  -D 'rflavor %(rflavor)s' \
+  -D 'flavor %(flavor)s' \
+  --target=i686-linux rpm.spec
+""" % self.info,shell=True).wait() != 0:
+            sys.stdout.flush()
+            raise Exception("Problem building %s rpms" % self.info['flavor'])
+        try:
+            cmd="/bin/sh -c 'rpmsign --addsign --define=\"_signature gpg\" --define=\"_gpg_name MDSplus\" %(flavor)s/RPMS/*/*.rpm'" % self.info
+            child = pexpect.spawn(cmd,timeout=60)
+            child.expect("Enter pass phrase: ")
+            child.sendline("")
+            child.expect(pexpect.EOF)
+            child.close()
+            if child.status != 0:
+                sys.stdout.flush()
+                raise Exception("Error signing rpms. status=%d" % child.status)
+        except:
+            sys.stdout.flush()
+            raise Exception("Error signing rpms: %s" % sys.exc_info()[1])
+        if subprocess.Popen("createrepo -q %(flavor)s/RPMS" % self.info,shell=True).wait() != 0:
+            sys.stdout.flush()
+            raise Exception("Problem creating repository from RPMS")
+
+    def test(self):
+        print("Preparing test repository")
+        sys.stdout.flush()
+        self.info['workspace']=os.environ['WORKSPACE']
+        errors=list()
+        f=open("test-mdsplus%(rflavor)s.repo" % self.info,"w")
+        f.write("""
+[test-mdsplus%(rflavor)s]
+name=test-mdsplus%(rflavor)s
+baseurl=file://%(workspace)s/%(flavor)s/RPMS
+enabled=1
+skip_if_unavailable=1
+""" % self.info)
+        f.close()
+        if subprocess.Popen("""
+sudo yum remove -y 'mdsplus*' 
+sudo yum-config-manager --add-repo test-mdsplus%(rflavor)s.repo &&
+sudo yum clean metadata &&
+sudo yum makecache""" % self.info,shell=True).wait() != 0:
+            errors.append("Erro preparing repository")
+        else:
+            print("Testing package installation")
+            sys.stdout.flush()
+            pkgs=_packages()
+            pkgs.append('mdsplus')
+            for pkg in pkgs:
+                if pkg != 'repo':
+                    if pkg=='mdsplus':
+                        pkg=""
+                    else:
+                        pkg="-"+pkg
+                    self.info['package']=pkg
+                    if subprocess.Popen("sudo yum install -y mdsplus%(rflavor)s%(package)s && sudo yum remove -y 'mdsplus*'" % self.info,shell=True).wait() != 0:
+                        errors.append("Error installing package mdsplus%(rflavor)s%(package)s" % self.info)
+        if len(errors) == 0:
+            if subprocess.Popen("""
+sudo yum install -y mdsplus%(rflavor)s-mitdevices &&
+. /etc/profile.d/mdsplus.sh
+python <<EOF
+import sys,os
+import MDSplus
+sys.path.append(os.path.dirname(MDSplus.__file__))
+from tests import test_all
+from unittest import TextTestRunner
+result=TextTestRunner().run(test_all())
+if not result.wasSuccessful():
+  sys.exit(1)
+EOF""" % self.info,shell=True).wait() != 0:
+                errors.append("Error running regression tests")
+        subprocess.Popen("""
+sudo yum-config-manager --disable test-mdsplus%(rflavor)s >/dev/null
+sudo yum remove -y 'mdsplus*'""" % self.info,shell=True).wait()
+        if len(errors) > 0:
+            errors.insert(0,"Testing failed")
+            raise Exception('\n'.join(errors))
+
+    def deploy(self):
+            pass
