820cd8726b2660d6cbdd3f24fb38c4d15e279dd1
Keith Erickson <kerickso@pppl.gov>
2014-06-04 07:46:43 +0000

    Include DSO import/exports in mdsplus.h

--- include/mdsplus/mdsplus.h
+++ include/mdsplus/mdsplus.h
@@ -19,4 +19,32 @@
 #define EXPORT
 #endif
 
+#if defined(_WIN32)
+  #define MDS_API_IMPORT __declspec(dllimport)
+  #define MDS_API_EXPORT __declspec(dllexport)
+  #define MDS_API_HIDDEN
+#else
+  #if __GNUC__ >= 4
+    #define MDS_API_IMPORT __attribute__ ((visibility ("default")))
+    #define MDS_API_EXPORT __attribute__ ((visibility ("default")))
+    #define MDS_API_HIDDEN __attribute__ ((visibility ("hidden")))
+  #else
+    #define MDS_API_IMPORT
+    #define MDS_API_EXPORT
+    #define MDS_API_HIDDEN
+  #endif
+#endif
+
+#ifdef MDS_SHARED // defined if MDS is compiled as a shared library
+  #define MDS_API_LOCAL MDS_API_HIDDEN
+  #ifdef MDS_API_BUILD // defined if we are building the MDS library instead of using it
+    #define MDS_API MDS_API_EXPORT
+  #else
+    #define MDS_API MDS_API_IMPORT
+  #endif
+#else // static library
+  #define MDS_API
+  #define MDS_API_LOCAL
+#endif
+
 #endif
