f645badd1c48511a04fcc074d37f3616f60e7b32
Thomas W. Fredian <twf@psfc.mit.edu>
2014-06-17 14:37:37 +0000

    Use new packaging method via xml

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,4 +1,5 @@
-import subprocess,os,sys
+import subprocess,os,sys,xml.etree.ElementTree as ET,fnmatch
+
 __doc__="""Deploy installation packages
 
    Format:
@@ -27,18 +28,183 @@ def flushPrint(text):
   print(text)
   sys.stdout.flush()
 
+def packages(packagingXml):
+    """Get package list"""
+    pkgs=list()
+    tree=ET.parse(packagingXml)
+    root=tree.getroot()
+    pfiles=dict()
+    for package in root.getiterator('package'):
+      pkgs.append(package.attrib['name'])
+    return pkgs
+
+def getPackageFiles(buildroot,includes,excludes):
+    files=list()
+    for f in includes:
+        f = buildroot+f
+        if os.path.isdir(f):
+            for root, dirs, filenams in os.walk(f):
+                for filenam in filenams:
+                    files.append(os.path.join(root,filenam))
+        elif ('?' in f) or ('*' in f):
+            dirnam=f
+            while ('?' in dirnam) or ('*' in dirnam):
+                dirnam=os.path.dirname(dirnam)
+            for root, dirs, filenams in os.walk(dirnam):
+                if fnmatch.fnmatch(root,f):
+                    files=files+getPackageFiles(buildroot,[root[len(buildroot):],],excludes)
+                else:
+                    for filenam in filenams:
+                        filenam=os.path.join(root,filenam)
+                        if fnmatch.fnmatch(filenam,f):
+                            files.append(filenam)
+        else:
+            try:
+                os.stat(f)
+                files.append(f)
+            except:
+                pass
+    if len(excludes) > 0:
+        hasuid=False
+        for exclude in excludes:
+            if '/xuid' in exclude:
+                print "excluding: %s" % exclude
+                hasuid=True
+        excludefiles=getPackageFiles(buildroot,excludes,[])
+        if hasuid:
+            print "Found %d" % len(excludefiles)
+            for exclude in excludefiles:
+                print "excluding: %s" % exclude
+        for exclude in excludefiles:
+            if exclude in files:
+                files.remove(exclude)
+    return files
+
+def checkPackageFiles(buildroot,packagingXml,mode):
+    tree=ET.parse(packagingXml)
+    root=tree.getroot()
+    pfiles=dict()
+    for package in root.getiterator('package'):
+        includes=[]
+        for inc in package.getiterator('include'):
+            for inctype in inc.attrib:
+                if inctype != "dironly":
+                    include=inc.attrib[inctype]
+                    if include.endswith("/lib"):
+                        include=include+"*"
+                    elif "/lib/" in include:
+                        include=include.replace("/lib/","/lib*/")
+                    elif include.endswith("/bin"):
+                        include=include+"*"
+                    elif "/bin/" in include:
+                        include=include.replace("/bin/","/bin*/")
+                    elif include.endswith("/uid"):
+                        include=include+"*"
+                    elif "/uid/" in include:
+                        include=include.replace("/uid/","/uid*/")
+                    includes.append(include)
+        excludes=[]
+        for exc in package.getiterator('exclude'):
+            for exctype in exc.attrib:
+                exclude=exc.attrib[exctype]
+                if exclude.endswith("/lib"):
+                    exclude=exclude+"*"
+                elif "/lib/" in exclude:
+                    exclude=exclude.replace("/lib/","/lib*/")
+                elif exclude.endswith("/bin"):
+                    exclude=exclude+"*"
+                elif "/bin/" in exclude:
+                    exclude=exclude.replace("/bin/","/bin*/")
+                elif exclude.endswith("/uid"):
+                    exclude=exclude+"*"
+                elif "/uid/" in exclude:
+                    exclude=exclude.replace("/uid/","/uid*/")
+                excludes.append(exclude)
+        if package.find("exclude_staticlibs") is not None:
+            excludes.append("/usr/local/mdsplus/lib??/*.a")
+        if package.find("include_staticlibs") is not None:
+            includes.append("/usr/local/mdsplus/lib??/*a")
+        pfiles[package.attrib['name']]=getPackageFiles(buildroot,includes,excludes)
+        if mode == "contents":
+            print("Packge: %s (Arch: %s) contains:" % (package.attrib['name'],package.attrib['arch']))
+            for f in pfiles[package.attrib['name']]:
+                f=f.replace('/bin32/','/bin/')
+                f=f.replace('/bin64/','/bin64/')
+                f=f.replace('/lib32/','/lib/')
+                f=f.replace('/lib64/','/lib/')
+                f=f.replace('/uid32/','/uid/')
+                f=f.replace('/uid64/','/uid/')
+                print('      '+f[len(buildroot):])
+    if mode == "check":
+        for pkg in pfiles:
+            for package in root.getiterator('package'):
+                if package.attrib['name']==pkg:
+                    arch = package.attrib['arch']
+                    for f in pfiles[pkg]:
+                        p=subprocess.Popen('file "%s"' % f,stdout=subprocess.PIPE,shell=True)
+                        ftype=p.stdout.readlines()
+                        status=p.wait()
+                        if status == 0:
+                            ok=False
+                            ftype=ftype[0][:-1]
+                            if arch == "noarch":
+                                accepted_types=("text","compressed","Zip","vi:","mnu:",".DS_Store:",
+                                                "/mdsplus/trees/","JPEG","PNG",
+                                                "dll:","symbolic link",".sav:","python","Java","PGP")
+                                for a_t in accepted_types:
+                                    if a_t in ftype:
+                                        ok=True
+                                        break
+                            else:
+                                accepted_types=("uid:","ELF","ar archive","symbolic link")
+                                for a_t in accepted_types:
+                                    if a_t in ftype:
+                                        ok=True
+                                        break
+                            if not ok:
+                                print "%s but: %s" % (arch,ftype)
+        files=pfiles.values()
+        for i in range(len(files)):
+            for j in range(i+1,len(files)):
+                for f in files[i]:
+                    if f in files[j]:
+                        print("%s is included in both %s and %s" % (f,pfiles.keys()[i],pfiles.keys()[j]))
+        allfiles=list()
+        for f in files:
+            allfiles+=f
+        for root, dirs, filenams in os.walk(buildroot):
+            for filenam in filenams:
+                f=os.path.join(root,filenam)
+                if f not in allfiles:
+                    print("%s is not in any package" % f)
+                else:
+                    allfiles.remove(f)
+        if len(allfiles) > 0:
+            for f in allfiles:
+                print("%s in a package but not in buildroot" % f)
+
 if __name__ == "__main__":
+
+  if len(sys.argv) > 1 and sys.argv[1]=='check':
+    buildroot=sys.argv[2]
+    checkPackageFiles(buildroot,'packaging.xml','check')
+    sys.exit(0)
+  elif len(sys.argv) > 1 and sys.argv[1]=='contents':
+    buildroot=sys.argv[2]
+    checkPackageFiles(buildroot,'packaging.xml','contents')
+    sys.exit(0)
+
   if "DIST" not in os.environ:
     flushPrint("DIST not defined!")
     flushPrint(__doc__)
     sys.exit(1)
 
-  if len(sys.argv) != 5:
+  if len(sys.argv) not in (5,6):
     flushPrint("Invalid argument list")
     flushPrint(__doc__)
     sys.exit(1)
 
-  info={'flavor':sys.argv[1],'major':int(sys.argv[2]),'minor':int(sys.argv[3]),'release':int(sys.argv[4]),'dist':os.environ['DIST']}
+  info={'flavor':sys.argv[1],'major':int(sys.argv[2]),'minor':int(sys.argv[3]),'release':int(sys.argv[4]),'dist':os.environ['DIST'],'workspace':os.environ['WORKSPACE']}
   if info['flavor']=='stable':
     info['rflavor']=""
   else:
@@ -53,7 +219,22 @@ if __name__ == "__main__":
   elif os.environ['DIST'].startswith('win'):
     module=__import__('windows',globals())
   InstallationPackage=module.InstallationPackage(info)
-  if not InstallationPackage.exists():
-    InstallationPackage.build()
-    InstallationPackage.test()
-    InstallationPackage.deploy()
+  if len(sys.argv)==5:
+    if not InstallationPackage.exists():
+      if subprocess.Popen("tar zxf /repository/SOURCES/mdsplus%(rflavor)s-%(major)d.%(minor)d-%(release)d.tgz" % info,shell=True,cwd=info['workspace']).wait() != 0:
+        raise Exception("Error unpacking sources for this release")
+      InstallationPackage.build()
+      InstallationPackage.test()
+      InstallationPackage.deploy()
+  else:
+    if sys.argv[5]=='exists':
+      print(InstallationPackage.exists())
+    elif sys.argv[5]=='build':
+      if subprocess.Popen("tar zxf /repository/SOURCES/mdsplus%(rflavor)s-%(major)d.%(minor)d-%(release)d.tgz" % info,shell=True,cwd=info['workspace']).wait() != 0:
+        raise Exception("Error unpacking sources for this release")
+      InstallationPackage.build()
+    elif sys.argv[5]=='test':
+      InstallationPackage.test()
+    elif sys.argv[5]=='deploy':
+      InstallationPackage.deploy()
+       
--- deploy/rpms.py
+++ deploy/rpms.py
@@ -1,15 +1,4 @@
-import subprocess,os,sys,pexpect
-
-def _packages():
-    """Get package list from rpm spec file"""
-    pkgs=list()
-    f=open('rpm.spec','r')
-    lines=f.readlines()
-    f.close()
-    for line in lines:
-        if line.startswith('%package'):
-            pkgs.append(line.split()[1])
-    return pkgs
+import subprocess,os,sys,pexpect,xml.etree.ElementTree as ET,fnmatch,tempfile
 
 class InstallationPackage(object):
     """Provides exists,build,test and deploy methods"""
@@ -18,96 +7,205 @@ class InstallationPackage(object):
         self.info['workspace']=os.environ['WORKSPACE']
 
     def exists(self):
-        """Check to see if rpms for this release do not already exist."""
-        for pkg in _packages():
-            self.info['package']=pkg
-            found=list()
-            for arch in ('i686','noarch','x86_64'):
+        """Check to see if rpms for this release already exist."""
+        tree=ET.parse('packaging.xml')
+        root=tree.getroot()
+        for package in root.getiterator('package'):
+            self.info['package']=package.attrib['name']
+            if package.attrib['arch']=='noarch':
+                arches=('noarch',)
+            else:
+                arches=('x86_64','i686')
+            for arch in arches:
                 self.info['arch']=arch
                 rpm='/repository/%(dist)s/%(flavor)s/RPMS/%(arch)s/mdsplus%(rflavor)s-%(package)s-%(major)d.%(minor)d-%(release)d.%(dist)s.%(arch)s.rpm' % self.info
                 try:
                     os.stat(rpm)
-                    found.append(arch)
                 except:
-                    pass
-            if not ((len(found) == 1 and found[0]=='noarch') or (len(found) == 2 and found[0]=='i686' and found[1]=='x86_64')):
-                print("/repository/%(dist)s/%(flavor)s/RPMS/%(arch)s/mdsplus%(rflavor)s-%(package)s-%(major)d.%(minor)d-%(release)d.%(dist)s rpm's not found" % self.info)
-                sys.stdout.flush()
-                return False
+                    print("%s not found" % rpm)
+                    sys.stdout.flush()
+                    return False
+        print("Latest %(flavor) release %(major)d.%(minor)d-%(release)d is already available. Nothing to do for this release." % self.info)
+        sys.stdout.flush()
         return True
 
+    def buildInstall(self):
+        """Build MDSplus from the sources and install into a BUILDROOT directory"""
+        self.info['mdsplus_version']="%(major)d.%(minor)d_%(release)d" % self.info
+        if self.info['flavor']!='stable':
+            self.info['mdsplus_version']="%(flavor)s-%(mdsplus_version)s" % self.info
+        tree=ET.parse('packaging.xml')
+        root=tree.getroot()
+        install_script=root.find('rpm').find('install_script').text
+        install_script=install_script[install_script.find('#'):]
+        fd,filename=tempfile.mkstemp()
+        os.write(fd,install_script % self.info)
+        os.close(fd)
+        os.chmod(filename,0700)
+        status = subprocess.Popen(filename,shell=True).wait()
+        os.remove(filename)
+        if status != 0:
+            raise Exception("Problem building MDSplus from sources")
 
-    def build(self):
-        """Build rpms using rpmbuild"""
-        if len(self.info['rflavor'])==0:
-            self.info['D_RFLAVOR']=""
-        else:
-            self.info['D_RFLAVOR']="--define='rflavor %(rflavor)s'" % self.info
-        if "el5" in self.info['DIST']:
-            cmds="""
-mkdir -p %(workspace)s/%(flavor)s/{BUILD,RPMS,SPECS,SRPMS} && \
-ln -sf /repository/SOURCES %(workspace)s/%(flavor)s/ && \
-rpmbuild -bb  \
-  --define='DIST %(DIST)s' \
-  --define='BITS 64' \
-  --define='_topdir %(workspace)s/%(flavor)s' \
-  --define='mdsplus_version %(major)d.%(minor)d' \
-  --define='mdsplus_release %(release)d' \
-  %(D_RFLAVOR)s \
-  --define='flavor %(flavor)s' \
-  --buildroot=%(workspace)s/%(flavor)s/BUILDROOT \
-  --target=x86_64-linux rpm_el5_bin.spec && \
-rpmbuild -bb  \
-  --define='DIST %(DIST)s' \
-  --define='BITS 32' \
-  --define '_topdir %(workspace)s/%(flavor)s' \
-  --define='mdsplus_version %(major)d.%(minor)d' \
-  --define='mdsplus_release %(release)d' \
-  %(D_RFLAVOR)s \
-  --define='flavor %(flavor)s' \
-  --buildroot=%(workspace)s/%(flavor)s/BUILDROOT \
-  --target=i686-linux rpm_el5_bin.spec && \
-rpmbuild -bb  \
-  --define='DIST %(DIST)s' \
-  --define='BITS 32' \
-  --define '_topdir %(workspace)s/%(flavor)s' \
-  --define='mdsplus_version %(major)d.%(minor)d' \
-  --define='mdsplus_release %(release)d' \
-  %(D_RFLAVOR)s \
-  --define='flavor %(flavor)s' \
-  --buildroot=%(workspace)s/%(flavor)s/BUILDROOT \
-  --target=i686-linux rpm_el5_noarch.spec
-""" % self.info
-        else:
-            cmds="""
-mkdir -p %(workspace)s/%(flavor)s/{BUILD,RPMS,SPECS,SRPMS} && \
-ln -sf /repository/SOURCES %(workspace)s/%(flavor)s/ && \
-rpmbuild -bb  \
-  --define='DIST %(DIST)s' \
-  --define='BITS 64' \
-  --define='_topdir %(workspace)s/%(flavor)s' \
-  --define='mdsplus_version %(major)d.%(minor)d' \
-  --define='mdsplus_release %(release)d' \
-  %(D_RFLAVOR)s \
-  --define='flavor %(flavor)s' \
-  --buildroot=%(workspace)s/%(flavor)s/BUILDROOT \
-  --target=x86_64-linux rpm.spec && \
-rpmbuild -bb  \
-  --define='DIST %(DIST)s' \
-  --define='BITS 32' \
-  --define '_topdir %(workspace)s/%(flavor)s' \
-  --define='mdsplus_version %(major)d.%(minor)d' \
-  --define='mdsplus_release %(release)d' \
-  %(D_RFLAVOR)s \
-  --define='flavor %(flavor)s' \
-  --buildroot=%(workspace)s/%(flavor)s/BUILDROOT \
-  --target=i686-linux rpm.spec
-""" % self.info
-        if subprocess.Popen(cmds,shell=True).wait() != 0:
-            sys.stdout.flush()
-            raise Exception("Problem building %s rpms" % self.info['flavor'])
+    def fixFilename(self,filename):
+        """For binary packages we need to refer to /libnn, /binnn and /uidnn where nn is 32 or 64. For
+        noarch packages we need to use /lib??, /bin?? to get the no arch dependent scripts and files"""
         try:
-            cmd="/bin/sh -c 'rpmsign --addsign --define=\"_signature gpg\" --define=\"_gpg_name MDSplus\" %(flavor)s/RPMS/*/*.rpm'" % self.info
+            if self.info['arch']=='noarch':
+                filename=filename.replace("/lib/","/lib??/") 
+                filename=filename.replace("/bin/","/bin??/")
+                if filename.endswith(('/lib','/bin')):
+                    filename=filename+"??"
+            else:
+                filename=filename.replace("/lib/","/lib%(bits)d/") 
+                filename=filename.replace("/bin/","/bin%(bits)d/")
+                filename=filename.replace("/uid/","/uid%(bits)d/")
+                if filename.endswith(('/lib','/bin','/uid')):
+                    filename=filename+"%(bits)d"
+            ans=filename % self.info
+        except:
+            raise Exception("Error fixing filename "+filename)
+        return ans
+
+    def buildRpms(self):
+        tree=ET.parse('packaging.xml')
+        root=tree.getroot()
+        rpmspec=root.find('rpm').find('spec_start').text
+        s=rpmspec.split('\n')
+        for idx in range(len(s)):
+            if len(s[idx].strip()) == 0:
+                s[idx]=""
+        rpmspec='\n'.join(s)
+            
+        bin_packages=list()
+        noarch_packages=list()
+        for package in root.getiterator('package'):
+            attr=package.attrib
+            if attr["arch"]=="noarch":
+                noarch_packages.append(package)
+            else:
+                bin_packages.append(package)
+                
+        for arch in ({"target":"x86_64-linux","bits":64,"arch_t":".x86_64"},{"target":"i686-linux","bits":32,"arch_t":".i686"}):
+            self.info['target']=arch['target']
+            self.info['bits']=arch['bits']
+            self.info['arch']="bin"
+            self.info['arch_t']=arch['arch_t']
+            for package in bin_packages:
+                if package.attrib['name'] != "MDSplus":
+                    self.info["packagename"]="-%s" % package.attrib["name"]
+                else:
+                    self.info["packagename"]=""
+                self.info['summary']=package.attrib['summary']
+                self.info['description']=package.attrib['description']
+                out,specfilename=tempfile.mkstemp()
+                os.write(out,rpmspec % self.info)
+                for require in package.getiterator("requires"):
+                    self.info['reqpkg']=require.attrib['package']
+                    os.write(out,"Requires: mdsplus%(rflavor)s-%(reqpkg)s >= %(major)d.%(minor)d-%(release)d\n" % self.info)
+                os.write(out,"""
+%%description
+%(description)s
+%%clean
+%%files
+%%defattr(-,root,root)
+""" % self.info)
+                for inc in package.getiterator('include'):
+                    for inctype in inc.attrib:
+                        include=self.fixFilename(inc.attrib[inctype])
+                        if inctype == "dironly":
+                            os.write(out,"%%dir %s\n" % include)
+                        else:
+                            os.write(out,"%s\n" % include)
+                for exc in package.getiterator('exclude'):
+                    for exctype in exc.attrib:
+                        exclude=self.fixFilename(exc.attrib[exctype])
+                        os.write(out,"%%exclude %s\n" % exclude)
+                if package.find("exclude_staticlibs") is not None:
+                    os.write(out,"%%exclude /usr/local/mdsplus/lib%(bits)d/*.a\n" % self.info)
+                if package.find("include_staticlibs") is not None:
+                    os.write(out,"/usr/local/mdsplus/lib%(bits)d/*.a\n" % self.info)
+                for s in (("preinst","pre"),("postinst","post"),("prerm","preun"),("postrm","postun")):
+                    script=package.find(s[0])
+                    if script is not None and ("type" not in script.attrib or script.attrib["type"]=="rpm"):
+                        os.write(out,"%%%s\n%s\n" % (s[1],script.text.replace("__INSTALL_PREFIX__","$RPM_INSTALL_PREFIX")))
+                triggerin=package.find("triggerin")
+                if triggerin is not None:
+                    os.write(out,"%%triggerin -- %s\n%s\n" % (triggerin.attrib['trigger'],
+                                                           triggerin.text.replace("__INSTALL_PREFIX__","$RPM_INSTALL_PREFIX")))
+                os.close(out)
+                self.info['specfilename']=specfilename
+                print("Building rpm for mdsplus%(rflavor)s%(packagename)s%(arch_t)s" % self.info)
+                sys.stdout.flush()
+                p = subprocess.Popen("""
+rpmbuild -bb --define '_topdir %(workspace)s/%(flavor)s' --buildroot=%(workspace)s/%(flavor)s/BUILDROOT --target=%(target)s %(specfilename)s 2>&1
+""" % self.info,stdout=subprocess.PIPE,shell=True)
+                messages=p.stdout.readlines()
+                status=p.wait()
+                os.remove(specfilename)
+                if status != 0:
+                    print(''.join(messages))
+                    sys.stdout.flush()
+                    raise Exception("Error building rpm for package mdsplus%(rflavor)s%(packagename)s%(arch_t)s" % self.info)
+                print("Done building rpm for mdsplus%(rflavor)s%(packagename)s%(arch_t)s" % self.info)
+                sys.stdout.flush()
+        for package in noarch_packages:
+            self.info['arch']="noarch"
+            if package.attrib['name'] != "MDSplus":
+                self.info["packagename"]="-%s" % package.attrib["name"]
+            else:
+                self.info["packagename"]=""
+            out,specfilename=tempfile.mkstemp()
+            os.write(out,rpmspec % self.info)
+            for require in package.getiterator("requires"):
+                self.info['reqpkg']=require.attrib['package']
+                os.write(out,"Requires: mdsplus%(rflavor)s-%(reqpkg)s >= %(major)d.%(minor)d-%(release)d\n" % self.info)
+            os.write(out,"""
+Buildarch: noarch
+%%description
+%(description)s
+%%clean
+%%files
+%%defattr(-,root,root)
+""" % self.info)
+            for inc in package.getiterator('include'):
+                for inctype in inc.attrib:
+                    include=self.fixFilename(inc.attrib[inctype])
+                    if inctype == "dironly":
+                        os.write(out,"%%dir %s\n" % include)
+                    else:
+                        os.write(out,"%s\n" % include)
+            for exc in package.getiterator('exclude'):
+                for exctype in exc.attrib:
+                    exclude=self.fixFilename(exc.attrib[exctype])
+                    os.write(out,"%%exclude %s\n" % exclude)
+            if package.find("exclude_staticlibs") is not None:
+                os.write(out,"%%exclude /usr/local/mdsplus/lib??/*.a\n" % self.info)
+            for s in (("preinst","pre"),("postinst","post"),("prerm","preun"),("postrm","postun")):
+                script=package.find(s[0])
+                if script is not None and ("type" not in script.attrib or script.attrib["type"]=="rpm"):
+                    os.write(out,"%%%s\n%s\n" % (s[1],script.text.replace("__INSTALL_PREFIX__","$RPM_INSTALL_PREFIX")))
+            triggerin=package.find("triggerin")
+            if triggerin is not None:
+                os.write(out,"%%triggerin -- %s\n%s\n" % (triggerin.attrib['trigger'],
+                                                       triggerin.text.replace("__INSTALL_PREFIX__","$RPM_INSTALL_PREFIX")))
+            os.close(out)
+            self.info['specfilename']=specfilename
+            print("Building rpm for mdsplus%(rflavor)s%(packagename)s.noarch" % self.info)
+            sys.stdout.flush()
+            p=subprocess.Popen("""
+rpmbuild -bb --define '_topdir %(workspace)s/%(flavor)s' --buildroot=%(workspace)s/%(flavor)s/BUILDROOT %(specfilename)s 2>&1
+""" % self.info,stdout=subprocess.PIPE,shell=True)
+            messages=p.stdout.readlines()
+            status = p.wait()
+            os.remove(specfilename)
+            if status != 0:
+                print(''.join(messages))
+                raise Exception("Error building rpm for package mdsplus%(rflavor)s%(packagename)s.noarch" % self.info)
+            print("Done building rpm for mdsplus%(rflavor)s%(packagename)s.noarch" % self.info)
+            sys.stdout.flush()
+        try:
+            cmd="/bin/sh -c 'rpmsign --addsign --define=\"_signature gpg\" --define=\"_gpg_name MDSplus\" %(workspace)s/%(flavor)s/RPMS/*/*.rpm'" % self.info
             child = pexpect.spawn(cmd,timeout=60)
             child.expect("Enter pass phrase: ")
             child.sendline("")
@@ -119,9 +217,15 @@ rpmbuild -bb  \
         except:
             sys.stdout.flush()
             raise Exception("Error signing rpms: %s" % sys.exc_info()[1])
-        if subprocess.Popen("createrepo -q %(flavor)s/RPMS" % self.info,shell=True).wait() != 0:
+        if subprocess.Popen("createrepo -q %(workspace)s/%(flavor)s/RPMS" % self.info,shell=True).wait() != 0:
             sys.stdout.flush()
             raise Exception("Problem creating repository from RPMS")
+ 
+
+    def build(self):
+        """Build rpms from sources"""
+        self.buildInstall()
+        self.buildRpms()
 
     def test(self):
         print("Preparing test repository")
@@ -138,20 +242,22 @@ skip_if_unavailable=1
 """ % self.info)
         f.close()
         if subprocess.Popen("""
-sudo yum remove -y 'mdsplus*' 
-sudo yum-config-manager --add-repo test-mdsplus%(rflavor)s.repo &&
+sudo yum remove -y 'mdsplus*'
+set -e
+sudo yum-config-manager --add-repo test-mdsplus%(rflavor)s.repo
 sudo yum-config-manager --enable test-mdsplus%(rflavor)s >/dev/null
-sudo yum clean metadata &&
+sudo yum clean metadata
 sudo yum makecache""" % self.info,shell=True).wait() != 0:
             errors.append("Erro preparing repository")
         else:
             print("Testing package installation")
             sys.stdout.flush()
-            pkgs=_packages()
-            pkgs.append('mdsplus')
-            for pkg in pkgs:
+            tree=ET.parse('packaging.xml')
+            root=tree.getroot()
+            for package in root.getiterator('package'):
+                pkg=package.attrib['name']
                 if pkg != 'repo':
-                    if pkg=='mdsplus':
+                    if pkg=='MDSplus':
                         pkg=""
                     else:
                         pkg="-"+pkg
@@ -160,7 +266,8 @@ sudo yum makecache""" % self.info,shell=True).wait() != 0:
                         errors.append("Error installing package mdsplus%(rflavor)s%(package)s" % self.info)
         if len(errors) == 0:
             if subprocess.Popen("""
-sudo yum install -y mdsplus%(rflavor)s-mitdevices &&
+set -e
+sudo yum install -y mdsplus%(rflavor)s-mitdevices
 . /etc/profile.d/mdsplus.sh
 python <<EOF
 import sys,os
@@ -182,6 +289,16 @@ sudo yum remove -y 'mdsplus*'""" % self.info,shell=True).wait()
 
     def deploy(self):
         """Deploy release to repository"""
-        if subprocess.Popen("rsync -a %(flavor)s/RPMS /repository/%(dist)s/%(flavor)s/" % self.info,shell=True).wait() != 0:
+        if subprocess.Popen("""
+rsync -a %(workspace)s/%(flavor)s/RPMS /repository/%(dist)s/%(flavor)s/
+""" % self.info,shell=True).wait() != 0:
             raise Exception("Error deploying %(flavor)s release to repository" % self.info)
-            pass
+        if subprocess.Popen("""
+set -e
+python setup.py -q bdist_egg -d /repository/EGGS
+if ( which python3 > /dev/null )
+then
+  python3 setup.py -q bdist_egg -d /repository/EGGS
+fi
+""" % self.info,shell=True,cwd="%(workspace)s/%(flavor)s/BUILDROOT/usr/local/mdsplus/mdsobjects/python" % self.info).wait() != 0:
+            raise Exception("Error deploying python release egg to repository" % self.info)
