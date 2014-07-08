b2c0525a0359a1ba8b5d6a562023cd55a470f205
Thomas W. Fredian <twf@psfc.mit.edu>
2014-07-08 13:35:37 +0000

    Clean out the workspace if necessary before building

--- deploy/release.py
+++ deploy/release.py
@@ -138,6 +138,7 @@ if __name__ == "__main__":
       info['executable']=sys.executable
       if subprocess.Popen("""
 set -e
+rm -Rf mdsplus* alpha beta stable
 tar zxf /repository/SOURCES/mdsplus%(rflavor)s-%(major)d.%(minor)d-%(release)d.tgz mdsplus%(rflavor)s-%(major)d.%(minor)d-%(release)d/deploy
 cd mdsplus%(rflavor)s-%(major)d.%(minor)d-%(release)d/deploy
 %(executable)s  deploy.py %(flavor)s %(major)s %(minor)d %(release)d
