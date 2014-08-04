90d4d2d33e9a296b49e3eee7a9650e26f13e0ed2
Thomas W. Fredian <twf@psfc.mit.edu>
2014-08-04 15:54:09 +0000

    Fix 32-bit path setting

--- WinInstall/WinInstall.c
+++ WinInstall/WinInstall.c
@@ -4,7 +4,7 @@
 #ifdef _WIN64
 const char *mdspath=";%MDSPLUSDIR%\\bin_x86_64;%MDSPLUSDIR%\\bin_x86;%MDSPLUSDIR%";
 #else
-const char *mdspath="%MDSPLUSDIR%\\bin_x86;%MDSPLUSDIR%";
+const char *mdspath=";%MDSPLUSDIR%\\bin_x86;%MDSPLUSDIR%";
 #endif
 const char *envkey="SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
 
