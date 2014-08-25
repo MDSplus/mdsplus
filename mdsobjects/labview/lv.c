927e509f3c1e9302294f23787e541173f898e472
Thomas W. Fredian <twf@psfc.mit.edu>
2014-08-25 20:39:23 +0000

    Use labview.def on Windows not libLV.so

--- mdsobjects/labview/lv.c
+++ mdsobjects/labview/lv.c
@@ -10,21 +10,9 @@
  *   Josh Stillerman 10/19/12
  */
 #include <stdint.h>
-#ifdef __MINGW32__
-  #undef WIN32
-  #undef _WIN32
-  #undef __WIN32__
-  #define linux 1
-#endif
 #include  <platdefines.h>
 #include <extcode.h>
 #include <fundtypes.h>
-#ifdef __MINGW32__
-  #undef linux
-  #define WIN32 1
-  #define _WIN32 1
-  #define __WIN32__ 1
-#endif
 #include <mdsdescrip.h>
 #include <libroutines.h>
 
