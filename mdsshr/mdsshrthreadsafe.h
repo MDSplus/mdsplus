0d8e84744b46e68cff3ee3bcf40013acad6271b9
Thomas W. Fredian <twf@psfc.mit.edu>
2014-08-25 17:23:38 +0000

    Fix for mingw

--- mdsshr/mdsshrthreadsafe.h
+++ mdsshr/mdsshrthreadsafe.h
@@ -1,23 +1,17 @@
 #include <mdstypes.h>
 #include <mdsdescrip.h>
-#ifdef HAVE_WINDOWS_H
-#ifndef NO_WINDOWS_H
-#include <windows.h>
-#endif
 #ifdef HAVE_PTHREAD_H
-#include <pthread.h>
+  #include <pthread.h>
 #else
-#define pthread_mutex_t HANDLE
-#define pthread_once_t int
-#define PTHREAD_ONCE_INIT 0
-#endif
-#else
-#include <pthread.h>
-#if (defined(_DECTHREADS_) && (_DECTHREADS_ != 1)) || !defined(_DECTHREADS_)
-#define pthread_attr_default NULL
-#define pthread_mutexattr_default NULL
-#define pthread_condattr_default NULL
-#endif
+  #ifdef HAVE_WINDOWS_H
+    #ifndef NO_WINDOWS_H
+      #include <windows.h>
+    #endif
+  #else
+    #define pthread_mutex_t HANDLE
+    #define pthread_once_t int
+    #define PTHREAD_ONCE_INIT 0
+  #endif
 #endif
 
 typedef struct _thread_static {
