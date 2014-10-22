0b714979dc0d8dc5eac4b6fb3eb00ffe54657946
Joshua Stillerman <jas@psfc.mit.edu>
2014-10-22 15:03:58 +0000

    userdata must be cast to an int

--- xmdsshr/XmdsPath.c
+++ xmdsshr/XmdsPath.c
@@ -141,7 +141,7 @@ Boolean XmdsIsPath(Widget w)
 {
   XtPointer user_data = 0;
   XtVaGetValues(w,XmNuserData, &user_data,NULL);
-  if (user_data && (user_data == PathUserData))
+  if (user_data && ((int)user_data == PathUserData))
     return 1;
   else
     return 0;
