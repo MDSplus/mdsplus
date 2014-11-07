6997d3c081403317908ddab3bc214a3a26ab1cc0
Tom Fredian <twf@psfc.mit.edu>
2014-11-07 17:05:39 -0500

    Build VS compatible dll and libs

--- mdsobjects/cpp/vsbuild.bat
+++ mdsobjects/cpp/vsbuild.bat
@@ -1,28 +1,34 @@
 @ECHO OFF
 
-set VS=z:\%1
-CALL :build_dll %2
+set topdir=..\..
+set VS=z:%1
+set arch=%2
+set outdir=%3
+set INCLUDE=.\;%VS%\include;%topdir%\include;%topdir%\tdishr
+set LIB=%VS%\%arch%\lib;%outdir%
+set PATH=%PATH%;%VS%\%arch%\bin
+CALL :build_dll
 goto :eof
 
 :build_dll
 SETLOCAL
-set INCLUDE=.\;%VS%\include;..\..\include;..\..\tdishr
-set arch=%1
-set LIB=%VS%\%arch%\lib
-set compiler=%VS%\%arch%\bin\cl.exe
 set options=/O2 /Ob1 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" ^
-/D "MDSOBJECTS_EXPORTS" /D "_VC80_UPGRADE=0x0600" /D "_WINDLL" /D "_MBCS" /GF /EHsc /MT /Gy /Fp"mdsobjects.pch" /W3 /nologo /c
-del *.obj *.lib
-%compiler% %options% /TP mdsdataobjects.cpp mdstreeobjects.cpp mdseventobjects.cpp mdsipobjects.cpp
+/D "MDSOBJECTS_EXPORTS" /D "_VC80_UPGRADE=0x0600" /D "_WINDLL" /D "_MBCS" ^
+/GF /EHsc /MT /Gy /W3 /nologo /c
+del *.obj 
+cl.exe %options% /TP mdsdataobjects.cpp mdstreeobjects.cpp mdseventobjects.cpp mdsipobjects.cpp
 if errorlevel 1 exit %errorlevel%
-%compiler% %options% /TC mdsdata.c mdstree.c
+cl.exe %options% /TC mdsdata.c mdstree.c
 if errorlevel 1 exit %errorlevel%
-copy ..\..\bin_%arch%\MdsShr.dll.a .\MdsShr.lib
-copy ..\..\bin_%arch%\TdiShr.dll.a .\TdiShr.lib
-copy ..\..\bin_%arch%\TreeShr.dll.a .\TreeShr.lib
-copy ..\..\bin_%arch%\MdsIpShr.dll.a .\MdsIpShr.lib
-copy ..\..\bin_%arch%\RtEventsShr.dll.a .\RtEventsShr.lib
-%compiler% /LD /o ..\..\bin_%arch%\MdsObjectsCppShr-VS.dll *.obj MdsShr.lib TdiShr.lib TreeShr.lib MdsIpShr.lib rteventsshr.lib
+lib /MACHINE:%arch% /DEF:%outdir%/MdsShr.def
+lib /MACHINE:%arch% /DEF:%outdir%/TdiShr.def
+lib /MACHINE:%arch% /DEF:%outdir%/TreeShr.def
+lib /MACHINE:%arch% /DEF:%outdir%/RtEventsShr.def
+link.exe /OUT:"%outdir%\MdsObjectsCppShr-VS.dll" /INCREMENTAL:NO ^
+/DLL /MANIFEST /MANIFESTFILE:"mdsobjects.dll.intermediate.manifest" /MANIFESTUAC:"level='asInvoker' uiAccess='false'" ^
+/PDB:"./mdsobjects.pdb" /DYNAMICBASE:NO /IMPLIB:"%outdir%\MdsObjectsCppShr-VS.lib" ^
+/MACHINE:%arch% *.obj /LIBPATH:"%outdir%" MdsShr.lib TdiShr.lib TreeShr.lib MdsIpShr.lib RtEventsShr.lib ^
+Kernel32.Lib  libcmt.lib  libcpmt.lib  oldnames.lib  Uuid.Lib 
 if errorlevel 1 exit %errorlevel%
 del *.obj *.lib
 ENDLOCAL
