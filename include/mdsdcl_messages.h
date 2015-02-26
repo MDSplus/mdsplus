28e50aa1898c046c60842fd7de84519114d8d4ab
Tom Fredian <twf@psfc.mit.edu>
2015-02-26 15:02:53 -0500

    Add new status message handling

---
+++ include/mdsdcl_messages.h
@@ -0,0 +1,22 @@
+
+
+#ifndef MDSDCL_MESSAGES
+#define MDSDCL_MESSAGES
+
+#define MdsdclSUCCESS              0x8020009
+#define MdsdclEXIT                 0x8020011
+#define MdsdclERROR                0x8020018
+#define MdsdclNORMAL               0x8020329
+#define MdsdclPRESENT              0x8020331
+#define MdsdclIVVERB               0x802033a
+#define MdsdclABSENT               0x8020340
+#define MdsdclNEGATED              0x802034b
+#define MdsdclNOTNEGATABLE         0x8020352
+#define MdsdclIVQUAL               0x802035a
+#define MdsdclPROMPT_MORE          0x8020362
+#define MdsdclTOO_MANY_PRMS        0x802036a
+#define MdsdclTOO_MANY_VALS        0x8020372
+#define MdsdclMISSING_VALUE        0x802037a
+
+
+#endif
