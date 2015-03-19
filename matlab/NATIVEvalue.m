73edac01c948e62109fcd4f2cc35ac5c0820f061
Tom Fredian <twf@psfc.mit.edu>
2015-03-19 15:45:20 -0400

    Add fixes from alpha to stable releases

--- matlab/NATIVEvalue.m
+++ matlab/NATIVEvalue.m
@@ -23,12 +23,20 @@ else
         switch class(mdsthing)
             case 'MDSplus.Int64Array'
                 result = reshape(mdsthing.getLongArray, shape);
+            case 'MDSplus.Uint64Array'
+                result = reshape(typecast(mdsthing.getLongArray, 'uint64'), shape);
             case 'MDSplus.Int32Array'
                 result = reshape(mdsthing.getIntArray, shape);
+            case 'MDSplus.Uint32Array'
+                result = reshape(typecast(mdsthing.getIntArray, 'uint32'), shape);
             case 'MDSplus.Int16Array'
                 result = reshape(mdsthing.getShortArray, shape);
+            case 'MDSplus.Uint16Array'
+                result = reshape(typecast(mdsthing.getShortArray, 'uint16'), shape);
             case 'MDSplus.Int8Array'
                 result = reshape(mdsthing.getByteArray, shape);
+            case 'MDSplus.Uint8Array'
+                result = reshape(typecast(mdsthing.getByteArray, 'uint8'), shape);
             case 'MDSplus.Float64Array'
                 result = double(reshape(mdsthing.getDoubleArray, shape));
             case 'MDSplus.Float32Array'
@@ -41,19 +49,27 @@ else
     elseif isa (mdsthing, 'MDSplus.Scalar')
         switch class(mdsthing)
             case 'MDSplus.Int64'
-                result = mdsthing.getLong;
+                result = mdsthing.getLongArray();
+            case 'MDSplus.Uint64'
+                result = typecast(mdsthing.getLongArray(), 'uint64');
             case 'MDSplus.Int32'
-                result = int32(mdsthing.getInt);
+                result = int32(mdsthing.getIntArray());
+            case 'MDSplus.Uint32'
+                result = typecast(mdsthing.getIntArray(), 'uint32');
             case 'MDSplus.Int16'
-                result = mdsthing.getShort;
+                result = mdsthing.getShortArray();
+            case 'MDSplus.Uint16'
+                result = typecast(mdsthing.getShortArray(), 'uint16');
             case 'MDSplus.Int8'
-                result = mdsthing.getByte;
+                result = mdsthing.getByteArray();
+            case 'MDSplus.Uint8'
+                result = typecast(mdsthing.getByteArray(), 'uint8');
             case 'MDSplus.Float64'
-                result = mdsthing.getDouble;
+                result = mdsthing.getDoubleArray();
             case 'MDSplus.Float32'
-                result = double(mdsthing.getFloat);
+                result = double(mdsthing.getFloatArray());
             case 'MDSplus.String'
-                result = mdsthing.getString;
+                result = char(mdsthing.getString());
             otherwise
                 throw(MException('MDSplus:NATIVEvalue', 'class %s not supported by NATIVEvalue function\n', class(mdsthing)));
         end
