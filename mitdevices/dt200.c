0219cb9fecacf6a41e0304f8a9fb22330ca50841
Tom Fredian <twf@psfc.mit.edu>
2015-03-30 14:46:01 -0400

    cleanup VMS tests

--- mitdevices/dt200.c
+++ mitdevices/dt200.c
@@ -10,24 +10,6 @@
 static void ManageChild(Widget w, char *name);
 static void UnManageChild(Widget w, char *name);
 
-#ifdef __VMS
-int DT200$DW_SETUP(struct dsc$descriptor *niddsc, struct dsc$descriptor *methoddsc, Widget parent)
-{
-  static String uids[] = { {"DECW$SYSTEM_DEFAULTS:DT200.UID"} };
-  static int nid;
-  static MrmRegisterArg uilnames[] = {
-    {"nid", (XtPointer) 0},
-    {"ManageChild", (XtPointer) ManageChild},
-    {"UnManageChild", (XtPointer) UnManageChild},
-  };
-  static NCI_ITM nci[] =
-      { {4, NCI$_CONGLOMERATE_NIDS, (unsigned char *)&nid, 0}, {0, NCI$_END_OF_LIST, 0, 0} };
-  TREE$GETNCI((int *)niddsc->dsc$a_pointer, nci);
-  uilnames[0].value = (XtPointer) nid;
-  return XmdsDeviceSetup(parent, (int *)niddsc->dsc$a_pointer, uids, XtNumber(uids), "DT200",
-			 uilnames, XtNumber(uilnames), 0);
-}
-#else
 int dt200__dw_setup(struct descriptor *niddsc, struct descriptor *methoddsc, Widget parent)
 {
   static String uids[] = { "DT200.uid" };
@@ -44,7 +26,6 @@ int dt200__dw_setup(struct descriptor *niddsc, struct descriptor *methoddsc, Wid
   return XmdsDeviceSetup(parent, (int *)niddsc->pointer, uids, XtNumber(uids), "DT200", uilnames,
 			 XtNumber(uilnames), 0);
 }
-#endif
 
 static Widget FindTop(Widget w)
 {
