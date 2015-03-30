b7c7eb66a5d3291980c4b7d7096bde86e2cd83a4
Tom Fredian <twf@psfc.mit.edu>
2015-03-30 13:23:16 -0400

    cleanup config.h

--- include/mdstypes.h
+++ include/mdstypes.h
@@ -2,19 +2,7 @@
 
 #define MDSTYPES_H
 
-#include <config.h>
-
 #ifdef _MSC_VER
-#undef HAVE_STDINT_H
-#endif
-
-#ifdef HAVE_STDINT_H
-
-#include <stdint.h>
-
-#else				/* HAVE_STDINT_H */
-
-#ifdef _WIN32
 
 #ifndef _OFF_T_DEFINED
 typedef __int64 off_t;
@@ -27,32 +15,11 @@ typedef off_t _off_t;
 
 #define _STDINT_H
 
-#else				/* _WIN32 */
+#else
 
-#if (SIZEOF__INT64 != 8)
+#include <stdint.h>
 
-#if (SIZEOF_LONG == 8)
+#endif
 
-typedef long int64_t;
-
-typedef unsigned long uint64_t;
-
-#elif (SIZEOF_LONG_LONG == 8)
-
-typedef long long int64_t;
-
-typedef unsigned long long uint64_t;
-
-#endif				/* SIZEOF_LONG */
-
-#else				/*SIZEOF_INT64 */
-
-typedef unsigned int64_t uint64_t;
-
-#endif				/* SIZEOF_INT64 */
-
-#endif				/* _WIN32 */
-
-#endif				/* HAVE_STDINT_H */
 
 #endif				/* MDSTYPES_H */
