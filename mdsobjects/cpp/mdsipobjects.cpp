af687874594573dd0edd8a3f8bb725f227d44df0
AndreaRigoni <andrea.rigoni@pd.infn.it>
2015-05-04 16:56:36 +0200

    adds compression level in Connection contructor

--- mdsobjects/cpp/mdsipobjects.cpp
+++ mdsobjects/cpp/mdsipobjects.cpp
@@ -29,6 +29,7 @@ extern "C" int MdsSetDefault(int sock, char *node);
 extern "C" int MdsClose(int sock);
 extern "C" int ConnectToMds(char *host);
 extern "C" int ConnectToMdsEvents(char *host);
+extern "C" int SetCompressionLevel(int level);
 extern "C" void DisconnectFromMds(int sockId);
 extern "C" void FreeMessage(void *m);
 
@@ -150,10 +151,11 @@ void *putManyObj(char *serializedIn)
 
 Mutex Connection::globalMutex;
 
-Connection::Connection(char *mdsipAddr) //mdsipAddr of the form <IP addr>[:<port>]
+Connection::Connection(char *mdsipAddr, int clevel) //mdsipAddr of the form <IP addr>[:<port>]
 {
     lockGlobal();
-	sockId = ConnectToMds(mdsipAddr);
+    SetCompressionLevel(clevel);
+    sockId = ConnectToMds(mdsipAddr);
     unlockGlobal();
 	if(sockId <= 0) {
 		std::string msg("Cannot connect to ");
