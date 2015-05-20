5b82b6df62c33bbd280cd5ad76869bc5f4f76849
Tom Fredian <twf@psfc.mit.edu>
2015-05-20 08:37:18 -0400

    Revert changes that broke builds

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
     info['rflavor']="-"+info['flavor']
   if os.environ['DIST'].startswith('el') or os.environ['DIST'].startswith('fc'):
     module=__import__('rpms',globals())
-  elif os.environ['DIST'].startswith('Ubuntu'):
+  elif os.environ['DIST'].startswith('Ubuntu') or os.environ['DIST'].startswith('Debian'):
     module=__import__('ubuntu',globals())
   elif os.environ['DIST'].startswith('raspberrypi'):
     module=__import__('raspberrypi',globals())
