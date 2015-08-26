557b25d94f1bcafab4a5af729560d0414999975d
zack-vii <zack.vii@live.com>
2015-08-26 13:26:21 +0200

    mdsconnect+compareIP

--- matlab/mdsconnect.m
+++ matlab/mdsconnect.m
@@ -1,39 +1,41 @@
 function [ status ] = mdsconnect( host )
-%mdsconnect - connect to a remote mdsplus data server. 
-%   
+%mdsconnect - connect to a remote mdsplus data server.
+%
 %      This routine will make a thin client connection to the specified
-%      mdsplus data server.  It will cause subsequent invocations of 
+%      mdsplus data server.  It will cause subsequent invocations of
 %      mdsopen, mdsvalue, mdsput, and mdsclose to be executed remotely
 %      on the specified host.
 %
 %      mdsdisconnect will destroy this connection, reverting the above
 %      described routines to their local behaviors
 %
-  
-    import MDSplus.Connection
-    global MDSplus_Connection_Host
-    global MDSplus_Connection_Obj
-   
-    status = 0;
-    if isa(MDSplus_Connection_Host, 'char')
-      if compareIP(MDSplus_Connection_Host, host) == 1 
-         status = 1;
-      end
-    end
-    if status == 0
-      if strcmp(upper(host),'LOCAL') == 1
+
+import MDSplus.Connection
+global MDSplus_Connection_Host
+global MDSplus_Connection_Obj
+
+status = compareIP(MDSplus_Connection_Host, host);
+if ~status
+    if strcmpi(host,'LOCAL')
         clearvars -global MDSplusConnection_Obj
         MDSplus_Connection_Host='LOCAL';
-        status = 1;
-      else
+        status = true;
+    else
         try
-          MDSplus_Connection_Obj=Connection(host);
-          MDSplus_Connection_Host=host;
-          status = 1;
+            MDSplus_Connection_Obj=Connection(host);
+            MDSplus_Connection_Host=host;
+            status = true;
         catch err
-	  status = -1;
+            if nargout==1
+                status = false;
+            else
+                rethrow(err)
+            end
         end
-      end
     end
 end
 
+
+function [ status ] = compareIP( ip1, ip2 )
+% so far only compares if the strings match
+status = ischar(ip1) && strcmp(ip1, ip2);
