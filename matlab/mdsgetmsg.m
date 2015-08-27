1055cef65224fda5d1d6f1a379d6c017a45f697c
zack-vii <zack.vii@live.com>
2015-08-27 11:22:53 +0200

    updated the help text
    explained how the error handling works

--- matlab/mdsgetmsg.m
+++ matlab/mdsgetmsg.m
@@ -1,5 +1,14 @@
-% translates the tdi message id into the tdi message
-% messages are cached in 'tdimsg.mat'
+%% msg = mdsgetmsg(id, [throwerror, except])
+% id         : message id
+% throwerror : throw an error by default flag      : false
+% except     : exception list of ids               : []
+%
+% translates the tdi message ID into the tdi message MSG
+% - If THROWERROR is true, an error with the tdi message is thrown,
+%   unless the ID is a member of the EXCEPT list.
+% - If THROWERROR is false, no error will be thrown,
+%   unless the ID is a member of the EXCEPT list.
+
 function msg = mdsgetmsg(id,throwerror,except)
 narginchk(1,3)
 if nargin<3, except     = [];
@@ -10,7 +19,7 @@ if ischar(id)
     error(id);
 else
     try
-        msg =  mdsvalue('_msg = REPEAT(0,255);MdsShr->MdsGetMsgDsc(val($),descr(_msg));TRIM(_msg)',id);
+        msg = mdsvalue('_msg = REPEAT(0,255);MdsShr->MdsGetMsgDsc(val($),descr(_msg));TRIM(_msg)',id);
     catch
         msg = sprintf('message not found: 0x%08x',id);
     end
