d0c1fd9dc8ad2ebc483076136774e741387379e5
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2015-09-04 15:58:32 +0200

    Interface for density control

---
+++ tdi/RfxDevices/RFXVICONTROL.py
@@ -0,0 +1,32 @@
+from MDSplus import *
+from ctypes import *
+
+class RFXVICONTROL(Device):
+  parts=[{'path':':COMMENT', 'type':'text'},
+  {'path':':FILLING_TYPE', 'type':'numeric', 'value':3},
+  {'path':':IMP_LEVEL', 'type':'numeric', 'value':100},
+  {'path':':PRESSURE', 'type':'numeric', 'value':0},
+  {'path':':CONTROL', 'type':'numeric', 'value':0},
+  {'path':':CTRL_START', 'type':'numeric', 'value':0},
+  {'path':':NE_SCALE', 'type':'numeric', 'value':0}]
+  
+  parts.append({'path':':FILL_WAVE','type':'signal', 'options':'compress_on_put'})
+  parts.append({'path':':FILL_MIN_X','type':'numeric', 'value':0})
+  parts.append({'path':':FILL_MAX_X','type':'numeric', 'value':1})
+  parts.append({'path':':FILL_MIN_Y','type':'numeric', 'value':0})
+  parts.append({'path':':FILL_MAX_Y','type':'numeric', 'value':10})
+
+  parts.append({'path':':PUFF_WAVE','type':'signal', 'options':'compress_on_put'})
+  parts.append({'path':':PUFF_MIN_X','type':'numeric', 'value':0})
+  parts.append({'path':':PUFF_MAX_X','type':'numeric', 'value':1})
+  parts.append({'path':':PUFF_MIN_Y','type':'numeric', 'value':0})
+  parts.append({'path':':PUFF_MAX_Y','type':'numeric', 'value':10})
+
+  parts.append({'path':':CTRL_WAVE','type':'signal', 'options':'compress_on_put'})
+  parts.append({'path':':CTRL_MIN_X','type':'numeric', 'value':0})
+  parts.append({'path':':CTRL_MAX_X','type':'numeric', 'value':1})
+  parts.append({'path':':CTRL_MIN_Y','type':'numeric', 'value':0})
+  parts.append({'path':':CTRL_MAX_Y','type':'numeric', 'value':10})
+
+  print 'RFXVICONTROL activated'
+  
