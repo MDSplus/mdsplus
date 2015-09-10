763fc74b995dcf616ef5966e126139822314b949
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2015-09-10 11:48:54 +0200

    Fixed inverted codes control

--- javadevices/RFXVICONTROLSetup.java
+++ javadevices/RFXVICONTROLSetup.java
@@ -75,7 +75,7 @@ public class RFXVICONTROLSetup extends DeviceSetup {
         deviceChoice1.setUpdateIdentifier("");
         jPanel4.add(deviceChoice1);
 
-        deviceChoice2.setChoiceIntValues(new int[] {0, 1});
+        deviceChoice2.setChoiceIntValues(new int[] {1, 0});
         deviceChoice2.setChoiceItems(new String[] {"Enabled", "Disabled"});
         deviceChoice2.setConvert(true);
         deviceChoice2.setIdentifier("");
