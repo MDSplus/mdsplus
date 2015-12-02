514c6424ddd2d9697c074b1484de7aa103c55fb7
zack-vii <zack.vii@live.com>
2015-12-02 14:25:08 +0100

    PyDoMethod.py workover

--- tdi/dev_support/PyDoMethod.py
+++ tdi/dev_support/PyDoMethod.py
@@ -1,31 +1,29 @@
-from __future__ import print_function
-import sys as _sys
-from MDSplus import TreeRef,MDSplusException,StringArray,Int32,Data
+from MDSplus import StringArray, Int32
 from MDSplus.mdsExceptions import TreeNOMETHOD
-
+from sys import stderr
 
 def PyDoMethod(n,method,*args):
-    top=n.conglomerate_nids[0]
-    c=top.record
-    q=c.qualifiers
-    model=str(c.model)
-    method=str(method)
-    status = TreeNOMETHOD.status
+    top = n.conglomerate_nids[0]
+    c = top.record
+    q = c.qualifiers
+    model = str(c.model)
+    method = str(method)
     if isinstance(q,StringArray):
         for i in range(len(q)):
-            exec( str(q[i])) in globals()
+            exec(str(q[i])) in globals()
     else:
         exec(str(q)) in globals()
     if (model in globals()) and (method in (globals()[model].__dict__)):
         try:
             device=globals()[model]
-            status = device.__dict__[str(method)](device(n),args)
-        except:
-            e = _sys.exc_info()[1]
-            print("Error: "+str(e),file=_sys.stderr)
-            if hassatr(e,'status'):
-                status = e.status
+            status = device.__dict__[str(method)](device(n),*args)
+        except Exception as exc:
+            stderr.write("Error: "+repr(exc)+"\n")
+            if hasattr(exc,'status'):
+                status = exc.status
             else:
                 status = 0
         Int32(status).setTdiVar("_method_status")
-    return status
+        return status
+    else:
+        return TreeNOMETHOD.status
