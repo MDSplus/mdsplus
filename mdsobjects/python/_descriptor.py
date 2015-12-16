fc1bee9891746ea6293dfe4a426495bad0301ab8
Tom Fredian <twf@psfc.mit.edu>
2015-12-16 16:20:57 -0500

    Fix tracking of tree instances to make sure all trees get closed after use

--- mdsobjects/python/_descriptor.py
+++ mdsobjects/python/_descriptor.py
@@ -381,10 +381,7 @@ class descriptor(_C.Structure):
                     raise Exception("_dtypes.DTYPE_DC is not yet supported")
                     return None
                 if (self.dtype == _dtypes.DTYPE_NID):
-                    if descriptor.tree is None:
-                      return _treenode.TreeNode(_C.cast(self.pointer,_C.POINTER(_C.c_int32)).contents.value,_tree.TreeRef())
-                    else:
-                      return _treenode.TreeNode(_C.cast(self.pointer,_C.POINTER(_C.c_int32)).contents.value,descriptor.tree)
+                    return _treenode.TreeNode(_C.cast(self.pointer,_C.POINTER(_C.c_int32)).contents.value,descriptor.tree,treeref=descriptor.tree is None)
                 if (self.dtype == _dtypes.DTYPE_PATH):
                     if descriptor.tree is None:
                       return _treenode.TreePath(_C.cast(self.pointer,_C.POINTER(_C.c_char*self.length)).contents.value,_tree.TreeRef())
