04cb31f96d52685fa217e21e21975c16eabbd780
AndreaRigoni <andrea.rigoni@pd.infn.it>
2015-12-24 13:18:29 +0100

    removes dummy leak

--- mdsobjects/cpp/testing/buildtest.cpp
+++ mdsobjects/cpp/testing/buildtest.cpp
@@ -1,6 +1,7 @@
 
 #include <stdlib.h>
 #include "testing.h"
+#include <mdsobjects.h>
 
 #include <limits>
 
@@ -9,12 +10,7 @@ int main(int argc, char *argv[])
     BEGIN_TESTING(build);
 
     TEST1(1);
-    TEST0(0);
-
-    char * p = (char *)malloc(1234);
-    strcpy(p,"ciao beo");
-    printf("%s",p);
-    
+    TEST0(0);    
     
     END_TESTING
 }
