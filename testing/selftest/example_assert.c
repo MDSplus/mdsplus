f60fc62dabe0ac3fc320080b9ca897814932f19f
AndreaRigoni <andrea.rigoni@pd.infn.it>
2016-01-08 10:02:24 +0100

    fix exit code...

--- testing/selftest/example_assert.c
+++ testing/selftest/example_assert.c
@@ -1,24 +1,18 @@
 #include <assert.h>
 #include "testing.h"
-#include "mdstestdummy.h"
 
+// #include "mdstestdummy.h"
 // extern void generate_false_assert();
 
 int main(int argc, char *argv[])
 {
-//    BEGIN_TESTING(ok);    
-//    int a=1;
-//    TEST1(a==1);
-//    END_TESTING;    
-    
-//    BEGIN_TESTING(not ok);    
-//    int a=1;
-//    TEST1(a==0);
-//    END_TESTING;    
-    
+    BEGIN_TESTING(ok);    
+    int a=1;
+    TEST1(a==1);
+    END_TESTING;    
+        
     BEGIN_TESTING(test direct assert);    
     int a=1;
-//    TEST1(a==1);
     assert( a==0 );
     assert( "test direct assert"&&0 );
     END_TESTING;    
