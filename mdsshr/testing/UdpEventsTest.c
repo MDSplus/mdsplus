512fa41f740b815f6422d5bfba5df6b39bfeb790
Tom Fredian <twf@psfc.mit.edu>
2016-02-11 14:38:40 -0500

    Add waits and loop possibility to test

--- mdsshr/testing/UdpEventsTest.c
+++ mdsshr/testing/UdpEventsTest.c
@@ -1,32 +1,39 @@
-
-#include <stdio.h>
 #include <pthread.h>
+#include <mdsshr.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
-
-#include <mdsshr.h>
-#include <testing.h>
-
-
-int ev_id;
+#include <time.h>
 
 void eventAst(void *arg, int len, char *buf) {
-    printf("received event in thread %d\n",syscall(__NR_gettid));
+    printf("received event in thread %d, name=%s\n",
+           syscall(__NR_gettid),
+           (char *)arg);
 }
 
-int main()
+#define WAIT nanosleep(&tspec,0)
+int main(int argc, char **args)
 {
+  struct timespec tspec = {0,1000000};
   int status;
-
-  MDSEventAst("test_event", eventAst, 0, &ev_id);
-  MDSEvent("test_event",0,0);
-  MDSEventCan(ev_id);
-  
-  MDSEventAst("test_event2", eventAst, 0, &ev_id);
-  MDSEvent("test_event2",0,0);
-  MDSEventCan(ev_id);
-
+  int i,iterations,ev_id;
+  if (argc < 2) {
+    iterations=3;
+  } else {
+    iterations=atoi(args[1]);
+    printf("Doing %d iterations\n",iterations);
+  }
+  for (i=0;i<iterations;i++) {
+    char *eventname = malloc(100);
+    sprintf(eventname,"ev_test_%d",i);
+    MDSEventAst(eventname, eventAst, eventname, &ev_id);
+    MDSEvent(eventname,0,0);
+    WAIT;
+    MDSEventCan(ev_id);
+    WAIT;
+    free(eventname);
+  }
   return 0;
 }
-
