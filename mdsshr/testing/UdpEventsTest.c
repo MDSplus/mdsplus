d96587be0ff93839fcb0dfcd9c594629b8714310
AndreaRigoni <andrea.rigoni@pd.infn.it>
2016-02-18 18:10:25 +0100

    added some Tap tests

--- mdsshr/testing/UdpEventsTest.c
+++ mdsshr/testing/UdpEventsTest.c
@@ -7,33 +7,86 @@
 #include <sys/syscall.h>
 #include <time.h>
 
+#include "testing.h"
+
+static int astCount = 0;
+
 void eventAst(void *arg, int len, char *buf) {
     printf("received event in thread %d, name=%s\n",
            syscall(__NR_gettid),
-           (char *)arg);
+           (char *)arg);    
+    astCount++;
 }
 
-#define WAIT nanosleep(&tspec,0)
+
+
+
+static int first = 0,second = 0;
+
+void eventAstFirst(void *arg, int len, char *buf) {
+    printf("received event in thread %d, name=%s\n",
+           syscall(__NR_gettid),
+           (char *)arg);    
+    first=1;
+}
+
+void eventAstSecond(void *arg, int len, char *buf) {
+    printf("received event in thread %d, name=%s\n",
+           syscall(__NR_gettid),
+           (char *)arg);    
+    second=1;
+}
+
+
+
+static void wait() {
+    static const struct timespec tspec = {0,1000000};
+    nanosleep(&tspec,0);
+}
+
 int main(int argc, char **args)
 {
-  struct timespec tspec = {0,1000000};
-  int status;
-  int i,iterations,ev_id;
-  if (argc < 2) {
-    iterations=3;
-  } else {
-    iterations=atoi(args[1]);
-    printf("Doing %d iterations\n",iterations);
-  }
-  for (i=0;i<iterations;i++) {
-    char *eventname = malloc(100);
-    sprintf(eventname,"ev_test_%d",i);
-    MDSEventAst(eventname, eventAst, eventname, &ev_id);
-    MDSEvent(eventname,0,0);
-    WAIT;
-    MDSEventCan(ev_id);
-    WAIT;
-    free(eventname);
-  }
-  return 0;
+    BEGIN_TESTING(UdpEvents); 
+    int status;
+    int i,iterations,ev_id;
+    if (argc < 2) {
+        iterations=3;
+    } else {
+        iterations=atoi(args[1]);
+        printf("Doing %d iterations\n",iterations);
+    }
+    
+    for (i=0;i<iterations;i++) {
+        char *eventname = malloc(100);
+        sprintf(eventname,"ev_test_%d",i);
+
+        status = MDSEventAst(eventname, eventAst, eventname, &ev_id);
+        TEST0( status%1 );        
+        status = MDSEvent(eventname,0,0);
+        TEST0( status%1 );        
+        status = MDSEvent(eventname,0,0);
+        TEST0( status%1 );                
+        wait();        
+        status = MDSEventCan(ev_id);
+        TEST0( status%1 );
+        wait();        
+        free(eventname);
+    }
+    TEST1(astCount == 2*iterations);
+    
+
+    // Testing two listening events //
+    int id1,id2;
+    status = MDSEventAst("test_event", eventAstFirst, "first", &id1);
+    status = MDSEventAst("test_event", eventAstSecond, "second", &id2);        
+    wait();
+    status = MDSEvent("test_event",0,0);    
+    wait();
+    printf("first = %d, second = %d\n",first,second);
+    TEST1(first);
+    TEST1(second);
+    status = MDSEventCan(id1);
+    //    status = MDSEventCan(id2);
+    
+    END_TESTING;
 }
