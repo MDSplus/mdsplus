14e9f791c7915d92f4390c87b83b8cdcaa9c4b73
Tom Fredian <twf@psfc.mit.edu>
2016-06-10 15:13:35 -0400

    Add testing to macosx

--- deploy/platform/macosx/macosx_build.sh
+++ deploy/platform/macosx/macosx_build.sh
@@ -6,6 +6,25 @@
 #
 #
 
+RED() {
+    if [ "$1" = "yes" ]
+    then
+	echo -e "\033[31;47m"
+    fi
+}
+GREEN() {
+    if [ "$1" = "yes" ]
+    then
+	echo -e "\033[32;47m"
+    fi
+}
+NORMAL() {
+    if [ "$1" = "yes" ]
+    then
+	echo -e "\033[m"
+    fi
+}
+
 set -e
 
 if [ -r ${SRCDIR}/deploy/os/${OS}.env ]
@@ -16,6 +35,36 @@ fi
 MAKE=${MAKE:="env LANG=en_US.UTF-8 make"}
 
 export PYTHONDONTWRITEBYTECODE=no
+if [ "$TEST" = "yes" ]
+then
+    rm -Rf ${WORKSPACE}/test
+    mkdir -p ${WORKSPACE}/test
+    MDSPLUS_DIR=${WORKSPACE}/test/buildroot/usr/local/mdsplus
+    mkdir -p ${MDSPLUS_DIR}
+    pushd ${WORKSPACE}/test/
+    ${SRCDIR}/configure \
+	    --prefix=${MDSPLUS_DIR} \
+	    --exec_prefix=${MDSPLUS_DIR} \
+	    --with-java_target=6 \
+	    --with-java_bootclasspath=${SRCDIR}/rt.jar \
+            --enable-debug
+    $MAKE
+    $MAKE install
+    if ( ! $MAKE -k tests 2>&1 )
+    then
+	RED $COLOR
+	cat <<EOF >&2
+======================================================
+
+Failure doing normal tests.
+
+======================================================
+EOF
+	NORMAL $COLOR
+	exit
+    fi
+fi
+    
 if [ "$RELEASE" = "yes" ]
 then
     mkdir -p ${RELEASEDIR}/${BRANCH}
