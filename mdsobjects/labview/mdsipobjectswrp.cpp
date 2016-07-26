a7146fa38d5bf123df2b52d8193639c21e94561e
root <root@niolin3.rfx.local>
2016-07-26 12:13:36 +0200

    Added support for conenction put signal

--- mdsobjects/labview/mdsipobjectswrp.cpp
+++ mdsobjects/labview/mdsipobjectswrp.cpp
@@ -104,4 +104,32 @@ EXPORT void mdsplus_connection_closeTree(const void *lvConnectionPtr,  ErrorClus
 	fillErrorCluster(errorCode, errorSource, errorMessage, error);
 }
 
+EXPORT void mdsplus_connection_putSignal(const void *lvConnectionPtr, const void *lvDataPtrIn, const void *lvTimePtrIn, const char *pathIn, ErrorCluster *error)
+{
+	MDSplus::Data *dataPtrIn = NULL;
+	MDSplus::Data *timePtrIn = NULL;
+	MDSplus::Data *args[2];
+	MgErr errorCode = noErr;
+	const char *errorSource = __FUNCTION__;
+	char const * errorMessage = (char *)"";
+	try
+	{
+		MDSplus::Connection * connectionPtr = reinterpret_cast<MDSplus::Connection *>(const_cast<void *>(lvConnectionPtr));
+		dataPtrIn = reinterpret_cast<MDSplus::Data *>(const_cast<void *>(lvDataPtrIn));
+		timePtrIn = reinterpret_cast<MDSplus::Data *>(const_cast<void *>(lvTimePtrIn));
+		args[0] = dataPtrIn;
+		args[1] = timePtrIn;
+		if(connectionPtr && timePtrIn)
+		    connectionPtr->put(pathIn, (char *)"BUILD_SIGNAL($1,*,$2)", args, 2);
+	}
+	catch (const MDSplus::MdsException & e)
+	{
+		errorCode = bogusError;
+		errorMessage = e.what();
+		fillErrorCluster(errorCode, errorSource, errorMessage, error);
+		return;
+	}
+	fillErrorCluster(errorCode, errorSource, errorMessage, error);
+}
+
 
