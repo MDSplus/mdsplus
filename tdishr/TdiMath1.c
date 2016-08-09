5424f37a5c4a46edf0ec5e01cba5d9c1f4ba2cbf
kgerickson <kerickso@pppl.gov>
2016-08-09 10:59:06 -0400

    tdishr/TdiMath1.c: Use C math PI constants

--- tdishr/TdiMath1.c
+++ tdishr/TdiMath1.c
@@ -251,20 +251,20 @@ STATIC_ROUTINE void log_complex(double *in, double *out)
   if (in[0] > 0.0)
     theta = 0.0;
   else if (in[0] < 0.0 && in[1] >= 0.0)
-    theta = 3.1415927;
+    theta = M_PI;
   else if (in[0] < 0.0)
-    theta = -3.1415927;
+    theta = -M_PI;
   else if (in[0] == 0 && in[1] == 0) {
     out[0] = 0.0;
     out[1] = 0.0;
     return;
   } else if (in[0] == 0.0 && in[1] > 0.0) {
     out[0] = log(in[1]);
-    out[1] = 1.570963;
+    out[1] = M_PI_2;
     return;
   } else if (in[0] == 0.0) {
     out[0] = log(fabs(in[1]));
-    out[1] = -1.570963;
+    out[1] = -M_PI_2;
     return;
   }
   out[1] = atan(in[1] / in[0]) + theta;
