e31aca461dcd1b4b8ef8c5db57455f3f120a99c1
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2016-08-29 17:52:13 +0200

    retain only mdsexpr record

--- epics/mdsrecords/devMeSoft.c
+++ epics/mdsrecords/devMeSoft.c
@@ -4,6 +4,7 @@
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -29,92 +30,102 @@
 #include "epicsThread.h"
 
 /* Create the dset for devWfSoft */
-static long init_record(waveformRecord * prec);
-static long read_me(waveformRecord * prec);
+static long init_record(waveformRecord *prec);
+static long read_me(waveformRecord *prec);
 extern int registerMdsEvent(char *eventName);
 extern int waitMdsEvent(char *eventName, char *buf, int maxLen, int *retLen);
 struct {
-  long number;
-  DEVSUPFUN report;
-  DEVSUPFUN init;
-  DEVSUPFUN init_record;
-  DEVSUPFUN get_ioint_info;
-  DEVSUPFUN read_me;
+    long      number;
+    DEVSUPFUN report;
+    DEVSUPFUN init;
+    DEVSUPFUN init_record;
+    DEVSUPFUN get_ioint_info;
+    DEVSUPFUN read_me;
 } devMeSoft = {
-5, NULL, NULL, init_record, NULL, read_me};
-
+    5,
+    NULL,
+    NULL,
+    init_record,
+    NULL,
+    read_me
+};
 epicsExportAddress(dset, devMeSoft);
 
-static void callbackFunc(CALLBACK * pcallback)
+static void callbackFunc(CALLBACK *pcallback)
 {
-  struct dbCommon *precord;
-  rset *prset;
-
-  callbackGetUser(precord, pcallback);
-  prset = (rset *) (precord->rset);
-  dbScanLock(precord);
-  (*prset->process) (precord);
-  dbScanUnlock(precord);
+     struct dbCommon  *precord;
+     rset      *prset;
+     
+     callbackGetUser(precord,pcallback);
+     prset = (rset *)(precord->rset);
+     dbScanLock(precord);
+     (*prset->process)(precord);
+     dbScanUnlock(precord);
 }
 
-static long init_record(waveformRecord * prec)
+
+
+static long init_record(waveformRecord *prec)
 {
-  CALLBACK *pcallback;
-  pcallback = (CALLBACK *) (calloc(1, sizeof(CALLBACK)));
-  callbackSetCallback(callbackFunc, pcallback);
-  callbackSetUser(prec, pcallback);
-  prec->dpvt = (void *)pcallback;
-  registerMdsEvent(prec->inp.value.instio.string);
-  return 0;
+     CALLBACK *pcallback;
+     pcallback = (CALLBACK *)(calloc(1,sizeof(CALLBACK)));
+     callbackSetCallback(callbackFunc,pcallback);
+     callbackSetUser(prec,pcallback);
+     prec->dpvt = (void *)pcallback;
+     registerMdsEvent(prec->inp.value.instio.string);
+     return 0;
 }
 
-static long asynchRead(CALLBACK * pcallback)
+static long asynchRead(CALLBACK *pcallback)
 {
-  struct dbCommon *precord;
-  waveformRecord *prec;
-  int retLen, maxLen, elSize = 1;
+     struct dbCommon  *precord;
+     waveformRecord *prec;
+     int retLen, maxLen, elSize = 1;
 
-  callbackGetUser(precord, pcallback);
-  prec = (waveformRecord *) precord;
-  switch (prec->ftvl) {
-  case DBF_UCHAR:
-  case DBF_CHAR:
-    elSize = 1;
-    break;
-  case DBF_USHORT:
-  case DBF_SHORT:
-    elSize = 2;
-    break;
-  case DBF_ULONG:
-  case DBF_LONG:
-  case DBF_FLOAT:
-    elSize = 4;
-    break;
-  case DBF_DOUBLE:
-    elSize = 8;
-    break;
-  default:
-    printf("Unexpected type for Waveform MDSplus event record\n");
+     callbackGetUser(precord,pcallback);
+     prec = (waveformRecord *)precord;
+     switch(prec->ftvl)  {
+     	case DBF_UCHAR: 
+     	case DBF_CHAR: 
+     		elSize = 1;
+     		break;
+     	case DBF_USHORT: 
+     	case DBF_SHORT: 
+     		elSize = 2;
+     		break;
+      	case DBF_ULONG: 
+      	case DBF_LONG: 
+      	case DBF_FLOAT: 
+     		elSize = 4;
+     		break;
+      	case DBF_DOUBLE: 
+     		elSize = 8;
+     		break;
+    	default:	
+        	printf("Unexpected type for Waveform MDSplus event record\n");
+        	return 0;
+     }	
+     maxLen = prec->nelm*elSize;
+     waitMdsEvent(prec->inp.value.instio.string, prec->bptr, maxLen, &retLen); 
+     	prec->nord = retLen/elSize;
+
+     callbackRequest(pcallback);
+     return 0;
+}
+
+
+static long read_me(waveformRecord *prec)
+{
+     CALLBACK *pcallback = (CALLBACK *)prec->dpvt;
+     if(prec->pact) {
+          prec->udf = FALSE;
+          return 0;
+     }
+     prec->pact=TRUE;
+
+    epicsThreadCreate("MDSPUT_THREAD", epicsThreadPriorityHigh, epicsThreadGetStackSize(epicsThreadStackMedium), asynchRead, pcallback);
     return 0;
-  }
-  maxLen = prec->nelm * elSize;
-  waitMdsEvent(prec->inp.value.instio.string, prec->bptr, maxLen, &retLen);
-  prec->nord = retLen / elSize;
-
-  callbackRequest(pcallback);
-  return 0;
 }
 
-static long read_me(waveformRecord * prec)
-{
-  CALLBACK *pcallback = (CALLBACK *) prec->dpvt;
-  if (prec->pact) {
-    prec->udf = FALSE;
-    return 0;
-  }
-  prec->pact = TRUE;
 
-  epicsThreadCreate("MDSPUT_THREAD", epicsThreadPriorityHigh,
-		    epicsThreadGetStackSize(epicsThreadStackMedium), asynchRead, pcallback);
-  return 0;
-}
+
