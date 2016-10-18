35e744ea3edb333a6e7232a489e4c6948824abfc
GabrieleManduchi <gabriele.manduchi@igi.cnr.it>
2016-10-18 15:29:29 +0200

    Jscope fix (#672)
    * Fix time dependent X-Y plot
    
    * Fix missing semicolon in TDI expression
    
    * Fix missing pharenthesis in TDI expression
    
    * Support for date x min and xmax

--- javascope/jScope/XYWaveData.java
+++ javascope/jScope/XYWaveData.java
@@ -143,7 +143,11 @@ public class XYWaveData implements WaveData
     /*
      * Read data within specified interval. either Xmin or xmax cna specify no limit (-Float.MAX_VALUE, Float.MAX_VALUE)
      */
-    public XYData getData(double xmin, double xmax, int numPoints)
+     public XYData getData(long xmin, long xmax, int numPoints)
+     {
+         return getData((double)xmin, (double)xmax, numPoints);
+     }
+     public XYData getData(double xmin, double xmax, int numPoints)
     {
         int minIdx, maxIdx;
         
