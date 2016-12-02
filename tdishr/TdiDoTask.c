MDSplus/mdsplus

commit f18f13a696463269039531e6ec55813d6acd69d9

libtrtl user int

tdishr/TdiDoTask.c
@@ -61,7 +61,7 @@ STATIC_ROUTINE void TASK_AST(int astpar, int r0, int r1, int *pc, int psl)
 STATIC_ROUTINE int Doit(struct descriptor_routine *ptask, struct descriptor_xd *out_ptr)
 {
   int dtype, ndesc, j, status;
-  int *arglist[256];
+  void **arglist[256];
   ndesc = ptask->ndesc;
   while (ndesc > 3 && ptask->arguments[ndesc - 4] == 0)
     ndesc--;
@@ -76,20 +76,18 @@ STATIC_ROUTINE int Doit(struct descriptor_routine *ptask, struct descriptor_xd *
       status = TdiData(pmethod->method, &method_d MDS_END_ARG);
       if (status & 1)
 	status = TdiGetNid(pmethod->object, &nid);
-      arglist[0] = (int *)(long)ndesc + 1;
-      arglist[1] = (int *)&nid_dsc;
-      arglist[2] = (int *)&method_d;
+      *(int*)&arglist[0] = ndesc + 1;
+      arglist[1] = (void*)&nid_dsc;
+      arglist[2] = (void*)&method_d;
 
       /*** skip timeout,method,object ***/
       for (j = 3; j < ndesc; ++j)
-	arglist[j] = (int *)pmethod->arguments[j - 3];
-      arglist[ndesc] = (int *)out_ptr;
+	arglist[j] = (void *)pmethod->arguments[j - 3];
+      arglist[ndesc] = (void *)out_ptr;
       arglist[ndesc + 1] = MdsEND_ARG;
       if (status & 1) {
-	status = (long)LibCallg(arglist, TreeDoMethod);
-	if (!(status & 1)) {
-	  status = TdiPutLong(&status, out_ptr);
-	}
+        struct descriptor_s out = { sizeof(void *) , DTYPE_POINTER, CLASS_S , LibCallg(arglist, TreeDoMethod) };
+        MdsCopyDxXd((struct descriptor*)&out, out_ptr);
       }
       StrFree1Dx(&method_d);
       break;
@@ -100,7 +98,6 @@ STATIC_ROUTINE int Doit(struct descriptor_routine *ptask, struct descriptor_xd *
       struct descriptor_program *prog_task = (struct descriptor_program *)ptask;
       if (prog_task->program && prog_task->program->dtype == DTYPE_T)
 	status = LibSpawn(prog_task->program, 1, 0) == 0;
-
       status = TdiPutLong(&status, out_ptr);
 
     }

mdsshr/librtl.c
@@ -87,7 +87,7 @@ EXPORT int LibWait(const float *secs)
 ///
 EXPORT void *LibCallg(void **arglist, void *(*routine) ())
 {
-  switch (*(long *)arglist & 0xff) {
+  switch (*(int*)&arglist[0] & 0xff) {
   case 0:
     return (*routine) ();
   case 1:
@@ -235,7 +235,7 @@ EXPORT void *LibCallg(void **arglist, void *(*routine) ())
 
 #ifdef _WIN32
 
-STATIC_ROUTINE char *GetRegistry(HKEY where, char *pathname)
+STATIC_ROUTINE char *GetRegistry(HKEY where, const char *pathname)
 {
   HKEY regkey;
   unsigned char *path = NULL;
@@ -253,9 +253,7 @@ STATIC_ROUTINE char *GetRegistry(HKEY where, char *pathname)
 }
 
 
-EXPORT int LibSpawn(struct descriptor *cmd, int waitFlag, int notifyFlag)
-{
-
+EXPORT int LibSpawn(struct descriptor *cmd, int waitFlag, int notifyFlag __attribute__ ((unused))){
   char *cmd_c = MdsDescrToCstring(cmd);
   int status;
   void *arglist[255];

tdishr/TdiCall.c
@@ -140,8 +140,7 @@ int TdiCall(int opcode, int narg, struct descriptor *list[], struct descriptor_x
     printf("%s\n", LibFindImageSymbolErrString());
   MdsFree1Dx(&entry, NULL);
   MdsFree1Dx(&image, NULL);
-
-  newdsc[0] = (struct descriptor *)(long)(narg - 2);
+  *(int *)&newdsc[0] = narg - 2;
   for (j = 2; j < narg && status & 1; ++j) {
     for (pfun = (struct descriptor_function *)list[j]; pfun && pfun->dtype == DTYPE_DSC;)
       pfun = (struct descriptor_function *)pfun->pointer;

tdishr/TdiExtFunction.c
@@ -110,7 +110,7 @@ int Tdi1ExtFunction(int opcode __attribute__ ((unused)),
         Requires: image found and routine symbol found.
         **********************************************/
   if (status & 1) {
-    new[0] = (struct descriptor *)(long)(narg - 1);
+    *(int*)&new[0] = narg - 1;
     new[narg - 1] = (struct descriptor *)out_ptr;
     for (j = 2; j < narg && status & 1; ++j) {
       pfun = (struct descriptor_function *)(new[j - 1] = list[j]);
@@ -159,15 +159,15 @@ int Tdi1ExtFunction(int opcode __attribute__ ((unused)),
          Watch, may not be XD.
          *************************/
     if (status & 1) {
-      status = (long)LibCallg(&new[0], routine);
+      struct descriptor_s out = { sizeof(void *) , DTYPE_POINTER, CLASS_S , LibCallg(&new[0], routine) };
+      MdsCopyDxXd((struct descriptor*)&out, out_ptr);
     }
     for (; --ntmp >= 0;)
       MdsFree1Dx(&tmp[ntmp], NULL);
-  }
-	/***************
-     Gather, compile.
-        ***************/
-  else {
+  } else {
+    /***************
+    Gather, compile.
+    ***************/
     struct descriptor_d file = { 0, DTYPE_T, CLASS_D, 0 };
     status =
       StrConcat((struct descriptor *)&file,

commit cd195e70230d63bc03415a0d449ca1821abdf628

fixed error introduced by change made in f18f13a

tdishr/TdiDoTask.c
@@ -85,10 +85,8 @@ STATIC_ROUTINE int Doit(struct descriptor_routine *ptask, struct descriptor_xd *
 	arglist[j] = (void *)pmethod->arguments[j - 3];
       arglist[ndesc] = (void *)out_ptr;
       arglist[ndesc + 1] = MdsEND_ARG;
-      if (status & 1) {
-        struct descriptor_s out = { sizeof(void *) , DTYPE_POINTER, CLASS_S , LibCallg(arglist, TreeDoMethod) };
-        MdsCopyDxXd((struct descriptor*)&out, out_ptr);
-      }
+      void* result = LibCallg(arglist, TreeDoMethod);
+      status = TdiPutLong((int*)&result, out_ptr);
       StrFree1Dx(&method_d);
       break;
     }