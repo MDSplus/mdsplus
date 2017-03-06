0ca61ca324ddeb77162ede041dedc73d1258b02c
AndreaRigoni <andrea.rigoni@pd.infn.it>
2017-03-06 19:08:45 +0100

    Fix: jScope was missing jdocs in jar file

--- javascope/jScope/AboutWindow.java
+++ javascope/jScope/AboutWindow.java
@@ -8,52 +8,51 @@ import java.io.IOException;
 
 
 public class AboutWindow extends JLabel  {
-	static String javaVersion = System.getProperty("java.version");
-	ImageIcon io = null;
-  
-	public AboutWindow() {
-		String icon_file = jScopeFacade.findFileInClassPath("about_jscope.jpg");
-		if (icon_file != null)
-			io = new ImageIcon(icon_file);
-		else
-			io = new ImageIcon(getClass().getClassLoader().getResource("about_jscope.jpg"));
+    static String javaVersion = System.getProperty("java.version");
+    ImageIcon io = null;
 
-		if (jScopeFacade.is_debug)
-			System.out.println("about_jscope image path " + icon_file + io);
-		setIcon(io);
-	}
-  
-	public void update(Graphics g) {
-		paint(g);
+    public AboutWindow() {
+	String icon_file = jScopeFacade.findFileInClassPath("jdocs/about_jscope.jpg");
+	if (icon_file != null)
+	io = new ImageIcon(icon_file);
+	else
+	io = new ImageIcon(getClass().getClassLoader().getResource("jdocs/about_jscope.jpg"));
+
+	if (jScopeFacade.is_debug)
+	System.out.println("about_jscope image path " + icon_file + io);
+	if(io != null) setIcon(io);
 	}
 
-	public void paint(Graphics gReal) {
-		if(io == null)
-			return;
+    public void update(Graphics g) {
+	paint(g);
+	}
 
-  	//Double buffering
-	  Image imageBuffer = createImage( getWidth(), getHeight() );
-	  Graphics g = imageBuffer.getGraphics();
+    public void paint(Graphics gReal) {
+	if(io == null)
+	return;
 
-    
-    Image image = io.getImage();
-  
-    g.drawImage( image, 1, 1, null );
-    Toolkit.getDefaultToolkit().sync();
+	//Double buffering
+	Image imageBuffer = createImage( getWidth(), getHeight() );
+	Graphics g = imageBuffer.getGraphics();
 
-    int start  = 32+2;
-    int top    = 102+1;
-    int botton = 268+1;
-    int delta  = 14;
-    
-    g.setColor( new Color( 128, 128, 128 ) );
-    g.drawRect( -1, -1, getWidth(), getHeight() ); // makes a bevel border likeness
-    
-    g.drawString( jScopeFacade.VERSION, start, botton - 3*delta);
-    g.drawString( "http://www.igi.cnr.it",  start, botton - 2*delta );
-    g.drawString( "JVM used :" +  javaVersion, start, botton -  delta );
-    
+	Image image = io.getImage();
+
+	g.drawImage( image, 1, 1, null );
+	Toolkit.getDefaultToolkit().sync();
+
+	int start  = 32+2;
+	int top    = 102+1;
+	int botton = 268+1;
+	int delta  = 14;
+
+	g.setColor( new Color( 128, 128, 128 ) );
+	g.drawRect( -1, -1, getWidth(), getHeight() ); // makes a bevel border likeness
+
+	g.drawString( jScopeFacade.VERSION, start, botton - 3*delta);
+	g.drawString( "http://www.igi.cnr.it",  start, botton - 2*delta );
+	g.drawString( "JVM used :" +  javaVersion, start, botton -  delta );
+
 	gReal.drawImage( imageBuffer, 0, 0, this );
-  }
-  
-}
+	}
+
+    }
