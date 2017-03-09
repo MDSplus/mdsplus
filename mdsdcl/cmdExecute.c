9182706f4b3206fc0d74ebed2d92cb87f68d4f0e
Timo Schroeder <timo.schroeder@ipp.mpg.de>
2017-03-09 17:00:59 +0000

    Fix: use INITIALIZER if supported

--- mdsdcl/cmdExecute.c
+++ mdsdcl/cmdExecute.c
@@ -1,10 +1,10 @@
-#define _GNU_SOURCE
+#include <config.h>
+#include <pthread_port.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 #include <ctype.h>
-#include <config.h>
 #include <libroutines.h>
 
 #include <libxml/tree.h>
@@ -14,16 +14,11 @@
 #include <mdsshr.h>
 #include <dcl.h>
 #include <mdsdcl_messages.h>
-#include <pthread.h>
-#ifndef PTHREAD_MUTEX_RECURSIVE
-#define PTHREAD_MUTEX_RECURSIVE PTHREAD_MUTEX_RECURSIVE_NP
-#endif
 #include "dcl_p.h"
 
 static dclDocListPtr dclDocs = 0;
 
-dclDocListPtr mdsdcl_getdocs()
-{
+dclDocListPtr mdsdcl_getdocs(){
   return dclDocs;
 }
 
@@ -31,8 +26,7 @@ dclDocListPtr mdsdcl_getdocs()
  \param p [in,out] the address of a pointer to  a dclParameter struct.
 */
 
-void freeParameter(dclParameterPtr * p_in)
-{
+void freeParameter(dclParameterPtr * p_in){
   dclParameterPtr p = *p_in;
   if (p) {
     int i;
@@ -832,7 +826,7 @@ static int dispatchToHandler(char *image, dclCommandPtr cmd, dclCommandPtr cmdDe
 	 - A pointer to an error string where any specific error information can
 	   be returned.
 	 - A pointer to an output string.
-   
+
   \param docList [in] The list of loaded command tables.
   \param verbNode [in] The node of the matching verb in the command xml table.
   \param cmd [in] The parsed user command definition.
@@ -1352,54 +1346,50 @@ int mdsdcl_get_input_nosymbols(char *prompt __attribute__ ((unused)),
 static void (*MDSDCL_OUTPUT_RTN) (char *output) = 0;
 static void (*MDSDCL_ERROR_RTN) (char *error) = 0;
 
-EXPORT void *mdsdclSetOutputRtn(void (*rtn) ())
-{
+EXPORT void *mdsdclSetOutputRtn(void (*rtn) ()){
   void *old_rtn = MDSDCL_OUTPUT_RTN;
   MDSDCL_OUTPUT_RTN = rtn;
   return old_rtn;
 }
 
-EXPORT void mdsdclFlushOutput(char *output)
-{
+EXPORT void mdsdclFlushOutput(char *output){
   if (MDSDCL_OUTPUT_RTN) {
     MDSDCL_OUTPUT_RTN(output);
   }
 }
 
-EXPORT void *mdsdclSetErrorRtn(void (*rtn) ())
-{
+EXPORT void *mdsdclSetErrorRtn(void (*rtn) ()){
   void *old_rtn = MDSDCL_ERROR_RTN;
   MDSDCL_ERROR_RTN = rtn;
   return old_rtn;
 }
 
-EXPORT void mdsdclFlushError(char *error)
-{
+EXPORT void mdsdclFlushError(char *error){
   if (MDSDCL_ERROR_RTN) {
     MDSDCL_ERROR_RTN(error);
   }
 }
 
-static pthread_mutex_t lock;
-
+#ifdef PTHREAD_RECURSIVE_MUTEX_INITIALIZER
+static pthread_mutex_t dcl_mutex = PTHREAD_RECURSIVE_MUTEX_INITIALIZER;
+void dclLock()  {pthread_mutex_lock  (&dcl_mutex);}
+#else
+static pthread_mutex_t dcl_mutex;
 void dclLock()
 {
   static pthread_mutex_t initMutex = PTHREAD_MUTEX_INITIALIZER;
-  static int initialized=0;
+  static int initialized = 0;
   pthread_mutex_lock(&initMutex);
   if (!initialized) {
     pthread_mutexattr_t m_attr;
     pthread_mutexattr_init(&m_attr);
     pthread_mutexattr_settype(&m_attr, PTHREAD_MUTEX_RECURSIVE);
     pthread_mutex_init(&lock, &m_attr);
-    initialized=1;
+    pthread_mutexattr_destroy(&m_attr);
+    initialized = 1;
   }
   pthread_mutex_unlock(&initMutex);
   pthread_mutex_lock(&lock);
 }
-
-void dclUnlock()
-{
-  pthread_mutex_unlock(&lock);
-}
-
+#endif
+void dclUnlock(){pthread_mutex_unlock(&dcl_mutex);}
