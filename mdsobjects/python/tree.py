9605aabba3b9bd505be45ae6918a054bd20a2329
zack-vii <zack.vii@live.com>
2017-05-16 14:49:16 +0200

    Fix: reuse global context if current context is not initiated by Tree(name, shot,[mode])

--- mdsobjects/python/tree.py
+++ mdsobjects/python/tree.py
@@ -93,9 +93,9 @@ class _TreeCtx(object): # HINT: _TreeCtx begin
         default to its own context. One may also provide a context ctx,
         as done by _tree.Tree.tcl()
         """
-        if ctx:
-            _TreeCtx.local.tctx = _TreeCtx(_treeshr.switchDbid(ctx),None)
-            return None
+        if ctx: return _treeshr.switchDbid(ctx)
+        tctx = _TreeCtx.gettctx()
+        if tctx: return _treeshr.switchDbid(tctx.ctx)
         dbid = _treeshr.switchDbid()
         if dbid:
             _treeshr.switchDbid(dbid)
@@ -104,7 +104,8 @@ class _TreeCtx(object): # HINT: _TreeCtx begin
 
     @staticmethod
     def restoreCtx(ctx,opened):
-        if not ctx:
+        if ctx: _treeshr.switchDbid(opened)
+        else:
             dbid =_treeshr.switchDbid()
             if dbid: _treeshr.switchDbid(dbid)
             _TreeCtx.local.tctx = _TreeCtx(dbid,opened)
@@ -114,6 +115,8 @@ class Tree(object):
 
     _lock=_threading.RLock()
     _id=0
+    tctx = None
+    ctx  = None
 
     def _checkCtx(self,*args):
         if len(args) == 0:
@@ -130,7 +133,7 @@ class Tree(object):
     def __enter__(self):
     	return self
     def __del__(self):
-        if self.ctx and _TreeCtx.canClose(self.ctx.value):
+        if self.tctx and self.ctx and _TreeCtx.canClose(self.ctx.value):
             self.__exit__()
     def __exit__(self, *args):
         """ Cleanup for with statement. If tree is open for edit close it. """
@@ -141,7 +144,8 @@ class Tree(object):
                  self.close()
         except _Exceptions.TreeNOT_OPEN:
             pass
-
+        if self.tctx:
+            del(self.tctx,self.ctx)
     def __getattr__(self,name):
         """
         Implements value=tree.attribute
@@ -230,7 +234,8 @@ class Tree(object):
             self.tctx = _TreeCtx(self.ctx.value,opened)
             self.tree = self.name
             self.shot = self.shotid
-            _TreeCtx.local.tctx = _TreeCtx(_treeshr.switchDbid(self.ctx.value),None)
+            dbid = _treeshr.switchDbid(self.ctx.value)
+            #_TreeCtx.local.tctx = _TreeCtx(_treeshr.switchDbid(self.ctx.value),None)
         finally:
             _hard_lock.release()
 
