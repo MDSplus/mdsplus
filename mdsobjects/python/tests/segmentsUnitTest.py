c896e3d13652e650ab7529d5afef224c845ddc12
zack-vii <zack.vii@live.com>
2017-06-21 20:57:44 +0200

    Tests: Added writeSegments test

--- mdsobjects/python/tests/segmentsUnitTest.py
+++ mdsobjects/python/tests/segmentsUnitTest.py
@@ -55,12 +55,81 @@ class segmentsTests(TestCase):
         self.assertEqual(shape[1],retShape[1])
         self.assertEqual(shape[2],retShape[2])
 
+    def writeSegments(self):
+        ptree = Tree('seg_tree',-1,'NEW')
+        ptree.addNode('MS')
+        ptree.addNode('MS_MD')
+        ptree.addNode('MTS')
+        ptree.addNode('MTS_MD')
+        ptree.addNode('PS')
+        ptree.addNode('PR')
+        ptree.addNode('PTS')
+        ptree.write()
+        ptree = Tree('seg_tree',-1)
+        dim = [2,4,6,8]
+        dat = [[1,1],[2,2],[3,3],[4,4]]
+        ### makeSegment ###
+        node = ptree.MS
+        for i in range(4):
+            node.makeSegment(dim[i]-1,dim[i]+1,[dim[i]],[dat[i]])
+        self.assertEqual(node.getSegmentLimits(1),(3,5))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+        ### makeSegment multidim ###
+        node = ptree.MS_MD
+        for i in range(0,4,2):
+            node.makeSegment(dim[i]-1,dim[i+1]+1,dim[i:i+2],dat[i:i+2])
+        self.assertEqual(node.getSegmentLimits(1),(5,9))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+        ### makeTimestampedSegment ###
+        node = ptree.MTS
+        for i in range(4):
+            node.makeTimestampedSegment([dim[i]],[dat[i]])
+        self.assertEqual(node.getSegmentLimits(1),(4,4))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+        ### makeTimestampedSegment multidim ###
+        node = ptree.MTS_MD
+        for i in range(0,4,2):
+            node.makeTimestampedSegment(dim[i:i+2],dat[i:i+2])
+        self.assertEqual(node.getSegmentLimits(1),(6,8))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+        ### putSegment ###
+        node = ptree.PS
+        for i in range(0,4,2):
+            node.beginSegment(dim[i]-1,dim[i+1]+1,dim[i:i+2],[[0,0],[0,0]])
+            node.putSegment(dat[i],0)
+            node.putSegment(dat[i+1],1)
+        self.assertEqual(node.getSegmentLimits(1),(5,9))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+        ### putRow ###
+        node = ptree.PR
+        for i in range(0,4,2):
+            node.beginTimestampedSegment([[0,0],[0,0]])
+            node.putRow(64,dat[i],dim[i])
+            node.putRow(64,dat[i+1],dim[i+1])
+        self.assertEqual(node.getSegmentLimits(1),(6,8))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+        ### putTimestampedSegment ###
+        node = ptree.PTS
+        for i in range(0,4,2):
+            node.beginTimestampedSegment([[0,0],[0,0]])
+            node.putTimestampedSegment([dim[i]],[dat[i]])
+            node.putTimestampedSegment([dim[i+1]],[dat[i+1]])
+        self.assertEqual(node.getSegmentLimits(1),(6,8))
+        self.assertEqual(node.record.dim_of().tolist(),dim)
+        self.assertEqual(node.record.data().tolist(),dat)
+
     def runTest(self):
         for test in self.getTests():
             self.__getattribute__(test)()
     @staticmethod
     def getTests():
-        return ['arrayDimensionOrder']
+        return ['arrayDimensionOrder','writeSegments']
     @classmethod
     def getTestCases(cls):
         return map(cls,cls.getTests())
@@ -70,7 +139,7 @@ def suite():
 
 def run():
     from unittest import TextTestRunner
-    TextTestRunner().run(suite())
+    TextTestRunner(verbosity=10).run(suite())
 
 if __name__=='__main__':
     import sys
