85b4ee402093ab5f2cfa3dba96ed5c9628ddebf3
AndreaRigoni <andrea.rigoni@pd.infn.it>
2017-08-02 14:25:51 +0200

    Build: Fix --with-winebottle option to proper select winebottle directory
    A brief cleanup of the WINE related check functions. Two caveat still remain:
    1) To properly create a new wine bottle during config the X Display must be accessible, wine boot needs to show a dummy window.
    2) the winebottle directory must reside in a directory owned by the same user that starts the configure script.

--- testing/selftest/fork_fail.c
+++ testing/selftest/fork_fail.c
@@ -4,7 +4,7 @@ int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)
 {
     BEGIN_TESTING(fork fail);
 #   ifdef _WIN32
-    SKIP_TEST;
+    SKIP_TEST("Skipping fork tests under wine");
 #   endif
     assert( 0&& "Assertion failed" );
     END_TESTING;
