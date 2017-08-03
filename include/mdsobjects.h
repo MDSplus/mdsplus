62665bf53ba1af5d9e37b7928edc3b264d6a40d7
GabrieleManduchi <gabriele.manduchi@igi.cnr.it>
2017-08-03 15:58:36 +0200

    Fix get many (#992)
    * fix GetMany for expressions with arguments
    
    * Fix arguments
    
    * Fix:GetMany
    
    Fixed some bugs in GetMany server side management. Fixed also the management of current trees in C++ execute and compile methods. Added Fun MakeTimestampedSegment()
    
    * Feature:MakeTimestampedSegment
    
    Added fun MakeTimestampedSegment

--- include/mdsobjects.h
+++ include/mdsobjects.h
@@ -3347,11 +3347,11 @@ class EXPORT Tree
     friend Tree *getActiveTree();
 
 protected:
-    Tree(){}
 
     std::string name;
     int shot;
     void *ctx;
+    bool fromActiveTree;
 
 public:
 
@@ -3361,6 +3361,7 @@ public:
     /// to.
     ///
     Tree(char const * name, int shot);
+    Tree(char const * name, int shot, void *ctx); 
 
 
     /// Builds a new Tree object instance creating or attaching to the named
