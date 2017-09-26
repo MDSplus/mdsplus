5fb407b4dfdbc58bec9e6afa03dc68bda08afd2a
Josh Stillerman <jas@psfc.mit.edu>
2017-09-26 14:07:04 -0400

    Fix: Add MIT license to source files (#1045)
    * Fix: Add MIT license to source files
    
    As discussed in developer's meeting, adding the MIT license to
    most source files.
    
    We should look into adding License / copywrite information to
    documentation files also.
    
    * Fix: previous commit messed up the file permisssions on some files
    
    * Fix: Python shell script must start with command

--- xmdsshr/XmdsExpr.c
+++ xmdsshr/XmdsExpr.c
@@ -1,3 +1,27 @@
+/*
+Copyright (c) 2017, Massachusetts Institute of Technology All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 /*  CMS REPLACEMENT HISTORY, Element XMDSEXPR.C */
 /*  *79   18-MAR-1997 10:22:38 TWF "Fix for DECC 5.3" */
 /*  *78   18-MAR-1997 09:30:09 TWF "Fix for DECC 5.3" */
