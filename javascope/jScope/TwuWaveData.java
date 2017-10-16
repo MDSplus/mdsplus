95fc1ed28bdd0b2a784eb004ae0bc442f95073fb
kerickso-local <kerickso-local@kerickso-lt.pppl.gov>
2017-10-15 20:33:44 -0400

    Fix: Access static members and methods statically
    Replace object accesses to statics with class accesses, removing
    ambiguity and about 25 compiler warnings.

--- javascope/jScope/TwuWaveData.java
+++ javascope/jScope/TwuWaveData.java
@@ -194,7 +194,7 @@ class TwuWaveData
             }
             catch (Exception   e)
             {
-                mainSignal.handleException(e);
+                TwuSingleSignal.handleException(e);
                 throw new IOException(e.toString());
             }
         }
