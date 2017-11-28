dd1c8bc51c392b9b4477844846cc033fbd296080
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2017-11-28 11:28:29 +0100

    Fix: MdsDevcies for in case no tdi function is found

--- tdi/dev_support/MdsDevices.py
+++ tdi/dev_support/MdsDevices.py
@@ -1,13 +1,18 @@
 def MdsDevices():
-    from MDSplus import Device,tdi,StringArray,version
+    from MDSplus import Device,tdi,version
+    from numpy import array
+    def importDevices(name):
+        bname = version.tobytes(name)
+        try:
+            module = __import__(name)
+            ans = [[version.tobytes(k),bname] for k,v in module.__dict__.items() if isinstance(v,int.__class__) and issubclass(v,Device)]
+        except ImportError: ans = []
+        tdidev = tdi("if_error(%s(),*)"%name)
+        if tdidev is None: return ans
+        return tdidev.value.reshape((int(tdidev.value.size/2),2)).tolist()+ans
     ans = [[version.tobytes(d),b'pydevice'] for d in Device.findPyDevices()]
-    try: import MitDevices;ans += [[k,'MitDevices'] for k,v in MitDevices.__dict__.items() if isinstance(v,int.__class__) and issubclass(v,Device)]
-    except ImportError: pass
-    try: import RfxDevices;ans += [[k,'RfxDevices'] for k,v in RfxDevices.__dict__.items() if isinstance(v,int.__class__) and issubclass(v,Device)]
-    except ImportError: pass
-    try: import W7xDevices;ans += [[k,'W7xDevices'] for k,v in W7xDevices.__dict__.items() if isinstance(v,int.__class__) and issubclass(v,Device)]
-    except ImportError: pass
-    devices = tdi("[if_error(MitDevices(),*),if_error(RfxDevices(),*),if_error(W7xDevices(),*)]").data()
-    ans = dict(devices.reshape((int(devices.shape[0]/2),2)).tolist()+ans)
-    keys = list(ans.keys());    keys.sort()
-    return StringArray([(k,ans[k]) for k in keys])
+    for module in ["KbsiDevices","MitDevices","RfxDevices","W7xDevices"]:
+        ans += importDevices(module)
+    ans = array(list(dict(ans).items()))
+    ans.view('%s,%s'%(ans.dtype,ans.dtype)).sort(order=['f0'], axis=0)
+    return ans
