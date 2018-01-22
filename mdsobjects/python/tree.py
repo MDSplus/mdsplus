c43c6f3004f202c6d6e83be3ea486f3a32c2d340
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2018-01-22 20:01:52 +0100

    Fix: dw_setup for python3

--- mdsobjects/python/tree.py
+++ mdsobjects/python/tree.py
@@ -3178,10 +3178,8 @@ If you did intend to write to a subnode of the device you should check the prope
         The gtk.main() procedure must be run in a separate thread to avoid locking the main program.
         """
         try:
-            from widgets import MDSplusWidget
-            import gtk.glade
-            import os,gtk,inspect,threading
-            import sys
+            _widgets = _mimport('widgets')
+            import os,gtk,inspect,threading,sys,gtk.glade
             class gtkMain(threading.Thread):
                 def run(self):
                     gtk.main()
@@ -3200,7 +3198,7 @@ If you did intend to write to a subnode of the device you should check the prope
             sys.stderr = sys.__stderr__
             window.device_node=self
             window.set_title(window.get_title()+' - '+str(self)+' - '+str(self.tree))
-            MDSplusWidget.doToAll(window,"reset")
+            _widgets.MDSplusWidget.doToAll(window,"reset")
         except Exception as exc:
             raise Exception("No setup available, %s" % (str(exc),))
 
