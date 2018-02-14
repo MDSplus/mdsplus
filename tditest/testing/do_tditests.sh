ca4db701d12359c7c7b8add8c34526f68b660c82
Tom Fredian <twf@psfc.mit.edu>
2018-02-14 11:28:55 -0500

    Tests: Add tditest testing
    This commit adds a set of tests driven by files containing
    simply tdi expressions. The tdi expressions are processed by the
    tditest program and the output (both stdout and stderr) are checked
    to match an expected output. New tests can be added to existing
    files of tdi expressions or new files can be added. If new test files
    are added the tests.list file must be modified to include the new
    tests. When adding new tests you need to invoke the do_tditest.sh
    script passing update as an argument and it will regenerate the
    test-name.ans files which then become the expected results.

---
+++ tditest/testing/do_tditests.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+#
+tdir=$(realpath $(dirname ${0}))
+tmpdir=$(mktemp -d)
+export main_path="${tmpdir};$(realpath ${tdir}/../../trees)"
+export subtree_path="${tmpdir};$(realpath ${tdir}/../../trees/subtree)"
+export MDS_PATH="${tmpdir};$(realpath ${tdir}/../../tdi)"
+export MDS_PYDEVICE_PATH="${tmpdir};$(realpath ${tdir}/../../pydevices)"
+status=0
+if ( echo "if_error(py(1),0)" | tditest 2>/dev/null | grep -v if_error | grep 0 >/dev/null )
+then
+    testlist="$(cat ${tdir}/test-nopy.list)"
+else
+    testlist="$(cat ${tdir}/test.list)"
+fi
+											    
+for t in ${testlist}
+do
+    if [ "$1" == "update" ]
+    then
+	tmpdir=$tmpdir tditest ${tdir}/${t}.tdi 2>&1 | \
+	    grep -v 'Data inserted:' | \
+	    grep -v 'Length:' > ${tdir}/${t}.ans
+    else
+	tmpdir=$tmpdir tditest ${tdir}/${t}.tdi 2>&1 | \
+	    grep -v 'Data inserted:' | \
+	    grep -v 'Length:' | \
+	    diff /dev/stdin ${tdir}/${t}.ans > ${t}-diff.log
+	tstat=$?
+	if [ "$tstat" != "0" ]
+	then
+  	    echo "FAIL: ${t}"
+	    status=$tstat
+	else
+	    echo "PASS: ${t}"
+	    rm -f ${t}-diff.log
+	fi
+    fi
+done
+if [ ! -z "${tmpdir}" ]
+then
+  rm -Rf ${tmpdir}
+fi
+exit $status
