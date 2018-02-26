1363fde38a62e9166a88cf298a4d815acc769721
AndreaRigoni <andrea.rigoni@pd.infn.it>
2018-02-26 18:43:11 +0100

    Tests: MdsConnectionTest: replace fork with posix_spawn and include setTimeContext test

--- mdsobjects/cpp/testing/MdsConnectionTest.cpp
+++ mdsobjects/cpp/testing/MdsConnectionTest.cpp
@@ -22,30 +22,22 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-// if windows skip this test .. //
-#ifdef _WIN32
-#include "testing.h"
-int main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused))){
- SKIP_TEST("Connection test requires fork")
-}
-#else
 
-
+#include "testutils/MdsIpInstancer.h"
 #include <unistd.h>
 #include <fstream>
 #include <sys/types.h>
 #include <signal.h>
 
-
-#include <sys/wait.h>
 #include <mdsobjects.h>
-
 
 #include "testutils/testutils.h"
 #include "testutils/unique_ptr.h"
-#include "testutils/MdsIpInstancer.h"
 #include "testing.h"
 
+#ifdef _WIN32
+#define setenv(name,val,extra) _putenv_s(name,val)
+#endif
 
 //
 // TODO: Finish tests with PutMany and GetMany ...
@@ -72,6 +64,12 @@ void test_tree_open(const char *prot, const unsigned short port)
     unique_ptr<Data> data = cnx.get("ZERO(10)");
     TEST1( AutoString(data->getString()).string
            == "[0.,0.,0.,0.,0.,0.,0.,0.,0.,0.]" );
+
+    data = cnx.get("TreeShr->TreeDbid()");
+    TEST0(data->getInt() == 0);
+
+    data = cnx.get("setTimeContext()");
+    TEST1(AutoString(data->getString()).string == "1");
 
     // test tree opening //
     cnx.openTree((char*)"t_connect",1);
@@ -112,26 +110,14 @@ int main(int argc UNUSED_ARGUMENT, char *argv[] UNUSED_ARGUMENT)
 
     // this makes the t_connect in a separate process so that all static
     // variables instanced are destroied when the child ends.
-    pid_t pid = fork();
-    if(pid == 0)
-    {
-        {
-            unique_ptr<Tree> tree = new Tree("t_connect",-1,"NEW");
-            unique_ptr<TreeNode>(tree->addNode("test_cnx","NUMERIC"));
-            tree->write();
-            tree->edit(false);
-            tree->createPulse(1);
-        }
-        exit(0);
-    }
-    else {
-        // wait for child to finish the tree generation //
-        int status;
-        while (-1 == waitpid(pid, &status, 0));
-        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-            std::cerr << "Process " << " (pid " << pid << ") failed" << "\n";
-            exit(1);
-        }
+    try {
+      unique_ptr<Tree> tree = new Tree("t_connect",-1,"NEW");
+      unique_ptr<TreeNode>(tree->addNode("test_cnx","NUMERIC"));
+      tree->write();
+      tree->edit(false);
+      tree->createPulse(1);
+    } catch (...) {
+      std::cerr << "Error creating model tree";
     }
 
     ////////////////////////////////////////////////////////////////////////////////
@@ -198,5 +184,3 @@ int main(int argc UNUSED_ARGUMENT, char *argv[] UNUSED_ARGUMENT)
     END_TESTING;
 */
 }
-
-#endif
