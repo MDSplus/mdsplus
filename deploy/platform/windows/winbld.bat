221ce1db5131f325324eac73436eb58d4e406f15
Tom Fredian <twf@psfc.mit.edu>
2018-04-09 17:04:13 -0400

    Feature: Restore the Visual Studio MdsObjectsCppShr-VS.DLL
    This should restore a cpp dll which is compatible for use with
    Visual Studio built applications on Windows.

--- deploy/platform/windows/winbld.bat
+++ deploy/platform/windows/winbld.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
-set VisualStudioVersion=12.0
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+set VisualStudioVersion=15.0
 devenv %~dp0..\mdsobjects\MdsObjectsCppShr-VS.sln /build "Release|X64"
 devenv %~dp0..\mdsobjects\MdsObjectsCppShr-VS.sln /build "Release|win32"
