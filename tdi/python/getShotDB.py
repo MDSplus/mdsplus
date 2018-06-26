324456a75ecf85100bafca887e536b3bbf775524
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2018-06-26 10:52:47 +0200

    Feature: make getShotDB directly available from within python

--- tdi/python/getShotDB.py
+++ tdi/python/getShotDB.py
@@ -1,4 +1,4 @@
-# 
+#
 # Copyright (c) 2017, Massachusetts Institute of Technology All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -23,8 +23,9 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-import os, MDSplus, numpy
+import MDSplus
 def getShotDB(expt,path=None,lower=None,upper=None):
+  try:
     """
     getShotDB("mytree")
     getShotDB("mytree",0)
@@ -35,59 +36,7 @@ def getShotDB(expt,path=None,lower=None,upper=None):
     getShotDB("mytree",*,*,_upto)
     getShotDB("mytree",*,_from,_upto)
     """
-    isTdi = isinstance(expt, MDSplus.mdsdata.Data)
-    expt = str(expt).lower()
-
-    def getTreePath():
-        # split path variable into single paths and replaces the ~t treename
-        expt_paths = os.getenv(expt+'_path')
-        if expt_paths is None:
-            raise MDSplus.mdsExceptions.TreeNOPATH
-        return expt_paths.replace('~t',expt).split(';')
-
-    def getshots(expt_path,lower,upper):
-        if expt_path.find('::')>=0:
-            # path is referring to remote tree
-            server,path = expt_path.split('::',2)
-            # check if thick or distributed
-            path = "*"  if len(path)==0 else '"'+path+'"'
-            # handle None for upper and lower
-            if lower is None: lower = "*"
-            if upper is None: upper = "*"
-            # fetch data from server
-            return MDSplus.Connection(server).get('getShotDb("%s",%s,%s,%s)'%(expt,path,str(lower),str(upper)),).data().tolist()
-        start = expt+'_'
-        files = [f[len(expt):-5].split('_') for f in os.listdir(expt_path) if f.endswith('.tree') and f.startswith(start)]
-        return [int(f[1]) for f in files if len(f)==2 and f[1]!='model']
-    """The path argument is interpreted"""
-    # try to convert to native datatype
-    try: path = path.data().tolist()
-    except: pass
-    if isinstance(path, (int,)):
-        # path is int and refering to the index of the path list
-        path = getTreePath()[path]
-    if isinstance(path, MDSplus.version.basestring):
-        # path is str and used as path
-        shots = getshots(path,lower,upper)
-    else:
-        # path is undefined and the total list will be collected
-        shots = []
-        for expt_path in getTreePath():
-            try:
-                shots += getshots(expt_path,lower,upper)
-            except:
-                import traceback
-                traceback.print_exc()
-
-    """filter result by upper and lower limits"""
-    if lower is not None or upper is not None:
-        if lower is None:
-            shots = numpy.array(filter(lambda x: x <= int(upper), shots))
-        elif upper is None:
-            shots = numpy.array(filter(lambda x: x >= int(lower), shots))
-        else:
-            shots = numpy.array(filter(lambda x: x >= int(lower) and x <= int(upper), shots))
-    shots.sort()
-
-    # return as MDSplus type if input was MDSplus type
-    return MDSplus.Int32Array(shots) if isTdi else shots
+    return MDSplus.Int32Array(MDSplus.Tree.getShotDB(expt,path,lower,upper))
+  except Exception:
+    import traceback
+    traceback.print_exc()
