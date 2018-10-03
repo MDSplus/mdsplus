c0a8f321153900543906ea353921e501e0ccbe3c
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2018-10-03 16:43:16 +0200

    Fix:Add forgotten files
    Added new files jScope/MdsConnectionLocal.java jScope/MdsConnectionSSH.java jScope/SSHDataProvider.java

---
+++ javascope/jScope/MdsConnectionSSH.java
@@ -0,0 +1,48 @@
+package jScope;
+import java.io.*;
+import java.util.*;
+
+public class MdsConnectionSSH extends MdsConnection
+{
+    Process p;
+    public MdsConnectionSSH()
+    {
+//        this.provider = "MdsConnectionSSH";
+    }
+    public MdsConnectionSSH(String provider)
+    {
+        this.provider = provider;
+    }
+    public void connectToServer() throws IOException
+    {
+        host = getProviderHost();
+        port = 0;
+        user = "";
+	ProcessBuilder pb = new ProcessBuilder("mdsip-client-ssh", host, "mdsip-server-ssh");
+//	pb.redirectInput(ProcessBuilder.Redirect.PIPE);
+//	pb.redirectOutput(ProcessBuilder.Redirect.PIPE);
+//	pb.redirectError(ProcessBuilder.Redirect.PIPE);
+	Process p = pb.start();
+	dis = new BufferedInputStream(p.getInputStream());
+	dos = new DataOutputStream(new BufferedOutputStream(p.getOutputStream()));
+   }  
+    public String getProvider() { return "ssh";}
+ //   public void setProvider(String provider){}
+    public  int DisconnectFromMds()
+    {
+	try 
+	{
+//	    dos.close();   Closing pipes charshes JVM!!!!!!!!!!!!!!!!!!!!!!!!11
+//          dis.close();
+	    dis = null;
+	    dos = null;
+	    p.destroy();
+            receiveThread.waitExited();
+	}
+	catch(Exception e)
+	{
+	   // System.out.println("Error disconnecting From local mdsip: " + e);
+	}
+	return 1;
+    }
+}
