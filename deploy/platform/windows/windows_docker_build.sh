1e19a37ac3e6374e3fbe521a845e56b51a1025c5
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2018-10-24 22:52:58 +0200

    Tests: windows_docker_build overrides WINEPREFIX

--- deploy/platform/windows/windows_docker_build.sh
+++ deploy/platform/windows/windows_docker_build.sh
@@ -8,16 +8,15 @@
 # publish:
 # /publish/$branch/MDSplus-*.exe
 #
-winebottle64=$(mktemp --tmpdir -d winebottle64.XXXXXXXX)
-test64="64 x86_64-w64-mingw32 bin_x86_64 bin_x86_64 --with-winebottle=$winebottle64"
-winebottle32=$(mktemp --tmpdir -d winebottle32.XXXXXXXX)
-test32="32 i686-w64-mingw32   bin_x86    bin_x86 --with-winebottle=$winebottle32"
 
 srcdir=$(readlink -e $(dirname ${0})/../..)
 
 export JNI_INCLUDE_DIR=${srcdir}/3rd-party-apis/windows-jdk
 export JNI_MD_INCLUDE_DIR=${srcdir}/3rd-party-apis/windows-jdk/win32
-
+mkdir -p /workspace/winebottle64
+test64="64 x86_64-w64-mingw32 bin_x86_64 bin_x86_64 --with-winebottle=/workspace/winebottle64"
+mkdir -p /workspace/winebottle32
+test32="32 i686-w64-mingw32   bin_x86    bin_x86    --with-winebottle=/workspace/winebottle32"
 
 buildrelease() {
     abort=0
