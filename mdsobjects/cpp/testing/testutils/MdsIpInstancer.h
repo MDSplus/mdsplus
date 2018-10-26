f1e5b44bf4d22b762a0d91ce112cbda812f84123
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2018-10-26 02:31:43 +0200

    Tests: disable tcpv6/udtv6 test as it might not be supported by docker

--- mdsobjects/cpp/testing/testutils/MdsIpInstancer.h
+++ mdsobjects/cpp/testing/testutils/MdsIpInstancer.h
@@ -8,6 +8,7 @@
  #endif
  #define _WIN32_WINNT _WIN32_WINNT_WIN8 // Windows 8.0
  #include <winsock2.h>
+ #include <ws2tcpip.h>
  #define REUSEADDR_TYPE BOOL
 #else
 #include <string.h>
@@ -73,23 +74,24 @@ public:
 	char port_str[20];
 	char *argv[] = {(char*)"mdsip",(char*)"-P",(char*)m_protocol.c_str(),(char*)"-h",(char*)m_host_file->name(),(char*)"-p",port_str,(char*)mode, NULL};
 	// get first available port  //
-	int offset = 0;
+	/*
+        int offset = 0;
 	while(!available(m_port,m_protocol) && offset<100 )
 	  m_port += offset++;
 	if(offset==100)
 	  throw std::out_of_range("any port found within 100 tries");
+	*/
 	sprintf(port_str,"%i",m_port);
 #ifdef _WIN32
-	if (!(m_pid = _spawnvpe(P_NOWAIT, argv[0], argv+1, environ)))
+	if (!(m_pid = _spawnvpe(P_NOWAIT, argv[0], argv, environ)))
 #elif defined(USE_FORK)
-	std::cout << "FORKING PROCESS!" << std::flush;
-	m_pid = fork();
-	if(m_pid==0) // child process
+	std::cout << "FORKING PROCESS!\n" << std::flush;
+	m_pid = fork();	if(m_pid==0) // child process
 	//   exit(execvpe(argv[0], argv, environ));
 	   exit(execvp(argv[0], argv) ? errno : 0);
 	else if (m_pid<0) // spawn failed
 #else
-	if (posix_spawnp(&m_pid, "mdsip", NULL, NULL, argv, environ))
+	if (posix_spawnp(&m_pid, argv[0], NULL, NULL, argv, environ))
 #endif
 	  throw std::runtime_error("Could not start mdsip server");
 	else
@@ -175,13 +177,23 @@ private:
     // at the moment and return 1 if it's avaiable, zero otherwise
     int available(int port, const std::string protocol) {
         int sock = allocate(protocol);
-	// TODO: AF_INET6 supported sockaddr_in
-        struct sockaddr_in addr;
-        memset(&addr, 0, sizeof(addr));
-        addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY); //inet_addr("0.0.0.0")
-        int error = bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+	int error;
+        if(protocol == "tcpv6" || protocol == "udtv6"){
+          const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
+          struct sockaddr_in6 addr;
+	  memset(&addr,0,sizeof(addr));
+          addr.sin6_family = AF_INET6;
+	  addr.sin6_port = htons(port);
+	  addr.sin6_addr = in6addr_any;
+          error = bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+        } else {
+          struct sockaddr_in addr;
+	  memset(&addr,0,sizeof(addr));
+	  addr.sin_family = AF_INET;
+	  addr.sin_port = htons(port);
+	  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	  error = bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+	}
         if(!error) {
             shutdown(sock,2);
             close(sock);
