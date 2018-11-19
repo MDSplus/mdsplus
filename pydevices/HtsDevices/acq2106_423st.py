35ceff2600710974e2dea2e24f2dbef6948762b4
Josh Stillerman <jas@psfc.mit.edu>
2018-11-19 11:15:24 -0500

    Feature: Add Cryocon118i, fix Acq2106_423
    A new device Cryocon model 18i cryogenic temperature monitor
    controller. This is an 8 channel network based thermocouple
    monitor.
    
    The d-tacq acq2106_423_stN (1-6) devices are now in their own files.
    - new python devices discovery makes the use of __init__.py in the
    device directorys not effective.
    
    update the distribution files

--- pydevices/HtsDevices/acq2106_423st.py
+++ pydevices/HtsDevices/acq2106_423st.py
@@ -60,7 +60,7 @@ class Acq2106_423st(MDSplus.Device):
         {'path':':TRIG_MODE','type':'text', 'value': 'hard', 'options':('no_write_shot')},
         {'path':':EXT_CLOCK','type':'axis', 'options':('no_write_shot')},
         {'path':':FREQ','type':'numeric', 'value': 16000, 'options':('no_write_shot')},        
-        {'path':':DEF_DCIM','type':'numeric', 'value': 1, 'options':('no_write_shot')},
+        {'path':':DEF_DECIMATE','type':'numeric', 'value': 1, 'options':('no_write_shot')},
         {'path':':SEG_LENGTH','type':'numeric', 'value': 8000, 'options':('no_write_shot')},
         {'path':':MAX_SEGMENTS','type':'numeric', 'value': 1000, 'options':('no_write_shot')},
         {'path':':SEG_EVENT','type':'text', 'value': 'STREAM', 'options':('no_write_shot')},
@@ -95,7 +95,7 @@ class Acq2106_423st(MDSplus.Device):
         return ans
 
     def init(self):
-        import HtsDevices.acq400_hapi as acq400_hapi
+        import acq400_hapi
         uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
         uut.s0.set_knob('set_abort', '1')
         if self.ext_clock.length > 0:
@@ -134,7 +134,7 @@ class Acq2106_423st(MDSplus.Device):
     STOP=stop
 
     def trig(self):
-        import HtsDevices.acq400_hapi as acq400_hapi
+        import acq400_hapi
         uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
         uut.so.set_knob('soft_trigger','1')
         return 1
