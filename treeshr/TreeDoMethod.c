89e91e931fa4f1e1f0dd6a018c341cef3e845362
zack-vii <zack.vii@live.com>
2018-11-22 05:18:19 +0100

    Fix: TreeDoMethod eliminate count

--- treeshr/TreeDoMethod.c
+++ treeshr/TreeDoMethod.c
@@ -68,26 +68,28 @@ int TreeDoMethod( nid_dsc, method_dsc [,args]...)
 #include <mdsshr_messages.h>
 #include <ctype.h>
 
-#define  count(num) va_start(incrmtr, method_ptr); \
-                     for (num=2; (num < 256) && (va_arg(incrmtr, struct descriptor *) != MdsEND_ARG);  num++)
-
 extern void **TreeCtx();
+#define VA_LIST_TO_ARGLIST(arglist,nargs,pre,post,frst) \
+void *arglist[256];\
+{\
+  va_list args;\
+  va_start(args, frst);\
+  for (nargs = pre; nargs < 256 ; nargs++)\
+    if (MdsEND_ARG == (arglist[nargs+1] = va_arg(args, struct descriptor *)))\
+      break;\
+  va_end(args);\
+  nargs+=post;\
+  arglist[0] = (void*)(intptr_t)nargs;\
+}
 
 #define _TREE_TDI(_TreeFun,TdiFun) \
 static int *(*TdiFun)() = NULL;\
 EXPORT int _TreeFun(void *dbid, ...) {\
   int status = LibFindImageSymbol_C("TdiShr", #TdiFun, &TdiFun);\
   if IS_NOT_OK(status) return status;\
-  void *arglist[256];\
-  va_list args;\
-  va_start(args, dbid);\
   int nargs;\
-  for (nargs = 0; nargs < 255 ; nargs++)\
-    if (MdsEND_ARG == (arglist[nargs+1] = va_arg(args, struct descriptor *)))\
-      break;\
-  va_end(args);\
-  arglist[0] = (void*)(intptr_t)nargs;\
-  fprintf(stderr,"%s, %d\n",#_TreeFun,nargs);\
+  VA_LIST_TO_ARGLIST(arglist,nargs,0,1,dbid);\
+  arglist[nargs] = MdsEND_ARG;\
   DBID_PUSH(dbid);\
   status = (int)(intptr_t)LibCallg(arglist, TdiFun);\
   DBID_POP(dbid);\
@@ -99,37 +101,26 @@ _TREE_TDI(_TreeEvaluate ,TdiEvaluate )
 _TREE_TDI(_TreeCompile  ,TdiCompile  )
 _TREE_TDI(_TreeDecompile,TdiDecompile)
 
-int TreeDoMethod(struct descriptor *nid_dsc, struct descriptor *method_ptr, ...)
-
-{
-  int i;
+int TreeDoMethod(struct descriptor *nid_dsc, struct descriptor *method_ptr, ...) {
   int nargs;
-  void *arglist[256];
-  va_list incrmtr;
-  count(nargs);
-  arglist[0] = (void*)(intptr_t)(nargs + 2);
+  VA_LIST_TO_ARGLIST(arglist,nargs,3,1,method_ptr);
   arglist[1] = *TreeCtx();
   arglist[2] = nid_dsc;
   arglist[3] = method_ptr;
-  va_start(incrmtr, method_ptr);
-  for (i = 3; i <= nargs; i++)
-    arglist[i + 1] = va_arg(incrmtr, struct descriptor *);
-  va_end(incrmtr);
-  arglist[nargs + 2] = MdsEND_ARG;
   return (int)(intptr_t)LibCallg(arglist, _TreeDoMethod);
 }
 
-int do_fun(struct descriptor *funname, int nargs, struct descriptor **args, struct descriptor_xd *out_ptr){
+int do_fun(void* dbid, struct descriptor *funname, int nargs, struct descriptor **args, struct descriptor_xd *out_ptr){
   short OpcExtFunction = 162;
   DESCRIPTOR_FUNCTION(fun, &OpcExtFunction, 255);
-  void *call_arglist[] = { (void *)3, (void *)&fun, (void *)out_ptr, MdsEND_ARG };
+  void *call_arglist[] = { (void *)4, dbid, (void *)&fun, (void *)out_ptr, MdsEND_ARG };
   fun.ndesc = nargs>253 ? 255 : (unsigned char)(nargs + 2);
   fun.arguments[0] = 0;
   fun.arguments[1] = funname;
   int i;
   for (i = 0; i < nargs; i++)
     fun.arguments[2 + i] = args[i];
-  return (int)(intptr_t)LibCallg(call_arglist, TdiEvaluate);
+  return (int)(intptr_t)LibCallg(call_arglist, _TreeEvaluate);
 }
 
 #pragma GCC diagnostic push
@@ -137,7 +128,6 @@ int do_fun(struct descriptor *funname, int nargs, struct descriptor **args, stru
 
 int _TreeDoMethod(void *dbid, struct descriptor *nid_dsc, struct descriptor *method_ptr, ...){
   INIT_STATUS;
-  va_list incrmtr;
   short conglomerate_elt;
   unsigned char data_type;
   int head_nid;
@@ -146,11 +136,6 @@ int _TreeDoMethod(void *dbid, struct descriptor *nid_dsc, struct descriptor *met
   {4, NciCONGLOMERATE_NIDS, (unsigned char *)&head_nid, 0},
   {0, NciEND_OF_LIST, 0, 0}
   };
-  void (*addr) ();
-  int nargs;
-  void *arglist[256];
-  count(nargs);
-  arglist[0] = (void*)(intptr_t)(nargs);
   if (!nid_dsc->pointer
    || (nid_dsc->dtype!=DTYPE_NID
     && nid_dsc->dtype!=DTYPE_L
@@ -166,18 +151,17 @@ int _TreeDoMethod(void *dbid, struct descriptor *nid_dsc, struct descriptor *met
   FREEXD_ON_EXIT(&xd);
   FREED_ON_EXIT(&method);
   if (conglomerate_elt || (data_type == DTYPE_CONGLOM)) {
-    int i;
-    arglist[1] = nid_dsc;
-    arglist[2] = method_ptr;
-    va_start(incrmtr, method_ptr);
-    for (i = 2; i < nargs; i++)
-      arglist[i + 1] = va_arg(incrmtr, struct descriptor *);
-    va_end(incrmtr);
     status = _TreeGetRecord(dbid, head_nid ? head_nid : *((int *)nid_dsc->pointer), &xd);
     if STATUS_NOT_OK goto end;
     conglom_ptr = (struct descriptor_conglom *)xd.pointer;
     if (conglom_ptr->dtype != DTYPE_CONGLOM)
       {status = TreeNOT_CONGLOM;goto end;}
+
+    int i,nargs;
+    VA_LIST_TO_ARGLIST(arglist,nargs,2,0,method_ptr);
+    arglist[1] = nid_dsc;
+    arglist[2] = method_ptr;
+
     if (conglom_ptr->image
      && conglom_ptr->image->length == strlen("__python__")
      && strncmp(conglom_ptr->image->pointer, "__python__", strlen("__python__")) == 0) {
@@ -197,19 +181,14 @@ int _TreeDoMethod(void *dbid, struct descriptor *nid_dsc, struct descriptor *met
       strcat(exp, "$)");
       exp_dsc.length=strlen(exp);
       if STATUS_OK {
-        static void* (*TdiExecute)  () = NULL;
-        status = LibFindImageSymbol_C("TdiShr", "TdiExecute", &TdiExecute);
-        if STATUS_OK {
-	  _nargs += 2;
-	  for (i = _nargs; i > 0; i--)
-	    arglist[i + 1] = arglist[i];
+	  _nargs += 3;
+	  for (i = _nargs; i > 2; i--)
+	    arglist[i] = arglist[i-2];
 	  arglist[0] = (void*)(intptr_t)(_nargs);
-	  arglist[1] = &exp_dsc;
+	  arglist[1] = dbid;
+	  arglist[2] = &exp_dsc;
 	  arglist[_nargs] = MdsEND_ARG;
-          DBID_PUSH(dbid);
-	  status = (int)(intptr_t)LibCallg(arglist, TdiExecute);
-          DBID_POP(dbid);
-        }
+	  status = (int)(intptr_t)LibCallg(arglist, _TreeExecute);
       }
       goto end;
     }
@@ -217,10 +196,12 @@ int _TreeDoMethod(void *dbid, struct descriptor *nid_dsc, struct descriptor *met
     StrConcat((struct descriptor *)&method, conglom_ptr->model, (struct descriptor *)&underunder, method_ptr MDS_END_ARG);
     for (i = 0; i < method.length; i++)
       method.pointer[i] = (char)tolower(method.pointer[i]);
-    if (conglom_ptr->image && conglom_ptr->image->dtype == DTYPE_T)
-      status = LibFindImageSymbol(conglom_ptr->image, &method, &addr);
-    else
+    if (conglom_ptr->image && conglom_ptr->image->dtype != DTYPE_T) {
       status = TdiINVDTYDSC;
+      goto end;
+    }
+    void (*addr) ();
+    status = LibFindImageSymbol(conglom_ptr->image, &method, &addr);
     if STATUS_OK {
       DBID_PUSH(dbid);
       status = (int)(intptr_t)LibCallg(arglist, addr);
@@ -237,8 +218,7 @@ int _TreeDoMethod(void *dbid, struct descriptor *nid_dsc, struct descriptor *met
       }
     } else {
       /**** Try tdi fun ***/
-      status =
-	  do_fun((struct descriptor *)&method, nargs - 1, (struct descriptor **)&arglist[1], (struct descriptor_xd *)arglist[nargs]);
+      status = do_fun(dbid,(struct descriptor *)&method, nargs - 1, (struct descriptor **)&arglist[1], (struct descriptor_xd *)arglist[nargs]);
       if (status == TdiUNKNOWN_VAR)
 	status = TreeNOMETHOD;
     }
