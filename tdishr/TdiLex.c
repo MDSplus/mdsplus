cb87188dc52dd023a376c16a7a426c7d5704c4da
zack-vii <zack.vii@live.com>
2019-01-23 13:59:13 +0100

    Fix: TdiYyReset() has no body

--- tdishr/TdiLex.c
+++ tdishr/TdiLex.c
@@ -154,13 +154,6 @@ extern unsigned short Opcdollar, OpcZero,
 #define _MOVC3(a,b,c) memcpy(c,b,a)
 STATIC_ROUTINE int TdiLexBinEq(int token);
 
-extern void TdiYyReset()
-{
-/*
-  tdiyy_reset();
-*/
-}
-
 extern int TdiHash();
 
 STATIC_ROUTINE void upcase(unsigned char *str, int str_len)
