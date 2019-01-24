56127e7436c07c5da0d555f625e05d17474d847a
zack-vii <zack.vii@live.com>
2019-01-24 14:10:47 +0100

    Build: rename def and exports after lib name; export -> exports

--- gendeffiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-dir=$(dirname $0)
-for def in $(find $dir -type f -name *.def 2>/dev/null)
-do
-  defname=$(basename $def)
-  dllname=${defname%.*}.dll
-  gendef - $dir/build/windows/tests/64/bin_x86_64/$dllname > $def
-done
