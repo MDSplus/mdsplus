048855b962c004f0c0693351ba90c0aa3d465235
zack-vii <zack.vii@live.com>
2019-01-26 02:58:14 +0100

    Build: trigger core dump on timeout

--- deploy/platform/timeout.sh
+++ deploy/platform/timeout.sh
@@ -8,21 +8,23 @@
         if ( -z kill -s 0 $2 )
         then
             echo WARNING: Sending $3 after $1
-            kill -s SIGINT $2
+            kill -s $3 $2
         else
             exit $4
         fi
     }
-    # try to interrupt main program at first
-    waitandkill $1 $$ SIGINT  0
-    # try to gracefully terminate main program if sigint did not help
-    waitandkill 10 $$ SIGTERM 1
-    # kill main program if sigterm did not help
-    waitandkill  5 $$ SIGKILL 2
+    # kill wiith sigabrt to trigger core dump
+    waitandkill $1 $$ SIGABRT 3
+    ## try to interrupt main program at first
+    #waitandkill $1 $$ SIGINT  0
+    ## try to gracefully terminate main program if sigint did not help
+    #waitandkill 10 $$ SIGTERM 1
+    ## kill main program if sigterm did not help
+    #waitandkill  5 $$ SIGKILL 2
 ) 2> /dev/null &
 # store pid of watchdog fork
 watchdog=$!
-# strip timeout.sh from args
+# strip timeout.sh  from args
 shift
 # execute main program in same pid
 :&& exec "$@"
