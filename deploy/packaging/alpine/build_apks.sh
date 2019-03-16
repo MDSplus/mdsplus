e309aa9c62c41f4d603acc26fd64bcfe75e9f1e3
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2019-03-16 19:40:40 +0100

    Build: HOME=/workspace

--- deploy/packaging/alpine/build_apks.sh
+++ deploy/packaging/alpine/build_apks.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 pckdir=$(dirname $(realpath $0))
+export HOME=/workspace
 cd /workspace
 signkeys="/sign_keys/mdsplus@mdsplus.org-589e05b6.rsa"
 mkdir -p /workspace/.abuild
 if [ -f $signkeys ]
 then
   echo -e "PACKAGER_PRIVKEY=\"$signkeys\"\n" > /workspace/.abuild/abuild.conf
-else
-  if [ ! -f /workspace/.abuild/abuild.conf ]
-  then
-    touch /workspace/.abuild/abuild.conf
-    abuild-keygen -aqn # generate new keys
-  fi
+elif [ ! -f /workspace/.abuild/abuild.conf ]
+then
+  touch /workspace/.abuild/abuild.conf
+  abuild-keygen -aqn # generate new keys
 fi
 # we have to unset srcdir as it is a reserved name in abuild
 # in the process of building the apks it will wipe $srcdir
