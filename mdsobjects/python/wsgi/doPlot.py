f313f253367a239f8f4782c3f2cf569a54337c91
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2019-03-24 00:32:53 +0100

    Fix: properly plot data in doPlot using mdsmisc

--- mdsobjects/python/wsgi/doPlot.py
+++ mdsobjects/python/wsgi/doPlot.py
@@ -23,104 +23,136 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-from MDSplus import DATA,DIM_OF,tdi
-import os,sys
+from MDSplus import tdi,version,String,Descriptor_xd
+import os,sys,ctypes,numpy,struct
 
 example = '/plot?expr=make_signal(sin((0:1:.01)*$2pi),*,0:1:.01)'
 
+_MdsMisc = version.load_library('MdsMisc')
+def getXYSignal(expr,num):
+    _MdsMisc.GetXYSignal.restype = ctypes.POINTER(Descriptor_xd._structure_class)
+    y,x = '`(__misc__=(%s;);)'%expr,'`DIM_OF(__misc__)'
+    cnum = ctypes.c_int32(num)
+    cmin = ctypes.c_float(-float('inf'))
+    cmax = ctypes.c_float( float('inf'))
+    xd   = _MdsMisc.GetXYSignal(ctypes.c_char_p(y),ctypes.c_char_p(x),ctypes.byref(cmin),ctypes.byref(cmax),ctypes.byref(cnum))
+    d    = Descriptor_xd(xd.contents).value
+    if isinstance(d,String): raise Exception(d.data())
+    bt  = d.data().tostring()
+    off = 0
+    fmt = '!fib'
+    res,length,typ = struct.unpack_from(fmt,bt,off)
+    off += struct.calcsize(fmt)
+    fmt = '!'+('f'*length)
+    y = numpy.array(struct.unpack_from(fmt,bt,off),'float32')
+    off += struct.calcsize(fmt)
+    if   typ == 1: # int64
+        typ = 'int64'
+        fmt = '!'+('q'*length)
+    elif typ == 2: # float64
+        typ = 'float64'
+        fmt = '!'+('d'*length)
+    else:          # float32
+        typ = 'float32'
+        #fmt = '!'+('f'*length)
+    x = numpy.array(struct.unpack_from(fmt,bt,off),typ)
+    title = xlabel = ylabel = ""
+    off += struct.calcsize(fmt)
+    fmt = '!I'
+    llen = struct.unpack_from(fmt,bt,off)[0]
+    off += struct.calcsize(fmt)+llen
+    title = bt[off-llen:off]
+    llen = struct.unpack_from(fmt,bt,off)[0]
+    off += struct.calcsize(fmt)+llen
+    ylabel = bt[off-llen:off]
+    llen = struct.unpack_from(fmt,bt,off)[0]
+    off += struct.calcsize(fmt)+llen
+    xlabel = bt[off-llen:off]
+    return length,x,y,title,xlabel,ylabel
+
 def gridScaling(min_in,max_in,divisions,span):
-        from math import log10,pow,fabs,ceil,floor,modf
-        resdivlog = log10(fabs(max_in-min_in))
-        if resdivlog < 0:
-            res = int(pow(10,ceil(resdivlog) - 4))
-        else:
-            res = int(pow(10,floor(resdivlog) - 4))
-        divlog = log10(fabs(max_in - min_in) / divisions)
-        fractpart,wholepart = modf(divlog)
-        intinc = pow(10,fabs(fractpart))
-        if intinc < 1.3333333:
-            intinc=1
-        elif intinc < 2.857:
-            intinc=2
-        else:
-            intinc=10
-        if divlog < 0:
-            val_inc = pow(10,-log10(intinc) + wholepart)
-        else:
-            val_inc = pow(10,log10(intinc) + wholepart)
-        grid = ceil(min_in/val_inc) * val_inc
-        divs_out = int(floor((max_in - grid + val_inc) / val_inc))
-        if max_in > min_in:
-            first_pix = span * ((grid - min_in) / (max_in - min_in))
-            pix_inc = span * val_inc / (max_in - min_in);
-            first_val = (first_pix * ((max_in - min_in)/span)) + min_in
-        else:
-            divs_out = 2
-            first_pix = 0
-            pix_inc = span
-            val_inc = 0
-        return {"divisions":  divs_out,
-                "first_pixel":first_pix, "pixel_increment":pix_inc, 
-                "first_value":first_val, "value_increment":val_inc,
-                "resolution": res}       
+    from math import log10,pow,fabs,ceil,floor,modf
+    resdivlog = log10(fabs(max_in-min_in))
+    if resdivlog < 0:
+        res = int(pow(10,ceil(resdivlog) - 4))
+    else:
+        res = int(pow(10,floor(resdivlog) - 4))
+    divlog = log10(fabs(max_in - min_in) / divisions)
+    fractpart,wholepart = modf(divlog)
+    intinc = pow(10,fabs(fractpart))
+    if intinc < 1.3333333:
+        intinc=1
+    elif intinc < 2.857:
+        intinc=2
+    else:
+        intinc=10
+    if divlog < 0:
+        val_inc = pow(10,-log10(intinc) + wholepart)
+    else:
+        val_inc = pow(10,log10(intinc) + wholepart)
+    grid = ceil(min_in/val_inc) * val_inc
+    divs_out = int(floor((max_in - grid + val_inc) / val_inc))
+    if max_in > min_in:
+        first_pix = span * ((grid - min_in) / (max_in - min_in))
+        pix_inc = span * val_inc / (max_in - min_in);
+        first_val = (first_pix * ((max_in - min_in)/span)) + min_in
+    else:
+        divs_out = 2
+        first_pix = 0
+        pix_inc = span
+        val_inc = 0
+    return res,(first_pix, pix_inc),(first_val,val_inc),divs_out       
 
 def doPlot(self):
-    viewbox=[0,0,10000,10000];
+    viewbox = (0,0,10000,10000);
+    expr = self.args['expr'][-1]
     if len(self.path_parts) > 2:
-        tree = self.openTree(self.path_parts[1],self.path_parts[2])
-        _tdi = tree.tdiExecute
-    else:
-        tree = None
-        _tdi = tdi
-    expr=self.args['expr'][-1]
+        tdi('treeopen("%s",%s)'%tuple(self.path_parts[1:3]))
     try:
-        sig = _tdi(expr)
-        y = DATA(sig).evaluate()
-        x = DATA(DIM_OF(sig)).evaluate()
-        print(sig,y,x)
-    except Exception as e:
-        raise Exception("Error evaluating expression: '%s', error: %s" % (expr,e))
-    response_headers=list()
-    response_headers.append(('Cache-Control','no-store, no-cache, must-revalidate'))
-    response_headers.append(('Pragma','no-cache'))
-    response_headers.append(('XDTYPE',x.__class__.__name__))
-    response_headers.append(('YDTYPE',y.__class__.__name__))
-    response_headers.append(('XLENGTH',str(len(x))))
-    response_headers.append(('YLENGTH',str(len(y))))
-    response_headers.append(('Content-type','text/xml'))
-    if tree is not None:
-        response_headers.append(('TREE',tree.tree))
-        response_headers.append(('SHOT',str(tree.shot)))
-    output  = '<?xml version="1.0" standalone="no"?>'
-    output += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
-    output += '<svg viewBox="%d %d %d %d" xmlns="http://www.w3.org/2000/svg" version="1.1">' % (viewbox[0],viewbox[1],viewbox[2],viewbox[3])
-    output += '<title>Plot of %s </title>' % (expr,)
-    output += '<path fill="none" stroke="black" stroke-width="30" d="M '
+        length,x,y,title,xlabel,ylabel = getXYSignal(expr,viewbox[2])
+    finally:
+        if len(self.path_parts) > 2:
+            tdi('treeclose("%s",%s)'%tuple(self.path_parts[1:3]))
+    response_headers = [
+        ('Cache-Control','no-store, no-cache, must-revalidate'),
+        ('Pragma','no-cache'),
+        ('XDTYPE',x.__class__.__name__),
+        ('YDTYPE',y.__class__.__name__),
+        ('XLENGTH',str(length)),
+        ('YLENGTH',str(length)),
+        ('Content-type','text/xml'),
+    ]
+    if len(self.path_parts) > 2:
+        response_headers.append(('TREE',self.path_parts[1].upper()))
+        response_headers.append(('SHOT',self.path_parts[2]))
+    output  = '<?xml version="1.0" standalone="no"?>\n'
+    output += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
+    output += '<svg viewBox="%d %d %d %d" xmlns="http://www.w3.org/2000/svg" version="1.1">\n' % viewbox
+    output += '<title>Plot of %s</title>\n' % (expr,)
+    output += '<path fill="none" stroke="black" stroke-width="30" d="M\n'
     xmin,  xmax   = float(min(x)),float(max(x))
     ymin,  ymax   = float(min(y)),float(max(y))
     xmin_s,ymin_s = float(viewbox[0]),float(viewbox[1])
     xmax_s,ymax_s = float(viewbox[2]),float(viewbox[3])
     # scale
-    x =  (x-xmin)*(xmax_s-xmin_s)/(xmax-xmin)
-    y =  viewbox[3]-(y-ymin)*(ymax_s-ymin_s)/(ymax-ymin)
-    for i in range(len(x)): output += " %d %d" % (int(x[i]),int(y[i]))
-    output += ' " />\n'
-    scaling = gridScaling(xmin,xmax,5,viewbox[2]-viewbox[0])
-    for i in range(scaling['divisions']):
-        output += '<text text-anchor="middle" x="%d" y="%d" font-size="300" >%g</text>' % (
-            scaling["first_pixel"]+i*scaling["pixel_increment"], viewbox[2]-200,
-            scaling['first_value']+i*scaling["value_increment"])
-        output += '<path fill="none" stroke="black" stroke-width="5" stroke-dasharray="200,100,50,50,50,100" d="M%d %d %d %d" />' % ( 
-            scaling["first_pixel"]+i*scaling["pixel_increment"], viewbox[2],
-            scaling["first_pixel"]+i*scaling["pixel_increment"], viewbox[0])   
-    scaling = gridScaling(ymin,ymax,5,viewbox[3]-viewbox[1])
-    for i in range(scaling['divisions']):
-        output += '<text text-anchor="left" x="%d" y="%d" font-size="300" >%g</text>' % (
-            100, viewbox[3]-(scaling["first_pixel"]+i*scaling["pixel_increment"]),
-            scaling['first_value']+i*scaling["value_increment"])
-        output += '<path fill="none" stroke="black" stroke-width="5" stroke-dasharray="200,100,50,50,50,100" d="M%d %d %d %d" />' % (
-            viewbox[1], viewbox[3]-(scaling["first_pixel"]+i*scaling["pixel_increment"]),
-            viewbox[3], viewbox[3]-(scaling["first_pixel"]+i*scaling["pixel_increment"]) )
+    x =  (x-xmin)*((xmax_s-xmin_s-1200)/(xmax-xmin))+1000
+    y =  (ymax-y)*((ymax_s-ymin_s-1200)/(ymax-ymin))+200
+    for i in range(len(x)): output += "%d %d\n" % (int(x[i]),int(y[i]))
+    output += '"/>\n'
+    res,pix,val,div = gridScaling(xmin,xmax,5,viewbox[2]-viewbox[0]-1200)
+    for i in range(div):
+        output += '<text text-anchor="middle" x="%d" y="%d" font-size="300">%g</text>\n' % (
+            pix[0]+i*pix[1]+1000, viewbox[2]-200, val[0]+i*val[1])
+        output += '<path fill="none" stroke="black" stroke-width="5" stroke-dasharray="200,100,50,50,50,100" d="M%d %d %d %d"/>\n' % ( 
+            pix[0]+i*pix[1]+1000, viewbox[2],
+            pix[0]+i*pix[1]+1000, viewbox[0])   
+    res,pix,val,div = gridScaling(ymin,ymax,5,viewbox[3]-viewbox[1]-1200)
+    for i in range(div):
+        output += '<text text-anchor="left" x="%d" y="%d" font-size="300">%g</text>\n' % (
+            100, viewbox[3]-(pix[0]+i*pix[1]+1000), val[0]+i*val[1])
+        output += '<path fill="none" stroke="black" stroke-width="5" stroke-dasharray="200,100,50,50,50,100" d="M%d %d %d %d"/>\n' % (
+            viewbox[1], viewbox[3]-(pix[0]+i*pix[1]+1000),
+            viewbox[3], viewbox[3]-(pix[0]+i*pix[1]+1000) )
     output += '</svg>\n'
     status = '200 OK'
     return (status, response_headers, output)
