02303019d2e0db503ddf11416824a6b2e35ab64d
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2019-03-29 15:09:19 +0100

    Fix: finish const in librtl

--- include/libroutines.h
+++ include/libroutines.h
@@ -20,14 +20,13 @@
 /// defined in MdsShr.so
 
 extern void *LibCallg();
-extern int LibConvertDateString(const char *asc_time, int64_t * qtime);
 extern int LibCreateVmZone(ZoneList **const zone);
-extern time_t LibCvtTim(int *time_in, double *t);
 extern int LibDeleteVmZone(ZoneList **const zone);
-extern int LibFindFile(mdsdsc_t *filespec, mdsdsc_t *result, void **ctx);
-extern int LibFindFileCaseBlind(mdsdsc_t *filespec, mdsdsc_t *result, void **ctx);
-extern int LibFindFileEnd(void **ctx);
-extern int LibFindFileRecurseCaseBlind(mdsdsc_t *filespec, mdsdsc_t *result, void **ctx);
+extern int LibResetVmZone(ZoneList **const zone);
+extern int LibFindFile(const mdsdsc_t *const filespec, mdsdsc_t *const result, void **const ctx);
+extern int LibFindFileCaseBlind(const mdsdsc_t *const filespec, mdsdsc_t *const result, void **const ctx);
+extern int LibFindFileEnd(void **const ctx);
+extern int LibFindFileRecurseCaseBlind(const mdsdsc_t *const filespec, mdsdsc_t *const result, void **const ctx);
 extern int LibFindImageSymbol();
 extern int LibFindImageSymbol_C();
 extern char *LibFindImageSymbolErrString();
@@ -37,19 +36,19 @@ extern int libfreevm (const uint32_t *const len, void **const vm, ZoneList **con
 extern int LibGetVm (const uint32_t *const len, void **const vm, ZoneList **const zone);
 extern int libgetvm_(const uint32_t *const len, void **const vm, ZoneList **const zone);
 extern int Libgetvm (const uint32_t *const len, void **const vm, ZoneList **const zone);
-extern int LibInsertTree(LibTreeNode **treehead, void *symbol_ptr, int *control_flags,
-			 int (*compare_rtn) (), int (*alloc_rtn) (), LibTreeNode **blockaddr,
-			 void *user_data);
-extern int LibLookupTree(LibTreeNode **treehead, void *symbolstring, int (*compare_rtn) (),
-		  LibTreeNode **blockaddr);
-extern int LibResetVmZone(ZoneList ** zone);
-extern int LibSpawn(struct descriptor *cmd, int waitFlag, int notifyFlag);
-extern int LibSysAscTim(unsigned short *len, struct descriptor *str, int *time_in);
-extern int LibTraverseTree(LibTreeNode **treehead, int (*user_rtn) (), void *user_data);
-extern int LibWait(const float *secs);
-extern int LibTimeToVMSTime(const time_t * time_in, int64_t * time_out);
-extern int libffs(int *position, int *size, char *base, int *find_position);
-extern uint32_t LibGetHostAddr(char *host);
+extern int LibInsertTree(LibTreeNode **const treehead, void *const symbol_ptr, int *const control_flags,
+	int (*const compare_rtn)(), int (*const alloc_rtn)(), LibTreeNode **const blockaddr, void *const user_data);
+extern int LibLookupTree(LibTreeNode **const treehead, void *const symbol_ptr,
+	int (*const compare_rtn)(), LibTreeNode **const blockaddr);
+extern int LibSpawn(const mdsdsc_t *const cmd, const int waitFlag, const int notifyFlag);
+extern time_t LibCvtTim(const int *const time_in, double *const t);
+extern int LibConvertDateString(const char *asc_time, int64_t *const qtime);
+extern int LibSysAscTim(length_t *const len, struct descriptor *const str, const int *const time_in);
+extern int LibTimeToVMSTime(const time_t *const time_in, int64_t *const time_out);
+extern int LibTraverseTree(LibTreeNode **treehead, int (*user_rtn)(), void *user_data);
+extern int LibWait(const float *const secs);
+extern int libffs(const int *const position, const int *const size, const char *const base, int *const find_position);
+extern uint32_t LibGetHostAddr(const char *const host);
 
 /// @}
 
