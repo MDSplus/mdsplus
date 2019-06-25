bb67d8ed1d7b325b67a301e5a1dd09759e6203fd
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2019-06-25 20:05:35 +0200

    Fix: previous after waiting for other task in disconnect

--- mdstcpip/Connections.c
+++ mdstcpip/Connections.c
@@ -224,6 +224,7 @@ int DisconnectConnection(int conid){
       while (c->state & ~CON_DISCONNECT && !pthread_cond_timedwait(&c->cond,&connection_mutex,&tp));
       if (c->state & ~CON_DISCONNECT)
         fprintf(stderr,"DisconnectConnection: Timeout waiting for connection %d state=%d", conid, c->state);
+      c = _FindConnection(conid, &p); // we were waiting, so we need to update p
     }
     // remove after task is complete
     if (p)      p->next = c->next;
