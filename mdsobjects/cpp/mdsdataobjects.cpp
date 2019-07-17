189915c143795bae70edb5d191a2728afb3294e7
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2019-07-17 12:02:22 +0200

    Fix: remove wrong deallocation

--- mdsobjects/cpp/mdsdataobjects.cpp
+++ mdsobjects/cpp/mdsdataobjects.cpp
@@ -694,8 +694,9 @@ Data * MDSplus::compileWithArgs(const char *expr, int nArgs ...) {
 	int status;
     Data * res;
     try {
-	AutoPointer<Tree> actTree(getActiveTree());
-	res = (Data *)compileFromExprWithArgs(expr, nArgs, &args[0], actTree.ptr, &status);
+//	AutoPointer<Tree> actTree(getActiveTree());
+	Tree *actTree = getActiveTree();
+	res = (Data *)compileFromExprWithArgs(expr, nArgs, &args[0], actTree, &status);
     } catch(MdsException &exc)
     {
 	res = (Data *)compileFromExprWithArgs(expr, nArgs, &args[0], 0, &status);
