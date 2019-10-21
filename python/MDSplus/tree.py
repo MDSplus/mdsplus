0c46098cf6634c90a03ed28f9cc5c0b209841177
Tom Fredian <twf@psfc.mit.edu>
2019-10-21 14:37:46 -0400

    Fix: repair python TreeNode.doMethod() method
    A change to the python TreeNode implementation broke the doMethod
    method. This should fix it.

--- python/MDSplus/tree.py
+++ python/MDSplus/tree.py
@@ -1645,12 +1645,13 @@ class TreeNode(_dat.TreeRef,_dat.Data): # HINT: TreeNode begin  (maybe subclass 
         @rtype: None
         """
         arglist=[self.ctx]
-        xd=_dsc.descriptor_xd()
+        xd=_dsc.Descriptor_xd()
         argsobj = [_scr.Int32(self.nid),_scr.String(method)]
-        argsobj+= list(map(_dat.Data,args))
+        arglist+=list(map(_dat.Data.byref,argsobj))
+        arglist+=[len(args)]
+        argsobj= list(map(_dat.Data,args))
         arglist+= list(map(_dat.Data.byref,argsobj))
-        arglist+= [xd.ref,_ver.MdsEND_ARG]
-        _exc.checkStatus(_TreeShr._TreeDoMethod(*arglist))
+        _exc.checkStatus(_TreeShr._TreeDoMethodA(*arglist))
         return xd._setTree(self.tree).value
 
     @classmethod
