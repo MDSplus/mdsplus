2735f96b18819add7f417ecf4ab52793fd3aa00f
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2019-12-20 15:36:38 -0500

    Updating to 'working' version

--- pydevices/HtsDevices/influxhistorian.py
+++ pydevices/HtsDevices/influxhistorian.py
@@ -1,24 +1,23 @@
-
 import MDSplus
 import time
 from datetime import datetime
 from influxdb import InfluxDBClient
 
-class INFLUXDB(MDSplus.Device):
+class INFLUXHISTORIAN(MDSplus.Device):
 
     DATA_COUNT = 10
 
     parts = [
-        { 'path': 'ADDRESS',    'type': 'text',                                         'options':('no_write_shot') },
-        { 'path': 'DATABASE',   'type': 'text',                                         'options':('no_write_shot') },
-        { 'path': 'SERIES',     'type': 'text',     'value': 'scada-ignition.default',  'options':('no_write_shot') },
-        { 'path': 'DATA_EVENT', 'type': 'text',     'value': 'INFLUXDB_TREND',          'options':('no_write_shot')})
-        { 'path': 'last_read',  'type': 'numeric',                                      'options':('write_shot') },
-        { 'path': 'DATA',       'type': 'text'},
+        { 'path': ':ADDRESS',    'type': 'text',                                         'options':('no_write_shot') },
+        { 'path': ':DATABASE',   'type': 'text',                                         'options':('no_write_shot') },
+        { 'path': ':SERIES',     'type': 'text',     'value': 'scada-ignition.default',  'options':('no_write_shot') },
+        { 'path': ':DATA_EVENT', 'type': 'text',     'value': 'INFLUXDB_TREND',          'options':('no_write_shot') },
+        { 'path': ':last_read',  'type': 'numeric',                                      'options':('write_shot') },
+        { 'path': ':DATA',       'type': 'text'},
     ]
 
     for i in range(DATA_COUNT):
-        name = "DATA:D{:02d}".format(i + 1)
+        name = ":DATA:D{:02d}".format(i + 1)
         parts.append({ 'path': name,                'type': 'signal',                       'options':('write_shot') })
         parts.append({ 'path': name + ":TAG_PATH",  'type': 'text',                         'options':('no_write_shot') })
         parts.append({ 'path': name + ":FUNCTION",  'type': 'text',     'value': 'fVal',    'options':('no_write_shot') })
@@ -51,7 +50,7 @@ class INFLUXDB(MDSplus.Device):
 
         client = InfluxDBClient(address, port, 'root', 'root', self.database.data())
 
-        for i in range(DATA_COUNT):
+        for i in range(self.DATA_COUNT):
             try:
                 node    = self.__getattr__("data_d{:02d}".format(i + 1))
                 tagPath = self.__getattr__("data_d{:02d}_tag_path".format(i + 1))
@@ -60,20 +59,20 @@ class INFLUXDB(MDSplus.Device):
 
                 whereList = []
                 if tagPath != '':
-                    whereList.append('tagPath == \'{}\''.format(tagPath))
+                    whereList.append('tagPath = \'{}\''.format(tagPath.data()))
 
                 if start > 0:
                     start /= 1000.0
-                    whereList.append('time > "{}"'.format(datetime.fromtimestamp(start).strftime("%Y-%m-%dT%H:%M:%S.%f")))
+                    whereList.append('time > \'{}000Z\''.format(datetime.fromtimestamp(start).strftime("%Y-%m-%dT%H:%M:%S.%f")))
                 if end > 0:
                     end /= 1000.0
-                    whereList.append('time < "{}"'.format(datetime.fromtimestamp(end).strftime("%Y-%m-%dT%H:%M:%S.%f")))
+                    whereList.append('time < \'{}000Z\''.format(datetime.fromtimestamp(end).strftime("%Y-%m-%dT%H:%M:%S.%f")))
 
                 where = ''
                 if len(whereList) > 0:
                     where = 'WHERE {}'.format(' AND '.join(whereList))
                 
-                query = 'SELECT {} AS value FROM \'{}\' {}'.format(
+                query = 'SELECT {} AS value FROM "{}" {}'.format(
                     node.FUNCTION.data(), 
                     self.series.data(), 
                     where
@@ -85,14 +84,17 @@ class INFLUXDB(MDSplus.Device):
                 result = client.query(query)
                 
                 for row in result.get_points():
+                    # TODO: Cleanup
                     # Strip off ###Z from nanosecond times
-                    timestamp = row['time'][0:-4]
+                    timestamp = row['time'][0:row['time'].find('.') + 4]
+                    if timestamp[-1] == 'Z':
+                        timestamp = timestamp[0:-1]
                     # Parse into datetime
                     timestamp = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
                     # Convert to millisecond UNIX timestamp
                     timestamp = time.mktime(timestamp.timetuple()) * 1000
                     
-                    node.putRow(1, MDSplus.Float32(row['value']), MDSplus.Int64(timestamp))
+                    node.putRow(1000, MDSplus.Float32(float(row['value'])), MDSplus.Int64(timestamp))
                 
             except MDSplus.TreeNODATA:
                 pass
