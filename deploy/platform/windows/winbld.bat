c45fb3641f19361e3118ab4beb0fd3e4a9ce11a3
Hervé Ancher <herve.ancher@cea.fr>
2020-02-19 15:08:22 +0100

    Move Visual Studio config files from 2017 to 2019

--- deploy/platform/windows/winbld.bat
+++ deploy/platform/windows/winbld.bat
@@ -1,4 +1,17 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-set VisualStudioVersion=15.0
+@ECHO off
+
+REM ============================= /!\ WARNING /!\ =============================
+REM Before running this script, make sure that the DEF export files exist!!
+REM It is possible to generate these files by calling command "make defs"
+REM from "mdsobjects\cpp" directory under MinGW terminal for example
+REM ============================= /!\ WARNING /!\ =============================
+
+REM Change VSVER and VSVERNUM to use your current version of Visual Studio
+
+set VSVER=2019
+set VSVERNUM=16.0
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\%VSVER%\Community\VC\Auxiliary\Build\vcvars32.bat"
+set VisualStudioVersion=%VSVERNUM%
 devenv %~dp0..\mdsobjects\MdsObjectsCppShr-VS.sln /build "Release|X64"
 devenv %~dp0..\mdsobjects\MdsObjectsCppShr-VS.sln /build "Release|win32"
--- mdsobjects/cpp/visual-studio-build.bat
+++ mdsobjects/cpp/visual-studio-build.bat
@@ -8,50 +8,53 @@ set SRCDIR=%CD%
 cd %OLDDIR%
 
 REM A subset of the Visual Studio binaries, libs and headers
-REM \Microsoft-Visual-Studio directory is from the C:\Program Files (x86)\Microsoft Visual Studio\ directory on
-REM a Windows system with the free Visual Studio 2017 Community edition installed.
-REM \Windows-Kits is from the C:\Program Files (x64)\Windows Kits\ directory.
-REM The \vc directory is obtained from the following directories when Visual Studio 2017 Community is
-REM installed on a Windows system:
+REM Change VSVER, VCVER and WKVER to use your current version of Visual Studio and SDK
 
-SET MVS=\Microsoft-Visual-Studio\2017\Community\VC\Tools\MSVC\14.12.25827
-SET WK=\Windows-Kits\10
-set PATH=%MVS%\bin\HostX64\x64;%WK%\bin\10.0.16299.0\x64
-set INCLUDE=%MVS%\include;%WK%\include\10.0.16299.0\shared;%WK%\include\10.0.16299.0\ucrt;%WK%\include\10.0.16299.0\um;
-set LIB=%MVS%\lib\x64;%WK%\Lib\10.0.16299.0\um\x64
+set VSVER=2019
+set VCVER=14.24.28314
+set WKVER=10.0.17134.0
+
+set MVS=C:\Program Files (x86)\Microsoft Visual Studio\%VSVER%\Community\VC\Tools\MSVC\%VCVER%
+set WK=C:\Program Files (x86)\Windows Kits\10
+set PATH=%MVS%\bin\HostX64\x64;%WK%\bin\%WKVER%\x64
+set INCLUDE=%MVS%\include;%WK%\include\%WKVER%\shared;%WK%\include\%WKVER%\ucrt;%WK%\include\%WKVER%\um;
+set LIB=%MVS%\lib\x64;%MVS%\lib\onecore\x64; %MVS%\atlmfc\lib\x64;%WK%\lib\%WKVER%\ucrt\x64;%WK%\Lib\%WKVER%\um\x64
 
 REM Various directories used for compiles, links and lib generation
 
 set CPP=%SRCDIR%\mdsobjects\cpp
-set BUILD64=\workspace\releasebld\64
-set BUILD32=\workspace\releasebld\32
-set DEFS=%BUILD64%\defs
+set BUILD64=%SRCDIR%\releasebld\64\bin_x86_64
+set BUILD32=%SRCDIR%\releasebld\32\bin_x86
+
+mkdir %BUILD64%
+mkdir %BUILD32%
+
 REM Build Visual Studio compatible lib files for the C based libraries (both 64 and 32 bit versions)
 
-lib /def:%DEFS%\MdsShr.def	/out:%BUILD64%\bin_x86_64\MdsShr.lib	/machine:x64
-lib /def:%DEFS%\TreeShr.def	/out:%BUILD64%\bin_x86_64\TreeShr.lib	/machine:x64
-lib /def:%DEFS%\TdiShr.def	/out:%BUILD64%\bin_x86_64\TdiShr.lib	/machine:x64
-lib /def:%DEFS%\MdsIpShr.def	/out:%BUILD64%\bin_x86_64\MdsIpShr.lib	/machine:x64
-lib /def:%DEFS%\MdsMisc.def	/out:%BUILD64%\bin_x86_64\MdsMisc.lib	/machine:x64
-lib /def:%DEFS%\MdsLib.def	/out:%BUILD64%\bin_x86_64\MdsLib.lib	/machine:x64
-lib /def:%DEFS%\Mdsdcl.def	/out:%BUILD64%\bin_x86_64\Mdsdcl.lib	/machine:x64
-lib /def:%DEFS%\MdsMath.def	/out:%BUILD64%\bin_x86_64\MdsMath.lib	/machine:x64
-lib /def:%DEFS%\MdsServerShr.def /out:%BUILD64%\bin_x86_64\MdsServerShr.lib /machine:x64
+lib /def:%SRCDIR%\defs\MdsShr.def /out:%BUILD64%\MdsShr.lib /machine:x64
+lib /def:%SRCDIR%\defs\TreeShr.def /out:%BUILD64%\TreeShr.lib /machine:x64
+lib /def:%SRCDIR%\defs\TdiShr.def /out:%BUILD64%\TdiShr.lib /machine:x64
+lib /def:%SRCDIR%\defs\MdsIpShr.def /out:%BUILD64%\MdsIpShr.lib /machine:x64
+lib /def:%SRCDIR%\defs\MdsMisc.def /out:%BUILD64%\MdsMisc.lib /machine:x64
+lib /def:%SRCDIR%\defs\MdsServerShr.def /out:%BUILD64%\MdsServerShr.lib /machine:x64
+lib /def:%SRCDIR%\defs\MdsLib.def /out:%BUILD64%\MdsLib.lib /machine:x64
+lib /def:%SRCDIR%\defs\Mdsdcl.def /out:%BUILD64%\Mdsdcl.lib /machine:x64
+lib /def:%SRCDIR%\defs\MdsMath.def /out:%BUILD64%\MdsMath.lib /machine:x64
 
-lib /def:%DEFS%\MdsShr.def	/out:%BUILD32%\bin_x86\MdsShr.lib	/machine:x86
-lib /def:%DEFS%\TreeShr.def	/out:%BUILD32%\bin_x86\TreeShr.lib	/machine:x86
-lib /def:%DEFS%\TdiShr.def	/out:%BUILD32%\bin_x86\TdiShr.lib	/machine:x86
-lib /def:%DEFS%\MdsIpShr.def	/out:%BUILD32%\bin_x86\MdsIpShr.lib	/machine:x86
-lib /def:%DEFS%\MdsMisc.def	/out:%BUILD32%\bin_x86\MdsMisc.lib	/machine:x86
-lib /def:%DEFS%\MdsLib.def	/out:%BUILD32%\bin_x86\MdsLib.lib	/machine:x86
-lib /def:%DEFS%\Mdsdcl.def	/out:%BUILD32%\bin_x86\Mdsdcl.lib	/machine:x86
-lib /def:%DEFS%\MdsMath.def	/out:%BUILD32%\bin_x86\MdsMath.lib	/machine:x86
-lib /def:%DEFS%\MdsServerShr.def /out:%BUILD32%\bin_x86\MdsServerShr.lib /machine:x86
+lib /def:%SRCDIR%\defs\MdsShr.def /out:%BUILD32%\MdsShr.lib /machine:x86
+lib /def:%SRCDIR%\defs\TreeShr.def /out:%BUILD32%\TreeShr.lib /machine:x86
+lib /def:%SRCDIR%\defs\TdiShr.def /out:%BUILD32%\TdiShr.lib /machine:x86
+lib /def:%SRCDIR%\defs\MdsIpShr.def /out:%BUILD32%\MdsIpShr.lib /machine:x86
+lib /def:%SRCDIR%\defs\MdsMisc.def /out:%BUILD32%\MdsMisc.lib /machine:x86
+lib /def:%SRCDIR%\defs\MdsServerShr.def /out:%BUILD32%\MdsServerShr.lib /machine:x86
+lib /def:%SRCDIR%\defs\MdsLib.def /out:%BUILD32%\MdsLib.lib /machine:x86
+lib /def:%SRCDIR%\defs\Mdsdcl.def /out:%BUILD32%\Mdsdcl.lib /machine:x86
+lib /def:%SRCDIR%\defs\MdsMath.def /out:%BUILD32%\MdsMath.lib /machine:x86
 
 REM Use Visual studio compiler to compile the CPP modules
 
 set CL_OPTS=/c /GS /W3 /Zc:wchar_t /I"%SRCDIR%\include" /I"%SRCDIR%\tdishr" /Zi /Gm- /O2 ^
- /Fd"\tmp\vc141.pdb" /Zc:inline /fp:precise /D "WIN32" /D "NDEBUG" /D "WINDOWS_H" ^
+ /Fd"%TEMP%\vc%VCVER%.pdb" /Zc:inline /fp:precise /D "WIN32" /D "NDEBUG" /D "WINDOWS_H" ^
  /D "_WINDOWS" /D "_USRDLL" /D "MDSOBJECTSCPPSHRVS_EXPORTS" /D "NOMINMAX" ^
  /D "_CRT_SECURE_NO_WARNINGS" /D "_WINDLL" /errorReport:prompt /WX- /Zc:forScope ^
  /Gd /MD /EHsc /nologo  /Fp"MdsObjectsCppShr-VS.pch" /diagnostics:classic
@@ -65,11 +68,11 @@ cl %CL_OPTS% %CPP%\mdstreeobjects.cpp
 
 REM Link MdsObjectsCppShr-VS.DLL
 
-link /OUT:"%BUILD64%\bin_x86_64\MdsObjectsCppShr-VS.dll" /NXCOMPAT /PDB:"MdsObjectsCppShr-VS.pdb" ^
+link /OUT:"%BUILD64%\MdsObjectsCppShr-VS.dll" /NXCOMPAT /PDB:"MdsObjectsCppShr-VS.pdb" ^
  /DYNAMICBASE "MdsShr.lib" "TreeShr.lib" "TdiShr.lib" "MdsIpShr.lib" "kernel32.lib" "user32.lib" ^
  "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" ^
- "uuid.lib" "odbc32.lib" "odbccp32.lib" /IMPLIB:"%BUILD64%\bin_x86_64\MdsObjectsCppShr-VS.lib" /DLL /MACHINE:X64 ^
- /OPT:REF /INCREMENTAL:NO  /SUBSYSTEM:WINDOWS /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" ^
+ "uuid.lib" "odbc32.lib" "odbccp32.lib" /IMPLIB:"%BUILD64%\MdsObjectsCppShr-VS.lib" /DLL /MACHINE:X64 ^
+ /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:WINDOWS /MANIFEST /MANIFESTUAC:"level='asInvoker' uiAccess='false'" ^
  /MANIFEST:embed /OPT:ICF /ERRORREPORT:PROMPT /NOLOGO ^
- /LIBPATH:"%BUILD64%\bin_x86_64" /TLBID:1 ^
+ /LIBPATH:"%BUILD64%" /TLBID:1 ^
  mdsdata.obj mdsdataobjects.obj mdseventobjects.obj mdsipobjects.obj mdstree.obj mdstreeobjects.obj
