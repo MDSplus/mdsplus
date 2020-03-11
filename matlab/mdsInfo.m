86f782f30647e8e289fc382f6048dd0743ef24ab
cloud <timo.schroeder@ipp.mpg.de>
2020-03-11 14:55:24 +0100

    Fix: modern matlab requires less setup for java bride

--- matlab/mdsInfo.m
+++ matlab/mdsInfo.m
@@ -1,68 +1,109 @@
-function info = mdsInfo()
-%mdsInfo - used internally by other functions to retrieve configuration info
-%   
-%
- global MDSINFO
-  global MDSplus_legacy_behavior
-  if isempty(MDSINFO)
-    isOctave = (exist ('OCTAVE_VERSION', 'builtin') > 0);
-    usePython=false;
-    try
-      MDSPLUS_DIR=getenv('MDSPLUS_DIR');
-      if ismac
-        lib=strcat(MDSPLUS_DIR,'/lib/libJavaMds.dylib');
-      elseif isunix
-        lib=strcat(MDSPLUS_DIR,'/lib/libJavaMds.so');
-      elseif ispc
-        lib='C:\WINDOWS\SYSTEM32\JavaMds.dll';
-      end
-      if not(isOctave)
-        java.lang.System.setProperty('JavaMdsLib',lib);
-      end
-      p=javaclasspath('-all');
-      matches = strfind(p,'mdsobjects.jar');
-      if not(any(vertcat(matches{:})))
-        javaaddpath(strcat(MDSPLUS_DIR,'/java/classes/mdsobjects.jar'))
-      end
-      if isOctave
-        x=javaObject('MDSplus.Int32',1);
-      else
-        x=MDSplus.Data.execute('1',javaArray('MDSplus.Data',1));
-      end
-    catch javaerror
-      if isOctave
-        disp('Unable to connect to MDSplus using the java bridge')
-	return
-      end
-      usePython=true;
+function info = mdsInfo(varargin)
+% MDSINFO  used internally by other functions to retrieve configuration info
+    global MDSINFO
+    if ~isempty(MDSINFO)
+        info = MDSINFO;
+    else
+        l = getenv('MDSplus_legacy_behavior');
+        if strcmp(l,'yes')
+            MDSplus_legacy_behavior = true;
+        else
+            MDSplus_legacy_behavior = false;
+        end
+        info.isConnected = false;
+        info.connection = [];
+        info.connectedHost = '';
+        info.usePython = false;
+        info.isPythonConnection = false;
+        info.useLegacy = MDSplus_legacy_behavior;
+        MDSINFO = info;
     end
-    if ~islogical(MDSplus_legacy_behavior)
-      l = getenv('MDSplus_legacy_behavior');
-      if strcmp(l,'yes')
-        MDSplus_legacy_behavior = true;
-      else
-	MDSplus_legacy_behavior = false;
-      end
+    if nargin > 0
+        MDSINFO.usePython = varargin{1};
     end
-    if usePython
-      try
-        py_MDSplus_Int32=str2func('py.MDSplus.Int32');
-        x=py_MDSplus_Int32(int32(1));
-      catch pythonerror
-        disp('Unable to connect to MDSplus using either a java bridge or a python bridge')
-        disp(strcat(' Java error: ',javaerror.message))
-        disp(strcat(' Python error: ',pythonerror.message))
-        return
-      end
+    err = [true,true];
+    if MDSINFO.usePython
+        err(1) = setPython;
+        if ~islogical(err(1))
+            MDSINFO.usePython = false;
+            err(2) = setJavaMds;
+        end
+    else
+        err(2) = setJavaMds;
+        if ~islogical(err(2))
+            MDSINFO.usePython = true;
+            err(1) = setPython;
+        end
     end
-    info.isConnected=false;
-    info.connection=[];
-    info.connectedHost='';
-    info.usePython=usePython;
-    info.isPythonConnection=false;
-    info.useLegacy=MDSplus_legacy_behavior;
-    MDSINFO=info;
-  end
-  info=MDSINFO;
+    if any(~islogical(err))
+        if all(~islogical(err))
+            disp('Unable to connect to MDSplus using either a java bridge or a python bridge')
+            disp(strcat(' Java error: ',err(2).message))
+            disp(strcat(' Python error: ',err(1).message))
+            info = [];
+            MDSINFO = info;
+        elseif ~islogical(err(2))
+            disp('Unable to connect to MDSplus using java bridge, using python bridge instead')
+            disp(strcat(' Java error: ',err(2).message))
+        else % ~islogical(err(1))
+            disp('Unable to connect to MDSplus using python bridge, using python bridge instead')
+            disp(strcat(' Python error: ',err(1).message))
+        end
+    end
 end
-  
+
+function err = setPython()
+    persistent cache
+    if ~isempty(cache)
+        err = cache;
+    else
+        try
+            py.MDSplus.Int32(1);
+            err = false;
+            cache = err;
+        catch err
+        end
+    end
+end
+
+function err = setJavaMds()
+    persistent cache
+    if ~isempty(cache)
+        err = cache;
+    else
+        isOctave = (exist ('OCTAVE_VERSION', 'builtin') > 0);
+        try
+            MDSPLUS_DIR = getenv('MDSPLUS_DIR');
+            if ismac
+                lib = strcat(MDSPLUS_DIR,'/lib/libJavaMds.dylib');
+            elseif isunix
+                lib = strcat(MDSPLUS_DIR,'/lib/libJavaMds.so');
+            elseif ispc
+                lib = 'C:\WINDOWS\SYSTEM32\JavaMds.dll';
+                if ~isfile(lib)
+                    if strcmp(computer('arch'),'win64')
+                        lib = fullfile(MDSPLUS_DIR,'bin64','JavaMds.dll');
+                    else
+                        lib = fullfile(MDSPLUS_DIR,'bin32','JavaMds.dll');
+                    end
+                end
+            end
+            if not(isOctave)
+                java.lang.System.setProperty('JavaMdsLib',lib);
+            end
+            p=javaclasspath('-all');
+            matches = strfind(p,'mdsobjects.jar');
+            if not(any(vertcat(matches{:})))
+               javaaddpath(fullfile(MDSPLUS_DIR,'java','classes','mdsobjects.jar'))
+            end
+            if isOctave
+                javaObject('MDSplus.Int32',1);
+            else
+                MDSplus.Data.execute('1',javaArray('MDSplus.Data',1));
+            end
+            err = false;
+            cache = err;
+        catch err
+        end
+    end
+end
