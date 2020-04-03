71e76d4c86716d12f13bb174b567f3188eb69c2a
Sean <sean.alsop@d-tacq.co.uk>
2020-04-03 10:54:11 +0100

    Removed redundant imports.

--- pydevices/acq400Devices/acq2106_st.py
+++ pydevices/acq400Devices/acq2106_st.py
@@ -23,14 +23,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import numpy as np
-import MDSplus
-import threading
-# import Queue
-from queue import Queue, Empty
-import socket
-import time
-import inspect
 import acq400_base
 
 
