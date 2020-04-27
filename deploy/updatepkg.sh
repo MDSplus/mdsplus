eb54c3667ce66a67bea5667b9bdd34e28ff8013b
Timo Schroeder <cloud@ipp-hgw.mpg.de>
2020-04-27 13:23:30 +0200

    Build: use static WORKSPACE in updatepkg

--- deploy/updatepkg.sh
+++ deploy/updatepkg.sh
@@ -34,11 +34,13 @@ echo "$(date) updating package files for $(pwd)"
 deploy/build.sh --os=bootstrap > "$logfile" 2>&1
 # build jars on first cycle (build on debian9-64)
 jars=""
+build_dir="$(pwd)/build/updatepkg"
 # debian-amd64 debian-i386 debian-armhf redhat-*
 for os in debian9-64 debian7-32 raspberrypi fc25
 do
     echo "$(date) Build of $os starting"
-    if ( deploy/build.sh --os=$os --test=skip --release ${jars} --updatepkg >> "$logfile" 2>&1 )
+    mkdir -p ${build_dir}/$os
+    if ( deploy/build.sh --os=$os --workspace=${build_dir}/$os --test=skip --release ${jars} --updatepkg >> "$logfile" 2>&1 )
     then
 	echo "$(date) Build of $os completed successfully"
     else
@@ -47,5 +49,5 @@ do
 	exit $stat
     fi
     # use jars from first build in subsequention cycles
-    jars="--jars-dir=build/debian9-64/alpha/releasebld/64"
+    jars="--jars-dir=${build_dir}/debian9-64/releasebld/64"
 done
