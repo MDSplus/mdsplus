33d082f00bf1489af8c82f1f8400a577b4f92ce4
Timo Schroeder <cloud@ipp-hgw.mpg.de>
2020-04-27 12:36:59 +0200

    Build: ensure working directory, added comments

--- deploy/updatepkg.sh
+++ deploy/updatepkg.sh
@@ -17,20 +17,25 @@
 # Note this requires a running docker service on your system and your
 # account to be enabled to run docker.
 #
+# ensure we are in source folder, so build path is found
+cd $(dirname $0)/..
+echo "$(date) updating package files for $(pwd)"
+# bootstrap makefiles
+deploy/build.sh --os=bootstrap
+# build jars on first cycle (build on debian9-64)
 jars=""
-#         redhat debian32/64 debian armhf
-$(dirname $0)/build.sh --os=bootstrap
+# debian-amd64 debian-i386 debian-armhf redhat-*
 for os in debian9-64 debian7-32 raspberrypi fc25
 do
     echo "$(date) Build of $os starting"
-    if ( $(dirname $0)/build.sh --os=$os --test=skip --release ${jars} --updatepkg > ./updatepkg.log 2>&1 )
+    if ( deploy/build.sh --os=$os --test=skip --release ${jars} --updatepkg )
     then
 	echo "$(date) Build of $os completed successfully"
     else
 	stat=$?
-	echo "$(date) Build of $os failed. See ./updatepkg.log for details"
+	echo "$(date) Build of $os failed."
 	exit $stat
     fi
+    # use jars from first build in subsequention cycles
     jars="--jars-dir=build/debian9-64/alpha/releasebld/64"
 done
-
