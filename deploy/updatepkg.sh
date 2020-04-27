eb40883c5e74a70474c0d8ff06da5e16174246a6
Timo Schroeder <zack-vii@users.noreply.github.com>
2020-04-27 14:14:17 +0200

    Fixed: updatepkg (#2005)
    * Build: ensure working directory, added comments
    
    * Build: updatepkg log file optional
    
    * Build: use static WORKSPACE in updatepkg
    
    Co-authored-by: Timo Schroeder <cloud@ipp-hgw.mpg.de>

--- deploy/updatepkg.sh
+++ deploy/updatepkg.sh
@@ -17,20 +17,37 @@
 # Note this requires a running docker service on your system and your
 # account to be enabled to run docker.
 #
+# ensure we are in source folder, so build path is found
+if [ -z $1 ]
+then
+logfile=/dev/stdout
+logmessage=
+else
+logfile=$(realpath $1)
+logmessage=" Check '$logfile' for details."
+fi
+cd $(dirname $0)/..
+
+
+echo "$(date) updating package files for $(pwd)"
+# bootstrap makefiles
+deploy/build.sh --os=bootstrap > "$logfile" 2>&1
+# build jars on first cycle (build on debian9-64)
 jars=""
-#         redhat debian32/64 debian armhf
-$(dirname $0)/build.sh --os=bootstrap
+build_dir="$(pwd)/build/updatepkg"
+# debian-amd64 debian-i386 debian-armhf redhat-*
 for os in debian9-64 debian7-32 raspberrypi fc25
 do
     echo "$(date) Build of $os starting"
-    if ( $(dirname $0)/build.sh --os=$os --test=skip --release ${jars} --updatepkg > ./updatepkg.log 2>&1 )
+    mkdir -p ${build_dir}/$os
+    if ( deploy/build.sh --os=$os --workspace=${build_dir}/$os --test=skip --release ${jars} --updatepkg >> "$logfile" 2>&1 )
     then
 	echo "$(date) Build of $os completed successfully"
     else
 	stat=$?
-	echo "$(date) Build of $os failed. See ./updatepkg.log for details"
+	echo "$(date) Build of $os failed.${logmessage}"
 	exit $stat
     fi
-    jars="--jars-dir=build/debian9-64/alpha/releasebld/64"
+    # use jars from first build in subsequention cycles
+    jars="--jars-dir=${build_dir}/debian9-64/releasebld/64"
 done
-
