063dc3f4c9fe2189b1b106ae21144b747b69b9ed
Timo Schroeder <cloud@ipp-hgw.mpg.de>
2020-04-30 18:29:28 +0200

    Fix: __init__ is called on every node instance

--- pydevices/RfxDevices/NI6682.py
+++ pydevices/RfxDevices/NI6682.py
@@ -39,29 +39,35 @@ from time import sleep
 class NI6682(Device):
     """National Instrument 6682 device. This implementation uses only a limited set of the hardware facilities.
        In particular the board is just used as a Time-to-Digital Converter (TDC). """
-    parts = [{'path':':BOARD_ID', 'type':'numeric', 'value':0},
+    parts = [
+        {'path':':BOARD_ID', 'type':'numeric', 'value':0},
         {'path':':COMMENT', 'type':'text'},
-        {'path':':CLOCK_SOURCE', 'type':'text', 'value':'PTP'}]
+        {'path':':CLOCK_SOURCE', 'type':'text', 'value':'PTP'},
+    ]
 
-    for i in range(0,3):
-        parts.append({'path':'.CHANNEL_PFI%d'%(i), 'type':'structure'})
-        parts.append({'path':'.CHANNEL_PFI%d:TRIG_MODE'%(i), 'type':'text', 'value':'EDGE_RISING'})
-        parts.append({'path':'.CHANNEL_PFI%d:DECIMATION'%(i), 'type':'numeric', 'value':1})
-        parts.append({'path':'.CHANNEL_PFI%d:TIME_SEC'%(i), 'type':'numeric', 'options':('no_write_model',)})
-        parts.append({'path':'.CHANNEL_PFI%d:TIME_NSEC'%(i), 'type':'numeric', 'options':('no_write_model',)})
+    for i in range(3):
+        parts.extend([
+            {'path':'.CHANNEL_PFI%d'%(i), 'type':'structure'},
+            {'path':'.CHANNEL_PFI%d:TRIG_MODE'%(i), 'type':'text', 'value':'EDGE_RISING'},
+            {'path':'.CHANNEL_PFI%d:DECIMATION'%(i), 'type':'numeric', 'value':1},
+            {'path':'.CHANNEL_PFI%d:TIME_SEC'%(i), 'type':'numeric', 'options':('no_write_model',)},
+            {'path':'.CHANNEL_PFI%d:TIME_NSEC'%(i), 'type':'numeric', 'options':('no_write_model',)},
+        ])
     del(i)
-    parts.append({'path':':INITIALISE','type':'action',
+    parts.extend([
+        {'path':':INITIALISE','type':'action',
         'valueExpr':"Action(Dispatch('PXI_SERVER','INITIALISE',50,None),Method(None,'INITIALISE',head))",
-        'options':('no_write_shot',)})
-    parts.append({'path':':FINALISE','type':'action',
+        'options':('no_write_shot',)},
+        {'path':':FINALISE','type':'action',
         'valueExpr':"Action(Dispatch('PXI_SERVER','FINALISE',50,None),Method(None,'FINALISE',head))",
-        'options':('no_write_shot',)})
-    parts.append({'path':':START_STORE','type':'action',
+        'options':('no_write_shot',)},
+        {'path':':START_STORE','type':'action',
         'valueExpr':"Action(Dispatch('PXI_SERVER','START_STORE',50,None),Method(None,'START_STORE',head))",
-        'options':('no_write_shot',)})
-    parts.append({'path':':STOP_STORE','type':'action',
+        'options':('no_write_shot',)},
+        {'path':':STOP_STORE','type':'action',
         'valueExpr':"Action(Dispatch('PXI_SERVER','STOP_STORE',50,None),Method(None,'STOP_STORE',head))",
-        'options':('no_write_shot',)})
+        'options':('no_write_shot',)},
+    ])
 
     # Corresponds to the nisync_edge_t type in nisynch.h
     EDGE_RISING = 0;
@@ -81,31 +87,6 @@ class NI6682(Device):
 
     # Declaration of global dictionary of storemen
     ni6682Storemen = {}
-
-    # Why this init is not working!!!!! Also __new__ is called many times the action is
-    #def __init__(self, node):
-
-        #print "NI6682::__init__(): INIZIO."
-
-        #print "node.getNid() = ", node.getNid()
-        #print "node.getFullPath() = ", node.getFullPath()
-
-        #super(NI6682, self).__init__(node);
-
-        #print "self.nid = ", self.nid, " self.getNid() = ", self.getNid()
-	#try:
-            #NI6682.ni6682Storemen[self.getNid()];
-        #except:
-
-            #NI6682.ni6682Storemen[self.getNid()] = self.Storeman(self);
-            #print 'NI6682::__init__(): INFO CREATED'
-
-        #print NI6682.ni6682Storemen
-        #self.ni6682Storeman = NI6682.ni6682Storemen[self.getNid()];
-        #print 'NI6682::__init__(): INFO RESTORED'
-
-        #print "NI6682::__init__(): FINE."
-
 
     def manageInfo(self):
 
@@ -128,7 +109,7 @@ class NI6682(Device):
         print("NI6682::manageInfo(): END")
 
 
-# INIT
+    # INIT
     def INITIALISE(self):
 
         print('INITIALISE')
@@ -175,9 +156,8 @@ class NI6682(Device):
 
         # If executed for the first time
 
-        return;
 
-# STORE
+    # STORE
     def FINALISE(self):
 
         print('FINALISE')
@@ -198,8 +178,6 @@ class NI6682(Device):
 
         res = NI6682.libts.NI6682_Destroy(c_int(boardId));
         print('NI6682_Destroy(): ', res)
-
-        return;
 
 
     class Storeman(Thread):
@@ -281,7 +259,6 @@ class NI6682(Device):
         #self.restoreInfo();
         self.ni6682Storeman.start();
 
-        return;
 
     def STOP_STORE(self):
 
@@ -302,5 +279,3 @@ class NI6682(Device):
 
         del self.ni6682Storeman;
         del NI6682.ni6682Storemen[self.getNid()];
-
-        return;
