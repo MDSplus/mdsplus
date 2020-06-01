ff00f1267df897e76ca48667557669d979563b43
Fernando Santoro <fsantoro@psfc.mit.edu>
2020-06-01 18:32:57 -0400

    Added has_wr=True to use site cC

--- pydevices/HtsDevices/acq2106_WRTD.py
+++ pydevices/HtsDevices/acq2106_WRTD.py
@@ -68,7 +68,8 @@ class ACQ2106_WRTD(MDSplus.Device):
 
     def init(self, newmsg=''):
         import acq400_hapi
-        uut = acq400_hapi.Acq400(self.node.data())
+        #uut = acq400_hapi.Acq400(self.node.data())
+        uut = acq400_hapi.Acq2106('172.20.240.13', has_wr=True)
 
         msgs  = str(newmsg)
         wrmgs = msgs.split(":")
