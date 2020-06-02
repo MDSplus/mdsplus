7a8d74a12c1526bc2f2e60da7d97bbd5260efd11
cloud <timo.schroeder@ipp.mpg.de>
2020-06-02 23:50:54 +0200

    Fix: use jar manifest instead of lengthy classpath

--- java/jtraverser/jTraverser.bat
+++ java/jtraverser/jTraverser.bat
@@ -1,7 +1,1 @@
-@start "" javaw -Xss5M -cp ^
-"%MDSPLUS_DIR%\java\classes\jTraverser.jar^
-;%MDSPLUS_DIR%\java\classes\mdsobjects.jar^
-;%MDSPLUS_DIR%\java\classes\jScope.jar^
-;%MDSPLUS_DIR%\java\classes\DeviceBeans.jar^
-;%MDSPLUS_DIR%\java\classes\jDevices.jar^
-" jTraverser %*
+@start "" javaw -Xss5M -jar "%MDSPLUS_DIR%\java\classes\jTraverser.jar" %*
