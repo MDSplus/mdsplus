0c34957264ba28546a9d606592b1a3e3b93cda50
Timo Schroeder <zack-vii@users.noreply.github.com>
2020-06-11 00:17:28 +0200

    Fix: add mdsobjects to maven and a minor improvement for mdsplus-api (#2056)
    * Build: maven define RELEASEDIR for test
    
    * Build: added mdsobjects for maven
    
    * Fix: use array if ByteBuffer is backed for performance
    
    * Fix: provider private host may be null: use getHost in equals

--- java/mdsplus-api/src/main/java/mds/mdsip/MdsIpJsch.java
+++ java/mdsplus-api/src/main/java/mds/mdsip/MdsIpJsch.java
@@ -3,65 +3,62 @@ package mds.mdsip;
 import java.awt.Window;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ConnectException;
-import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Vector;
 
-import javax.swing.JComponent;
-import javax.swing.JOptionPane;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.ConfigRepository;
-import com.jcraft.jsch.ConfigRepository.Config;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.OpenSSHConfig;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UIKeyboardInteractive;
-import mds.mdsip.MdsIp.Connection;
 
-public final class MdsIpJsch implements Connection{
-	private static class Logger implements com.jcraft.jsch.Logger{
-		public Logger(){}
+import com.jcraft.jsch.*;
+import com.jcraft.jsch.ConfigRepository.Config;
+
+import mds.mdsip.MdsIp.MdsIpIOStream;
+
+public final class MdsIpJsch extends MdsIpIOStream
+{
+	private static class Logger implements com.jcraft.jsch.Logger
+	{
+		public Logger()
+		{}
 
 		@Override
-		public boolean isEnabled(final int level) {
+		public boolean isEnabled(final int level)
+		{
 			return level >= com.jcraft.jsch.Logger.INFO;
 		}
 
 		@Override
-		public void log(final int level, final String message) {
+		public void log(final int level, final String message)
+		{
 			final String type;
-			switch(level){
-				case com.jcraft.jsch.Logger.INFO:
-					type = "INFO";
-					break;
-				case com.jcraft.jsch.Logger.ERROR:
-					type = "ERROR";
-					break;
-				case com.jcraft.jsch.Logger.FATAL:
-					type = "FATAL";
-					break;
-				case com.jcraft.jsch.Logger.WARN:
-					type = "WARN";
-					break;
-				case com.jcraft.jsch.Logger.DEBUG:
-					type = "DEBUG";
-					break;
-				default:
-					type = "DEFAULT";
+			switch (level)
+			{
+			case com.jcraft.jsch.Logger.INFO:
+				type = "INFO";
+				break;
+			case com.jcraft.jsch.Logger.ERROR:
+				type = "ERROR";
+				break;
+			case com.jcraft.jsch.Logger.FATAL:
+				type = "FATAL";
+				break;
+			case com.jcraft.jsch.Logger.WARN:
+				type = "WARN";
+				break;
+			case com.jcraft.jsch.Logger.DEBUG:
+				type = "DEBUG";
+				break;
+			default:
+				type = "DEFAULT";
 			}
 			System.out.println(String.format("%s: %s", type, message));
 		}
 	}
-	static private final class SshServerInfo {
+
+	static private final class SshServerInfo
+	{
 		final SshServerInfo proxy;
 		final String proxyjump;
 		final String user;
@@ -69,52 +66,62 @@ public final class MdsIpJsch implements Connection{
 		final Config config;
 		final int port;
 
-		static public SshServerInfo parse(final String serverstring, ConfigRepository cr) {
+		static public SshServerInfo parse(final String serverstring, ConfigRepository cr)
+		{
 			final int at = serverstring.indexOf("@");
 			final int cn = serverstring.indexOf(":");
 			final String user = at < 0 ? null : serverstring.substring(0, at);
-			final String host = cn < 0 ? serverstring.substring(at + 1).toLowerCase() : serverstring.substring(at + 1, cn).toLowerCase();
+			final String host = cn < 0 ? serverstring.substring(at + 1).toLowerCase()
+					: serverstring.substring(at + 1, cn).toLowerCase();
 			final int port = cn < 0 ? 0 : Integer.parseInt(serverstring.substring(cn + 1));
 			return new SshServerInfo(user, host, port, cr);
 		}
 
-		public SshServerInfo(final String user, final String host, final int port) {
+		public SshServerInfo(final String user, final String host, final int port)
+		{
 			this(user, host, port, MdsIpJsch.getConfigRepository());
 		}
 
-		private SshServerInfo(final String user, final String host, final int port, ConfigRepository cr) {
+		private SshServerInfo(final String user, final String host, final int port, ConfigRepository cr)
+		{
 			config = cr.getConfig(host);
-			String hostname = config.getHostname();
-			this.hostname = hostname!=null ? hostname : host;
-			if (user!=null)
+			final String hostname = config.getHostname();
+			this.hostname = hostname != null ? hostname : host;
+			if (user != null)
 				this.user = user;
-			else {
+			else
+			{
 				final String cuser = config.getUser();
-				this.user = cuser!=null ? cuser : System.getProperty("user.name");
+				this.user = cuser != null ? cuser : System.getProperty("user.name");
 			}
-			if (port!=0)
+			if (port != 0)
 				this.port = port;
-			else {
+			else
+			{
 				final int pport = config.getPort();
-				this.port = pport>0 ? pport : 22;
+				this.port = pport > 0 ? pport : 22;
 			}
 			this.proxyjump = config.getValue("ProxyJump");
-			this.proxy = this.proxyjump==null ? null : SshServerInfo.parse(this.proxyjump, cr);
+			this.proxy = this.proxyjump == null ? null : SshServerInfo.parse(this.proxyjump, cr);
 		}
 
-		public Vector<Session> connect(int timeout) throws JSchException {
+		public Vector<Session> connect(int timeout) throws JSchException
+		{
 			Vector<Session> sessions;
 			Session session;
-			if (this.proxy!=null) {
+			if (this.proxy != null)
+			{
 				sessions = this.proxy.connect(timeout);
-				int pport = sessions.firstElement().setPortForwardingL(0, this.hostname, this.port);
-				session = ((JSch)MdsIpJsch.jsch).getSession(this.user, "127.0.0.1", pport);
-			} else {
+				final int pport = sessions.firstElement().setPortForwardingL(0, this.hostname, this.port);
+				session = ((JSch) MdsIpJsch.jsch).getSession(this.user, "127.0.0.1", pport);
+			}
+			else
+			{
 				sessions = new Vector<Session>();
-				session = ((JSch)MdsIpJsch.jsch).getSession(this.user, this.hostname, this.port);
+				session = ((JSch) MdsIpJsch.jsch).getSession(this.user, this.hostname, this.port);
 			}
 			final String strictHostKeyChecking = config.getValue("StrictHostKeyChecking");
-			if (strictHostKeyChecking!=null)
+			if (strictHostKeyChecking != null)
 				session.setConfig("StrictHostKeyChecking", strictHostKeyChecking);
 			sessions.insertElementAt(session, 0);
 			session.setUserInfo(userinfo);
@@ -124,165 +131,222 @@ public final class MdsIpJsch implements Connection{
 		}
 	}
 
-	public static final class UserInfo implements com.jcraft.jsch.UserInfo, UIKeyboardInteractive{
-		static HashMap<String, String[]>	keyboard_ans			= new HashMap<String, String[]>();
-		static HashMap<String, UserInfo>	keyboard_this			= new HashMap<String, UserInfo>();
-		private final JTextField			passphraseField			= new JPasswordField(20);
-		private final JTextField			passwordField			= new JPasswordField(20);
-		private final AncestorListener		RequestFocusListener	= new AncestorListener(){
+	public static final class UserInfo implements com.jcraft.jsch.UserInfo, UIKeyboardInteractive
+	{
+		static HashMap<String, String[]> keyboard_ans = new HashMap<String, String[]>();
+		static HashMap<String, UserInfo> keyboard_this = new HashMap<String, UserInfo>();
+		private final JTextField passphraseField = new JPasswordField(20);
+		private final JTextField passwordField = new JPasswordField(20);
+		private final AncestorListener RequestFocusListener = new AncestorListener()
+		{
 			@Override
-			public void ancestorAdded(final AncestorEvent e) {
-				final JComponent component = ((JComponent)e.getSource());
+			public void ancestorAdded(final AncestorEvent e)
+			{
+				final JComponent component = ((JComponent) e.getSource());
 				component.grabFocus();
-				final Window win = (Window)component.getTopLevelAncestor();
+				final Window win = (Window) component.getTopLevelAncestor();
 				win.setAlwaysOnTop(true);
 			}
 
 			@Override
-			public void ancestorMoved(final AncestorEvent arg0) {}
+			public void ancestorMoved(final AncestorEvent arg0)
+			{}
 
 			@Override
-			public void ancestorRemoved(final AncestorEvent arg0) {}
+			public void ancestorRemoved(final AncestorEvent arg0)
+			{}
 		};
-		public boolean						tried_pw				= false;
+		public boolean tried_pw = false;
 		{
 			this.passphraseField.addAncestorListener(this.RequestFocusListener);
 			this.passwordField.addAncestorListener(this.RequestFocusListener);
 		}
 
 		@Override
-		public final String getPassphrase() {
-			return this.passphraseField.getText();
-		}
+		public final String getPassphrase()
+		{ return this.passphraseField.getText(); }
 
 		@Override
-		public final String getPassword() {
-			return this.passwordField.getText();
-		}
+		public final String getPassword()
+		{ return this.passwordField.getText(); }
 
 		@Override
-		public String[] promptKeyboardInteractive(final String destination, final String name, final String instruction, final String[] prompt, final boolean[] echo) {
+		public String[] promptKeyboardInteractive(final String destination, final String name, final String instruction,
+				final String[] prompt, final boolean[] echo)
+		{
 			final UserInfo old = UserInfo.keyboard_this.putIfAbsent(destination, this);
-			if(old != null && !this.equals(old)) return UserInfo.keyboard_ans.get(destination).clone();
+			if (old != null && !this.equals(old))
+				return UserInfo.keyboard_ans.get(destination).clone();
 			final Object[] ob = new Object[prompt.length * 2 + 2];
 			ob[0] = name;
 			ob[1] = instruction;
-			for(int i = 0; i < prompt.length; i++){
+			for (int i = 0; i < prompt.length; i++)
+			{
 				ob[i * 2 + 2] = prompt[i];
 				ob[i * 2 + 3] = echo[i] ? new JTextField(20) : new JPasswordField(20);
 			}
-			if(prompt.length > 0) ((JTextField)ob[3]).addAncestorListener(this.RequestFocusListener);
-			final int result = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), ob, destination, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-			if(result != JOptionPane.OK_OPTION) return null;
+			if (prompt.length > 0)
+				((JTextField) ob[3]).addAncestorListener(this.RequestFocusListener);
+			final int result = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), ob, destination,
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if (result != JOptionPane.OK_OPTION)
+				return null;
 			final String[] ans = new String[prompt.length];
-			for(int i = 0; i < prompt.length; i++)
-				ans[i] = ((JTextField)ob[i * 2 + 3]).getText();
+			for (int i = 0; i < prompt.length; i++)
+				ans[i] = ((JTextField) ob[i * 2 + 3]).getText();
 			UserInfo.keyboard_ans.put(destination, ans.clone());
 			UserInfo.keyboard_this.put(destination, this);
 			return ans;
 		}
 
 		@Override
-		public final boolean promptPassphrase(final String message) {
-			if(!this.passphraseField.getText().isEmpty() && !this.tried_pw){
+		public final boolean promptPassphrase(final String message)
+		{
+			if (!this.passphraseField.getText().isEmpty() && !this.tried_pw)
+			{
 				this.tried_pw = true;
 				return true;
 			}
-			final Object[] ob = {message, this.passphraseField};
-			final int result = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), ob, message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-			if(result == JOptionPane.OK_OPTION) return true;
+			final Object[] ob =
+			{ message, this.passphraseField };
+			final int result = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), ob, message,
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if (result == JOptionPane.OK_OPTION)
+				return true;
 			this.passphraseField.setText(null);
 			return false;
 		}
 
 		@Override
-		public final boolean promptPassword(final String message) {
-			if(!this.passwordField.getText().isEmpty() && !this.tried_pw){
+		public final boolean promptPassword(final String message)
+		{
+			if (!this.passwordField.getText().isEmpty() && !this.tried_pw)
+			{
 				this.tried_pw = true;
 				return true;
 			}
-			final Object[] ob = {message, this.passwordField};
-			final int result = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), ob, message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-			if(result == JOptionPane.OK_OPTION) return true;
+			final Object[] ob =
+			{ message, this.passwordField };
+			final int result = JOptionPane.showConfirmDialog(JOptionPane.getRootFrame(), ob, message,
+					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if (result == JOptionPane.OK_OPTION)
+				return true;
 			this.passwordField.setText(null);
 			return false;
 		}
 
 		@Override
-		public final boolean promptYesNo(final String str) {
-			final Object[] options = {"yes", "no"};
-			final int foo = JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), str, "Warning", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+		public final boolean promptYesNo(final String str)
+		{
+			final Object[] options =
+			{ "yes", "no" };
+			final int foo = JOptionPane.showOptionDialog(JOptionPane.getRootFrame(), str, "Warning",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
 			return foo == 0;
 		}
 
 		@Override
-		public final void showMessage(final String message) {
+		public final void showMessage(final String message)
+		{
 			JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), message);
 		}
 	}
-	private static final Object		jsch;
-	private static final UserInfo	userinfo;
+
+	private static final Object jsch;
+	private static final UserInfo userinfo;
 	private static final File dotssh = new File(System.getProperty("user.home"), ".ssh");
-	private static final ConfigRepository getConfigRepository() {
+
+	private static final ConfigRepository getConfigRepository()
+	{
 		final File config = new File(dotssh, "config");
-		if(config.exists()) try{
-			return OpenSSHConfig.parseFile(config.getAbsolutePath());
-		}catch(final IOException e){
-			e.printStackTrace();
-		}
+		if (config.exists())
+			try
+			{
+				return OpenSSHConfig.parseFile(config.getAbsolutePath());
+			}
+			catch (final IOException e)
+			{
+				e.printStackTrace();
+			}
 		return OpenSSHConfig.nullConfig;
 	}
-	static{
+
+	static
+	{
 		JSch _jsch;
-		try{
+		try
+		{
 			_jsch = new JSch();
 			final File known_hosts = new File(dotssh, "known_hosts");
 			final File id_rsa = new File(dotssh, "id_rsa");
-			if(!dotssh.exists()) dotssh.mkdirs();
-			if(known_hosts.exists()) try{
-				_jsch.setKnownHosts(known_hosts.getAbsolutePath());
-			}catch(final JSchException e){
-				e.printStackTrace();
-			}
-			else try{
-				known_hosts.createNewFile();
-			}catch(final IOException e){
-				e.printStackTrace();
-			}
-			if(id_rsa.exists()) try{
-				_jsch.addIdentity(id_rsa.getAbsolutePath());
-			}catch(final JSchException e){
-				e.printStackTrace();
-			}
-		}catch(final Exception e){
+			if (!dotssh.exists())
+				dotssh.mkdirs();
+			if (known_hosts.exists())
+				try
+				{
+					_jsch.setKnownHosts(known_hosts.getAbsolutePath());
+				}
+				catch (final JSchException e)
+				{
+					e.printStackTrace();
+				}
+			else
+				try
+				{
+					known_hosts.createNewFile();
+				}
+				catch (final IOException e)
+				{
+					e.printStackTrace();
+				}
+			if (id_rsa.exists())
+				try
+				{
+					_jsch.addIdentity(id_rsa.getAbsolutePath());
+				}
+				catch (final JSchException e)
+				{
+					e.printStackTrace();
+				}
+		}
+		catch (final Exception e)
+		{
 			System.err.println("Error loading JSch, no ssh support!");
 			_jsch = null;
 		}
 		jsch = _jsch;
 		UserInfo _userinfo = null;
-		try{
+		try
+		{
 			_userinfo = new MdsIpJsch.UserInfo();
-		}catch(final Exception e){
+		}
+		catch (final Exception e)
+		{
 			e.printStackTrace();
 		}
 		userinfo = _userinfo;
 	}
-	private final Channel		channel;
-	private final InputStream	dis;
-	private final OutputStream	dos;
+	private final Channel channel;
 	private final Vector<Session> sessions;
 
-	public MdsIpJsch(String user, String host, int port) throws IOException{
-		if(MdsIpJsch.jsch == null) throw new IOException("JSch not found! SSH connection not available.");
-		try{
-			if(debug.DEBUG.ON) JSch.setLogger(new MdsIpJsch.Logger());
-			SshServerInfo serverinfo = new SshServerInfo(user, host, port);
+	public MdsIpJsch(String user, String host, int port) throws IOException
+	{
+		if (MdsIpJsch.jsch == null)
+			throw new IOException("JSch not found! SSH connection not available.");
+		try
+		{
+			if (debug.DEBUG.ON)
+				JSch.setLogger(new MdsIpJsch.Logger());
+			final SshServerInfo serverinfo = new SshServerInfo(user, host, port);
 			sessions = serverinfo.connect(10_000); // timeout in ms
 			channel = sessions.firstElement().openChannel("exec");
-			((ChannelExec)channel).setCommand("/bin/sh -l -c mdsip-server-ssh");
+			((ChannelExec) channel).setCommand("/bin/sh -l -c mdsip-server-ssh");
 			channel.connect();
-		}catch(final Exception e){
-			if(e instanceof IOException) throw(IOException)e;
+		}
+		catch (final Exception e)
+		{
+			if (e instanceof IOException)
+				throw (IOException) e;
 			throw new ConnectException(e.toString());
 		}
 		this.dis = this.channel.getInputStream();
@@ -290,76 +354,43 @@ public final class MdsIpJsch implements Connection{
 	}
 
 	@Override
-	synchronized public void close() throws IOException {
-		try{
-			try {
+	synchronized public void close() throws IOException
+	{
+		try
+		{
+			try
+			{
 				this.dos.close();
-			}finally {
-				try {
+			}
+			finally
+			{
+				try
+				{
 					this.dis.close();
-				}finally {
-					try{
+				}
+				finally
+				{
+					try
+					{
 						this.channel.disconnect();
-					}finally{
-						for (Session session:sessions)
+					}
+					finally
+					{
+						for (final Session session : sessions)
 							session.disconnect();
 					}
 				}
 			}
-		}catch(final Exception e){
-			if(e instanceof IOException) throw(IOException)e;
+		}
+		catch (final Exception e)
+		{
+			if (e instanceof IOException)
+				throw (IOException) e;
 			throw new IOException(e.toString());
 		}
 	}
 
 	@Override
-	public boolean isOpen() {
-		return !this.channel.isClosed() && this.channel.isConnected() && !this.channel.isEOF();
-	}
-
-	private final int wait_available() throws IOException {
-		int i = 1, tot = 0;
-		int avail = 0;
-		synchronized (dis) {
-			try {
-				while ((avail = dis.available()) == 0 && tot < internal_timeout)
-				{
-					dis.wait(i);
-					tot += i++;
-				}
-			} catch (InterruptedException e) {
-			}
-		}
-		return avail;
-	}
-
-	@Override
-	public int read(ByteBuffer b) throws IOException {
-		final int rem = b.remaining();
-		assert (rem > 0);
-		if (!this.isOpen()) return -1;
-		if (wait_available() == 0) return 0;
-		while (b.hasRemaining())
-		{
-			final int read = dis.read();
-			if (read<0) return read;
-			b.put((byte)read);
-		}
-		return rem - b.remaining();
-	}
-
-	@Override
-	public int write(ByteBuffer b) throws IOException {
-		final int rem = b.remaining();
-		assert (rem > 0);
-		if (!this.isOpen()) return -1;
-		while (b.hasRemaining())
-			dos.write(b.get());
-		final int left = b.remaining();
-		if (left == 0) {
-			dos.flush();
-			return rem;
-		}
-		return rem - left;
-	}
+	public boolean isOpen()
+	{ return !this.channel.isClosed() && this.channel.isConnected() && !this.channel.isEOF(); }
 }
