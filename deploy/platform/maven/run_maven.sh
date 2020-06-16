d4f1ffbde1ca81a990b669d26faba090264819a5
cloud <timo.schroeder@ipp.mpg.de>
2020-06-16 18:52:38 +0200

    Build: fixed maven to be ok wiht projecte.directory

--- deploy/platform/maven/run_maven.sh
+++ deploy/platform/maven/run_maven.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 #
 # runs maven within docker
+
+mkdir -p /workspace/maven
+cd /workspace/maven||exit $?
+mkdir -p /release/maven||exit $?
+
+# setup project.directory
 for f in $(find /maven -name pom.xml)
 do
   target_dir=$(dirname /workspace/maven/${f:6})
   mkdir -p $target_dir
-  cp $f    $target_dir
+  cp -f $f $target_dir
+  ln -sfT $(dirname $f)/src $target_dir/src
 done
-cd /workspace/maven||exit $?
-mkdir -p /release/maven
 
 $MVN versions:set -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION -DartifactId=*
 $MVN $MVNGOAL
