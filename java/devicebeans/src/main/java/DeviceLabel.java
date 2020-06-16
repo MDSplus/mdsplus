1df5cf46ba6a56d9f420f19fb688fecd7f48e670
cloud <timo.schroeder@ipp.mpg.de>
2020-06-16 19:03:43 +0200

    Fix: bug in DeviceLabel detecting change

--- java/devicebeans/src/main/java/DeviceLabel.java
+++ java/devicebeans/src/main/java/DeviceLabel.java
@@ -1,11 +1,6 @@
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.FontMetrics;
+import java.awt.*;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class DeviceLabel extends DeviceComponent
 {
@@ -15,36 +10,21 @@ public class DeviceLabel extends DeviceComponent
 	private static final long serialVersionUID = 1L;
 	String data;
 	public boolean textOnly = false;
-//public boolean showState = false;
 	public boolean displayEvaluated = false;
 	public String labelString = "<empty>";
 	public int numCols = 10;
 	protected boolean initializing = false;
-//  GridBagLayout gridbag;
 	protected int preferredWidth = -1;
 	JPanel jp;
-//protected boolean isGridBag = false;
 	protected String initialField = "<empty>";
 	private final boolean reportingChange = false;
-
-	//  protected JCheckBox checkB;
 	protected JLabel label;
-//  protected JTextField textF;
 
 	public DeviceLabel()
 	{
 		initializing = true;
-//   jp = new JPanel();
-//    jp.add(checkB = new JCheckBox());
-//    checkB.setVisible(false);
 		add(label = new JLabel("<empty>"));
-//    add(jp);
-//    add(textF = new JTextField(10));
-//    textF.setEnabled(editable);
-//    textF.setEditable(editable);
-		// setLayout(gridbag = new GridBagLayout());
 		setLayout(new FlowLayout(FlowLayout.LEFT, 2, 0));
-		// this.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
 		initializing = false;
 	}
 
@@ -139,26 +119,7 @@ public class DeviceLabel extends DeviceComponent
 		}
 		else
 			label.setText("<empty>");
-		/*
-		 * label.setEnabled(is_on); textF.setEnabled(is_on & editable);
-		 * textF.setEditable(is_on & editable);
-		 */
 	}
-	/*
-	 * protected Data getData() { String dataString = textF.getText(); if
-	 * (dataString == null) return null; if (textOnly) return Tree.dataFromExpr("\""
-	 * + dataString + "\""); else return Tree.dataFromExpr(dataString); }
-	 *
-	 *
-	 * protected boolean getState() { if (!showState) return initial_state; else
-	 * return checkB.isSelected(); }
-	 *
-	 * public void setEnabled(boolean state) { if (!editable && state) return; //Do
-	 * not set enabled if not editable //if(checkB != null)
-	 * checkB.setEnabled(state); if (textF != null) { textF.setEnabled(state);
-	 * textF.setEditable(state); } if (label != null) label.setEnabled(state);
-	 * //if(checkB != null) checkB.setSelected(state); //initial_state = state; }
-	 */
 
 	@Override
 	protected String getData()
@@ -172,11 +133,6 @@ public class DeviceLabel extends DeviceComponent
 			return dataString;
 	}
 
-/*
-	 * public void setEditable(boolean editable) { this.editable = editable; }
-	 *
-	 * public boolean getEditable() { return editable; }
-	 */
 	public boolean getDisplayEvaluated()
 	{ return displayEvaluated; }
 
@@ -200,38 +156,7 @@ public class DeviceLabel extends DeviceComponent
 	protected void initializeData(String data, boolean is_on)
 	{
 		initializing = true;
-		// initialField = Tree.dataToString(data);
-		/*
-		 * Container parent = getParent();
-		 *
-		 * if (parent.getLayout() == null) { isGridBag = false; } else isGridBag = true;
-		 *
-		 * GridBagConstraints gc = null; if (isGridBag) { setLayout(gridbag = new
-		 * GridBagLayout()); gc = new GridBagConstraints(); gc.anchor =
-		 * GridBagConstraints.WEST; gc.gridx = gc.gridy = 0; gc.gridwidth =
-		 * gc.gridheight = 1; gc.weightx = gc.weighty = 1.; gc.fill =
-		 * GridBagConstraints.NONE; gridbag.setConstraints(jp, gc); }
-		 */
-		/*
-		 * if (showState) { //add(checkB = new JCheckBox()); checkB.setVisible(true);
-		 * checkB.setSelected(is_on); checkB.addChangeListener(new ChangeListener() {
-		 * public void stateChanged(ChangeEvent e) { boolean state =
-		 * checkB.isSelected(); if (label != null) label.setEnabled(state); if (textF !=
-		 * null && editable) { textF.setEnabled(state); textF.setEditable(state); } }
-		 * }); } if (textF != null && isGridBag) { gc.gridx++; gc.anchor =
-		 * GridBagConstraints.EAST; gridbag.setConstraints(textF, gc); }
-		 */
 		displayData(data, is_on);
-		/*
-		 * setEnabled(is_on); textF.addKeyListener(new KeyAdapter() { public void
-		 * keyTyped(KeyEvent e) { reportingChange = true;
-		 * reportDataChanged(textF.getText()); reportingChange = false; } });
-		 *
-		 * textF.setEnabled(editable); textF.setEditable(editable); if (preferredWidth >
-		 * 0) { setPreferredSize(new Dimension(preferredWidth,
-		 * getPreferredSize().height)); setSize(new Dimension(preferredWidth,
-		 * getPreferredSize().height)); }
-		 */
 		redisplay();
 		initializing = false;
 	}
@@ -240,14 +165,11 @@ public class DeviceLabel extends DeviceComponent
 	protected boolean isDataChanged()
 	{
 		if (displayEvaluated && initialField != null)
-			return !(label.getSize().equals(initialField));
+			return !(label.getText().equals(initialField));
 		else
 			return true;
 	}
 
-	/*
-	 * public boolean supportsState() { return showState; }
-	 */
 	@Override
 	public void setBounds(int x, int y, int width, int height)
 	{
@@ -262,7 +184,6 @@ public class DeviceLabel extends DeviceComponent
 	{
 		this.labelString = labelString;
 		label.setText(labelString);
-		// redisplay();
 	}
 
 	public void setNumCols(int numCols)
@@ -270,20 +191,11 @@ public class DeviceLabel extends DeviceComponent
 		this.numCols = numCols;
 		final FontMetrics fm = this.getFontMetrics(this.getFont());
 		this.setSize(numCols * fm.charWidth('A'), fm.getHeight() + 4);
-		// redisplay();
 	}
 
 	public void setPreferredWidth(int preferredWidth)
 	{ this.preferredWidth = preferredWidth; }
 
-	/*
-	 * public void setShowState(boolean showState) { this.showState = showState; if
-	 * (showState) checkB.setVisible(true); else checkB.setVisible(false);
-	 * //redisplay(); }
-	 */
-	/*
-	 * public boolean getShowState() { return showState; }
-	 */
 	public void setTextOnly(boolean textOnly)
 	{ this.textOnly = textOnly; }
 }
