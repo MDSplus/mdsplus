e9da487df1ab8d4a6b6c27edf31d17e6ebb5529a
Fernando Santoro <44955673+santorofer@users.noreply.github.com>
2020-10-08 00:52:44 -0500

    Feature: ACQ2106 with 435ELF modules (#2118)
    * New device for a acq2106 + 435 modules
    
    * Fixed references to 423 in the code
    
    * Fixed name of DEF_DCIM node to match the default in the input nodes
    
    * Changed frombuffer dtype to int32
    
    * Removed some commented out lines
    
    * Added slots[] to set the HW decim for each slot
    
    * Fixed wrong int type in definition of segment_bytes (int16 vs int32)
    
    * acq435st.py has been removed, function superseded by acq2106_435st.py
    
    * Asking the value of the SR directly to the ACQ (SIG:SYN_S1:FREQ)
    
    * Asking the value of the SR directly to the ACQ using SIG:CLK_S1:FREQ/512
    
    * Added comment about HR_512 to clarify the calculation
    
    * Corrected a small mistake when casting string to float
    
    * Getting the settle value of the SR from the ACQ
    
    * Removing the logic to set value of the SR from the ACQ.
    
    * Fixed a debugging print statement
    
    * Remove sleep used to ensure the clock measurement has stabilzed when using soft trigger. Not needed

--- pydevices/HtsDevices/acq2106_435st.py
+++ pydevices/HtsDevices/acq2106_435st.py
@@ -22,9 +22,12 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-import numpy as np
 import MDSplus
 import threading
+
+import time
+import socket
+import numpy as np
 import sys
 if sys.version_info < (3,):
     from Queue import Queue, Empty
@@ -32,11 +35,11 @@ else:
     from queue import Queue, Empty
 
 
-class ACQ435ST(MDSplus.Device):
+class _ACQ2106_435ST(MDSplus.Device):
     """
-    D-Tacq ACQ435 Digitizer real time streaming support.
+    D-Tacq ACQ2106 with ACQ435 Digitizers (up to 6)  real time streaming support.
 
-    32 Channels
+    32 Channels * number of slots
     Minimum 2Khz Operation
     24 bits == +-10V
 
@@ -45,18 +48,15 @@ class ACQ435ST(MDSplus.Device):
       Soft - starts recording on trigger method (reboot / configuration required to switch )
       Hard - starts recording on hardware trigger input
 
-    Hardware averaging / decimation by power of 2 samples
-      1,2,4,8,16,32
-
     Software sample decimation
 
     Settable segment length in number of samples
 
-    debug - is debugging enabled.  Controlled by environment variable DEBUG_DEVICES
+    debugging() - is debugging enabled.  Controlled by environment variable DEBUG_DEVICES
 
     """
 
-    parts=[
+    carrier_parts=[
         {'path':':NODE','type':'text','value':'192.168.0.254', 'options':('no_write_shot',)},
         {'path':':SITE','type':'numeric', 'value': 1, 'options':('no_write_shot',)},
         {'path':':COMMENT','type':'text', 'options':('no_write_shot',)},
@@ -84,16 +84,7 @@ class ACQ435ST(MDSplus.Device):
          'options':('no_write_shot',)},
         ]
 
-
-    for i in range(32):
-        parts.append({'path':':INPUT_%2.2d'%(i+1,),'type':'signal','options':('no_write_model','write_once',),
-                      'valueExpr':'head.setChanScale(%d)' %(i+1,)})
-        parts.append({'path':':INPUT_%2.2d:DECIMATE'%(i+1,),'type':'NUMERIC', 'value':1, 'options':('no_write_shot')})
-        parts.append({'path':':INPUT_%2.2d:COEFFICIENT'%(i+1,),'type':'NUMERIC', 'value':1, 'options':('no_write_shot')})
-        parts.append({'path':':INPUT_%2.2d:OFFSET'%(i+1,),'type':'NUMERIC', 'value':1, 'options':('no_write_shot')})
-    del i
-
-    debug=None
+    data_socket = -1
 
     trig_types=[ 'hard', 'soft', 'automatic']
 
@@ -101,24 +92,25 @@ class ACQ435ST(MDSplus.Device):
         NUM_BUFFERS = 20
 
         def __init__(self,dev):
-            super(ACQ435ST.MDSWorker,self).__init__(name=dev.path)
+            super(_ACQ2106_435ST.MDSWorker,self).__init__(name=dev.path)
             threading.Thread.__init__(self)
 
             self.dev = dev.copy()
 
             self.chans = []
             self.decim = []
-            self.nchans = 32
+            self.nchans = self.dev.sites*32
 
             for i in range(self.nchans):
-                self.chans.append(getattr(self.dev, 'input_%2.2d'%(i+1)))
-                self.decim.append(getattr(self.dev, 'input_%2.2d_decimate' %(i+1)).data())
+                self.chans.append(getattr(self.dev, 'input_%3.3d'%(i+1)))
+                self.decim.append(getattr(self.dev, 'input_%3.3d_decimate' %(i+1)).data())
 
             self.seg_length = self.dev.seg_length.data()
             self.segment_bytes = self.seg_length*self.nchans*np.int32(0).nbytes
 
+
             self.empty_buffers = Queue()
-            self.full_buffers = Queue()
+            self.full_buffers  = Queue()
 
             for i in range(self.NUM_BUFFERS):
                 self.empty_buffers.put(bytearray(self.segment_bytes))
@@ -128,6 +120,7 @@ class ACQ435ST(MDSplus.Device):
         def run(self):
             import acq400_hapi
             import ast
+            import re
             uut = acq400_hapi.Acq400(self.dev.node.data(), monitor=False)
 
             def lcm(a,b):
@@ -145,21 +138,25 @@ class ACQ435ST(MDSplus.Device):
 
             event_name = self.dev.seg_event.data()
 
-            trig = self.dev.trigger.data()
-            
-            # Retrive the actual value of NACC (samples) already set in the ACQ box
-            nacc_str   = uut.s1.get_knob('nacc')
-            if nacc_str == '0,0,0':
-                nacc_sample = 1
-            else:
-                nacc_tuple  = ast.literal_eval(nacc_str)
-                nacc_sample = nacc_tuple[0]
+            for card in range(self.dev.sites):
+                # Retrive the actual value of NACC (samples) already set in the ACQ box
+                # nacc_str = uut.s1.get_knob('nacc')
+                nacc_str = self.dev.slots[card].nacc
+
+                if nacc_str == '0,0,0':
+                    nacc_sample = 1
+                else:
+                    nacc_tuple  = ast.literal_eval(nacc_str)
+                    nacc_sample = nacc_tuple[0]
                 
             if self.dev.debug:
-                print("The ACQ NACC sample value was set to {}".format(nacc_sample))
+                print("The ACQ NACC sample value is {}".format(nacc_sample))
 
-            # nacc_sample values are always between 1 and 32, set in the ACQ box by the device INIT() function, therefore:
-            dt = 1./self.dev.freq.data() * nacc_sample
+            # nacc_sample values are always between 1 and 32, set in the ACQ box by the device INIT() function
+            dt = float(1./self.dev.freq.data() * nacc_sample)
+
+            if self.dev.debug:
+                print("The SR is {} and timebase delta t is {}".format(self.dev.freq.data(), dt))
 
             decimator = lcma(self.decim)
 
@@ -169,7 +166,6 @@ class ACQ435ST(MDSplus.Device):
             self.device_thread.start()
 
             segment = 0
-            first = True
             running = self.dev.running
             max_segments = self.dev.max_segments.data()
             while running.on and segment < max_segments:
@@ -218,18 +214,17 @@ class ACQ435ST(MDSplus.Device):
                 self.running = False
 
             def run(self):
-                import socket
-                import time
                 if self.debug:
                     print("DeviceWorker running")
 
                 self.running = True
-                first = True
+
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect((self.node_addr,4210))
                 s.settimeout(6)
 
                 # trigger time out count initialization:
+                first = True
                 while self.running:
                     try:
                         buf = self.empty_buffers.get(block=False)
@@ -237,7 +232,7 @@ class ACQ435ST(MDSplus.Device):
                         print("NO BUFFERS AVAILABLE. MAKING NEW ONE")
                         buf = bytearray(self.segment_bytes)
 
-                    toread = self.segment_bytes
+                    toread =self.segment_bytes
                     try:
                         view = memoryview(buf)
                         while toread:
@@ -249,14 +244,14 @@ class ACQ435ST(MDSplus.Device):
                             toread -= nbytes
 
                     except socket.timeout as e:
-                        print("We have Got a timeout.")
+                        print("Got a timeout.")
                         err = e.args[0]
                         # this next if/else is a bit redundant, but illustrates how the
                         # timeout exception is setup
 
                         if err == 'timed out':
                             time.sleep(1)
-                            print (' received timed out, retry later')
+                            print (' recv timed out, retry later')
                             continue
                         else:
                             print (e)
@@ -273,20 +268,20 @@ class ACQ435ST(MDSplus.Device):
                         else:
                             self.full_buffers.put(buf)
 
-    def setChanScale(self,num):
-        chan=self.__getattr__('INPUT_%2.2d' % num)
-        chan.setSegmentScale(MDSplus.ADD(MDSplus.MULTIPLY(chan.COEFFICIENT,MDSplus.dVALUE()),chan.OFFSET))
-
     def init(self):
-        import os
-        import sys
-        from threading import Thread
-        import tempfile
-        import subprocess
         import acq400_hapi
-
         uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
         uut.s0.set_knob('set_abort', '1')
+
+        try:
+            self.slots = [uut.s1]
+            self.slots.append(uut.s2)
+            self.slots.append(uut.s3)
+            self.slots.append(uut.s4)
+            self.slots.append(uut.s5)
+            self.slots.append(uut.s6)
+        except:
+            pass
 
         if self.ext_clock.length > 0:
             raise Exception('External Clock is not supported')
@@ -304,34 +299,30 @@ class ACQ435ST(MDSplus.Device):
         # modifiers [CLK|TRG:SENSE=falling|rising] [CLK|TRG:DX=d0|d1]
         # modifiers [TRG=int|ext]
         # modifiers [CLKDIV=div]  
-        # If the user has specified a trigger.
         uut.s0.sync_role = '%s %s TRG:DX=%s' % ('master', self.freq.data(), trg_dx)
 
         if self.debug:
             print("Hardware Filter (NACC) from tree node is {}".format(int(self.hw_filter.data())))
-
+        
         # Hardware Filter: Accumulate/Decimate filter. Accumulate nacc_samp samples, then output one value.
         nacc_samp = int(self.hw_filter.data())
-        if 1 <= nacc_samp <= 32:
-            uut.s1.nacc = ('%d'%nacc_samp).strip()
-        else:
-            print("WARNING: Hardware Filter samples must be in the range [0,32]. 0 => Disabled == 1")
-            uut.s1.nacc = '1'
-#
-#  Read the coeffients and offsets
-#  for each channel
-#    store its' coeff and offset
-#
-#        for chan in range(32):
-#
-        coeffs  =  map(float, uut.s1.AI_CAL_ESLO.split(" ")[3:] )
-        offsets =  map(float, uut.s1.AI_CAL_EOFF.split(" ")[3:] )
+        print("Number of sites in use {}".format(self.sites))
 
-        for i in range(32):
-            coeff = self.__getattr__('input_%2.2d_coefficient'%(i+1))
-            coeff.record = coeffs[i]
-            offset = self.__getattr__('input_%2.2d_offset'%(i+1))
-            offset.record = offsets[i]
+        for card in range(self.sites):
+            if 1 <= nacc_samp <= 32:
+                self.slots[card].nacc = ('%d'%nacc_samp).strip()
+            else:
+                print("WARNING: Hardware Filter samples must be in the range [0,32]. 0 => Disabled == 1")
+                self.slots[card].nacc = '1'
+
+            coeffs  =  map(float, self.slots[card].AI_CAL_ESLO.split(" ")[3:] )
+            offsets =  map(float, uut.s1.AI_CAL_EOFF.split(" ")[3:] )
+            for i in range(32):
+                coeff = self.__getattr__('input_%3.3d_coefficient'%(card*32+i+1))
+                coeff.record = coeffs[i]
+                offset = self.__getattr__('input_%3.3d_offset'%(card*32+i+1))
+                offset.record = offsets[i]
+
         self.running.on=True
         thread = self.MDSWorker(self)
         thread.start()
@@ -346,3 +337,33 @@ class ACQ435ST(MDSplus.Device):
         uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
         uut.s0.set_knob('soft_trigger','1')
     TRIG=trig
+
+    def setChanScale(self,num):
+        chan=self.__getattr__('INPUT_%3.3d' % num)
+        chan.setSegmentScale(MDSplus.ADD(MDSplus.MULTIPLY(chan.COEFFICIENT,MDSplus.dVALUE()),chan.OFFSET))
+
+
+def assemble(cls):
+    cls.parts = list(_ACQ2106_435ST.carrier_parts)
+    for i in range(cls.sites*32):
+        cls.parts += [
+            {'path':':INPUT_%3.3d'%(i+1,),            'type':'SIGNAL', 'valueExpr':'head.setChanScale(%d)' %(i+1,),'options':('no_write_model','write_once',)},
+            {'path':':INPUT_%3.3d:DECIMATE'%(i+1,),   'type':'NUMERIC','valueExpr':'head.def_dcim',            'options':('no_write_shot',)},           
+            {'path':':INPUT_%3.3d:COEFFICIENT'%(i+1,),'type':'NUMERIC',                                            'options':('no_write_model', 'write_once',)},
+            {'path':':INPUT_%3.3d:OFFSET'%(i+1,),     'type':'NUMERIC',                                            'options':('no_write_model', 'write_once',)},
+        ]
+
+class ACQ2106_435_1ST(_ACQ2106_435ST): sites=1
+assemble(ACQ2106_435_1ST)
+class ACQ2106_435_2ST(_ACQ2106_435ST): sites=2
+assemble(ACQ2106_435_2ST)
+class ACQ2106_435_3ST(_ACQ2106_435ST): sites=3
+assemble(ACQ2106_435_3ST)
+class ACQ2106_435_4ST(_ACQ2106_435ST): sites=4
+assemble(ACQ2106_435_4ST)
+class ACQ2106_435_5ST(_ACQ2106_435ST): sites=5
+assemble(ACQ2106_435_5ST)
+class ACQ2106_435_6ST(_ACQ2106_435ST): sites=6
+assemble(ACQ2106_435_6ST)
+
+del(assemble)
