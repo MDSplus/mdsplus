529b5a335c5d08d5f38fc977cdc77c0eb5689f31
Fernando Santoro <fsantoro@psfc.mit.edu>
2020-12-02 18:34:39 -0500

    Fix: remove bug from suoer().__init__()

--- pydevices/HtsDevices/acq2106_423st.py
+++ pydevices/HtsDevices/acq2106_423st.py
@@ -165,7 +165,7 @@ class _ACQ2106_423ST(MDSplus.Device):
             running = False
 
             def __init__(self,mds):
-                super(_ACQ2106_423ST.DeviceWorker, self).__init__()
+                super(_ACQ2106_423ST.MDSWorker.DeviceWorker, self).__init__()
                 self.debug = mds.dev.debug
                 self.node_addr = mds.dev.node.data()
                 self.seg_length = mds.dev.seg_length.data()
