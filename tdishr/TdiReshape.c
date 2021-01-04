1b547b4e2c4356bcbdedd842978f757c45116bd2
cloud <timo.schroeder@ipp.mpg.de>
2021-01-04 18:20:59 +0100

    Feature: added first implementation for SQUEEZE and FLATTEN;
    TODO: handle complex array variants like array_bounds

---
+++ tdishr/TdiReshape.c
@@ -0,0 +1,80 @@
+/*
+Copyright (c) 2017, Massachusetts Institute of Technology All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* implements Reshape, Squeeze, and Flatten */
+
+#include <tdishr.h>
+#include <mdsdescrip.h>
+#include <mdsshr.h>
+
+int Tdi1Reshape(opcode_t opcode, int narg, struct descriptor *list[], struct descriptor_xd *out_ptr)
+{
+    if (narg < 1)
+        return TdiMISS_ARG;
+    if (list[0]->class != CLASS_A
+     && list[0]->class != CLASS_CA
+     && list[0]->class != CLASS_APD)
+        return TdiINVCLADSC;
+    int status = MdsCopyDxXd(list[0], out_ptr);
+    array_coeff *arr = (array_coeff *)out_ptr->pointer;
+    int i;
+    switch (opcode)
+    {
+        default:
+            return TdiNO_OPC;
+        case OPC_RESHAPE:
+            return TdiNO_OPC;
+        case OPC_FLATTEN:
+        {
+            for (i = 1 ; i < arr->dimct ; i++)
+            {
+                arr->m[0] *= arr->m[i];
+                arr->m[i] = 0;
+            }
+            arr->dimct = arr->m[0] > 0 ? 1 : 0;
+            break;
+        }
+        case OPC_SQUEEZE:
+        {
+            int j = 0;
+            for (i = 0 ; i < arr->dimct ; i++)
+            {
+                if (arr->m[i] == 1)
+                {
+                    j++;
+                }
+                else
+                {
+                    arr->m[i-j] = arr->m[i];
+                }
+            }
+            for (i = arr->dimct-j ; i < arr->dimct ; i++)
+                arr->m[i] = 0;
+            arr->dimct = arr->dimct-j;
+            break;
+        }
+    }
+    return status;
+}
