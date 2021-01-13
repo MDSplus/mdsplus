40d16fded280dde40863bf9b51081b8fbf7ba6be
Fernando Santoro <fsantoro@psfc.mit.edu>
2021-01-13 03:26:16 -0500

    Add site specific GAINS and OFFSETS control plus GLOBAL

--- pydevices/HtsDevices/acq2106_435sc.py
+++ pydevices/HtsDevices/acq2106_435sc.py
@@ -26,6 +26,7 @@
 import MDSplus
 import importlib
 import os
+import numbers
 
 acq2106_435st = importlib.import_module('acq2106_435st')
 acq400_hapi = importlib.import_module('acq400_hapi')  
@@ -36,26 +37,95 @@ class _ACQ2106_435SC(acq2106_435st._ACQ2106_435ST):
     """
 
     sc_parts = [
-        {'path':':DEF_GAIN1',  'type':'numeric', 'value': 1, 'options':('no_write_shot',)},
-        {'path':':DEF_GAIN2',  'type':'numeric', 'value': 1, 'options':('no_write_shot',)},
-        {'path':':DEF_OFFSET','type':'numeric', 'value': 0, 'options':('no_write_shot',)},
+        {'path':':DEF_GAIN1',  'type':'numeric', 'value': '1', 'options':('no_write_shot',)},
+        {'path':':DEF_GAIN2',  'type':'numeric', 'value': '1', 'options':('no_write_shot',)},
+        {'path':':DEF_OFFSET', 'type':'numeric', 'value': '0', 'options':('no_write_shot',)},
     ]
 
     def init(self):
         super(_ACQ2106_435SC, self).init()
         uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
 
-        uut.s1.SC32_OFFSET_ALL = self.def_offset.data()
-        print("OFFSET ALL {}".format(self.def_offset.data()))
+        uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
 
-        uut.s1.SC32_G1_ALL     = self.def_gain1.data()
-        print("GAIN 1 ALL {}".format(self.def_gain1.data()))
+        chans_sc  = []
+        nchannels = uut.nchan()
+        for ii in range(nchannels):
+            chans_sc.append(getattr(self, 'INPUT_%3.3d'%(ii+1)))
 
-        uut.s1.SC32_G2_ALL     = self.def_gain2.data()
-        print("GAIN 2 ALL {}".format(self.def_gain2.data()))
+        for site in range(1,6,2):
+            if site == 1:
+                for ic, ch in enumerate(chans_sc):
+                    if ic < 33:
+                        uut_s1_sc32_offset = uut.s1.__getattr__('SC32_OFFSET_%2.2d' %(ic+1))
+                        uut_s1_sc32_g1     = uut.s1.__getattr__('SC32_G1_%2.2d' %(ic+1))
+                        uut_s1_sc32_g2     = uut.s1.__getattr__('SC32_G2_%2.2d' %(ic+1))
+                        if ch.on:
+                            uut_s1_sc32_offset = self.__getattr__('INPUT_%3.3d:SC_GAIN1' %(ic+1)).data()
+                            uut_s1_sc32_g1     = self.__getattr__('INPUT_%3.3d:SC_GAIN2' %(ic+1)).data()
+                            uut_s1_sc32_g2     = self.__getattr__('INPUT_%3.3d:SC_OFFSET'%(ic+1)).data()
+                if isinstance(self.def_offset.data() , numbers.Number) and isinstance(self.def_gain1.data() , numbers.Number) and isinstance(self.def_gain2.data() , numbers.Number):
+                   # Global controls for GAINS and OFFSETS
+                    uut.s1.SC32_OFFSET_ALL = self.def_offset.data()
+                    print("OFFSET ALL {}".format(self.def_offset.data()))
 
-        uut.s1.SC32_GAIN_COMMIT = 1
-        print("GAIN Committed")
+                    uut.s1.SC32_G1_ALL     = self.def_gain1.data()
+                    print("GAIN 1 ALL {}".format(self.def_gain1.data()))
+
+                    uut.s1.SC32_G2_ALL     = self.def_gain2.data()
+                    print("GAIN 2 ALL {}".format(self.def_gain2.data()))
+
+                uut.s1.SC32_GAIN_COMMIT = 1
+                print("GAIN Committed for site {}".format(site))
+
+            elif site == 3:
+                for ic, ch in enumerate(chans_sc):
+                    if ic < 33:
+                        uut_s1_sc32_offset = uut.s3.__getattr__('SC32_OFFSET_%2.2d' %(ic+1))
+                        uut_s1_sc32_g1     = uut.s3.__getattr__('SC32_G1_%2.2d' %(ic+1))
+                        uut_s1_sc32_g2     = uut.s3.__getattr__('SC32_G2_%2.2d' %(ic+1))
+                        if ch.on:
+                            uut_s1_sc32_offset = self.__getattr__('INPUT_%3.3d:SC_GAIN1' %(ic+1)).data()
+                            uut_s1_sc32_g1     = self.__getattr__('INPUT_%3.3d:SC_GAIN2' %(ic+1)).data()
+                            uut_s1_sc32_g2     = self.__getattr__('INPUT_%3.3d:SC_OFFSET'%(ic+1)).data()
+                if isinstance(self.def_offset.data() , numbers.Number) and isinstance(self.def_gain1.data() , numbers.Number) and isinstance(self.def_gain2.data() , numbers.Number):
+                   # Global controls for GAINS and OFFSETS
+                    uut.s3.SC32_OFFSET_ALL = self.def_offset.data()
+                    print("OFFSET ALL {}".format(self.def_offset.data()))
+
+                    uut.s3.SC32_G1_ALL     = self.def_gain1.data()
+                    print("GAIN 1 ALL {}".format(self.def_gain1.data()))
+
+                    uut.s3.SC32_G2_ALL     = self.def_gain2.data()
+                    print("GAIN 2 ALL {}".format(self.def_gain2.data()))
+
+                uut.s3.SC32_GAIN_COMMIT = 1
+                print("GAIN Committed for site {}".format(site))
+
+            elif site == 5:
+                for ic, ch in enumerate(chans_sc):
+                    if ic < 33:
+                        uut_s5_sc32_offset = uut.s5.__getattr__('SC32_OFFSET_%2.2d' %(ic+1))
+                        uut_s5_sc32_g1     = uut.s5.__getattr__('SC32_G1_%2.2d' %(ic+1))
+                        uut_s5_sc32_g2     = uut.s5.__getattr__('SC32_G2_%2.2d' %(ic+1))
+                        if ch.on:
+                            uut_s5_sc32_offset = self.__getattr__('INPUT_%3.3d:SC_GAIN1' %(ic+1)).data()
+                            uut_s5_sc32_g1     = self.__getattr__('INPUT_%3.3d:SC_GAIN2' %(ic+1)).data()
+                            uut_s5_sc32_g2     = self.__getattr__('INPUT_%3.3d:SC_OFFSET'%(ic+1)).data()
+                if isinstance(self.def_offset.data() , numbers.Number) and isinstance(self.def_gain1.data() , numbers.Number) and isinstance(self.def_gain2.data() , numbers.Number):
+                   # Global controls for GAINS and OFFSETS
+                    uut.s5.SC32_OFFSET_ALL = self.def_offset.data()
+                    print("OFFSET ALL {}".format(self.def_offset.data()))
+
+                    uut.s5.SC32_G1_ALL     = self.def_gain1.data()
+                    print("GAIN 1 ALL {}".format(self.def_gain1.data()))
+
+                    uut.s5.SC32_G2_ALL     = self.def_gain2.data()
+                    print("GAIN 2 ALL {}".format(self.def_gain2.data()))
+
+                uut.s5.SC32_GAIN_COMMIT = 1
+                print("GAIN Committed for site {}".format(site))
+
 
     def store(self):
         uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
@@ -86,9 +156,9 @@ def assemble(cls):
             {'path':':INPUT_%3.3d:DECIMATE'%(i+1,),    'type':'NUMERIC', 'valueExpr':'head.def_dcim',                'options':('no_write_shot',)},           
             {'path':':INPUT_%3.3d:COEFFICIENT'%(i+1,), 'type':'NUMERIC',                                             'options':('no_write_model', 'write_once',)},
             {'path':':INPUT_%3.3d:OFFSET'%(i+1,),      'type':'NUMERIC',                                             'options':('no_write_model', 'write_once',)},
-            {'path':':INPUT_%3.3d:SC_GAIN1'%(i+1,),    'type':'NUMERIC', 'valueExpr':'head.def_gain1',               'options':('no_write_shot', 'write_once',)},
-            {'path':':INPUT_%3.3d:SC_GAIN2'%(i+1,),    'type':'NUMERIC', 'valueExpr':'head.def_gain2',               'options':('no_write_shot', 'write_once',)},
-            {'path':':INPUT_%3.3d:SC_OFFSET'%(i+1,),   'type':'NUMERIC', 'valueExpr':'head.def_offset',              'options':('no_write_shot', 'write_once',)},   
+            {'path':':INPUT_%3.3d:SC_GAIN1'%(i+1,),    'type':'NUMERIC', 'valueExpr':'head.def_gain1',               'options':('no_write_shot',)},
+            {'path':':INPUT_%3.3d:SC_GAIN2'%(i+1,),    'type':'NUMERIC', 'valueExpr':'head.def_gain2',               'options':('no_write_shot',)},
+            {'path':':INPUT_%3.3d:SC_OFFSET'%(i+1,),   'type':'NUMERIC', 'valueExpr':'head.def_offset',              'options':('no_write_shot',)},   
             #{'path':':INPUT_%3.3d:NON_SC'%(i+1,),          'type':'SIGNAL',  'valueExpr':'SUBTRACT(DIVIDE(INPUT_%3.3d, MULTIPLY(INPUT_%3.3d_SC_GAIN1, INPUT_%3.3d_SC_GAIN2)), INPUT_%3.3d_SC_OFFSET)' %(i+1,i+1,i+1,i+1,), 'options':('no_write_model','write_once',)},
             {'path':':INPUT_%3.3d:NON_SC'%(i+1,),      'type':'SIGNAL',                                              'options':('no_write_model','write_once',)},
             {'path':':INPUT_%3.3d:LOW_RES'%(i+1,),     'type':'SIGNAL',                                              'options':('no_write_model','write_once',)},
