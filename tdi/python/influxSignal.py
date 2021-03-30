8ac1227b9069b7b71eecf8406b2a45775cc47915
Fernando Santoro <fsantoro@psfc.mit.edu>
2021-03-30 10:43:13 -0400

    Only use getTimeContext. Query was improved to be more general

--- tdi/python/influxSignal.py
+++ tdi/python/influxSignal.py
@@ -18,8 +18,8 @@ def influxSignal(dbname, measurement, field_value):
     measurement = measurement.data()
     field_value = field_value.data()
 
-    MDSplus.Data.execute('TreeOpen("influx", 0)')
     start_end_times = MDSplus.Tree.getTimeContext()
+
     print('Getting time context from the tree: {} '.format(start_end_times))
     start_time = str(int(start_end_times[0]))
     end_time   = str(int(start_end_times[1]))
@@ -30,7 +30,7 @@ def influxSignal(dbname, measurement, field_value):
     # measurement = h2o_feet
     # field_value = water_level
     # 'SELECT "water_level" FROM "h2o_feet" WHERE time >= 1568745000000000000 AND time <= 1568750760000000000;'
-    query = 'SELECT "%s" FROM "%s" WHERE time >= %s AND time <= %s;' % (field_value, measurement, start_time, end_time)
+    query = 'SELECT "%s" AS value FROM "%s" WHERE time >= %s AND time <= %s;' % (field_value, measurement, start_time, end_time)
     print('Query: %s' % query)
 
     result = client.query(query, params={'epoch': 'ms'})
@@ -42,7 +42,7 @@ def influxSignal(dbname, measurement, field_value):
 
     i = 0
     for row in data:
-        valueData[i] = float(row['water_level'])
+        valueData[i] = float(row['value'])
         timeData[i] = row['time']
         i += 1
 
