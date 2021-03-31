8a13f653e244d43b10d0cde654cb301449a50c37
Josh Stillerman <jas@psfc.mit.edu>
2021-03-31 12:40:05 -0400

    Fix: rename MdsFLib - MdsLib_fortran
    change the name to correspond to _client name.
    remove the test of FLib (failing - to be dealt with later)

--- mdslib/testing/mdslib_ftest.f
+++ mdslib/testing/mdslib_ftest.f
@@ -27,7 +27,7 @@
       enddo
 
       dsc = descr(IDTYPE_FLOAT,result,0,0)
-      sts = MdsValue("1.23"//CHAR(0),dsc,0,1)
+      sts = MdsValue("1.23"//CHAR(0),dsc,0,size)
       if (abs(result-1.23) .gt. 1e-5) then
         write (6,*) "MdsValue('1.23') : ",result,sts
         stop 1
