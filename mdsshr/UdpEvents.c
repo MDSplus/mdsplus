7c87c3aa17ee309d176ba30e0d46160ad7a05b62
mdsplus <mdsplus@scdevail.rfx.local>
2021-04-14 12:19:47 +0200

    MOVE include

--- mdsshr/UdpEvents.c
+++ mdsshr/UdpEvents.c
@@ -22,7 +22,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#define _GNU_SOURCE
+
 #include <mdsplus/mdsconfig.h>
 
 #include <errno.h>
@@ -31,11 +31,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sched.h>
 
 #include <libroutines.h>
 #include <mdsshr.h>
 #include <socket_port.h>
+
+#define _GNU_SOURCE
+#include <sched.h>
 
 extern int UdpEventGetPort(unsigned short *port);
 extern int UdpEventGetAddress(char **addr_format, unsigned char *arange);
