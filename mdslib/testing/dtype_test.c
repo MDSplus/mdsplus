c886ed45d29dd7f699b391263b754e5b15fa0cc1
cloud <timo.schroeder@ipp.mpg.de>
2021-04-30 16:02:52 +0200

    fixed mdslib status

--- mdslib/testing/dtype_test.c
+++ mdslib/testing/dtype_test.c
@@ -23,6 +23,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include "mdslib.h"
+#include <status.h>
 #include <limits.h>
 
 #include <testing.h>
