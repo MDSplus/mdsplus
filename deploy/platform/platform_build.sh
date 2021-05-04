1cdd50332cd668311277254d8db39c6b8ca751d6
cloud <timo.schroeder@ipp.mpg.de>
2021-05-04 15:02:09 +0200

    Build: isolate docker containers with --network=none

--- deploy/platform/platform_build.sh
+++ deploy/platform/platform_build.sh
@@ -8,60 +8,55 @@
 #
 set -e
 volume() {
-  if [ -n "$1" ]
-  then echo "-v $(realpath ${1}):${2}"
-  fi
+    if [ -n "$1" ]; then
+        echo "-v $(realpath ${1}):${2}"
+    fi
 }
 RED() {
-  if [ "$COLOR" = "yes" ]
-  then echo -e "\033[31m"
-  fi
+    if [ "$COLOR" = "yes" ]; then
+        echo -e "\033[31m"
+    fi
 }
 GREEN() {
-  if [ "$COLOR" = "yes" ]
-  then echo -e "\033[32m"
-  fi
+    if [ "$COLOR" = "yes" ]; then
+        echo -e "\033[32m"
+    fi
 }
 NORMAL() {
-  if [ "$COLOR" = "yes" ]
-  then echo -e "\033[0m"
-  fi
+    if [ "$COLOR" = "yes" ]; then
+        echo -e "\033[0m"
+    fi
 }
 spacedelim() {
-    if [ -n "$1" ]
-    then
-        if [ "$1" = "skip" ]
-        then
+    if [ -n "$1" ]; then
+        if [ "$1" = "skip" ]; then
             ans=""
         else
-            IFS=',' read -ra ARR <<< "$1"
+            IFS=',' read -ra ARR <<<"$1"
             ans="${ARR[*]}"
         fi
     fi
     echo $ans
 }
-rundocker(){
+rundocker() {
     images=(${DOCKERIMAGE})
     ARCH="$(spacedelim ${ARCH})"
     arches=(${ARCH})
     SANITIZE="$(spacedelim $SANITIZE)"
     VALGRIND_TOOLS="$(spacedelim $VALGRIND_TOOLS)"
     idx=0
-    if [ -z "$INTERACTIVE" ]
-    then
+    if [ -z "$INTERACTIVE" ]; then
         stdio="-a stdout -a stderr"
         program="${DOCKER_SRCDIR}/deploy/platform/platform_docker_build.sh"
     else
         stdio="-i"
         program="/bin/bash"
     fi
-    while [[ $idx -lt ${#images[*]} ]]
-    do
+    while [[ $idx -lt ${#images[*]} ]]; do
         image=${images[$idx]}
         arch=${arches[$idx]}
         echo "Building installers for ${arch} using ${image}"
-        if [ -n "$INTERACTIVE" ]
-        then
+        if [ -n "$INTERACTIVE" ]; then
             echo "env: source ${DOCKER_SRCDIR}/deploy/os/${OS}.env"
             echo "run: ${DOCKER_SRCDIR}/deploy/platform/platform_docker_build.sh"
             echo "or:  NOMAKE=1 ${DOCKER_SRCDIR}/deploy/platform/platform_docker_build.sh"
@@ -71,68 +66,68 @@ rundocker(){
         # only build the deb's after both 32-bit and 64-bit builds are
         # complete. Likewise only publish the release once.
         #
-        if [ -z $JARS_DIR ]
-        then jars_dir=
-        else jars_dir=/jars_dir
+        if [ -z $JARS_DIR ]; then
+            jars_dir=
+        else
+            jars_dir=/jars_dir
         fi
-        if [ -z $FORWARD_PORT ]
-        then port_forwarding=
-        else port_forwarding="-p ${FORWARD_PORT}:${FORWARD_PORT}"
-             echo $port_forwarding
+        if [ -z $FORWARD_PORT ]; then
+            port_forwarding=
+        else
+            port_forwarding="-p ${FORWARD_PORT}:${FORWARD_PORT}"
+            echo $port_forwarding
         fi
-	status=127
-	loop_count=0
-	while [ $status = 127 -a $loop_count -lt 5 ]
-	do
-	    let loop_count=$loop_count+1
-            docker run --cap-add=SYS_PTRACE -t $stdio --cidfile=${WORKSPACE}/${OS}_docker-cid \
-		   -u $(id -u):$(id -g) --privileged \
-		   -h $DISTNAME \
-		   -e "srcdir=${DOCKER_SRCDIR}"\
-		   -e "ARCH=${arch}" \
-		   -e "ARCHES=${ARCH}" \
-		   -e "BRANCH" \
-		   -e "COLOR" \
-		   -e "DISPLAY" \
-		   -e "DISTNAME" \
-		   -e "GIT_COMMIT" \
-		   -e "OS" \
-		   -e "PLATFORM" \
-		   -e "PUBLISH" \
-		   -e "RELEASE" \
-		   -e "RELEASE_VERSION"  \
-		   -e "SANITIZE" \
-		   -e "TEST" \
-		   -e "TESTFORMAT" \
-		   -e "UPDATEPKG" \
-		   -e "VALGRIND_TOOLS" \
-		   -e "MAKE_JARS" \
-		   -e "CONFIGURE_PARAMS" \
-		   -e "mdsevent_port=$EVENT_PORT" \
-		   -e "HOME=/workspace" \
-		   -e "JARS_DIR=$jars_dir" \
-		   -e "TEST_TIMEUNIT" \
-		   -e "ALPHA_DEBUG_INFO" \
-		   -v ${SRCDIR}:${DOCKER_SRCDIR} \
-		   -v ${WORKSPACE}:/workspace \
-		   $port_forwarding \
-		   $(volume "${JARS_DIR}" /jars_dir) \
-		   $(volume "${RELEASEDIR}" /release) \
-		   $(volume "${PUBLISHDIR}" /publish) \
-		   $(volume "${KEYS}" /sign_keys) \
-		   ${image} $program
+        status=127
+        loop_count=0
+        while [ $status = 127 -a $loop_count -lt 5 ]; do
+            let loop_count=$loop_count+1
+            docker run --cap-add=SYS_PTRACE -t $stdio --cidfile=${WORKSPACE}/${OS}_docker-cid\
+                ${DOCKER_NETWORK:---network=none}\
+                -u $(id -u):$(id -g) --privileged\
+                -h $DISTNAME\
+                -e "srcdir=${DOCKER_SRCDIR}"\
+                -e "ARCH=${arch}"\
+                -e "ARCHES=${ARCH}"\
+                -e "BRANCH"\
+                -e "COLOR"\
+                -e "DISPLAY"\
+                -e "DISTNAME"\
+                -e "GIT_COMMIT"\
+                -e "OS"\
+                -e "PLATFORM"\
+                -e "PUBLISH"\
+                -e "RELEASE"\
+                -e "RELEASE_VERSION"\
+                -e "SANITIZE"\
+                -e "TEST"\
+                -e "TESTFORMAT"\
+                -e "UPDATEPKG"\
+                -e "VALGRIND_TOOLS"\
+                -e "MAKE_JARS"\
+                -e "CONFIGURE_PARAMS"\
+                -e "mdsevent_port=$EVENT_PORT"\
+                -e "HOME=/workspace"\
+                -e "JARS_DIR=$jars_dir"\
+                -e "TEST_TIMEUNIT"\
+                -e "ALPHA_DEBUG_INFO"\
+                -v ${SRCDIR}:${DOCKER_SRCDIR}\
+                -v ${WORKSPACE}:/workspace\
+                $port_forwarding\
+                $(volume "${JARS_DIR}" /jars_dir)\
+                $(volume "${RELEASEDIR}" /release)\
+                $(volume "${PUBLISHDIR}" /publish)\
+                $(volume "${KEYS}" /sign_keys)\
+                ${image} $program
             status=$?
-            if [ -r ${WORKSPACE}/${OS}_docker-cid ]
-            then
-		sleep 3
-		docker rm $(cat ${WORKSPACE}/${OS}_docker-cid)
-		rm -f ${WORKSPACE}/${OS}_docker-cid
+            if [ -r ${WORKSPACE}/${OS}_docker-cid ]; then
+                sleep 3
+                docker rm $(cat ${WORKSPACE}/${OS}_docker-cid)
+                rm -f ${WORKSPACE}/${OS}_docker-cid
             fi
-	done
-        if [ ! "$status" = "0" ]
-        then
-    	RED
-    	cat <<EOF >&2
+        done
+        if [ ! "$status" = "0" ]; then
+            RED
+            cat <<EOF >&2
 ======================================================
 
 Docker ${PLATFORM}_docker_build.sh returned failure
@@ -140,34 +135,32 @@ status when exiting from ${image}
 
 ======================================================
 EOF
-    	NORMAL
-    	exit $status
+            NORMAL
+            exit $status
         fi
         let idx=idx+1
     done
     exit $status
 }
-default_build(){
-    if [ "${RELEASE}" = "yes" -o "${PUBLISH}" = "yes" ]
-    then
+default_build() {
+    if [ "${RELEASE}" = "yes" -o "${PUBLISH}" = "yes" ]; then
         mkdir -p ${RELEASEDIR}/${BRANCH}
     else
         RELEASEDIR=""
     fi
-    if [ "${PUBLISH}" = "yes" ]
-    then
+    if [ "${PUBLISH}" = "yes" ]; then
         mkdir -p ${PUBLISHDIR}/${BRANCH}
     fi
 }
 
-if [ "$BRANCH" = "alpha" ]
-then ALPHA_DEBUG_INFO=--enable-debug=info
-else ALPHA_DEBUG_INFO=
+if [ "$BRANCH" = "alpha" ]; then
+    ALPHA_DEBUG_INFO=--enable-debug=info
+else
+    ALPHA_DEBUG_INFO=
 fi
 set +e
 platform_build="${SRCDIR}/deploy/platform/${PLATFORM}/${PLATFORM}_build.sh"
-if [ -f "${platform_build}" ]
-then
+if [ -f "${platform_build}" ]; then
     echo "running ${platform_build}."
     source ${platform_build} "$@"
 else
