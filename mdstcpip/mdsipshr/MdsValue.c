51f0d8148c7876b9dac279c265cd9c1c22490292
cloud <timo.schroeder@ipp.mpg.de>
2021-06-27 14:09:32 +0200

    missing STATUS_OK in MdsIpGetDescriptor

--- mdstcpip/mdsipshr/MdsValue.c
+++ mdstcpip/mdsipshr/MdsValue.c
@@ -104,12 +104,15 @@ EXPORT int MdsIpGetDescriptor(int id, const char *expression, int nargs,
     status = GetAnswerInfoTS(id, (char *)&ser.dtype, (short int *)&ser.length,
                              &ndims, dims, (int *)&ser.arsize,
                              (void **)&ser.pointer, &mem);
-    ser.class = CLASS_A;
-    if ((expect_serial && ser.dtype == DTYPE_SERIAL) || ser.dtype == DTYPE_B)
-      status = MdsSerializeDscIn(ser.pointer, ans_ptr);
-    else
-      status = MdsCopyDxXd((mdsdsc_t *)&ser, ans_ptr);
-    free(mem);
+    if(STATUS_OK)
+    {
+      ser.class = CLASS_A;
+      if ((expect_serial && ser.dtype == DTYPE_SERIAL) || ser.dtype == DTYPE_B)
+        status = MdsSerializeDscIn(ser.pointer, ans_ptr);
+      else
+        status = MdsCopyDxXd((mdsdsc_t *)&ser, ans_ptr);
+      free(mem);
+    }
   }
   return status;
 }
