aad7c9e06ab5beb0993dc97cde6c20d5584d0493
Fernando Santoro <44955673+santorofer@users.noreply.github.com>
2021-07-12 20:38:06 -0500

    Feature: New TDI python function that query Influxdb data from an MDSplus node (#2348)
    * Feature: New TDI python fun to read data from Influxdb from an MDSplus tree
    
    * Only use getTimeContext. Query was improved to be more general
    
    * Add small comment
    
    * Read credentials from file
    
    * Change input order. Add address and credentials
    
    * Address and credentials input as MDSplus tree nodes
    
    * Add small comment
    
    * Consolidate WHERE and time start and end tiime
    
    * Refactor code
    
    * Several changes to input parameters and variable names
    
    * Fix to the 435st class so that the trig_time node is correctly populated with the star time of the shot
    
    * Add file in kernel packaging list

--- tdi/python/influxSignal.py
+++ tdi/python/influxSignal.py
@@ -23,10 +23,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-import re
 import MDSplus
-
-from os import environ
 
 try:
     from influxdb import InfluxDBClient
@@ -35,12 +32,8 @@ except:
         "You must install the `influxdb` python package.")
     exit(1)
 
-def influxSignal(fieldKey, aggregation, where, series=None, database=None, config=None, shotStartTime=None, shotEndTime=None): 
+def influxSignal(fieldKey, where, series=None, database=None, config=None, shotStartTime=None, shotEndTime=None): 
     tree = MDSplus.Tree()
-
-    debug = False
-    if "DEBUG_DEVICES" in environ:
-        debug = True
 
     if series is None:
         series = tree.getNode('\INFLUX_SERIES')
@@ -56,26 +49,23 @@ def influxSignal(fieldKey, aggregation, where, series=None, database=None, confi
     
     if shotEndTime is None:
         shotEndTime = tree.getNode('\INFLUX_END_TIME')
-    
-    if not isinstance(config, str):
-        config = config.data()
 
     """Instantiate a connection to the InfluxDB."""
     username = ''
     password = ''
     try:
-        with open(config) as file:
+        with open(config.data()) as file:
             lines = file.readlines()
 
             if len(lines) < 2:
-                print("Failed to read influx config from file %s" %(config,))
+                print("Failed to read influx config from file %s" %(config.data(),))
                 
             host = lines[0].strip('\n')
             username = lines[1].strip('\n')
             password = lines[2].strip('\n')
 
     except IOError as e:
-        print("Failed to open credentials file %s" %(config,))
+        print("Failed to open credentials file %s" %(config.data(),))
 
     port = 8086
     if ':' in host:
@@ -83,59 +73,36 @@ def influxSignal(fieldKey, aggregation, where, series=None, database=None, confi
         host = parts[0]
         port = int(parts[1])
 
-    if not isinstance(database, str):
-        database = database.data()
-
-    if not isinstance(series, str):
-        series = series.data()
-
-    if not isinstance(fieldKey, str):
-        fieldKey = fieldKey.data()
-
-    if not isinstance(aggregation, str):
-        aggregation = aggregation.data()
-
-    if isinstance(shotStartTime, str):
-        shotStartTime = int(shotStartTime)
-    elif not isinstance(shotStartTime, int):
-        shotStartTime = shotStartTime.data()
-
-    if isinstance(shotEndTime, str):
-        shotEndTime = int(shotEndTime)
-    elif not isinstance(shotEndTime, int):
-        shotEndTime = shotEndTime.getDataNoRaise()
-
-    if not isinstance(where, str):
-        where = where.data()
+    database = database.data()
+    series = series.data()
+    fieldKey = fieldKey.data()
+    shotStartTime = shotStartTime.data()
+    shotEndTime = shotEndTime.getDataNoRaise()
 
     if where == '':
         return
 
-    whereList = [where]
+    whereList = [where.data()]
 
     timeContext = MDSplus.Tree.getTimeContext()
 
     startTime = shotStartTime
     endTime = shotEndTime
-    deltaTime = None
 
     #print('Getting time context from the tree: %s '%(timeContext,))
 
     # The time context is in seconds relative to the start of the shot.
     if timeContext[0] is not None:
-        startTime = shotStartTime + (float(timeContext[0]) * 1000)
-
+        startTime = shotStartTime + (int(timeContext[0]) * 1000)
     if timeContext[1] is not None:
-        endTime   = shotStartTime + (float(timeContext[1]) * 1000)
+        endTime   = shotStartTime + (int(timeContext[1]) * 1000)
     
-    if timeContext[2] is not None:
-        deltaTime = float(timeContext[2])
+    # TODO: timeContext[2] is the interval which influx supports, therefore we should support it too.
 
     # Clamp the computed start/end time within the time bounds of the shot
     if startTime < shotStartTime:
         startTime = shotStartTime
-
-    if endTime is not None and shotEndTime is not None and endTime > shotEndTime:
+    if endTime is not None and endTime > shotEndTime:
         endTime = shotEndTime
 
     startTimeQuery = ''
@@ -158,26 +125,10 @@ def influxSignal(fieldKey, aggregation, where, series=None, database=None, confi
     if len(whereList) > 0:
         where = 'WHERE %s' % (' AND '.join(whereList),)
 
-    groupBy = ''
-    if deltaTime is not None:
-        # Convert deltaTime from seconds to milliseconds, if deltaTime is less than 1ms this will break
-        deltaTimeMS = int(deltaTime * 1000)
-        groupBy = 'GROUP BY time(%sms)' % (deltaTimeMS,) # The trailing 'ms' is for milliseconds
-        # If the fieldKey does not have an aggregation function, Influx will error out
-        # Attempt to wrap the field key with an aggregation function, but don't affect
-        # any equations stored in it
-        # For example:
-        #   10*fVal will become 10*MEAN(fVal)
-        fieldKey = re.sub(r'([a-zA-Z]+)', '%s(\\1)' % (aggregation,), fieldKey)
-        if debug:
-            print(aggregation, fieldKey)
-
-    """Instantiate a connection to the InfluxDB."""
     client = InfluxDBClient(host, port, username, password, database)
 
-    query = 'SELECT %s AS value FROM "%s" %s %s;' % (fieldKey, series, where, groupBy)
-    if debug:
-        print(query)
+    query = 'SELECT "%s" AS value FROM "%s" %s;' % (fieldKey, series, where)
+    #print('Query: %s' % query)
 
     result = client.query(query, params={'epoch': 'ms'})
 
@@ -186,23 +137,14 @@ def influxSignal(fieldKey, aggregation, where, series=None, database=None, confi
     valueData = [None] * len(data)
     timeData  = [None] * len(data)
 
-    count = 0
+    i = 0
     for row in data:
-        if debug:
-            print(row)
+        valueData[i] = float(row['value'])
+        timeData[i] = (row['time'] - shotStartTime) / 1000
+        i += 1
 
-        # If there are no data points within the time specified by deltaTime, influx
-        # returns a None for the value
-        if row['value'] is None:
-            continue
-
-        valueData[count] = float(row['value'])
-        timeData[count] = (row['time'] - shotStartTime) / 1000
-        count += 1
-
-    # If any data points were skipped, shrink the arrays to the actual number of points
-    values = MDSplus.Float32Array(valueData[:count])
-    times  = MDSplus.Float32Array(timeData[:count])
+    values = MDSplus.Float32Array(valueData)
+    times  = MDSplus.Float32Array(timeData)
 
     return MDSplus.Signal(values, None, times)
 
