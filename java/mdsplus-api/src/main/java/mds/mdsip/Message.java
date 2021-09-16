ec9f39f37c9b808d204c685ca235e4f1d8d0ee9a
Timo Schroeder <zack-vii@users.noreply.github.com>
2021-09-16 22:51:30 +0200

    Fix: add list fall-back implementation to support older servers (#2383)
    * Fix: add list fall-back implementation to support older servers
    
    MdsIpTunnel is special MdsIpFile, support space ' ' as %20
    
    * Fix: handle Process stream closed
    
    * Fix: fixed/suppressed all warnings
    
    * Build: added possible suppression for existing definite in dlopen
    
    * Fix: check message header before trying to read MSGLEN more bytes

--- java/mdsplus-api/src/main/java/mds/mdsip/Message.java
+++ java/mdsplus-api/src/main/java/mds/mdsip/Message.java
@@ -10,11 +10,13 @@ import java.nio.channels.WritableByteChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.zip.InflaterInputStream;
+
 import mds.MdsException;
 import mds.TransferEventListener;
 import mds.data.DTYPE;
 import mds.data.descriptor.ARRAY;
 import mds.data.descriptor.Descriptor;
+import mds.mdsip.MdsIp.Connection;
 
 public final class Message extends Object
 {
@@ -24,7 +26,7 @@ public final class Message extends Object
 		final ReadableByteChannel rbc;
 		final InflaterInputStream zis;
 
-		public CompressedChannel(ReadableByteChannel rbc)
+		public CompressedChannel(final ReadableByteChannel rbc)
 		{
 			this.rbc = rbc;
 			final InputStream is = new InputStream()
@@ -34,65 +36,56 @@ public final class Message extends Object
 				@Override
 				public int read() throws IOException
 				{
-					CompressedChannel.this.rbc.read(bb);
-					return bb.get();
+					CompressedChannel.this.rbc.read(this.bb);
+					return this.bb.get();
 				}
 			};
-			zis = new InflaterInputStream(is);
+			this.zis = new InflaterInputStream(is);
 		}
 
 		@Override
 		public void close() throws IOException
 		{
-			zis.skip(1);
+			this.zis.skip(1);
 		}
 
 		@Override
 		public boolean isOpen()
-		{ return rbc.isOpen(); }
+		{
+			return this.rbc.isOpen();
+		}
 
 		@Override
-		public int read(ByteBuffer dst) throws IOException
+		public int read(final ByteBuffer dst) throws IOException
 		{
-			final int read = zis.read(buf, 0, dst.remaining());
-			dst.put(buf, 0, read);
+			final int read = this.zis.read(this.buf, 0, dst.remaining());
+			dst.put(this.buf, 0, read);
 			return read;
 		}
 	}
 
-	public static final int _clntB = 14;
-	public static final int _dmctB = 15;
-	public static final int _dmsI = 16;
-	public static final int _idxB = 11;
-	public static final int _lenS = 8;
-	public static final int _midB = 12;
-	public static final int _mlenI = 0;
-	public static final int _nargsB = 10;
-	public static final int _statI = 4;
-	public static final int _typB = 13;
 	public static final byte BIG_ENDIAN_MASK = (byte) 0x80;
 	private static final byte COMPRESSED = (byte) 0x20;
 	protected static final String EVENTASTREQUEST = "---EVENTAST---REQUEST---";
 	protected static final String EVENTCANREQUEST = "---EVENTCAN---REQUEST---";
-	static final private int HEADER_CTYPE_B = 14;
-	static final private int HEADER_DIM0_I = 16;
-	static final private int HEADER_DSCIDX_B = 11;
-	static final private int HEADER_DTYPE_B = 13;
-	static final private int HEADER_LENGTH_S = 8;
-	static final private int HEADER_MSGIDX_B = 12;
-	static final private int HEADER_MSGLEN_I = 0;
-	static final private int HEADER_NARGS_B = 10;
-	static final private int HEADER_NDIMS_B = 15;
-	public static final int HEADER_SIZE = 48;
-	static final private int HEADER_STATUS_I = 4;
-	public static final byte JAVA_CLIENT = (byte) (Message.JAVA_CLIENT_LITTLE | Message.BIG_ENDIAN_MASK); // |
-																											// Message.SWAP_ENDIAN_ON_SERVER_MASK);
+	static final public int HEADER_CTYPE_B = 14;
+	static final public int HEADER_DIM0_I = 16;
+	static final public int HEADER_DSCIDX_B = 11;
+	static final public int HEADER_DTYPE_B = 13;
+	static final public int HEADER_LENGTH_S = 8;
+	static final public int HEADER_MSGIDX_B = 12;
+	static final public int HEADER_MSGLEN_I = 0;
+	static final public int HEADER_NARGS_B = 10;
+	static final public int HEADER_NDIMS_B = 15;
+	static final public int HEADER_SIZE = 48;
+	static final public int HEADER_STATUS_I = 4;
+	public static final byte JAVA_CLIENT = (byte) (Message.JAVA_CLIENT_LITTLE | Message.BIG_ENDIAN_MASK);
 	private static final byte JAVA_CLIENT_LITTLE = (byte) 0x3;
 	// private static final byte SENDCAPABILITIES = (byte)0xF;
 	private static final int SUPPORTS_COMPRESSION = 0x8000;
 
 	private static final synchronized void dispatchTransferEvent(final Set<TransferEventListener> mdslisteners,
-			final ReadableByteChannel dis, String info, int read, int to_read)
+			final ReadableByteChannel dis, final String info, final int read, final int to_read)
 	{
 		if (mdslisteners != null)
 			for (final TransferEventListener listener : mdslisteners)
@@ -104,7 +97,7 @@ public final class Message extends Object
 		return (arr[idx] == 0 && arr[idx + 1] == 0 && arr[idx + 2] == -128 && arr[idx + 3] == 0);
 	}
 
-	private static final ByteBuffer readBuf(int bytes_to_read, final ReadableByteChannel dis,
+	private static final ByteBuffer readBuf(final int bytes_to_read, final ReadableByteChannel dis,
 			final Set<TransferEventListener> mdslisteners, long abstimeout) throws IOException
 	{
 		final ByteBuffer buf = ByteBuffer.allocateDirect(bytes_to_read);
@@ -150,22 +143,29 @@ public final class Message extends Object
 		}
 	}
 
-	public final static Message receive(final ReadableByteChannel rbc, final Set<TransferEventListener> listeners,
+	public final static Message receive(final Connection con, final Set<TransferEventListener> listeners,
 			final int timeout) throws IOException
 	{
 		final long abstimeout = timeout >= 0 ? System.currentTimeMillis() + timeout : -1;
-		final ByteBuffer header = Message.readBuf(Message.HEADER_SIZE, rbc, null, abstimeout);
-		final byte c_type = header.get(Message._clntB);
+		final ByteBuffer header = Message.readBuf(Message.HEADER_SIZE, con, null, abstimeout);
+		con.checkMsgIdx(header.get(Message.HEADER_MSGIDX_B));
+		if (header.get(Message.HEADER_NDIMS_B) > ARRAY.MAX_DIMS)
+			throw new IOException("Invalid Message: MAX_DIMS");
+		final byte c_type = header.get(Message.HEADER_CTYPE_B);
 		if ((c_type & Message.BIG_ENDIAN_MASK) == 0)
 			header.order(ByteOrder.LITTLE_ENDIAN);
-		final int msglen = header.getInt(Message._mlenI);
+		final int msglen = header.getInt(Message.HEADER_MSGLEN_I);
 		final ByteBuffer body;
 		if (msglen > Message.HEADER_SIZE)
 		{
 			if ((c_type & Message.COMPRESSED) != 0)
-				body = Message.readCompressedBuf(rbc, header.order(), listeners);
+				body = Message.readCompressedBuf(con, header.order(), listeners);
 			else
-				body = Message.readBuf(msglen - Message.HEADER_SIZE, rbc, listeners, 0);
+				body = Message.readBuf(msglen - Message.HEADER_SIZE, con, listeners, 0);
+		}
+		else if (msglen < Message.HEADER_SIZE)
+		{
+			throw new IOException("Invalid Message: HEADER_SIZE");
 		}
 		else
 			body = ByteBuffer.allocateDirect(0);
@@ -186,14 +186,14 @@ public final class Message extends Object
 	public Message(final byte descr_idx, final DTYPE dtype, final byte nargs, final int dims[], final byte ndims_in,
 			final ByteBuffer body, final byte client_type, final int status, final byte mid)
 	{
-		this.header = ByteBuffer.allocateDirect(HEADER_SIZE);
+		this.header = ByteBuffer.allocateDirect(Message.HEADER_SIZE);
 		this.body = body.slice();
 		if ((client_type & Message.BIG_ENDIAN_MASK) == 0)
 		{
 			this.header.order(ByteOrder.LITTLE_ENDIAN);
 			this.body.order(ByteOrder.LITTLE_ENDIAN);
 		}
-		final int body_size = body == null ? 0 : body.remaining();
+		final int body_size = body.remaining();
 		final ByteBuffer h = this.getHeader();// 4
 		final byte ndims = (ndims_in > ARRAY.MAX_DIMS) ? ARRAY.MAX_DIMS : ndims_in;
 		h.putInt(Message.HEADER_SIZE + body_size).putInt(status);// 8
@@ -206,10 +206,8 @@ public final class Message extends Object
 	public Message(final byte descr_idx, final DTYPE bu, final byte nargs, final int dims[], final byte ndims,
 			final ByteBuffer body, final int status, final byte mid)
 	{
-		this(descr_idx, bu, nargs, dims, ndims, body,
-				(body == null || body.order() == ByteOrder.BIG_ENDIAN) ? Message.JAVA_CLIENT
-						: Message.JAVA_CLIENT_LITTLE,
-				status, mid);
+		this(descr_idx, bu, nargs, dims, ndims, body, (body == null || body.order() == ByteOrder.BIG_ENDIAN)
+				? Message.JAVA_CLIENT : Message.JAVA_CLIENT_LITTLE, status, mid);
 	}
 
 	public Message(final byte descr_idx, final DTYPE bu, final byte nargs, final int dims[], final ByteBuffer body,
@@ -310,49 +308,73 @@ public final class Message extends Object
 	}
 
 	public final ByteBuffer getBody()
-	{ return this.body.duplicate().order(this.body.order()); }
+	{
+		return this.body.duplicate().order(this.body.order());
+	}
 
 	public final byte getCType()
-	{ return (byte) (header.get(HEADER_CTYPE_B) & 0x1F); }
+	{
+		return (byte) (this.header.get(Message.HEADER_CTYPE_B) & 0x1F);
+	}
 
 	public final int getDescIdx()
-	{ return header.get(HEADER_DSCIDX_B) & 0xFF; }
+	{
+		return this.header.get(Message.HEADER_DSCIDX_B) & 0xFF;
+	}
 
 	public final int[] getDims()
 	{
 		final int dims[] = new int[ARRAY.MAX_DIMS];
 		final ByteBuffer h = this.getHeader();
-		h.position(HEADER_DIM0_I);
-		h.asIntBuffer().get(dims, 0, getNDims());
+		h.position(Message.HEADER_DIM0_I);
+		h.asIntBuffer().get(dims, 0, this.getNDims());
 		return dims;
 	}
 
 	public final DTYPE getDType()
-	{ return DTYPE.get(header.get(HEADER_DTYPE_B)); }
+	{
+		return DTYPE.get(this.header.get(Message.HEADER_DTYPE_B));
+	}
 
 	public final ByteBuffer getHeader()
-	{ return this.header.duplicate().order(this.header.order()); }
+	{
+		return this.header.duplicate().order(this.header.order());
+	}
 
 	public final int getLength()
-	{ return header.getShort(HEADER_LENGTH_S) & 0xFFFF; }
+	{
+		return this.header.getShort(Message.HEADER_LENGTH_S) & 0xFFFF;
+	}
 
 	public final int getMsgIdx()
-	{ return header.get(HEADER_MSGIDX_B) & 0xFF; }
+	{
+		return this.header.get(Message.HEADER_MSGIDX_B) & 0xFF;
+	}
 
 	public final int getMsgLen()
-	{ return header.getInt(HEADER_MSGLEN_I); }
+	{
+		return this.header.getInt(Message.HEADER_MSGLEN_I);
+	}
 
 	public final int getNArgs()
-	{ return header.get(HEADER_NARGS_B) & 0xFF; }
+	{
+		return this.header.get(Message.HEADER_NARGS_B) & 0xFF;
+	}
 
 	public final int getNDims()
-	{ return header.get(HEADER_NDIMS_B); }
+	{
+		return this.header.get(Message.HEADER_NDIMS_B);
+	}
 
 	public final int getStatus()
-	{ return header.getInt(HEADER_STATUS_I); }
+	{
+		return this.header.getInt(Message.HEADER_STATUS_I);
+	}
 
 	public final boolean isLittleEndian()
-	{ return ((this.getCType() & Message.BIG_ENDIAN_MASK) == 0); }
+	{
+		return ((this.getCType() & Message.BIG_ENDIAN_MASK) == 0);
+	}
 
 	public final void send(final WritableByteChannel wbc) throws MdsException
 	{
@@ -394,11 +416,11 @@ public final class Message extends Object
 
 	protected final void useCompression(final boolean use_cmp)
 	{
-		this.header.putInt(HEADER_STATUS_I, (use_cmp ? Message.SUPPORTS_COMPRESSION | 5 : 0));
+		this.header.putInt(Message.HEADER_STATUS_I, (use_cmp ? Message.SUPPORTS_COMPRESSION | 5 : 0));
 	}
 
 	public final void verify()
 	{
-		this.header.putInt(HEADER_STATUS_I, this.header.getInt(HEADER_STATUS_I) | 1);
+		this.header.putInt(Message.HEADER_STATUS_I, this.header.getInt(Message.HEADER_STATUS_I) | 1);
 	}
 }
