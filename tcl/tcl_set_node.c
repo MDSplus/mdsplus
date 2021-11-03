6bc04d431b8d28e1b6d72bd167b37aa27fa5ec33
Josh Stillerman <jas@psfc.mit.edu>
2021-11-03 14:00:38 -0400

    Fix: can not get cli_get_value a qualifier twice (#2410)
    * Fix: can not get cli_get_value a qualifier twice
    
    Can not and should not anyway ask for the value of the same
    qualifier more than one time.
    
    This change gets the compression method, if specified, outside of
    the node loop.

--- tcl/tcl_set_node.c
+++ tcl/tcl_set_node.c
@@ -116,40 +116,10 @@ EXPORT int TclSetNode(void *ctx, char **error, char **output)
     }
     if (cli_present(ctx, "COMPRESSION_METHOD"))
     {
-      char *compression_method_str = 0;
-      if(cli_get_value(ctx, "COMPRESSION_METHOD", &compression_method_str) & 1)
-      {
-        unsigned int i;
-        unsigned char compression_method=0;
-        char *p = compression_method_str;
-        for ( ; *p; ++p) *p = tolower(*p);
-        for (i=0; i < NUM_COMPRESSION_METHODS; i++)
-        {
-          if(strcmp(compression_method_str, compression_methods[i].name) == 0)
-          {
-            compression_method=i;
-            break;
-          }
-        }
-        if(i < NUM_COMPRESSION_METHODS)
-        {
-          NCI_ITM setnci[] = {{sizeof(compression_method), NciCOMPRESSION_METHOD, 0, 0},
-                              {0, NciEND_OF_LIST, 0, 0}};
-          setnci[0].pointer = (unsigned char *)&compression_method;
-          TreeSetNci(nid, setnci);
-        }
-        else
-        {
-          *error = malloc(strlen(nodename) + strlen(compression_method_str) + 100);
-          sprintf(*error,
-                  "Error: Problem setting compression method for node %s\n"
-                  "\t%s not a valid compression method\n",
-                  nodename, compression_method_str); 
-          free(compression_method_str);
-          goto error;          
-        }
-        free(compression_method_str);
-      }
+      NCI_ITM setnci[] = {{sizeof(compression_method), NciCOMPRESSION_METHOD, 0, 0},
+                          {0, NciEND_OF_LIST, 0, 0}};
+      setnci[0].pointer = (unsigned char *)&compression_method;
+      TreeSetNci(nid, setnci);
     }
  
     switch (cli_present(ctx, "SUBTREE"))
