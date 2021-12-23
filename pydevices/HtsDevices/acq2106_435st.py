e4302591599f356a5b7c5d546722f15ec170cc03
Fernando Santoro <44955673+santorofer@users.noreply.github.com>
2021-12-23 11:40:55 -0500

    Fix: move tree copyy to run() in MDSWorker thread (#2425)

--- pydevices/HtsDevices/acq2106_435st.py
+++ pydevices/HtsDevices/acq2106_435st.py
@@ -188,18 +188,11 @@ class _ACQ2106_435ST(MDSplus.Device):
         def __init__(self, dev):
             super(_ACQ2106_435ST.MDSWorker, self).__init__(name=dev.path)
 
-            self.dev = dev.copy()
+            self.dev = dev
 
-            self.chans = []
-            self.decim = []
             self.nchans     = self.dev.sites*32
             self.resampling = self.dev.resampling
-
-            for i in range(self.nchans):
-                self.chans.append(getattr(self.dev, 'input_%3.3d' % (i+1)))
-                self.decim.append(
-                    getattr(self.dev, 'input_%3.3d_decimate' % (i+1)).data())
-
+            
             self.seg_length = self.dev.seg_length.data()
             self.segment_bytes = self.seg_length*self.nchans*np.int32(0).nbytes
 
@@ -225,8 +218,17 @@ class _ACQ2106_435ST(MDSplus.Device):
                     ans = lcm(ans, e)
                 return int(ans)
 
+            self.dev = self.dev.copy()
+
             if self.dev.debug:
                 print("MDSWorker running")
+            
+            self.chans = []
+            self.decim = []
+            for i in range(self.nchans):
+                self.chans.append(getattr(self.dev, 'input_%3.3d' % (i+1)))
+                self.decim.append(
+                    getattr(self.dev, 'input_%3.3d_decimate' % (i+1)).data())
 
             event_name = self.dev.seg_event.data()
 
@@ -499,7 +501,7 @@ class _ACQ2106_435ST(MDSplus.Device):
             self.slots[card].nacc = '1'
 
         self.running.on = True
-        # If resampling == 1, then resampling is used during streaming:
+        # If resampling is True, then resampling is used during streaming:
         self.resampling = resampling
 
         thread = self.MDSWorker(self)
