b29e5033732122a47ac5664a61222dbdf78b4561
Fernando Santoro <fsantoro@psfc.mit.edu>
2022-05-27 08:48:25 -0400

    Add a new thread to check for new event files and process it

--- pydevices/HtsDevices/acq2106_435sm.py
+++ pydevices/HtsDevices/acq2106_435sm.py
@@ -28,6 +28,8 @@ import threading
 import time
 import numpy as np
 import sys
+import os
+
 if sys.version_info < (3,):
     from Queue import Queue, Empty
 else:
@@ -85,6 +87,37 @@ class _ACQ2106_435SM(MDSplus.Device):
             'valueExpr': "Action(Dispatch('CAMAC_SERVER','STORE',50,None),Method(None,'STOP',head))",      
             'options': ('no_write_shot',)
         },
+        {
+            'path': ':FREQ', 
+            'type': 'numeric',
+            'value': 20000, 
+            'options': ('no_write_shot',)
+        },
+        {
+            'path': ':TRIG_MODE', 
+            'type': 'text',
+            'value': 'master:hard', 
+            'options': ('no_write_shot',)
+        },
+        # PRE and POST samples
+        {
+            'path':':PRESAMPLES',
+            'type':'numeric',   
+            'value': int(1e5), 
+            'options':('no_write_shot',)
+        },
+        {
+            'path':':POSTSAMPLES',
+            'type':'numeric',  
+            'value': int(1e5), 
+            'options':('no_write_shot',)
+        },
+        {
+            'path':':MEV_PATH',
+            'type':'text',  
+            'value': '/srv/acq-multi-event/acq2106_161/', 
+            'options':('no_write_shot',)
+        },
     ]
 
 
@@ -100,8 +133,73 @@ class _ACQ2106_435SM(MDSplus.Device):
     # sample.time = time.time_ns()
     #
     # self.full_buffers.put(sample)
+    class MultiEventWorker(threading.Thread):
+        def __init__(self, mdsplus_device, uut):
+            super(_ACQ2106_435SM.MultiEventWorker, self).__init__(name=mdsplus_device.path)
+            self.mdsplus_device = mdsplus_device
+            self.uut = uut
+            self.presamples = mdsplus_device.presamples.data()
+            self.postsamples = mdsplus_device.postsamples.data()
+            self.clock_period  = 1.0 / mdsplus_device.freq.data()
+            self.sites = mdsplus_device.sites
 
-    class DataWorker(threading.Thread):
+        def run(self):
+            self.mdsplus_device = self.mdsplus_device.copy()
+
+            running = self.mdsplus_device.running
+            # Note: This value is not cleared between shots.
+            last_mev_latest = self.uut.s1.MEV_LATEST
+
+            self.chans = []
+            nchans = self.sites * 32
+            for i in range(nchans):
+                self.chans.append(getattr(self.mdsplus_device, 'INPUT_%3.3d:TRANSIENT'%(i+1)))
+
+            stride = nchans + 4
+
+            while running.on:
+                mev_latest = self.uut.s1.MEV_LATEST
+                
+                if mev_latest == last_mev_latest: 
+                    time.sleep(1)
+                    continue
+                
+                last_mev_latest = mev_latest
+
+                filename = mev_latest.split(' ')[1]
+                filepath = os.path.join(self.mev_path.data(), filename)
+
+                data = None
+                try:
+                    data = np.fromfile(filepath, dtype='int32', count=-1, sep='')
+                except FileNotFoundError:
+                    print("Unable to open multi-event data file", filepath)
+                    continue
+                
+                tai_cur = np.uint32(data[nchans + 1])
+                tai_ver = np.uint32(data[nchans + 2])
+
+                vernier   = (tai_ver & 0x0FFFFFFF)
+                vernier_ns = vernier * self.mdsplus_device.wrtd_tickns
+                tai_time = (tai_cur * 1_000_000_000) + vernier_ns # TAI time in ns
+
+                # For now, TAI time is 37 secs ahead of UTC
+                begin = tai_time - 37_000_000_000
+                
+                duration = (self.presamples + self.postsamples) * self.clock_period
+                deltat = self.clock_period * 1_000_000_000
+
+                end = begin + (duration * 1_000_000_000)
+                dim = MDSplus.Range(begin, end, deltat)
+
+                for i, ch in enumerate(self.chans):
+                    if ch.on:
+
+                        channel_data = data[ i :: stride ]
+                        ch.makeSegment(begin, end, dim, channel_data)
+
+        
+    class SlowMonitorWorker(threading.Thread):
         INITIAL_BUFFER_COUNT = 10
         
         # One data point per channel at a given time
@@ -112,8 +210,8 @@ class _ACQ2106_435SM(MDSplus.Device):
                 self.buffer = bytearray(nchans * np.int32(0).nbytes + self.SPAD_SIZE)
                 self.time = 0
 
-        def __init__(self, mdsplus_device):
-            super(_ACQ2106_435SM.DataWorker, self).__init__(name=mdsplus_device.path)
+        def __init__(self, mdsplus_device, uut):
+            super(_ACQ2106_435SM.SlowMonitorWorker, self).__init__(name=mdsplus_device.path)
             
             self.mdsplus_device = mdsplus_device
 
@@ -125,11 +223,11 @@ class _ACQ2106_435SM(MDSplus.Device):
             for i in range(self.INITIAL_BUFFER_COUNT):
                 self.empty_buffers.put(self.DataPoint(self.nchans))
 
-            self.acq_thread = self.ACQWorker(self)
+            self.acq_thread = self.ACQWorker(self, uut)
 
         def run(self):
             from influxdb import InfluxDBClient
-            self.client = InfluxDBClient('sparc-daq1', 8086, 'admin', 'password', 'slowmon')
+            self.client = InfluxDBClient('localhost', 8086, 'admin', 'password', 'slowmon435')
 
             self.mdsplus_device = self.mdsplus_device.copy()
 
@@ -153,7 +251,7 @@ class _ACQ2106_435SM(MDSplus.Device):
                 if not running.on and self.acq_thread.running:
                     self.acq_thread.stop()
 
-                start = time.time()
+                # start = time.time()
                 try:
                     data_point = self.full_buffers.get()
                 except Empty:
@@ -164,17 +262,14 @@ class _ACQ2106_435SM(MDSplus.Device):
 
                 spad_stride = int(self.nchans / 2) + 4
 
-                spad2 = spad[2::spad_stride] # sec
-                spad3 = spad[3::spad_stride]
+                # tai_cur = spad[1::spad_stride] # sec
+                # tai_ver = spad[2::spad_stride]
 
-                vernier   = (spad3 & 0x0FFFFFFF)
+                # vernier   = (tai_ver & 0x0FFFFFFF)
+                # vernierns = vernier * self.mdsplus_device.wrtd_tickns
+                # timeStamp = (tai_cur * 1e9) + vernierns # TAI time in ns
 
-                vernierns = vernier * self.mdsplus_device.wrtd_tickns
-                timeStamp = (spad2 * 1e9) + vernierns # wall TAI time in ns
-
-                # vernierns = vernier * self.dev.wrtd_tickns
-                # timeStamp = (spad2) + vernierns * 1e-9 # wall TAI time in secs
-                print("#### time stamp ", spad2[0], vernier[0], timeStamp[0], data_point.time)
+                # print("#### time stamp ", tai_cur[0], vernierns[0], timeStamp[0], data_point.time)
 
                 influx_points = []
                 mdsplus_points= []
@@ -205,27 +300,31 @@ class _ACQ2106_435SM(MDSplus.Device):
                 MDSplus.Event.setevent(event_name)
 
                 self.empty_buffers.put(data_point)
-                end  = time.time()
-                print('putRow and write_points', end - start)
+
+                # end  = time.time()
+                # print('putRow and write_points', end - start)
 
             if self.acq_thread.running:
                 self.acq_thread.stop()
         
         class ACQWorker(threading.Thread):
 
-            def __init__(self, data_worker):
-                super(_ACQ2106_435SM.DataWorker.ACQWorker, self).__init__()
+            def __init__(self, data_worker, uut):
+                super(_ACQ2106_435SM.SlowMonitorWorker.ACQWorker, self).__init__()
                 self.data_worker = data_worker
                 self.node_addr = data_worker.mdsplus_device.node.data()
+                self.uut = uut
 
             def stop(self):
                 self.running = False
 
             def run(self):
-
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect((self.node_addr, 53666))
                 s.settimeout(6)
+
+                # Trigger ACQ to starts:
+                #self.uut.s0.CONTINUOUS = 1
 
                 self.running = True
                 while self.running:
@@ -256,6 +355,7 @@ class _ACQ2106_435SM(MDSplus.Device):
                         err = e.args[0]
                         # this next if/else is a bit redundant, but illustrates how the
                         # timeout exception is setup
+                        self.data_worker.empty_buffers.put(sample)
 
                         if err == 'timed out':
                             time.sleep(1)
@@ -277,14 +377,101 @@ class _ACQ2106_435SM(MDSplus.Device):
                         else:
                             self.data_worker.full_buffers.put(sample)
     
+    # The minimum frequency we can operate at                   
+    MIN_FREQUENCY = 10000
+    # _faster_ than 20kHz will spoil SLOWMON (it will still work, but the average loses 100% data coverage).
+    # though, this is more a suggestion.
+    MAX_FREQUENCY = 20000
+
+    # These are partial lists of the options we support.
+    # For a complete list, consult D-Tacq
+
+    # Trigger Source Options for Signal Highway d0
+    TRIG_SRC_OPTS_0 = [
+        'ext',          # External Trigger
+        'hdmi',         # HDMI Trigger
+        'gpg0',         # Gateway Pulse Generator Trigger
+        'wrtt0'         # White Rabbit Trigger
+    ]
+
+    # Trigger Source Options for Signal Highway d1
+    TRIG_SRC_OPTS_1 = [
+        'strig',        # Software Trigger
+        'hdmi_gpio',    # HDMI General Purpose I/O Trigger
+        'gpg1',         # Gateway Pulse Generator Trigger
+        'fp_sync',      # Front Panel SYNC
+        'wrtt1'         # White Rabbit Trigger
+    ]
+
     def init(self):
         uut = self.getUUT()
 
         # Setting the slow monitoring frequency to 1Hz
         uut.s0.SLOWMON_FS = 1 #Hz
 
-        # WR Nanosec per tick:
+        # WR Nanosec per Tick:
         self.wrtd_tickns = float(uut.cC.WRTD_TICKNS)
+
+        freq = int(self.freq.data())
+        # D-Tacq Recommendation: 
+        #      - for fast rates, the minimum sample rate is 10kHz.
+        #      - for SLOWMON, the max sample rate is 20kHz.
+        if freq < self.MIN_FREQUENCY:
+            raise MDSplus.DevBAD_PARAMETER(
+                "Sample rate should be greater or equal than 10kHz")
+        elif freq > self.MAX_FREQUENCY:
+            raise MDSplus.DevBAD_PARAMETER(
+                "Sample rate should be less or equal than 20kHz for SLOWMON to work correctly (otherwise average would lose %100 data coverage)")
+
+        mode = str(self.trig_mode.data()).lower()
+        if mode == 'hard':
+            role = 'master'
+            trg = 'hard'
+        elif mode == 'soft':
+            role = 'master'
+            trg = 'soft'
+        else:
+            role = mode.split(":")[0]
+            trg  = mode.split(":")[1]
+
+        if self.debug:
+            print("Role is %s and %s trigger" % (role, trg))
+
+
+        src_trg_0 = None
+        src_trg_1 = None
+
+        if trg == 'hard':
+            trg_dx = 'd0'
+            src_trg_0 = 'EXT' # External Trigger
+        elif trg == 'soft' or trg == 'automatic':
+            trg_dx = 'd1'
+            src_trg_1 = 'STRIG' # Soft Trigger
+        elif trg in self.TRIG_SRC_OPTS_0:
+            trg_dx = 'd0'
+            src_trg_0 = trg
+        elif trg in self.TRIG_SRC_OPTS_1:
+            trg_dx = 'd1'
+            src_trg_1 = trg
+        elif trg != 'none':
+            raise MDSplus.DevBAD_PARAMETER("TRIG_MODE does not contain a valid trigger source")
+ 
+        uut.s0.sync_role = '%s %s TRG:DX=%s' % (role, self.freq.data(), trg_dx)
+
+        # snyc_role will set a default trigger source, we need to override it to the selected trigger source
+        # These must be uppercase
+        if src_trg_0:
+            uut.s0.SIG_SRC_TRG_0 = src_trg_0.upper()
+
+        if src_trg_1:
+            uut.s0.SIG_SRC_TRG_1 = src_trg_1.upper()
+
+        # Setting Capture/Transient trigger event:
+        uut.s1.EVENT0          = 'enable'
+        uut.s1.EVENT0_DX       = 'd1'
+        uut.s1.EVENT0_SENSE    = 'rising'
+        uut.s0.SIG_EVENT_SRC_1 = 'TRG'
+
 
         # Fetching all calibration information from every channel.
         uut.fetch_all_calibration()
@@ -305,27 +492,44 @@ class _ACQ2106_435SM(MDSplus.Device):
                 ch.OFFSET.putData(float(eoff[i]))
                 ch.COEFFICIENT.putData(float(coeffs[i]))
 
+
+        uut.s1.MEV_PRE = self.presamples.data()
+        uut.s1.MEV_POST = self.postsamples.data()
+        uut.s1.MEV_MAX = 10
+
         self.running.on = True
 
-        thread = self.DataWorker(self)
-        thread.start()
+        mev_thread = self.MultiEventWorker(self, uut)
+        mev_thread.start()
+
+        sm_thread = self.SlowMonitorWorker(self, uut)
+        sm_thread.start()
 
     INIT = init
 
 
+    def trigger_mev(self):
+        uut = self.getUUT()
+        uut.s0.soft_trigger
+    TRIGGER_MEV = trigger_mev
+        
     def stop(self):
+        uut = self.getUUT()
+
         self.running.on = False
+        # Trigger ACQ to stop:
+        uut.s0.CONTINUOUS = 0
     STOP = stop
 
     def getUUT(self):
         import acq400_hapi
-        #uut = acq400_hapi.Acq400(self.node.data(), monitor=False)
         uut = acq400_hapi.factory(self.node.data())
         return uut
 
     def setChanScale(self, num):
         chan = self.__getattr__('INPUT_%3.3d' % num)
         chan.setSegmentScale(MDSplus.ADD(MDSplus.MULTIPLY(chan.COEFFICIENT, MDSplus.dVALUE()), chan.OFFSET))
+    
 
 def assemble(cls):
     cls.parts = list(_ACQ2106_435SM.carrier_parts)
@@ -335,7 +539,7 @@ def assemble(cls):
                 'path': ':INPUT_%3.3d' % (i + 1,),            
                 'type': 'SIGNAL', 
                 'valueExpr': 'head.setChanScale(%d)' % (i + 1,),
-                'options': ('no_write_model',)
+                #'options': ('no_write_model',) # this seems to be broken
             },
             {
                 'path': ':INPUT_%3.3d:COEFFICIENT' % (i + 1,), 
@@ -346,6 +550,13 @@ def assemble(cls):
                 'path': ':INPUT_%3.3d:OFFSET' % (i + 1,),    
                 'type': 'NUMERIC',
                 'options': ('no_write_model', 'write_once',)
+            },
+            {
+                 # Capture transient signal goes here:
+                'path': ':INPUT_%3.3d:TRANSIENT' % (i + 1,),
+                'type': 'SIGNAL',
+                'valueExpr': 'head.setChanScale(%d)' % (i + 1,),
+                #'options': ('no_write_model',)
             },
         ]
 
