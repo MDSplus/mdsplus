7cbf70ba2970e23fe680a1f6c9dfd5b7460f1fb4
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2023-08-15 18:05:28 -0400

    Fixes for python and java tests We now respect $PYTHON and try to use that version for scripts and $PyLib

--- cmake/SetEnvironmentForTests.cmake
+++ cmake/SetEnvironmentForTests.cmake
@@ -6,11 +6,12 @@ macro(set_environment_for_tests ) # ARGN=...
         "MDSPLUS_DIR=set:${CMAKE_SOURCE_DIR}"
         "MDS_PATH=set:${CMAKE_SOURCE_DIR}/tdi"
         "MDS_PYDEVICE_PATH=set:${CMAKE_SOURCE_DIR}/pydevices"
-        "JAVA_PATH_NAME=set:${Java_JAVA_EXECUTABLE}"
+        "JAVA_PATH_NAME=set:${Java_JAVA_EXECUTABLE} -Djava.library.path=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}"
         "PYTHON=set:${Python_EXECUTABLE}"
+        "PyLib=set:${Python_LIBRARIES}"
         "PYTHONPATH=set:${CMAKE_SOURCE_DIR}/python"
         "PATH=path_list_prepend:${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
-        "PATH=path_list_prepend:${CMAKE_SOURCE_DIR}/mdstcpip" # mdsip-*
+        "PATH=path_list_prepend:${CMAKE_SOURCE_DIR}/mdstcpip" # Needed for mdsip-client-* and mdsip-server-*
     )
 
     # TODO: default_tree_path ?
@@ -19,10 +20,12 @@ macro(set_environment_for_tests ) # ARGN=...
 
     if(WIN32)
         list(APPEND _env_mods "PATH=path_list_prepend:${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
-    elseif(APPLE)
-        list(APPEND _env_mods "DYLD_LIBRARY_PATH=path_list_prepend:${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
     else()
         list(APPEND _env_mods "LD_LIBRARY_PATH=path_list_prepend:${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
+
+        if(APPLE)
+            list(APPEND _env_mods "DYLD_LIBRARY_PATH=path_list_prepend:${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
+        endif()
     endif()
 
     set_tests_properties(
