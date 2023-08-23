e4e6f238ffc36b8095ea3301124d610392274ad0
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2023-08-23 01:44:51 -0400

    Major overhaul of cmake options and flag checking Add mdsplus_option() macro to unify option()/set(CACHE STRING) options and messaging Rework mdsplus_check_flags to allow testing multiple compiler/linker flags at once (needed by the sanitizer) Rename ENABLE_PERF to ENABLE_TREE_PERF Remove USE_PERF from mdsconfig.h.in Add USE_TREE_PERF define in CMake instead Remove JAVA_TARGET_VERSION and JAVA_BOOTCLASSPATH, they were removed in Java 9 Remove Java -source for the same reason Add JAVA_VERSION to set a minimum version for find_package(Java) and to pass to java_home on OSX Standardize calls to cmake_parse_arguments Convert cmake macros to functions, where possible Standardize documentation for cmake functions

--- cmake/MDSplusAddCompilerFlag.cmake
+++ cmake/MDSplusAddCompilerFlag.cmake
@@ -3,21 +3,28 @@ include_guard(GLOBAL)
 include(MDSplusCheckFlags)
 
 #
-# Check if a flag is supported in the C, C++, and Fortran compilers, and add it to CMAKE_{LANG}_FLAGS if it is.
+# mdsplus_add_compiler_flags(<flags>)
 #
-macro(mdsplus_add_compiler_flag _flag)
+# Check if the flags are supported in the C, CXX, and Fortran compilers, and add it to CMAKE_{LANG}_FLAGS if it is.
+#
+function(mdsplus_add_compiler_flags _flags)
 
-    set(_prefix "_compiler${_flag}")
+    string(REPLACE ";" " " _flags "${_flags}")
+
+    set(_prefix "_compiler${_flags}")
+    string(REPLACE " " "_" _prefix "${_prefix}")
     string(REPLACE "-" "_" _prefix "${_prefix}")
     string(REPLACE "=" "_" _prefix "${_prefix}")
     string(REPLACE "/" "_" _prefix "${_prefix}")
 
-    mdsplus_check_compiler_flag(${_flag} ${_prefix})
+    mdsplus_check_flags(${_prefix}
+        COMPILER ${_flags}
+    )
 
     foreach(_lang IN ITEMS C CXX Fortran)
         if(${_prefix}_${_lang})
-            set(CMAKE_${_lang}_FLAGS "${CMAKE_${_lang}_FLAGS} ${_flag}")
+            set(CMAKE_${_lang}_FLAGS "${CMAKE_${_lang}_FLAGS} ${_flags}" PARENT_SCOPE)
         endif()
     endforeach()
 
-endmacro()
+endfunction()
