167ff22ff33ad7426f5a50962868b8533cb1d9aa
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2024-01-03 16:35:29 -0500

    Initial Jenkinsfile
    Add a basic Jenkinsfile that will build and test
    Includes fixes for deploy/*.sh scripts that didn't work on the new jenkins

--- deploy/platform/platform_build.sh
+++ deploy/platform/platform_build.sh
@@ -46,7 +46,8 @@ rundocker() {
   VALGRIND_TOOLS="$(spacedelim $VALGRIND_TOOLS)"
   idx=0
   if [ -z "$INTERACTIVE" ]; then
-    stdio="-a stdout -a stderr"
+    # stdio="-a stdout -a stderr"
+    stdio="--detach"
     program="${DOCKER_SRCDIR}/deploy/platform/platform_docker_build.sh"
   else
     stdio="-i"
@@ -90,6 +91,22 @@ rundocker() {
       DOCKER_NETWORK=bridge
       NETWORK=
     fi
+
+    function kill_docker() {
+      if [ -r ${WORKSPACE}/${OS}_docker-cid ]; then
+        docker kill $(cat ${WORKSPACE}/${OS}_docker-cid) || true
+        docker rm $(cat ${WORKSPACE}/${OS}_docker-cid) || true
+        rm -f ${WORKSPACE}/${OS}_docker-cid
+      fi
+    }
+
+    function abort() {
+      kill_docker
+      status=1
+    }
+
+    trap abort SIGINT
+
     status=127
     loop_count=0
     while [ $status = 127 -a $loop_count -lt 5 ]; do
@@ -131,10 +148,9 @@ rundocker() {
         $(volume "${KEYS}" /sign_keys) \
         ${image} $program
       status=$?
-      if [ -r ${WORKSPACE}/${OS}_docker-cid ]; then
-        sleep 3
-        docker rm $(cat ${WORKSPACE}/${OS}_docker-cid)
-        rm -f ${WORKSPACE}/${OS}_docker-cid
+
+      if [ -z "$INTERACTIVE" ]; then
+        docker logs -f $(cat ${WORKSPACE}/${OS}_docker-cid)
       fi
     done
     if [ -z ${DOCKER_NETWORK} ]; then
