a4bb427fe8e913c2bbc2a7ac7f1f80998d8df1b7
Darren Garnier <darren.garnier@openstar.nz>
2024-01-04 16:35:00 +1300

    seems to work without -e

--- python/MDSplus/setup.py
+++ python/MDSplus/setup.py
@@ -28,38 +28,45 @@ import os
 
 
 def setupkw():
+    from setuptools import find_namespace_packages
+    from runpy import run_path
+
     try:
-        version = ()
         mod_dir = os.path.dirname(os.path.abspath(__file__))
-        with open(os.path.join(mod_dir, '_version.py')) as f:
-            exec(f.read())
-        release = "%d.%d.%d" % version
+        version_data = run_path(os.path.join(mod_dir, "_version.py"))
+        release = "%d.%d.%d" % version_data["version"]
+        release_tag = version_data["release_tag"]
     except Exception:
-        release = '0.0.0'
-        release_tag = 'Unknown'
-    pth_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        release = "0.0.0"
+        release_tag = "Unknown"
+
+    # pth_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     return dict(
-        name='MDSplus',
-        extra_path=('mdsplus', pth_dir),
+        name="MDSplus",
+        # extra_path=("mdsplus", pth_dir),
         version=release,
-        description='MDSplus Python Objects - '+release_tag,
+        description="MDSplus Python Objects - " + release_tag,
         long_description=(
             "This module provides all of the functionality of MDSplus TDI natively in python.\n"
             "All of the MDSplus data types such as signal are represented as python classes.\n"
         ),
-        author='MDSplus Development Team',
-        author_email='twf@www.mdsplus.org',
-        url='http://www.mdsplus.org/',
-        license='MIT',
+        author="MDSplus Development Team",
+        author_email="twf@www.mdsplus.org",
+        url="http://www.mdsplus.org/",
+        license="MIT",
         classifiers=[
-            'Programming Language :: Python',
-            'Intended Audience :: Science/Research',
-            'Environment :: Console',
-            'Topic :: Scientific/Engineering',
+            "Programming Language :: Python",
+            "Intended Audience :: Science/Research",
+            "Environment :: Console",
+            "Topic :: Scientific/Engineering",
         ],
-        keywords=['physics', 'mdsplus', ],
-        packages = ['MDSplus'],
-        package_dir = {'MDSplus':''},
+        keywords=[
+            "physics",
+            "mdsplus",
+        ],
+        packages=["MDSplus"],
+        package_dir={"MDSplus": ""},
+        package_data={"MDSplus": ["*.py", "widgets/*"]},
     )
 
 
@@ -78,24 +85,25 @@ def use_distutils():
 
         def run(self):
             import subprocess
-            raise SystemExit(
-                subprocess.call([sys.executable, '-m', 'tests.__init__']))
 
-    setup(cmdclass={'test': TestCommand}, **setupkw())
+            raise SystemExit(subprocess.call([sys.executable, "-m", "tests.__init__"]))
+
+    setup(cmdclass={"test": TestCommand}, **setupkw())
 
 
 def use_setuptools():
     from setuptools import setup
+
     setup(
         include_package_data=True,
-        test_suite='tests.test_all',
+        test_suite="tests.test_all",
         zip_safe=False,
         **setupkw()
     )
 
 
-if __name__ == '__main__':
-    if 'pip-egg-info' in sys.argv:
+if __name__ == "__main__":
+    if "pip-egg-info" in sys.argv:
         print("When using pip to install MDSplus use 'pip install -e <dir>'")
         sys.exit(1)
     try:
