8524b0cfe8fc762a02922dc562cd9df6706fb03e
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2025-01-22 10:14:45 +0100

    Fix parameters

--- java/jdevices/src/main/java/MARTE2_SIMULINK_TCV_PROXY_OUTSetup.java
+++ java/jdevices/src/main/java/MARTE2_SIMULINK_TCV_PROXY_OUTSetup.java
@@ -38,7 +38,7 @@ public class MARTE2_SIMULINK_TCV_PROXY_OUTSetup extends DeviceSetup {
         setWidth(500);
         getContentPane().add(deviceButtons1, java.awt.BorderLayout.PAGE_END);
 
-        deviceParameters1.setNumParameters(2);
+        deviceParameters1.setNumParameters(4);
         deviceParameters1.setOffsetNid(4);
         jTabbedPane1.addTab("Parameters", deviceParameters1);
         jTabbedPane1.addTab("Inputs", deviceInputs1);
