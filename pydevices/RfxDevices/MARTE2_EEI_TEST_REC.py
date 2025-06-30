97a3ea1da7c74e1dfad13f4e21c3a57c8016291d
Gabriele Manduchi <gabriele.manduchi@igi.cnr.it>
2025-06-30 11:56:30 +0200

    FAT for EEI power supply

---
+++ pydevices/RfxDevices/MARTE2_EEI_TEST_REC.py
@@ -0,0 +1,76 @@
+#
+# Copyright (c) 2017, Massachusetts Institute of Technology All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+from MDSplus import Data, Int32Array
+MC = __import__('MARTE2_COMPONENT', globals())
+
+
+@MC.BUILDER('EEIIn', MC.MARTE2_COMPONENT.MODE_SYNCH_INPUT,
+            'build_range(0, 1000000, build_path(".parameters:par_3:value"))')
+class MARTE2_EEI_TEST_REC(MC.MARTE2_COMPONENT):
+    outputs = [
+        {'name': 'Time', 'type': 'int32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_1', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_2', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_3', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_4', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_5', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_6', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_7', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_8', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_9', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_10', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_11', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_12', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_13', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_14', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_15', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_16', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_17', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_18', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_19', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_20', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_21', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_22', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_23', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_24', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_25', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_26', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+        {'name': 'Ref_27', 'type': 'float32', 'dimensions': 0, 'parameters': []},
+]
+   
+    parameters = [
+        {'name': 'Ip', 'type': 'string'},
+        {'name': 'Port', 'type': 'int32', 'value': 44488},
+        {'name': 'Period', 'type': 'float32', 'value': 1E-3},
+        {'name': 'CpuMask', 'type': 'int32', 'value': 255},
+        {'name': 'IsSync', 'type': 'int32', 'value': 1},
+        {'name': 'CircuitId', 'type': 'int32', 'value': 1},
+    ]
+    parts = []
+
+    def prepareMarteInfo(self):
+        print('0 : 1000000 : '+self.getFullPath()+'.parameters:par_3:value')
+        self.timebase.putData(Data.compile('0 : 1000000 : '+self.getFullPath()+'.parameters:par_3:value'))
