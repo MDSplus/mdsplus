ef714cc29cc1da86cd74d02b249eaf732afe9fb8
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2025-09-26 15:17:15 -0400

    Make raw1394 an optional dependency

--- cmake/FindDC1394.cmake
+++ cmake/FindDC1394.cmake
@@ -46,15 +46,15 @@ find_package_handle_standard_args(
     REQUIRED_VARS # The first one is displayed in the message
         DC1394_dc1394_LIBRARY
         DC1394_INCLUDE_DIRS
-        DC1394_raw1394_LIBRARY
+        # DC1394_raw1394_LIBRARY
 )
 
 if(DC1394_FOUND)
 
-    set(DC1394_LIBRARIES
-        ${DC1394_dc1394_LIBRARY}
-        ${DC1394_raw1394_LIBRARY}
-    )
+    set(DC1394_LIBRARIES ${DC1394_dc1394_LIBRARY})
+    if(DC1394_raw1394_LIBRARY)
+        list(APPEND DC1394_LIBRARIES ${DC1394_raw1394_LIBRARY})
+    endif()
 
     if(NOT TARGET DC1394::DC1394)
 
