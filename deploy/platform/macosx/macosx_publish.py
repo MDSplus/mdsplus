118f813a4e4e2075389cdc6f273b34c55878e367
Stephen Lane-Walsh <slwalsh@psfc.mit.edu>
2025-10-02 14:27:15 -0400

    WIP OSX Packaging

--- deploy/platform/macosx/macosx_publish.py
+++ deploy/platform/macosx/macosx_publish.py
@@ -46,6 +46,7 @@ args = parser.parse_args()
 
 
 # * may be `macports` or `homebrew`
-package_filename = glob.glob(os.path.join(args.release_dir, f'mdsplus_{args.flavor}_{args.version}-*-{args.arch}.tgz'))
+package_filenames = glob.glob(os.path.join(args.release_dir, f'mdsplus_{args.flavor}_{args.version}-*-{args.arch}.tgz'))
 
-shutil.copy2(package_filename, args.publish_dir)
+for filename in package_filenames:
+    shutil.copy2(filename, args.publish_dir)
