0af6b876ac81e3edce6c04e6d2d2365487c1dc7f
Josh Stillerman <jas@psfc.mit.edu>
2025-10-21 14:42:37 -0400

    Fix: fix brother of subtree nodes.
    The optimizer is deciding that it does not need to fill in the
    brother nid of nodes that are subtree attachment points.
    Making the variable that holds it volatile forces the optimizer to
    do the right thing.
    
    Fixes: https://github.com/MDSplus/mdsplus/issues/2972

--- treeshr/TreeOpen.c
+++ treeshr/TreeOpen.c
@@ -1174,17 +1174,23 @@ static int GetVmForTree(TREE_INFO *info, int nomap)
 static void SubtreeNodeConnect(PINO_DATABASE *dblist, NODE *parent,
                                NODE *subtreetop)
 {
-  NID child_nid, parent_nid, brother_nid = {0, 0};
+  NID child_nid, parent_nid = {0, 0};
+  /*
+   * make brother_nid volatile so that the optimizer does not
+   * decide to optimize it out and replace it with a zero.
+   */
+  volatile brother_nid = {0,0};
   NODE *brother = brother_of(dblist, parent);
   parent->usage = TreeUSAGE_SUBTREE_REF;
   subtreetop->usage = TreeUSAGE_SUBTREE_TOP;
   node_to_nid(dblist, subtreetop, &child_nid);
-  node_to_nid(dblist, parent_of(dblist, parent), &parent_nid);
-  if (brother)
-    node_to_nid(dblist, brother_of(dblist, parent), &brother_nid);
+  node_to_nid(dblist, parent_of(dblist, parent), (NID *)&parent_nid);
   parent->child = *(int *)&child_nid;
-  subtreetop->parent = *(int *)&parent_nid;
+  if (brother) {
+    node_to_nid(dblist, brother_of(dblist, parent), (NID *)&brother_nid);
+  }
   subtreetop->brother = *(int *)&brother_nid;
+  subtreetop->parent = *(int *)&parent_nid;
   memcpy(subtreetop->name, parent->name, sizeof(subtreetop->name));
   return;
 }
