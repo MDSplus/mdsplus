731839561bdd36f55100f80274e0a4b410ba946c
mwinkel-dev <mwinkel@mit.edu>
2025-11-20 17:12:50 -0700

    Fix: mdstest now supports strings returned by the Java bridge

--- matlab/mdstest.m
+++ matlab/mdstest.m
@@ -6,6 +6,7 @@ function result = mdstest(varargin)
 %   mdstest(0)  % tests java bridge
 %   mdstest(1)  % tests python bridge
 
+    global MDSINFO;
     mdsInfo(varargin{:}); % update MDSINFO
     result = mdscheck('1BU', 'uint8', [1, 1]);
     result = result && mdscheck('1WU', 'uint16', [1, 1]);
@@ -13,7 +14,6 @@ function result = mdstest(varargin)
     result = result && mdscheck('100000000000QU', 'uint64', [1, 1]);
     result = result && mdscheck('1.', 'single', [1, 1]);
     result = result && mdscheck('1D0', 'double', [1, 1]);
-    result = result && mdscheck('"string test"', 'char', [1, 11]);
     result = result && mdscheck('BYTE_UNSIGNED(1:100)', 'uint8', [100, 1]);
     result = result && mdscheck('WORD_UNSIGNED(1:100)', 'uint16', [100, 1]);
     result = result && mdscheck('LONG_UNSIGNED(1:100)', 'uint32', [100, 1]);
@@ -28,8 +28,15 @@ function result = mdstest(varargin)
     result = result && mdscheck('$ : $', 'int32', [100, 1], int32(1), int32(100));
     result = result && mdscheck('$ == $', 'uint8', [1, 1], 1, 2);
     result = result && mdscheck('QUADWORD_UNSIGNED(1:100)', 'uint64', [100, 1]);
-    result = result && mdscheck('["a","b","c","d"]', 'cell', [4, 1]);
-    result = result && mdscheck('set_range(2,3,["a","b","c long string","d","e","f"])', 'cell', [2, 3]);
+    if MDSINFO.usePython
+        result = result && mdscheck('"string test"', 'char', [1, 11]);
+        result = result && mdscheck('["a","b","c","d"]', 'cell', [4, 1]);
+        result = result && mdscheck('set_range(2,3,["a","b","c long string","d","e","f"])', 'cell', [2, 3]);
+    else
+        result = result && mdscheck('"string test"', 'string', [1, 1]);
+        result = result && mdscheck('["a","b","c","d"]', 'string', [4, 1]);
+        result = result && mdscheck('set_range(2,3,["a","b","c long string","d","e","f"])', 'string', [2, 3]);
+    end
 end
 
 function result = mdscheck(exp, result_class, result_size, varargin)
