d3c9ffb98b677ae0e52d2b6474c50cef060e8263
mdsplus <mdsplus@roactive2.rfx.local>
2025-12-12 14:31:14 +0100

    numpy does not supportnow  fromstring

--- pydevices/RfxDevices/MARTE2_SUPERVISOR.py
+++ pydevices/RfxDevices/MARTE2_SUPERVISOR.py
@@ -1334,7 +1334,7 @@ $<APP_NAME> = {
         f = open('/tmp/'+name+'_marte_configuration.cfg', 'w')
         f.write(config)
         f.close()
-        self.getNode('MARTE_CONFIG').putData(MDSplus.Int8Array(np.fromstring(config, dtype=np.int8)))
+        self.getNode('MARTE_CONFIG').putData(MDSplus.Int8Array(np.frombuffer(config.encode('utf-8'), dtype=np.int8)))
 
 
 
